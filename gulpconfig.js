b54cbbd54fb862f96d31db304899f2d7a7df050e
Salem <me@salemghoweri.com>
2017-07-27 10:10:26 -0400

    Update Travis Build (#85)
    * WIP: stashing latest + testing new travis config
    
    * WIP: switching travis version to trustee instead
    
    * WIP: testing fix for travis php 7.1 install
    
    * WIP: fixing add-apt-repository command so travis doesn't time out
    
    * Update .travis.yml
    
    * Adding npm init script
    
    * Update package.json
    
    * WIP: testing simplified config
    
    * WIP: testing simplified config

--- gulpconfig.js
+++ gulpconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   patternLab: {
     enabled: true,
-    configFile: 'sandbox/pattern-library/config/config.yml',
+    configFile: 'sandbox/pattern-/config/config.yml',
     twigNamespaces: {
       addToDrupalThemeFile: true,
       sets: [
@@ -11,21 +11,202 @@ module.exports = {
         },
         {
           namespace: 'atoms',
-          paths: ['sandbox/pattern-library/source/_patterns/00-atoms'],
+          paths: ['sandbox/pattern-/source/_patterns/00-atoms'],
         }, {
           namespace: 'molecules',
-          paths: ['sandbox/pattern-library/source/_patterns/01-molecules'],
+          paths: ['sandbox/pattern-/source/_patterns/01-molecules'],
         }, {
           namespace: 'organisms',
-          paths: ['sandbox/pattern-library/source/_patterns/02-organisms'],
+          paths: ['sandbox/pattern-/source/_patterns/02-organisms'],
         }, {
           namespace: 'templates',
-          paths: ['sandbox/pattern-library/source/_patterns/03-templates'],
+          paths: ['sandbox/pattern-/source/_patterns/03-templates'],
         }, {
           namespace: 'pages',
-          paths: ['sandbox/pattern-library/source/_patterns/04-pages'],
+          paths: ['sandbox/pattern-/source/_patterns/04-pages'],
         },
       ],
     },
   }
 };
+  // watchedExtensions: [
+  //   'twig',
+  //   'json',
+  //   'yaml',
+  //   'yml',
+  //   'md',
+  //   'jpg',
+  //   'jpeg',
+  //   'png',
+  // ],
+  // injectFiles: [],
+  // bowerBasePath: './',
+  
+  // css: {
+  //   enabled: false,
+  //   src: [
+  //     'scss/**/*.scss',
+  //     'source/_patterns/**/*.scss',
+  //     'source/styleguide/*.scss',
+  //   ],
+  //   dest: 'dest/',
+  //   flattenDestOutput: true,
+  //   lint: {
+  //     enabled: true,
+  //     failOnError: true,
+  //     // in addition to linting `css.src`, this is added.
+  //     extraSrc: [],
+  //   },
+  //   // additional debugging info in comment of the output CSS - only use when necessary
+  //   sourceComments: false,
+  //   sourceMapEmbed: false,
+  //   // tell the compiler whether you want 'expanded' or 'compressed' output code
+  //   outputStyle: 'expanded',
+  //   // https://github.com/ai/browserslist#queries
+  //   autoPrefixerBrowsers: [
+  //     'last 2 versions',
+  //     'IE >= 10',
+  //   ],
+  //   includePaths: [
+  //     './node_modules',
+  //     './bower_components',
+  //   ],
+  //   // http://sassdoc.com
+  //   sassdoc: {
+  //     enabled: false,
+  //     dest: 'dest/sassdoc',
+  //     verbose: false,
+  //     basePath: 'https://github.com/phase2/pattern-lab-starter/blob/master/source/_patterns',
+  //     exclude: [],
+  //     theme: 'default',
+  //     // http://sassdoc.com/customising-the-view/#sort
+  //     sort: [
+  //       'file',
+  //       'group',
+  //       'line>',
+  //     ],
+  //   },
+  // },
+  // js: {
+  //   enabled: false,
+  //   src: [
+  //     'js/**/*.js',
+  //     'source/_patterns/**/*.js',
+  //   ],
+  //   dest: 'dest/',
+  //   destName: 'script.js',
+  //   sourceMapEmbed: false,
+  //   uglify: false,
+  //   babel: true,
+  //   // Will bundle all bower JS dependencies (not devDeps)
+  //   // creates a `bower_components.min.js` file in `js.dest`.
+  //   bundleBower: false,
+  //   eslint: {
+  //     enabled: true,
+  //     src: [
+  //       'js/**/*.js',
+  //       'source/_patterns/**/*.js',
+  //       '.*.js',
+  //       '*.js',
+  //     ],
+  //   },
+  // },
+  
+    // scssToJson: [
+    //   {
+    //     src: 'source/_patterns/00-base/05-colors/_color-vars.scss',
+    //     dest: 'source/_patterns/00-base/05-colors/colors.json',
+    //     lineStartsWith: '$c-',
+    //     allowVarValues: false,
+    //   },
+    //   {
+    //     src: 'source/_patterns/00-base/15-typography/fonts/_fonts.scss',
+    //     dest: 'source/_patterns/00-base/15-typography/fonts/font-sizes.json',
+    //     lineStartsWith: '$fs--',
+    //     allowVarValues: false,
+    //   },
+    //   {
+    //     src: 'source/_patterns/00-base/15-typography/fonts/_fonts.scss',
+    //     dest: 'source/_patterns/00-base/15-typography/fonts/font-families.json',
+    //     lineStartsWith: '$ff--',
+    //     allowVarValues: false,
+    //   },
+    //   {
+    //     src: 'source/_patterns/00-base/breakpoints/_breakpoints.scss',
+    //     dest: 'source/_patterns/00-base/breakpoints/breakpoints.json',
+    //     lineStartsWith: '$bp--',
+    //     allowVarValues: false,
+    //   },
+    //   {
+    //     src: 'source/_patterns/00-base/10-spacing/_spacing.scss',
+    //     dest: 'source/_patterns/00-base/10-spacing/spacing.json',
+    //     lineStartsWith: '$spacing--',
+    //     allowVarValues: false,
+    //   },
+    //   {
+    //     src: 'source/_patterns/00-base/animations/01-transitions/_transitions.scss',
+    //     dest: 'source/_patterns/00-base/animations/01-transitions/transitions.json',
+    //     lineStartsWith: '$trans-',
+    //     allowVarValues: true,
+    //   },
+    // ],
+  // https://github.com/nfroidure/gulp-iconfont
+  // icons: {
+  //   enabled: false,
+  //   src: 'images/icons/src/*.svg',
+  //   dest: 'dest/',
+  //   fontPathPrefix: '',
+  //   classNamePrefix: 'icon',
+  //   autohint: false,
+  //   normalize: true,
+  //   templates: {
+  //     enabled: true,
+  //     css: {
+  //       src: 'images/icons/templates/_icons-settings.scss',
+  //       dest: 'source/_patterns/01-atoms/images/',
+  //     },
+  //     demo: {
+  //       src: 'images/icons/templates/icons.twig',
+  //       dest: 'source/_patterns/01-atoms/images/',
+  //     },
+  //   },
+  //   formats: [
+  //     'ttf',
+  //     'eot',
+  //     'woff',
+  //     'svg',
+  //   ],
+  // },
+  // browserSync: {
+  //   enabled: true,
+  //   port: 3050,
+  //   watchFiles: [],
+  //   // enable when full CMS is set up
+  //   // domain: 'mysite.dev',
+  //   baseDir: './',
+  //   startPath: 'public/',
+  //   openBrowserAtStart: false,
+  //   // requires above to be true; allows non-default browser to open
+  //   browser: [
+  //     'Google Chrome',
+  //   ],
+  //   // Tunnel the Browsersync server through a random Public URL
+  //   // -> http://randomstring23232.localtunnel.me
+  //   tunnel: false,
+  //   reloadDelay: 50,
+  //   reloadDebounce: 750,
+  //   rewriteRules: [],
+  // },
+  // drupal: {
+  //   enabled: true,
+  //   themeFile: 'patternlab.info.yaml',
+  //   // when these files change
+  //   watch: [
+  //     'templates/**',
+  //     '*.theme',
+  //   ],
+  //   // run this command
+  //   command: 'drush cache-rebuild',
+  //   // in this directory
+  //   dir: './',
+  // },
--- packages/build-tools-styles/index.js
+++ packages/build-tools-styles/index.js
@@ -1,14 +1,11 @@
-'use strict';
-
-const gulp = require('gulp');
+// const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
-const jsonExporter = require('node-sass-export');
+const exportJson = require('node-sass-export');
 const npmSass = require('npm-sass');
 const postcss = require('gulp-postcss');
-const path = require('path');
 const magicImporter = require('node-sass-magic-importer');
 // const scssSyntax = require('postcss-scss');
 // const immutableCss = require('immutable-css');
@@ -26,108 +23,104 @@ const postcssReporter = require('postcss-reporter');
 // const atImport = require('postcss-import');
 const stylelint = require('stylelint');
 const scssSyntax = require('postcss-scss');
-const easyImport = require("postcss-easy-import");
 const merge = require('merge');
 const defaultConfig = require('./config.default');
 const autoprefixer = require('autoprefixer');
 // const precss = require('precss');
 
 
-
-module.exports = (gulp, userConfig, $) => {
-  
+module.exports = (gulp, userConfig) => {
   const tasks = {};
   const config = merge({
     postcss: [
-      
+
     ]
   }, defaultConfig, userConfig);
-  
-  
-  var processors = [
+
+
+  const processors = [
     stylelint(),
-    postcssReporter({ clearReportedMessages: true })
+    postcssReporter({ clearReportedMessages: true }),
+    autoprefixer()
   ];
-  
+
   function lintCSS() {
     gulp.src([
       'packages/**/*.scss',
       '!packages/_*/**/*'
     ])
-    .pipe(plumber({
-      errorHandler: function (error) {
-        notify.onError({
-          title: 'CSS <%= error.name %> - Line <%= error.line %>',
-          message: '<%= error.message %>'
-        })(error);
-      }
-    }))
-    .pipe(postcss(processors, {
-      syntax: scssSyntax
-    }));
+      .pipe(plumber({
+        errorHandler(error) {
+          notify.onError({
+            title: 'CSS <%= error.name %> - Line <%= error.line %>',
+            message: '<%= error.message %>'
+          })(error);
+        }
+      }))
+      .pipe(postcss(processors, {
+        syntax: scssSyntax
+      }));
     // .on('end', () => {
     //   done();
     // });
   }
   tasks.lint = lintCSS;
-  
-  
-  
+
+
   function compileCSS(done) {
-      
-      gulp.src(config.src)
-        .pipe(plumber({
-          errorHandler: function (error) {
-            notify.onError({
-              title: 'CSS <%= error.name %> - Line <%= error.line %>',
-              message: '<%= error.message %>'
-            })(error);
-          }
-        }))
-        // .pipe($.env.development($.sourcemaps.init()))
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-          includePaths: ['node_modules', 'packages'],
-          importer: [npmSass.importer, magicImporter],
-          outputStyle: 'expanded'
-        }).on('error', sass.logError))
-        .pipe(postcss(config.postcss))
-        // .pipe(duration('CSS Compile Time'))
-        
-        // .pipe(size({title: 'Total CSS Size'}))
-        // .pipe(gulp.dest('./styles'))
-        // .pipe(cleanCSS({
-        //   aggressiveMerging: false,
-        //   advanced: false,
-        //   keepSpecialComments: 0,
-        //   processImport: true
-        // }))
-        // .pipe(gulp.dest('./'))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(config.dest))
-        .on('end', () => {
-          // core.events.emit('reload', join(config.dest, '**/*.css'));
-          done();
-        });
+    gulp.src(config.src)
+      .pipe(plumber({
+        errorHandler(error) {
+          notify.onError({
+            title: 'CSS <%= error.name %> - Line <%= error.line %>',
+            message: '<%= error.message %>'
+          })(error);
+        }
+      }))
+    // .pipe($.env.development($.sourcemaps.init()))
+      .pipe(sourcemaps.init())
+      .pipe(sass({
+        includePaths: ['node_modules', 'packages'],
+        importer: [npmSass.importer, magicImporter],
+        functions: exportJson(config.dataDest, 'json'),
+        outputStyle: 'expanded'
+      }).on('error', sass.logError))
+      .pipe(postcss(config.postcss))
+    // .pipe(duration('CSS Compile Time'))
+
+    // .pipe(size({title: 'Total CSS Size'}))
+    // .pipe(gulp.dest('./styles'))
+    // .pipe(cleanCSS({
+    //   aggressiveMerging: false,
+    //   advanced: false,
+    //   keepSpecialComments: 0,
+    //   processImport: true
+    // }))
+    // .pipe(gulp.dest('./'))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest(config.dest))
+      .on('end', () => {
+        // core.events.emit('reload', join(config.dest, '**/*.css'));
+        done();
+      });
   }
   tasks.compile = compileCSS;
-  
-  
-  
+
+
   function watchCSS() {
-    let watchTasks = [compileCSS];
-    if (config.lint === true){
+    const watchTasks = [compileCSS];
+    if (config.lint === true) {
       watchTasks.push(lintCSS);
     }
     const src = config.extraWatches
-        ? [].concat(config.src, config.extraWatches)
-        : config.src;
+      ? [].concat(config.src, config.extraWatches)
+      : config.src;
     // console.log(watchTasks);
-    
+
     return gulp.watch(src, gulp.parallel(watchTasks));
   }
   tasks.watch = watchCSS;
-  
-  
+
+
   return tasks;
-}
+};
