8a154dc99c451238948cc0a542db80e97a7cba07
Salem Ghoweri <salem.ghoweri@pega.com>
2017-07-30 16:41:48 -0400

    WIP: stashing latest cleanup for v0.1.0 release

--- _tests/scripts/build-test-css.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-DIR="$( cd "$( dirname $0 )" && pwd )"
-
-. "$DIR/_build-test-parameter.sh"
-
-( cd "packages/$PACKAGE" && ../../node_modules/node-sass/bin/node-sass --importer ../../node_modules/node-sass-magic-importer/dist/cli.js "test/test.scss" | ../../node_modules/postcss-cli/bin/postcss -o "test/tmp/test.css" )
