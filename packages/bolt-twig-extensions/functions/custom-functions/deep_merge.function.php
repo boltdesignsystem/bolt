255f9913e639248f164ed5c0078f54852e39c6d5
Salem Ghoweri <salem.ghoweri@pega.com>
2017-10-11 13:52:03 -0400

    fix: fixing bugs with new image component. integrated w/ background component. fixed ui-list sizes

--- packages/bolt-twig-extensions/functions/custom-functions/deep_merge.function.php
+++ packages/bolt-twig-extensions/functions/custom-functions/deep_merge.function.php
@@ -1,25 +1,6 @@
 <?php
-
-  // if (function_exists('deepMerge') == false) {
-  //   function deepMerge($a, &$new) {
-  //     foreach($a as $kA1 => $vA1) {
-  //       if (!isset($new[$kA1])) {
-  //         $new[$kA1] = $vA1;
-  //       }
-  //       if (is_array($vA1) && is_array($new[$kA1])) {
-  //         deepMerge($vA1, $new[$kA1]);
-  //       }
-  //     }
-  //   }
-  // }
-
 $function = new Twig_SimpleFunction('deep_merge', function ($param1, $param2) {
-
-    
-    $result = array_replace_recursive( $param1, $param2 );
-    
-    print_r($result);
- 
-    return $result;
-
+  $result = array_merge_recursive( $param1, $param2 );
+  // $result = array_replace_recursive( $param1, $param2 );
+  return $result;
 });
