76f906d1c287d88998e7e95dfdee3c9ece26e124
Salem <me@salemghoweri.com>
2017-10-17 07:54:31 -0400

    Globally Updating "Items" Array to "ContentItems", Iterating on Card Template Logic (#156)
    * feat: globally updating items to contentItems, iterating on card filtering logic
    
    * feat: updating README + fixing button group styling
    
    * Rename _gcd.twig to gcd.twig
    
    * Update image.twig
    
    * Update gcd.twig
    
    * Update gulpfile.js
    
    * fix: fixing async font loader -- updated webpack build path
    
    * Update index.js

--- packages/bolt-twig-extensions/functions/custom-functions/responsive-images/ratio.function.php
+++ packages/bolt-twig-extensions/functions/custom-functions/responsive-images/ratio.function.php
@@ -10,15 +10,31 @@ $function = new Twig_SimpleFunction('ratio', function ($fileName, $heightOrWidth
       $svgfile = simplexml_load_file($filePath);
       
       $viewport = explode(" ", $svgfile['viewBox']);
+      $svgHeight = $svgfile['height']; // if it exists
+      $svgWidth = $svgfile['width']; // if it exists
+      $height = '';
+      $width = '';
       
-      $height = $viewport[3];
-      $width = $viewport[2];
+      // If the SVG height / width values exist, use those first
+      if ($svgHeight && $svgWidth) {
+        $height = $svgHeight;
+        $width = $svgWidth;
 
-      if ($heightOrWidthRatio == 'width'){
-        return $width;
-      } else {
-        return $height;
+      // Otherwise try to calculate the aspect ratio via the viewport
+      } else if ($viewport[3] && $viewport[2]){
+        $height = $viewport[3];
+        $width = $viewport[2];
       }
+      
+      // Only return a ratio value if we have the data to back it up
+      if ($height != '' && $width != ''){
+        if ($heightOrWidthRatio == 'width'){
+          return $width;
+        } else {
+          return $height;
+        }
+      }
+      
     } else if (($fileExt == "jpg") || ($fileExt == "png")){
       // Get the dimensions of the image
       $size = getimagesize($filePath);
---
+++ src/_patterns/02-components/bolt-headline.md
@@ -0,0 +1,6 @@
+---
+title: Headline
+order: 5
+---
+
+Example bolt headline docs!
