f6e5e14ebf4175658d44e18bbf6f5e51174c8585
Salem Ghoweri <salem.ghoweri@pega.com>
2017-10-31 09:24:21 -0400

    fix: fixing PL build so fonts load properly; cleaning up unused files

--- build-tools/gulp-tasks/build-patternlab/index.js
+++ build-tools/gulp-tasks/build-patternlab/index.js
@@ -26,7 +26,7 @@ const consolePath = path.join(patternLabRoot, 'core/console');
 // Build Pattern Lab via CLI command -- can exit or not based on 2nd param passed in
 function patternLab(done, errorShouldExit) {
   core.events.emit('pattern-lab:precompile');
-  core.notify.sh(`php -d memory_limit=4048M ${consolePath} --generate --patternsonly`, errorShouldExit, (err) => {
+  core.notify.sh(`php -d memory_limit=4048M ${consolePath} --generate`, errorShouldExit, (err) => {
     core.events.emit('reload', '**/*.html', true);
     done(err);
   });
