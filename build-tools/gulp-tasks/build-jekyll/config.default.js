7345d186d6d56329824355a4bdaf7ef70feb4d1a
Salem Ghoweri <salem.ghoweri@pega.com>
2017-11-09 13:46:48 -0500

    chore: fixing package versions in build tools; moving config presets over to packages folder.

--- build-tools/gulp-tasks/build-jekyll/config.default.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  source: './packages/website-jekyll/source',
-  dest: './bolt-website',
-  commandPrefix: 'bundle exec jekyll',
-  incremental: false,
-  watch: true,
-  drafts: true
-};
--- build-tools/gulp-tasks/build-lerna/config.default.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  sources: [
-    './packages/**/*/package.json',
-    '!./packages/*/node_modules'
-  ]
-};
