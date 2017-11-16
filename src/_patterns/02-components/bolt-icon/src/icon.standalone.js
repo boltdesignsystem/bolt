f85e5d936c89aab21a7fa1227a974f9830c181a3
Salem Ghoweri <salem.ghoweri@pega.com>
2017-11-16 17:54:10 -0500

    WIP: polishing up 1st of 2 product landing pages

--- src/_patterns/02-components/bolt-icon/src/icon.standalone.js
+++ src/_patterns/02-components/bolt-icon/src/icon.standalone.js
@@ -13,18 +13,25 @@ const backgroundStyles = [
 ];
 
 
+const colors = [
+  'teal'
+]
+
+
 class BoltIcon extends withComponent(withPreact()) {
   static props = {
     name: props.string,
     size: props.string,
-    background: props.string
+    background: props.string,
+    color: props.string
   }
 
   renderCallback({ props }) {
     const classes = css(
       'c-bolt-icon',
-      props.size && spacingSizes[props.size] && spacingSizes[props.size] !== '' ? `c-bolt-icon--${props.size}` : ``,
-      props.name ? `c-bolt-icon--${props.name}` : ''
+      props.name ? `c-bolt-icon--${props.name}` : '',
+      props.color && colors.includes (props.color) ? `c-bolt-icon--${props.color}` : ``,
+      props.size && spacingSizes[props.size] && spacingSizes[props.size] !== '' ? `c-bolt-icon--${props.size}` : ``
     );
 
     const backgroundClasses = css(
@@ -32,14 +39,20 @@ class BoltIcon extends withComponent(withPreact()) {
       props.background && backgroundStyles.includes(props.background) ? `c-bolt-icon__${props.background}-background` : ''
     )
 
+    const iconClasses = css(
+      'c-bolt-icon__icon',
+      // props.background && backgroundStyles.includes(props.background) ? `c-bolt-icon__${props.background}-background` : ''
+    )
+
+
     const iconName = props.name ? upperCamelCase(props.name) : '';
     const size = props.size && spacingSizes[props.size] ? (spacingSizes[props.size].replace('rem', '') * 16 / 2) : spacingSizes['medium'];
     const IconTag = Icon[iconName];
 
     return (
-      <div class="c-bolt-icon-wrapper">
+      <div className={classes}>
         <style>{styles[0][1]}</style>
-        <IconTag className={classes} size={size} />
+        <IconTag className={iconClasses} size={size} />
         {props.background && props.size == "xlarge" &&
           <span class={backgroundClasses}></span>
         }
