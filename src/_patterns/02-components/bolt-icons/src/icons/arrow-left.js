049f18393b492ba4a98a445b1d529454f80024ef
Salem Ghoweri <salem.ghoweri@pega.com>
2017-11-15 20:39:29 -0500

    WIP: prototyping first 1/3 of Product Landing page.

--- src/_patterns/02-components/bolt-icons/src/icons/arrow-left.js
+++ src/_patterns/02-components/bolt-icons/src/icons/arrow-left.js
@@ -3,19 +3,13 @@ const ArrowLeft = ({ color, size, ...otherProps }) => {
   color = color || 'currentColor';
   size = size || '24';
   return (
-    <svg width={size} height={size} viewBox="0 0 24 24" {...otherProps}>
-      <title>Icon/utility/Indigo/24px/arrow-left</title>
-      <g fill="currentColor" fill-rule="evenodd">
-        <path
-          d="M0,64a64,64 0 1,0 128,0a64,64 0 1,0 -128,0"
-          class="c-bolt-icon--background c-bolt-icon--circle-background"
-          fill="none"
-        />
-        <path
-          d="M20.917 12.378c.038-.112.063-.212.069-.318v-.105a.951.951 0 0 0-.069-.33 1.084 1.084 0 0 0-.162-.274c-.03-.036-.043-.049-.055-.06l-6-6a1.005 1.005 0 0 0-1.413 0 1.004 1.004 0 0 0 0 1.412l4.294 4.294H4c-.55 0-1 .45-1 1s.45 1 1 1h13.587l-4.294 4.294a1.005 1.005 0 0 0 0 1.413.999.999 0 0 0 1.412 0l5.994-5.994c.019-.025.031-.044.044-.056.087-.107.136-.188.174-.276z"
-          fill="currentColor"
-        />
-      </g>
+    <svg width="18" height="14" viewBox="0 0 18 14" {...otherProps}>
+      <title>arrow-left</title>
+      <path
+        d="M.083 7.378a1.181 1.181 0 0 1-.069-.318v-.105a.951.951 0 0 1 .069-.33c.044-.112.1-.193.162-.274.03-.036.043-.049.055-.06l6-6a1.005 1.005 0 0 1 1.413 0 1.004 1.004 0 0 1 0 1.412L3.419 5.997H17c.55 0 1 .45 1 1s-.45 1-1 1H3.413l4.294 4.294a1.005 1.005 0 0 1 0 1.413.999.999 0 0 1-1.412 0L.301 7.71C.282 7.685.27 7.666.257 7.654a1.128 1.128 0 0 1-.174-.276z"
+        fill="currentColor"
+        fill-rule="nonzero"
+      />
     </svg>
   );
 };
