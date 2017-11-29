6ec7daa001949d073ef4fae00db0bf00a517925c
Remy Denton <remy.denton@pega.com>
2017-11-29 13:43:32 -0800

    BK-66: WIP

--- src/_patterns/02-components/bolt-smooth-scroll/src/smooth-scroll.js
+++ src/_patterns/02-components/bolt-smooth-scroll/src/smooth-scroll.js
@@ -1,3 +1,18 @@
 import SmoothScroll from 'smooth-scroll';
 
-var scroll = new SmoothScroll('a[href*="#"]');
+const scroll = new SmoothScroll('a[href*="#"]', {
+    // Selectors
+    ignore: '[data-scroll-ignore]', // Selector for links to ignore (must be a valid CSS selector)
+    header: null, // Selector for fixed headers (must be a valid CSS selector)
+
+    // Speed & Easing
+    speed: 500, // Integer. How fast to complete the scroll in milliseconds
+    offset: 20, // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
+    easing: 'easeInOutCubic', // Easing pattern to use
+
+    // Callback API
+    before: function () { console.log('smooth-scroll-before') }, // Callback to run before scroll
+    after: function () { console.log('smooth-scroll-after'); } // Callback to run after scroll
+});
+
+console.log(scroll);
