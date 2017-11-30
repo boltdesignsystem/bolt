f8ec2c3f04d2ba15c8eabf293d859b94a1ae76ac
Remy Denton <remy.denton@pega.com>
2017-11-30 08:27:27 -0800

    BK-66: Add more complex smooth-scroll options

--- src/_patterns/02-components/bolt-smooth-scroll/src/smooth-scroll.js
+++ src/_patterns/02-components/bolt-smooth-scroll/src/smooth-scroll.js
@@ -1,3 +1,38 @@
 import SmoothScroll from 'smooth-scroll';
 
-var scroll = new SmoothScroll('a[href*="#"]');
+const scroll = new SmoothScroll();
+
+const defaultScrollOffset = 0;
+const defaultScrollSpeed = 500;
+
+const customScrollElems = document.querySelectorAll('a[href*="#"]');
+customScrollElems.forEach(elem => {
+    const scrollElem = elem;
+    const scrollOffset = scrollElem.dataset.scrollOffset ? scrollElem.dataset.scrollOffset : defaultScrollOffset;
+    const scrollSpeed = scrollElem.dataset.scrollSpeed ? scrollElem.dataset.scrollSpeed : defaultScrollSpeed;
+
+    const scrollOptions = {
+        ignore: '[data-scroll-ignore]', // Selector for links to ignore (must be a valid CSS selector)
+        header: null, // Selector for fixed headers (must be a valid CSS selector)
+
+        // Speed & Easing
+        speed: scrollSpeed, // Integer. How fast to complete the scroll in milliseconds
+        offset: scrollOffset, // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
+        easing: 'easeInOutCubic', // Easing pattern to use
+
+        // Callback API
+        before: function () { console.log('smooth-scroll-before') }, // Callback to run before scroll
+        after: function () { console.log('smooth-scroll-after'); } // Callback to run after scroll
+    };
+
+    let scrollElemHref = scrollElem.getAttribute('href');
+    scrollElemHref = scrollElemHref.replace('#', '');
+
+    const scrollTarget = document.getElementById(scrollElemHref);
+
+    if (scrollTarget) {
+        scrollElem.addEventListener('click', function(){
+            scroll.animateScroll(scrollTarget, scrollElem, scrollOptions);
+        });
+    }
+});
