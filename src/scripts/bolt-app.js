0ccabb9401f1535e7f925a0303a97b1fccb6472f
Denton, Remy <Remy.Denton@pega.com>
2017-12-04 08:47:19 -0800

    BK-67: Remove 'components-all' from import path for navbar js

--- src/scripts/bolt-app.js
+++ src/scripts/bolt-app.js
@@ -1,4 +1,4 @@
 import '@bolt/components-image/src/image.js';
 import '@bolt/components-icon/src/icon.js'; //Required - otherwise the `dist` version would get included by default which would result in double shimming. @TODO: abstract web components polyfills into separate require.ensure call
-import '@bolt/components-all/bolt-navbar/src/navbar.js';
+import '@bolt/components-navbar/src/navbar.js';
 // import '@bolt/components-card/src/card.js';
