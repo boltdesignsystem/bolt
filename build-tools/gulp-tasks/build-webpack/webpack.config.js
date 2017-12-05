e43b8538481fb1983229a6f6cf7e0216154e525b
Salem Ghoweri <salem.ghoweri@pega.com>
2017-12-05 16:17:52 -0500

    fix: revert webpack config update -- no longer needed.

--- build-tools/gulp-tasks/build-webpack/webpack.config.js
+++ build-tools/gulp-tasks/build-webpack/webpack.config.js
@@ -33,8 +33,6 @@ const defaultConfig = {
   // entry: './src/index.js',
   entry: {
     'critical-fonts': './src/_patterns/02-components/bolt-critical-fonts/src/critical-fonts',
-
-    'critical-fonts': './src/_patterns/02-components/bolt-button/src/button',
     // './src/components/bolt-icon/dist/icon': [
     //   // './src/scripts/native-shim.js', //Wrapper for custom-elements-es5-adapter.js so this doesn't break in other browsers like IE11
     //   // './node_modules/@webcomponents/webcomponentsjs/webcomponents-lite.js',
