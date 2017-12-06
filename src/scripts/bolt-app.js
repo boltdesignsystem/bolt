8a2dabc402dcaa7c5e6bca3ef77989da3fe6adbc
Denton, Remy <Remy.Denton@pega.com>
2017-12-06 12:29:51 -0800

    BK-74: Add sticky js using stickybits library

--- src/scripts/bolt-app.js
+++ src/scripts/bolt-app.js
@@ -3,4 +3,5 @@ import '@bolt/components-device-viewer/src/device-viewer.js';
 import '@bolt/components-icon/src/icon.js'; //Required - otherwise the `dist` version would get included by default which would result in double shimming. @TODO: abstract web components polyfills into separate require.ensure call
 import '@bolt/components-nav-bar/src/nav-bar.js';
 import '@bolt/components-smooth-scroll/src/smooth-scroll.js';
+import '@bolt/components-sticky/src/sticky.js';
 // import '@bolt/components-card/src/card.js';
