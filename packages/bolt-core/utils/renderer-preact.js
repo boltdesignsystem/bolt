ae84d9f8d946aa47219f430ac65366b42b56a133
Salem Ghoweri <salem.ghoweri@pega.com>
2017-12-08 08:37:43 -0500

    WIP: stashing latest

---
+++ packages/bolt-core/utils/renderer-preact.js
@@ -0,0 +1,48 @@
+/** @jsx h */
+// Temp copy till SkateJS fixes this upstream
+
+import { h, render } from 'preact';
+
+export function withPreact(Base = HTMLElement){
+  return class extends Base {
+    get props() {
+      // We override props so that we can satisfy most use
+      // cases for children by using a slot.
+      return {
+        ...super.props,
+        ...{ children: <slot /> },
+      };
+    }
+
+    // get state() {
+    //   // We override props so that we can satisfy most use
+    //   // cases for children by using a slot.
+    //   return {
+    //     ...super.state
+    //   };
+    // }
+
+    renderer(renderRoot, renderCallback) {
+      this._renderRoot = renderRoot;
+      this._preactDom = render(
+        renderCallback(),
+        this._renderRoot,
+        this._preactDom || this._renderRoot.children[0]
+      );
+    }
+
+    // updated(...args) {
+    //   super.updated && super.updated(...args);
+    //   this.rendering && this.rendering();
+    //   this.renderer(this.renderRoot, () => this.render && this.render(this));
+    //   this.rendered && this.rendered();
+    // }
+
+    disconnectedCallback() {
+      super.disconnectedCallback && super.disconnectedCallback();
+      // Preact hack https://github.com/developit/preact/issues/53
+      const Nothing = () => null;
+      this._preactDom = render(<Nothing />, this._renderRoot, this._preactDom);
+    }
+  }
+};
