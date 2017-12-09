7afdbb5126cbec9c1fa5ce919087c4bd442e2564
Salem Ghoweri <salem.ghoweri@pega.com>
2017-12-08 22:13:21 -0500

    WIP: stashing latest

--- src/_patterns/02-components/bolt-button/src/button.js
+++ src/_patterns/02-components/bolt-button/src/button.js
@@ -1,1 +1,42 @@
+import { define, props, withComponent } from 'skatejs';
+import { eventHandler, withPreact } from '@bolt/core'; // Latest v. broken so using local version for now
+import { h, render } from 'preact';
+
 import styles from './button.scoped.scss';
+
+@define
+export class BoltButton extends withComponent(withPreact()) {
+  static is = 'bolt-button';
+
+  static props = {
+    onClick: props.string,
+    onClickTarget: props.string
+  }
+
+  clickHandler(event){
+    const clickMethod = this.props.onClick;
+    const clickTarget = this.props.onClickTarget;
+
+    if (clickMethod){
+      if (clickTarget){
+        const elems = document.querySelectorAll(`.${clickTarget}`);
+        
+        if (elems){
+          elems.forEach(function (elem) {
+            elem[clickMethod]();
+          });
+        }
+      } else {
+        this[clickMethod]();
+      }
+    }
+  }
+
+  connectedCallback() {
+    this.addEventListener('click', this.clickHandler);
+  }
+
+  get renderRoot() {
+    return this;
+  }
+}
