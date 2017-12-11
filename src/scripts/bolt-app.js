6f619874145906a8f30d1e978319da0da052c880
Salem Ghoweri <salem.ghoweri@pega.com>
2017-12-10 21:06:05 -0500

    feat: working expand functionality w/ background videos, decoupled from bands and band collections. lots and lots of cleanup left but at least this gets us half-way there.

--- src/scripts/bolt-app.js
+++ src/scripts/bolt-app.js
@@ -2,6 +2,9 @@
 import '@bolt/objects-ratio';
 import '@bolt/components-brightcove-player';
 import '@bolt/components-button/src/button.polyfilled';
+
+import '@bolt/components-band/src/band.polyfilled';
+import '@bolt/components-band/src/band-collection.polyfilled';
 // import '@bolt/components-device-viewer/src/device-viewer.js';
 // import '@bolt/components-icon/src/icon.js'; //Required - otherwise the `dist` version would get included by default which would result in double shimming. @TODO: abstract web components polyfills into separate require.ensure call
 // import '@bolt/components-card/src/card.js';
