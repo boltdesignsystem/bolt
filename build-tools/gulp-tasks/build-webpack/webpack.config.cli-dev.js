6ffd33daf77418b75b1270e643d65baf7ea47d2f
Salem <me@salemghoweri.com>
2017-12-12 18:05:13 -0500

    fix: fixing webpack cli-dev config for non-minified JS builds -- updating video component to include non-minified bundles for debugging

--- build-tools/gulp-tasks/build-webpack/webpack.config.cli-dev.js
+++ build-tools/gulp-tasks/build-webpack/webpack.config.cli-dev.js
@@ -1,63 +1,38 @@
-// const ManifestPlugin = require('webpack-manifest-plugin');
-// const exportJson = require('node-sass-export');
-// const npmSass = require('npm-sass');
-// const fs = require('fs-extra');
-// const WorkboxPlugin = require('workbox-webpack-plugin');
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
-// const autoprefixer = require('autoprefixer');
-// const pkg = require('./package.json');
-// const ConcatPlugin = require('webpack-concat-plugin');
 const path = require('path');
 const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const merge = require('merge').recursive;
 
-// const isDev = process.argv.find(arg => arg.includes('webpack-dev-server'));
-// const ENV = isDev ? 'development' : 'production';
-// const outputPath = isDev ? path.resolve('src') : path.resolve('./bolt-website');
-
-
-// const BROWSERS = process.env.BROWSERS === 'module' ? ['last 2 Chrome versions', 'Safari 10'] : ['last 2 versions', 'not ie <= 11'];
-// const IS_MODULE_BUILD = BROWSERS[0].includes('Chrome');
-// const processEnv = {
-//   NODE_ENV: JSON.stringify(ENV),
-//   appVersion: JSON.stringify(pkg.version)
-// };
-
-// const sassDataExport = `${process.cwd()}/bolt-website/data`;
-// if (!fs.existsSync(sassDataExport)) {
-//   fs.mkdirp(sassDataExport);
-// }
-
 module.exports = (options) => {
-  // const webpack = require('webpack');
-  // const path = require('path');
-  // const ExtractTextPlugin = require('extract-text-webpack-plugin');
-  // // const outputPath = isDev ? resolve('src') : resolve('dist');
-  // const WorkboxPlugin = require('workbox-webpack-plugin');
-  
   const commonConfig = require('./webpack.config');
-  const webpackConfig = Object.create(commonConfig({
-    devtool: 'sourcemap'
+  
+  const devCliConfig = merge(commonConfig({
+    // sassDataExportPath: `${process.cwd()}/dist/`
   }));
   
-  webpackConfig.plugins = webpackConfig.plugins.concat(
-    new CleanWebpackPlugin([!process.env.cli && webpackConfig.output.path ? webpackConfig.output.path : ''], {
+  devCliConfig.entry = {};
+
+  devCliConfig.output = {
+    filename: '[name].js',
+    path: `${process.cwd()}/dist/`
+  }
+
+  devCliConfig.plugins = devCliConfig.plugins.concat(
+    new CleanWebpackPlugin([!process.env.cli && devCliConfig.output.path ? devCliConfig.output.path : ''], {
       verbose: true,
       root: process.cwd() // set root context to wherever webpack is getting run (globally or at the component level)
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"'
     }),
     new ExtractTextPlugin({
       filename: '[name].min.css?[hash]-[chunkhash]-[contenthash]-[name]',
       disable: false,
       allChunks: true
     }),
-    new webpack.NoEmitOnErrorsPlugin()
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    })
   );
 
-  return webpackConfig;
+  return devCliConfig;
 };
