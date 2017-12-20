dd7b5ad133aff6432cc48981ca59fa6e962fbe58
Salem <me@salemghoweri.com>
2017-12-20 15:54:04 -0500

    fix: Update webcomponentjs shims to address known IE11 errors. Switch on ES7 array.includes shim to address silent error w/ IE11 displaying icons correctly

--- packages/bolt-core/polyfills/polyfill-loader.js
+++ packages/bolt-core/polyfills/polyfill-loader.js
@@ -2,18 +2,19 @@ require('es6-promise').polyfill();
 require('core-js/modules/es6.array.iterator');
 require('core-js/modules/es6.symbol');
 require('core-js/modules/es6.array.from');
+require('core-js/modules/es7.array.includes');
 
 export const polyfillLoader = new Promise(function (resolve, reject) {
   if (window.customElements !== undefined) {
     Promise.all([
       import(/* webpackChunkName: "custom-elements-es5-adapter" */ '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js'),
-      import(/* webpackChunkName: "wc-polyfill-sd" */ './wc-polyfill-sd')
+      import(/* webpackChunkName: "webcomponents-hi-sd-ce" */ '@webcomponents/webcomponentsjs/webcomponents-hi-sd-ce.js')
     ]).then(() => {
       resolve();
     });
   } else {
     Promise.all([
-      import(/* webpackChunkName: "wc-polyfill-ce-sd" */ './wc-polyfill-ce-sd')
+      import(/* webpackChunkName: "webcomponents-lite" */ '@webcomponents/webcomponentsjs/webcomponents-lite.js')
     ]).then(() => {
       resolve();
     });
