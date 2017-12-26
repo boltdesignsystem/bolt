5e6ca89c7c6ca8ab5278597ba7fe788cb0b39ae3
Salem <me@salemghoweri.com>
2017-12-26 15:35:10 -0500

    feat: add new babel config to collection preset configs
    - Adds new .babelrc example
    - Exports Babel config in such a way to allow for including it via node CLI, Webpack config, or .babelrc file
    - Only includes the babel plugins and presets actually required for current cross browser compatibility & support -- no functional changes

---
+++ packages/bolt-config-presets/config-babel/index.js
@@ -0,0 +1,87 @@
+const preset = function(api, opts = {}) {
+  return {
+    presets: [
+      ['env', {
+        targets: {
+          node: 'current',
+          browsers: [
+            'last 3 versions',
+            'not ie < 9'
+          ]
+        },
+        modules: false,
+        debug: false
+      }]
+    ],
+    plugins: [
+      // ex. @define decorator
+      'transform-decorators-legacy',
+
+      // class { handleThing = () => { } }
+      'transform-class-properties',
+
+      // import('button.standalone.js')
+      'syntax-dynamic-import',
+
+      // critical for preact rendering
+      [
+        'transform-react-jsx',
+        {
+          pragma: 'h'
+        }
+      ],
+
+      // The following two plugins use Object.assign directly, instead of Babel's
+      // extends helper. Note that this assumes `Object.assign` is available.
+      // { ...todo, completed: true }
+      [
+        'transform-object-assign',
+        {
+          async: true
+        }
+      ],
+
+      [
+        'transform-object-rest-spread',
+        {
+          useBuiltIns: true
+        }
+      ],
+
+      // @TODO: only include this when being run on a NODE environment
+      // [
+      //   require.resolve('babel-plugin-transform-es2015-modules-commonjs'),
+      //   { loose: true }
+      // ],
+
+      // @TODO: only include this when being run on a NODE environment
+      // [
+      //   'transform-es2015-modules-commonjs',
+      //   { loose: true }
+      // ],
+
+      // @TODO: only include this when being run on a NODE environment
+      // 'dynamic-import-node'
+    ]
+  };
+};
+
+// @TODO: refactor -- block below is the general approach I've seen other babel presets take to conditionally
+// include plugins in certain environments
+
+// if (process.env.NODE_ENV === 'ssr') {
+//   preset.plugins.push.apply(preset.plugins, [
+//     require.resolve('babel-plugin-dynamic-import-node'),
+//     // We always include this plugin regardless of environment
+//     // because of a Babel bug that breaks object rest/spread without it:
+//     // https://github.com/babel/babel/issues/4851
+//     // require.resolve('babel-plugin-transform-es2015-parameters'),
+//     // // Jest needs this to work properly with import/export syntax
+//     // [
+//     //   require.resolve('babel-plugin-transform-es2015-modules-commonjs'),
+//     //   { loose: true }
+//     // ]
+//   ]);
+// }
+
+module.exports = preset;
