5c8c898d3408745ad371eef3ca1bfcd7886d5c6c
Salem <me@salemghoweri.com>
2017-12-28 14:20:21 -0500

    fix: fix eslint linter errors in browserslist config + working test

--- packages/bolt-config-presets/browserslist-config-bolt/test.js
+++ packages/bolt-config-presets/browserslist-config-bolt/test.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 const assert = require('assert');
 const browserslist = require('browserslist');
 const config = require('./index');
@@ -5,5 +7,7 @@ const config = require('./index');
 const browsers = browserslist(config);
 assert(browsers.length > 0, 'Successfully resolves a list of browsers');
 
-console.log('Resolved the following browsers:');
+console.log('Bolt is currently supporting the following browsers:');
 console.log(browsers.join('\n'));
+
+/* eslint-enable no-console */
