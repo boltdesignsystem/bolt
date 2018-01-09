e8d3db4767bcc1abbab4ac5d578d4b148a9a8820
Salem <me@salemghoweri.com>
2018-01-09 09:41:55 -0500

    fix: further simplify band animation calculation to speed up initial animation, independent on if the video player has finished loading

--- src/_patterns/02-components/bolt-band/src/band.standalone.js
+++ src/_patterns/02-components/bolt-band/src/band.standalone.js
@@ -38,10 +38,6 @@ export class BoltBand extends withComponent(withPreact()) {
     return ['expanded', 'expandedHeight', 'initialHeight'];
   }
 
-  static props = {
-    isCollapsable: props.boolean
-  }
-
   constructor() {
     super();
     this.attachShadow({
@@ -57,8 +53,14 @@ export class BoltBand extends withComponent(withPreact()) {
       bandTemplate.content.cloneNode(true)
     );
 
-    this.addEventListener('expandedHeightSet', this._adjustExpandedHeightToMatchChildren);
-    this._onWindowResize = this._onWindowResize.bind(this);
+    if (this.state.ready === false) {
+      this.state.ready = true;
+      this.classList.add('is-ready');
+    }
+
+    if (this.expandedHeight === null){
+      this.expandedHeight = '56.25vh';
+    }
 
     if (this.expanded) {
       this.expand();
@@ -82,17 +84,12 @@ export class BoltBand extends withComponent(withPreact()) {
     this.addEventListener('ended', this.finishedHandler);
     this.addEventListener('close', this.collapse);
 
-    if (this.isCollapsable){
-      window.addEventListener('optimizedResize', this._onWindowResize);
-    }
+    this.addEventListener('expandedHeightSet', this._adjustExpandedHeightToMatchChildren);
   }
 
   disconnectedCallback() {
-    if (this.isCollapsable) {
-      window.removeEventListener('optimizedResize', this._onWindowResize);
-    }
+    this.removeEventListener('expandedHeightSet', this._adjustExpandedHeightToMatchChildren);
   }
-
 
   /**
     * `attributeChangedCallback` processes changes to the `expanded` attribute.
@@ -103,26 +100,15 @@ export class BoltBand extends withComponent(withPreact()) {
       this.classList.add('is-ready');
     }
 
-
-
     // `expanded` is a boolean attribute it is either set or not set. The
     // actual value is irrelevant.
     const value = this.hasAttribute('expanded');
     this.setAttribute('aria-expanded', value);
   }
 
-  _onWindowResize() {
-    if (!this.expanded) {
-      this.updateInitialHeight();
-    }
-  }
-
-
   playHandler(event) {
-    if (event.detail.isBackgroundVideo) {
-
+    if (event.detail.isBackgroundVideo && this.expanded === false) {
       this.expand();
-      // this.expanded = true;
 
       this.dispatchEvent(
         new CustomEvent('change', {
@@ -135,50 +121,56 @@ export class BoltBand extends withComponent(withPreact()) {
 
 
   collapse() {
-    const elem = this;
+    const startingHeight = this.getBoundingClientRect().height;
+    const endingHeight = '0px';
 
-    if (this.state.ready){
-      this.style.height = this.expandedHeight + 'px';
+    requestAnimationFrame(() => {
+      this.style.minHeight = `${startingHeight}px`;
+      this.style.transition = 'all 0s';
 
-      this.style.height = this.initialHeight + 'px';
+      requestAnimationFrame(() => {
+        // In order to get the animation to play, we'll need to wait for
+        // the 'invert' animation frame to finish, so that its inverted
+        // position has propagated to the DOM.
+        //
+        // Then, we just remove the transform, reverting it to its natural
+        // state, and apply a transition so it does so smoothly.
+        this.style.transition = 'all 0.4s ease';
+        this.style.minHeight = endingHeight;
+      });
+    });
 
-      setTimeout(function () {
-        elem.style.height = 'auto'; // remove "height" from the element's inline styles, so it can return to its initial value
-      }, 700);
-    }
-
-    this.expanded = false; // mark the section as "currently collapsed"
+    this.expanded = false;
   }
 
-
   expand() {
-    if (this.state.ready) {
-      this.style.height = this.initialHeight + 'px';
+    const startingHeight = this.getBoundingClientRect().height;
+    const endingHeight = this.expandedHeight;
 
-      this.addEventListener('transitionend', function f() {
-        this.style.height = this.expandedHeight + 'px';
-        this.removeEventListener('transitionend', f);
+    requestAnimationFrame(() => {
+      this.style.minHeight = `${startingHeight}px`;
+      this.style.transition = 'all 0s';
+
+      requestAnimationFrame(() => {
+        // In order to get the animation to play, we'll need to wait for
+        // the 'invert' animation frame to finish, so that its inverted
+        // position has propagated to the DOM.
+        //
+        // Then, we just remove the transform, reverting it to its natural
+        // state, and apply a transition so it does so smoothly.
+        this.style.transition = 'all 0.4s ease';
+        this.style.minHeight = this.expandedHeight;
       });
-    }
+    });
 
-    this.expanded = true; // mark the section as "currently not collapsed"
+    this.expanded = true;
   }
 
 
   // Max Height of a child element has been set so use that to determine how tall a band should get.
   _adjustExpandedHeightToMatchChildren(event) {
-    this.updateInitialHeight();
-
-    this.isCollapsable = true;
-    window.addEventListener('optimizedResize', this._onWindowResize);
-
     if (event.detail.expandedHeight) {
       this.expandedHeight = event.detail.expandedHeight;
-    }
-
-    if (this.state.ready === false) {
-      this.state.ready = true;
-      this.classList.add('is-ready');
     }
   }
 
@@ -188,7 +180,6 @@ export class BoltBand extends withComponent(withPreact()) {
   }
 
   set expanded(value) {
-    value = Boolean(value);
     if (value) {
       this.setAttribute('expanded', '');
       this.classList.add('is-expanded');
@@ -201,66 +192,35 @@ export class BoltBand extends withComponent(withPreact()) {
   }
 
 
-  get initialHeight() {
-    return this.getAttribute('initialHeight');
-  }
-
-  set initialHeight(value) {
-    if (value) {
-      this.setAttribute('initialHeight', value);
-    } else {
-      this.removeAttribute('initialHeight');
-    }
-  }
-
-  updateInitialHeight() {
-    this.initialHeight = this.getBoundingClientRect().height;
-  }
-
   get expandedHeight() {
     return this.getAttribute('expandedHeight');
   }
 
   set expandedHeight(value) {
     if (value) {
-      this.setAttribute('expandedHeight', value);
+
+      // @TODO: come up with a better way to validate CSS unit possibilities here
+      // if (value.includes('px') ||
+      //   value.includes('vh') ||
+      //   value.includes('vw')) {
+      //   this.setAttribute('expandedHeight', value);
+      // }
+      // console.log(value.indexOf('vh'));
+
+      if (value.indexOf('vh') > -1 || value.indexOf('px') > -1) {
+        this.setAttribute('expandedHeight', value);
+      } else {
+        this.setAttribute('expandedHeight', value + 'px');
+      }
     }
     else {
       this.removeAttribute('expandedHeight');
     }
   }
 
-  // get renderRoot() {
-  //   return this;
-  // }
-
   render() {
     return (
       <slot />
     )
   }
-}
-
-// Create a custom 'optimizedResize' event that works just like window.resize but is more performant because it
-// won't fire before a previous event is complete.
-// This was adapted from https://developer.mozilla.org/en-US/docs/Web/Events/resize
-export function optimizedResize(){
-  function throttle(type, name, obj) {
-    obj = obj || window;
-    let running = false;
-
-    function func() {
-      if (running) { return; }
-      running = true;
-      requestAnimationFrame(function () {
-        obj.dispatchEvent(new CustomEvent(name));
-        running = false;
-      });
-    }
-    obj.addEventListener(type, func);
-  }
-
-  // Initialize on window.resize event.  Note that throttle can also be initialized on any type of event,
-  // such as scroll.
-  throttle('resize', 'optimizedResize');
 }
