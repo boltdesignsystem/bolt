c2d89a5470a857b9a219148e26aa109ec92841f9
Salem Ghoweri <salem.ghoweri@pega.com>
2018-01-15 18:11:30 -0500

    fix: removing WIP Node-integrated data work in Twig global data + commenting out data reference to avoid warnings till JSON data being referenced is re-added

--- packages/twig-extensions/globals/bolt.global.php
+++ packages/twig-extensions/globals/bolt.global.php
@@ -1,31 +1,18 @@
 <?php
-
 class Twig_Extension_bolt extends Twig_Extension implements Twig_Extension_GlobalsInterface {
     public static $data;
-
     // /Users/sghoweri/sites/bolt-master/
-
     function __construct(){
-      $cwd = getcwd();
-
-      $config = exec("node -e 'const config = require('\'$cwd/.boltrc.js\''); console.log(JSON.stringify(config));'");
-
-      // $buildPath = json_decode($config)->wwwDir;
-
-
-      // $dataPath = exec("node -e 'console.log(path.resolve('\'$cwd/$buildPath/data/image-sizes/image-sizes.data.json\''));'");
-
-      // $json = file_get_contents($dataPath);
-      // self::$data = json_decode($json)->boltImageSizes;
+        $documentRoot = getcwd();
+        // $json = file_get_contents($documentRoot . '/www/build/data/image-sizes/image-sizes.data.json');
+        // self::$data = json_decode($json)->boltImageSizes;
     }
-
     public static function getImageSizes() {
         return self::$data;
     }
-
    public function getGlobals(){
         return array(
-            'globalBoltImageSizes' => self::getImageSizes(),
+            // 'globalBoltImageSizes' => self::getImageSizes(),
             'TWIG_ENV' => 'pl',
             'disable_validate_json_schema' => false,
         );
