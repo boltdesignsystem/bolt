f1dffb6fd47aecd187acbf4e7381154396fe2928
Remy Denton <remy.denton@pega.com>
2018-01-18 07:14:58 -0800

    refactor: Update component for new build tools: nav-bar (#309)

--- apps/pattern-lab--workshop/.boltrc.js
+++ apps/pattern-lab--workshop/.boltrc.js
@@ -22,6 +22,7 @@ module.exports = {
       '@bolt/components-color-swatch',
       '@bolt/components-figure',
       '@bolt/components-link',
+      '@bolt/components-nav-bar',
       '@bolt/components-ordered-list',
       '@bolt/components-page-footer',
       '@bolt/components-page-header',
