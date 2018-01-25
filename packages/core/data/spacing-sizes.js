1658977cac91a5971e5d44685a3058200dfddafa
Evan Lovely <evanlovely@gmail.com>
2018-01-25 15:53:13 -0800

    Fixing icons by enabling temp static data for spacing sizes

--- packages/core/data/spacing-sizes.js
+++ packages/core/data/spacing-sizes.js
@@ -1,4 +1,17 @@
 // @todo Restore style data being pulled into JS
 // import * as boltSpacingSizes from '@bolt/core-data/spacing/bolt-spacing-sizes.data.json';
 
-// export const spacingSizes = boltSpacingSizes.boltSpacingSizes;
+// Need to get this in for icons to work; sorry!
+const tempStaticData = {
+  "boltSpacingSizes": {
+    "xxsmall": "0.25rem",
+    "xsmall": "0.5rem",
+    "small": "1rem",
+    "medium": "2rem",
+    "large": "4rem",
+    "xlarge": "8rem",
+    "xxlarge": "16rem"
+  }
+};
+
+export const spacingSizes = tempStaticData.boltSpacingSizes;
