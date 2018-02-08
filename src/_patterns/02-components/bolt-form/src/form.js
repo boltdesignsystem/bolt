de16a7df1e241e2af06a9d29d4ece132a3dbfb2c
Denton, Remy <Remy.Denton@pega.com>
2018-02-07 20:18:45 -0800

    WIP: BK-10

--- src/_patterns/02-components/bolt-form/src/form.js
+++ src/_patterns/02-components/bolt-form/src/form.js
@@ -1,19 +1,49 @@
 // TODO: limit to a .js class
-const inputs = document.querySelectorAll('.c-bolt-input');
+const inputs = document.querySelectorAll('input.c-bolt-input');
 inputs.forEach(input => {
-  input.onfocus = function() {
-    this.classList.add('is-touched');
-  };
 
-  input.onblur = function() {
-    if (this.value) {
-      this.classList.add('is-filled');
-    } else {
-      this.classList.remove('is-filled');
+  // Check if the field has pre-filled text from the server side
+  if (input.value) {
+    input.classList.add('is-filled');
+  }
+
+  input.onfocus = function() {
+    input.classList.remove('is-touched');
+
+    if (input.errors) {
+      input.errors.remove();
     }
   };
 
-  // TODO: display validation message (input.validationMessage) below a form
-  // item when it becomes invalid.
-  // See https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms/Form_validation
+  input.onblur = function() {
+    input.classList.add('is-touched');
+    if (input.value) {
+      input.classList.add('is-filled');
+    } else {
+      input.classList.remove('is-filled');
+    }
+
+    if (input.validationMessage) {
+      let error = document.createElement('div');
+      let messageText = document.createTextNode(input.validationMessage);
+      error.classList.add('c-bolt-input-message');
+      error.classList.add('c-bolt-input-message--invalid');
+      error.appendChild(messageText);
+
+      input.errors = input.insertAdjacentElement('afterend', error);
+    }
+  };
+});
+
+const customInputWrappers = document.querySelectorAll('.c-bolt-custom-input');
+customInputWrappers.forEach(wrapper => {
+  const input = wrapper.querySelector('.c-bolt-input');
+
+  input.onfocus = function() {
+    wrapper.classList.add('is-active');
+  };
+
+  input.onblur = function() {
+    wrapper.classList.remove('is-active');
+  };
 });
