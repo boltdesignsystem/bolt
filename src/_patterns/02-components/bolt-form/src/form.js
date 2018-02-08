38fa1faa2af9e689a94b27a362aafb062c151994
Denton, Remy <Remy.Denton@pega.com>
2018-02-08 14:04:33 -0800

    Enable stock Choices options for select form elements

--- src/_patterns/02-components/bolt-form/src/form.js
+++ src/_patterns/02-components/bolt-form/src/form.js
@@ -1,3 +1,38 @@
+import Choices from 'choices.js';
+
+//const elements = document.querySelectorAll('select');
+const choices = new Choices('.js-bolt-form-select', {
+  searchEnabled: false,
+  classNames: {
+    containerOuter: 'choices',
+    containerInner: 'choices__inner',
+    input: 'choices__input',
+    inputCloned: 'choices__input--cloned',
+    list: 'choices__list',
+    listItems: 'choices__list--multiple',
+    listSingle: 'choices__list--single',
+    listDropdown: 'choices__list--dropdown',
+    item: 'choices__item',
+    itemSelectable: 'choices__item--selectable',
+    itemDisabled: 'choices__item--disabled',
+    itemChoice: 'choices__item--choice',
+    placeholder: 'choices__placeholder',
+    group: 'choices__group',
+    groupHeading: 'choices__heading',
+    button: 'choices__button',
+    activeState: 'is-active',
+    focusState: 'is-focused',
+    openState: 'is-open',
+    disabledState: 'is-disabled',
+    highlightedState: 'is-highlighted',
+    hiddenState: 'is-hidden',
+    flippedState: 'is-flipped',
+    loadingState: 'is-loading',
+    noResults: 'has-no-results',
+    noChoices: 'has-no-choices'
+  }
+});
+
 // TODO: limit to a .js class
 const inputs = document.querySelectorAll('.c-bolt-input');
 inputs.forEach(input => {
