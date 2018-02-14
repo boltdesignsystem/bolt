acc85e0bb4d7050aab7b5a9bd830014365f664d0
Salem Ghoweri <salem.ghoweri@pega.com>
2018-02-14 11:45:57 -0800

    fix: conditionally swap out button rendering when being used in a form
    Workaround to eliminate the need to wire things up manually.

--- packages/bolt-core/index.js
+++ packages/bolt-core/index.js
@@ -12,3 +12,14 @@ export * from './data/spacing-sizes';
 
 // Export polyfill loader
 export * from './polyfills/polyfill-loader';
+
+
+// Util to recursively look to see if parent is a specific HTML tag
+export function findParentTag(el, tag) {
+  while (el.parentNode) {
+    el = el.parentNode;
+    if (el.tagName === tag)
+      return el;
+  }
+  return null;
+}
