365fa54f3962670c0346fc6891212e1ef65fa4b5
Jake Love <jake@basalt.io>
2018-02-14 23:23:29 -0800

    Pl Docs Polish - edit readme JS and SASS

--- apps/pattern-lab--workshop/src/scripts/pl.js
+++ apps/pattern-lab--workshop/src/scripts/pl.js
@@ -1,1 +1,17 @@
 // here if you need pl only JS
+
+/**
+ * Docs Edit README Link > Simple edit hover effect
+ */
+var editLink = document.getElementsByClassName('c-bolt-docs__page-nav__link');
+var editWrap = document.getElementsByClassName('c-bolt-docs__lead');
+
+editLink[0].addEventListener("mouseover", toggleEditOn, false);
+editLink[0].addEventListener("mouseout", toggleEditOff, false);
+
+function toggleEditOn() {
+    editWrap[0].classList.add('edit-this-readme');
+}
+function toggleEditOff() {
+    editWrap[0].classList.remove('edit-this-readme');
+}
