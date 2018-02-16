4d548b58d7461fc129da4253136c5be57c988375
Salem Ghoweri <salem.ghoweri@pega.com>
2018-02-16 11:43:12 -0500

    fix: adding updated polyfill loader to v1.0 - adds more refined cross browser polyfills + using new shadydom polyfill with massive IE 11 perf improvements

---
+++ packages/core/polyfills/bolt-webcomponents-sd-ce.js
@@ -0,0 +1,10 @@
+// Based on https://github.com/webcomponents/webcomponentsjs/blob/master/entrypoints/webcomponents-sd-ce-index.js
+
+/*
+ * Polyfills loaded: Custom Elements, Shady DOM/Shady CSS
+ * Used in: Safari 9, Firefox, Edge
+ */
+
+import '@webcomponents/shadydom/src/shadydom.js';
+import '@webcomponents/custom-elements/src/custom-elements.js';
+import '@webcomponents/shadycss/entrypoints/scoping-shim.js';
