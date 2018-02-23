034a3c2d597cddb2c8a828418d7b7a6d2eb33029
Salem Ghoweri <me@salemghoweri.com>
2018-02-23 17:22:52 -0500

    Update SVG Icons in `@bolt/icons` Component; Add Workaround Temp Build (#473)
    * fix: update handful of existing icons that are rendering incorrectly due to masks and hidden rectangles in SVG source
    
    * chore: add missing SVG icons from 0.x branch
    
    * refactor: add temporary build process to handle SVG icons sub-build in Bolt; update schema prop values to match actual icons in the folder that are available; add temporary standalone watch task to rebuild when icon source files change

--- packages/components/bolt-icons/src/icons/download.js
+++ packages/components/bolt-icons/src/icons/download.js
@@ -5,25 +5,18 @@ const Download = ({ color, size, ...otherProps }) => {
   return (
     <svg width={size} height={size} viewBox="0 0 24 24" {...otherProps}>
       <title>Icon/utility/Indigo/24px/Download</title>
-      <defs>
-        <path id="a" d="M15.998.049v1.95H.001V.05h15.997z" />
-      </defs>
       <g fill="currentColor" fill-rule="evenodd">
         <path
           d="M0,64a64,64 0 1,0 128,0a64,64 0 1,0 -128,0"
           class="c-bolt-icon--background c-bolt-icon--circle-background"
           fill="none"
         />
-        <g transform="translate(4 19.5)">
-          <mask id="b" fill="currentColor">
-            <use xlinkHref="#a" />
-          </mask>
-          <path
-            d="M15.023.049H.976A.978.978 0 0 0 0 1.025C0 1.56.44 2 .976 2h14.046a.978.978 0 0 0 .976-.975.979.979 0 0 0-.975-.976"
-            fill="currentColor"
-            mask="url(#b)"
-          />
-        </g>
+        <path
+          d="M15.023.049H.976A.978.978 0 0 0 0 1.025C0 1.56.44 2 .976 2h14.046a.978.978 0 0 0 .976-.975.979.979 0 0 0-.975-.976"
+          fill="currentColor"
+          mask="url(#mask-2)"
+          transform="translate(4 19.5)"
+        />
         <path
           d="M11.408 17.73a.77.77 0 0 0 .59.27c.23 0 .451-.101.591-.27l5.25-6.283a.652.652 0 0 0 .084-.733.76.76 0 0 0-.675-.393h-2.527V3H9.388v7.321H6.75a.76.76 0 0 0-.675.393.638.638 0 0 0-.075.306c0 .152.052.305.16.427l5.25 6.283z"
           fill="currentColor"
