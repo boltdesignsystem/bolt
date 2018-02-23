3d38bea3b6d97955dc3282307afd41ee8ef34ea3
Joe Karasek <joseph.karasek@gmail.com>
2018-02-23 11:53:27 -0800

    update core data, causing errors

--- packages/core/data/image-sizes.js
+++ packages/core/data/image-sizes.js
@@ -1,4 +1,3 @@
-// @todo Restore style data being pulled into JS
-// const imageSizes = require('@bolt/core-data/image-sizes/image-sizes.data.json').boltImageSizes;
+const imageSizes = require('@bolt/core-data/image-sizes/image-sizes.data.json').boltImageSizes;
 
-// module.exports.imageSizes = imageSizes;
+module.exports.imageSizes = imageSizes;
--- packages/core/data/spacing-sizes.js
+++ packages/core/data/spacing-sizes.js
@@ -1,17 +1,3 @@
-// @todo Restore style data being pulled into JS
-// import * as boltSpacingSizes from '@bolt/core-data/spacing/bolt-spacing-sizes.data.json';
+import * as boltSpacingSizes from '@bolt/core/data/spacing-sizes.js';
 
-// Need to get this in for icons to work; sorry!
-const tempStaticData = {
-  "boltSpacingSizes": {
-    "xxsmall": "0.25rem",
-    "xsmall": "0.5rem",
-    "small": "1rem",
-    "medium": "2rem",
-    "large": "4rem",
-    "xlarge": "8rem",
-    "xxlarge": "16rem"
-  }
-};
-
-export const spacingSizes = tempStaticData.boltSpacingSizes;
+export const spacingSizes = boltSpacingSizes.boltSpacingSizes;
