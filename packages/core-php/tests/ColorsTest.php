db5eba37a48205ec0a855647e05a1ba04d6f0f6e
Joe Karasek <joseph.karasek@gmail.com>
2018-02-27 21:07:29 -0800

    add phpUnit

---
+++ packages/core-php/tests/ColorsTest.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class ColorsTest extends TestCase
+{
+  public function testCanCalculateLuminosity() {
+    $this->assertTrue(true);
+  }
+}
