8dae4d2be84788e1f08ca07c10b81f40d3c8b921
Joe Karasek <joseph.karasek@gmail.com>
2018-02-27 21:46:19 -0800

    add first test for phpUnit

--- packages/core-php/tests/ColorsTest.php
+++ packages/core-php/tests/ColorsTest.php
@@ -1,11 +1,25 @@
 <?php
 declare(strict_types=1);
 
+namespace Bolt;
+
 use PHPUnit\Framework\TestCase;
+use Bolt;
 
 final class ColorsTest extends TestCase
 {
   public function testCanCalculateLuminosity() {
-    $this->assertTrue(true);
+    $colorBlack = '000000';
+    $colorWhite = 'ffffff';
+    $colorRandom = 'a8b3c1';
+    $epsilon = 0.00001; // accuracy for floating point test
+
+    $luminosityBlack = Colors::calculateLuminosity($colorBlack);
+    $luminosityWhite = Colors::calculateLuminosity($colorWhite);
+    $luminosityRandom = Colors::calculateLuminosity($colorRandom);
+
+    $this->assertSame(0.0, $luminosityBlack);
+    $this->assertSame(1.0, $luminosityWhite);
+    $this->assertTrue(abs(0.44415-$luminosityRandom) < $epsilon);
   }
 }
