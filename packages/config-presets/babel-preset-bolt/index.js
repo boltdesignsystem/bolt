622ab1e8dd5a4785e9a4d0b23487e0843a828265
Salem Ghoweri <salem.ghoweri@pega.com>
2018-03-02 10:42:00 -0500

    chore: wrapping up babel config upgrades, removing plugins no longer needed

--- packages/config-presets/babel-preset-bolt/index.js
+++ packages/config-presets/babel-preset-bolt/index.js
@@ -12,23 +12,17 @@ const preset = function(api, opts = {}) {
         modules: false,
         debug: false,
       }],
+      // '@babel/preset-stage-3',
     ],
     plugins: [
-/**
- * 1. Required till github.com/github/babel-plugin-transform-custom-element-classes/issues/11
- *    is closed. Currently this is required for Bolt's SVG icon component
- *
- * 2. Allows us to dynamically import JS via Webpack. ex. import('button.standalone.js')
- */
       '@babel/plugin-syntax-decorators', // ex. @define
       '@babel/plugin-proposal-decorators',
 
-      '@babel/plugin-syntax-export-default-from', // ex. `export Communications from './icons/communications';` - used in @bolt/components-icons
+      // ex. `export Communications from './icons/communications';` - used in @bolt/components-icons
+      '@babel/plugin-syntax-export-default-from',
       '@babel/plugin-proposal-export-default-from',
 
-      '@babel/plugin-transform-classes', /* [1] */
-
-
+      // Allows us to dynamically import JS via Webpack. ex. import('button.standalone.js')
       '@babel/plugin-syntax-dynamic-import', /* [2] */
 
       // ex. class { handleThing = () => { } }
@@ -46,71 +40,13 @@ const preset = function(api, opts = {}) {
           pragma: 'h',
           pragmaFrag: '\"span\"',
           throwIfNamespace: false,
-          useBuiltIns: false
-        }
+          useBuiltIns: false,
+        },
       ],
 
-      // required for preact + SVG icons to work
-      [
-        'jsx-pragmatic',
-        {
-          module: 'preact',
-          export: 'h',
-          import: 'h'
-        }
-      ],
-
-      // The following two plugins use Object.assign directly, instead of Babel's
-      // extends helper. Note that this assumes `Object.assign` is available.
-      // { ...todo, completed: true }
-      [
-        'transform-object-assign',
-        {
-          async: true
-        }
-      ],
-
-      [
-        'transform-object-rest-spread',
-        {
-          useBuiltIns: true
-        }
-      ],
-
-      // @TODO: only include this when being run on a NODE environment
-      // [
-      //   require.resolve('babel-plugin-transform-es2015-modules-commonjs'),
-      //   { loose: true }
-      // ],
-
-      // @TODO: only include this when being run on a NODE environment
-      // [
-      //   'transform-es2015-modules-commonjs',
-      //   { loose: true }
-      // ],
-
-      // @TODO: only include this when being run on a NODE environment
-      // 'dynamic-import-node'
-    ]
+      '@babel/plugin-proposal-object-rest-spread',
+    ],
   };
 };
-
-// @TODO: refactor -- block below is the general approach I've seen other babel presets take to conditionally
-// include plugins in certain environments
-
-// if (process.env.NODE_ENV === 'ssr') {
-//   preset.plugins.push.apply(preset.plugins, [
-//     require.resolve('babel-plugin-dynamic-import-node'),
-//     // We always include this plugin regardless of environment
-//     // because of a Babel bug that breaks object rest/spread without it:
-//     // https://github.com/babel/babel/issues/4851
-//     // require.resolve('babel-plugin-transform-es2015-parameters'),
-//     // // Jest needs this to work properly with import/export syntax
-//     // [
-//     //   require.resolve('babel-plugin-transform-es2015-modules-commonjs'),
-//     //   { loose: true }
-//     // ]
-//   ]);
-// }
 
 module.exports = preset;
