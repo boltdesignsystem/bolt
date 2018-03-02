810e8299a848c502e8068c9b820bac57ee2558f5
Salem Ghoweri <salem.ghoweri@pega.com>
2018-03-02 10:49:49 -0500

    chore: fixing eslint issues with pl.js file

--- apps/pattern-lab/src/scripts/pl.js
+++ apps/pattern-lab/src/scripts/pl.js
@@ -3,21 +3,21 @@
 /**
  * Docs Edit README Link > Simple edit hover effect
  */
-var editLink = document.getElementsByClassName('c-bolt-docs__page-nav__link');
-var editWrap = document.getElementsByClassName('c-bolt-docs__lead');
-
-if (editLink[0]){
-  editLink[0].addEventListener("mouseover", toggleEditOn, false);
-}
-
-if (editWrap[0]) {
-  editLink[0].addEventListener("mouseout", toggleEditOff, false);
-}
-
+const editLink = document.getElementsByClassName('c-bolt-docs__page-nav__link');
+const editWrap = document.getElementsByClassName('c-bolt-docs__lead');
 
 function toggleEditOn() {
   editWrap[0].classList.add('edit-this-readme');
 }
 function toggleEditOff() {
   editWrap[0].classList.remove('edit-this-readme');
+}
+
+
+if (editLink[0]) {
+  editLink[0].addEventListener('mouseover', toggleEditOn, false);
+}
+
+if (editWrap[0]) {
+  editLink[0].addEventListener('mouseout', toggleEditOff, false);
 }
