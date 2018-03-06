de3137f9646ecfb82f6a2c65fa05689bb92b444f
Salem Ghoweri <salem.ghoweri@pega.com>
2018-03-06 17:58:44 -0500

    fix: adding video player demos to PL, fixing issues with video player sass compiling

--- apps/pattern-lab/.boltrc.js
+++ apps/pattern-lab/.boltrc.js
@@ -36,6 +36,7 @@ module.exports = {
       '@bolt/components-background',
       '@bolt/components-background-shapes',
       '@bolt/components-band',
+      '@bolt/components-brightcove-player',
       '@bolt/components-blockquote',
       '@bolt/components-breadcrumb',
       '@bolt/components-button',
