d83f7481eb4283b2d34f090c6a8536b78702a5b0
Evan Lovely <evanlovely@gmail.com>
2018-03-05 17:21:02 -0800

    Updating script to only fire on master branch on Travis

--- scripts/update-read-only-git-repos.sh
+++ scripts/update-read-only-git-repos.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
 # Must be ran from `../` ie `./scripts/update-read-only-git-repos.sh`
+if [[ $TRAVIS == 'true' ]]; then
+  if [[ $TRAVIS_PULL_REQUEST == 'false' ]]; then
+    branch_name=$TRAVIS_BRANCH
+  else
+    branch_name=$TRAVIS_PULL_REQUEST_BRANCH
+  fi
+
+  # @todo Update to only trigger on tags
+  if [[ $branch_name != 'master' ]]; then
+    echo "We are in Travis and this IS NOT the master branch, so we WON'T attempt to update read-only git repos."
+    exit 0
+  else
+    echo "We are in Travis and this IS the master branch, so we WILL attempt to update read-only git repos..."
+  fi
+
+fi
 # if we have the remote `core-php` cool, if not, then let's add it
 git remote show core-php || git remote add --fetch core-php git@github.com:bolt-design-system/core-php.git
 # push updates to https://github.com/bolt-design-system/core-php
+# @todo Ensure tags are pushed so Packagist can publish them
 git subtree push --prefix=packages/core-php core-php master
