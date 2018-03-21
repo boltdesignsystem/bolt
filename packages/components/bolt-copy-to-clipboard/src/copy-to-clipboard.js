c1537cf42e108cc574f8de2af4ed7897c383744d
Jake Love <jake@basalt.io>
2018-03-21 16:53:12 -0700

    Copy to Clipboard - added comments to JS

--- packages/components/bolt-copy-to-clipboard/src/copy-to-clipboard.js
+++ packages/components/bolt-copy-to-clipboard/src/copy-to-clipboard.js
@@ -4,25 +4,27 @@ let theClipRef = document.querySelector('.c-bolt-copy-to-clipboard__default .c-b
 let theClipCopied = document.querySelector('.c-bolt-copy-to-clipboard__copied .c-bolt-link');
 
 if (theClipRef !== null) {
-  // Prevent the default link behavior
   theClipRef.addEventListener("click", function(event) {
-    event.preventDefault();
+    event.preventDefault(); // Prevent the default link behavior
 
     let clip = new ClipboardJS('.c-bolt-copy-to-clipboard__default .c-bolt-link');
 
+    /**
+     * [1] Adds a class onClick after successful copy and enables the first set of animations
+     * [2] Waits until the first set of animations complete and adds the last class for last animations
+     **/
     clip.on("success", function() {
-      document.querySelector('.js-bolt-copy-to-clipboard').classList.add('copied');
-      setTimeout(function(){
+      document.querySelector('.js-bolt-copy-to-clipboard').classList.add('copied'); // [1]
+      setTimeout(function(){ // [2]
          document.querySelector('.js-bolt-copy-to-clipboard').classList.add('transitioning');
       }, 2000);
     });
   }, false);
 }
 
-// We don't want the other link clickable
+// We don't want the other link clickable either
 if (theClipCopied !== null) {
-  // Prevent the default link behavior
   theClipCopied.addEventListener("click", function(event) {
-    event.preventDefault();
+    event.preventDefault(); // Prevent the default link behavior
   }, false);
 }
