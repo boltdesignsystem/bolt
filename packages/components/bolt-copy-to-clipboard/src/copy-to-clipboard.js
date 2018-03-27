0b81baf4c54a38d67c9e225d048d7cedd4e6d749
Salem Ghoweri <salem.ghoweri@pega.com>
2018-03-27 16:48:00 -0400

    refactor: adding JS selectors to parent node + link directly in Twig template; wrapping existing clipboardJS functionality as a component so lifecycle events apply automatically; adding disconnecting() callback (part of parent disconnectedCallback event) to clean up if/when removed from the page.

--- packages/components/bolt-copy-to-clipboard/src/copy-to-clipboard.js
+++ packages/components/bolt-copy-to-clipboard/src/copy-to-clipboard.js
@@ -1,34 +1,5 @@
-import ClipboardJS from 'clipboard';
+import { polyfillLoader } from '@bolt/core';
 
-const theClipRef = document.querySelectorAll('.js-bolt-copy-to-clipboard__default .c-bolt-link');
-const theClipCopied = document.querySelectorAll('.js-bolt-copy-to-clipboard__copied .c-bolt-link');
-
-if (theClipRef.length > 0) {
-  theClipRef.forEach((ref) => {
-    ref.addEventListener('click', (event) => {
-      event.preventDefault(); // Prevent the default link behavior
-    }, false);
-
-    const clip = new ClipboardJS(ref); // ClipboardJS adds it's own event listener
-
-    /*
-     * [1] Adds a class onClick after successful copy and enables the first set of animations
-     * [2] Waits until the first set of animations complete and adds the last class for last animations
-     */
-    clip.on('success', () => {
-      ref.parentElement.parentElement.classList.add('is-copied'); // [1]
-      setTimeout(() => { // [2]
-        ref.parentElement.parentElement.classList.add('is-transitioning');
-      }, 2000);
-    });
-  });
-}
-
-// We don't want the other link clickable either
-if (theClipCopied.length > 0) {
-  theClipCopied.forEach((ref) => {
-    ref.addEventListener('click', (event) => {
-      event.preventDefault(); // Prevent the default link behavior
-    }, false);
-  });
-}
+polyfillLoader.then((res) => {
+  import(/* webpackMode: 'lazy', webpackChunkName: 'bolt-copy-to-clipboard' */ './copy-to-clipboard.standalone.js');
+});
