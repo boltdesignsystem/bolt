2fa8dc6101b0bc4f5f4b1964d0a8157cf96f5739
Salem Ghoweri <salem.ghoweri@pega.com>
2018-03-29 11:40:32 -0400

    Feat: add testing converage to monorepo tests to check that bootstraped packages have a publishConfig propertly set; update @bolt/core to fix failing test

--- scripts/monorepo-tests.js
+++ scripts/monorepo-tests.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const path = require('path');
-const {promisify} = require('util');
+const { promisify } = require('util');
 const readdir = promisify(fs.readdir);
 const lstat = promisify(fs.lstat);
+
+const readPkgUp = require('read-pkg-up');
+const readPkg = require('read-pkg');
+const findPkg = require('find-pkg');
+const { forEach } = require('p-iteration');
+const assert = require('assert');
 
 /**
  * Monorepo symlink checker for internal `@bolt` packages
@@ -12,25 +18,23 @@ const lstat = promisify(fs.lstat);
 function checkMonorepoSymlinks() {
   const baseDir = path.resolve(__dirname, '../node_modules/@bolt');
   readdir(baseDir)
-    .then((dirNames) => {
-      return Promise.all(dirNames.map((dirName) => {
-        const item = {
-          path: path.join(baseDir, dirName),
-        };
-        return new Promise((resolve, reject) => {
-          lstat(item.path)
-            .then((stats) => {
-              item.stats = stats;
-              resolve(item);
-            })
-            .catch(reject);
-        });
-      }))
-    })
+    .then(dirNames => Promise.all(dirNames.map((dirName) => {
+      const item = {
+        path: path.join(baseDir, dirName),
+      };
+      return new Promise((resolve, reject) => {
+        lstat(item.path)
+          .then((stats) => {
+            item.stats = stats;
+            resolve(item);
+          })
+          .catch(reject);
+      });
+    })))
     .then((items) => {
       items.forEach((item) => {
         if (!item.stats.isSymbolicLink()) {
-          console.log('Error: Everything in "node_modules/@bolt/" should be a symbolic link to ensure the monorepo is set up correctly. You most likely have a version mismatch between this and something that is using it.');
+          console.log('🛑 Error: Everything in "node_modules/@bolt/" should be a symbolic link to ensure the monorepo is set up correctly. You most likely have a version mismatch between this and something that is using it.');
           console.log(item.path);
           process.exit(1);
         } else {
@@ -39,9 +43,48 @@ function checkMonorepoSymlinks() {
       });
     })
     .catch((error) => {
-      console.log('uh oh!', error);
-      process.exit(1)
+      console.log('🛑 uh oh!', error);
+      process.exit(1);
     });
+  console.log('✅  Monorepo check that `@bolt` packages are symlinked looks good!');
 }
 
 checkMonorepoSymlinks();
+
+
+/**
+ * Monorepo check to confirm components being published have the `publishConfig` config properly set in their package.json file.
+ * This is necessary to ensure the Lerna publish to NPM doesn't unexpectedly fail mid-way due to this config option missing.
+ */
+async function checkComponentPublicConfig(pkg, pkgPath) {
+  if (!pkg.private && pkg.private !== true) {
+    await assert.notEqual(typeof pkg.publishConfig, 'undefined',
+      `Please add a publishConfig to ${pkg.name}'s package.json, located at ${pkgPath}. Check out the Bolt Design System docs section on Adding a New Component for more info: https://bolt-design-system.com/docs/guides/adding-a-new-component.`);
+
+    await assert.equal(pkg.publishConfig.access, 'public',
+      `Please set publishConfig.access to "public" in ${pkg.name}'s package.json, located ${pkgPath}. Check out the Bolt Design System docs section on Adding a New Component for more info: https://bolt-design-system.com/docs/guides/adding-a-new-component.`);
+  }
+}
+
+
+/**
+ * Find the package.json files for Lerna-managed packages for running checks against.
+ */
+async function getComponentPackageConfigs() {
+  const rootPkg = await readPkgUp();
+  const pkgs = rootPkg.pkg.workspaces;
+  (await forEach(pkgs, async (pkg) => {
+    const pkgPath = await findPkg(pkg);
+    const pkgInfo = await readPkg(pkgPath);
+
+    try {
+      await checkComponentPublicConfig(pkgInfo, pkgPath);
+    } catch (error) {
+      console.log('🛑  Uh oh! Test for publishConfig config failed!', error);
+      process.exit(1);
+    }
+  }));
+  console.log('✅  Monorepo check that Bolt packages have a `publishConfig` config looks good!');
+}
+
+getComponentPackageConfigs();
