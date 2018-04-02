41b4bc59ce97f90d1c7dc04730e7d99ff2bb36bc
Joseph Karasek <joseph.karasek@gmail.com>
2018-04-02 11:18:17 -0700

    standalone player start

---
+++ apps/standalone-video-player/.boltrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  // Environmental variable / preset to use
+  env: 'static',
+  buildDir: '../../www/build/',
+  srcDir: './content',
+  wwwDir: '../../www',
+  images: {
+    sets: [],
+  },
+  components: {
+    global: [
+      '@bolt/core',
+      '@bolt/global',
+      '@bolt/components-video',
+      '@bolt/components-button',
+    ],
+    individual: [],
+  },
+};
---
+++ apps/standalone-video-player/content/00-index.md
@@ -0,0 +1,6 @@
+---
+title: Video Player
+subtitle: A stand-alone video player component for export to drupal.
+nav: main
+layout: pw-video-player
+---
