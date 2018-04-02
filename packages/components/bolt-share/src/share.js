0e524ebd37b333b3f0b0a752e64bff02b40fe77f
Jake Love <jake@basalt.io>
2018-04-02 13:40:34 -0700

    Share -- Removed unused js file (for time being)

--- packages/components/bolt-share/src/share.js
+++
@@ -1,5 +0,0 @@
-// import { polyfillLoader } from '@bolt/core';
-//
-// polyfillLoader.then((res) => {
-//   import(/* webpackMode: 'lazy', webpackChunkName: 'bolt-share' */ './share.standalone.js');
-// });
