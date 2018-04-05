698bda775637cfac0dd098d423597e79b5b6e671
Salem Ghoweri <salem.ghoweri@pega.com>
2018-04-05 18:32:08 -0400

    refactor: remove extra JS and Sass includes in lieu of simpler solution

--- apps/pw-site/src/components/pw-ratio/pw-ratio.js
+++
@@ -1,8 +0,0 @@
-import { polyfillLoader } from '@bolt/core';
-
-polyfillLoader.then(() => {
-  import(/* webpackChunkName: 'pw-ratio' */ '@bolt/global/styles/05-objects/objects-ratio/ratio.standalone.js')
-    .then((Component) => {
-      customElements.define(`${bolt.namespace}-ratio`, Component.BoltRatio());
-    });
-});
--- apps/pw-site/src/index.js
+++ apps/pw-site/src/index.js
@@ -1,2 +1,7 @@
-import './components/pw-ratio/pw-ratio.js';
-import './components/pw-video/pw-video.js';
+import '@bolt/core/polyfills/bolt-webcomponents-ce';
+import { BoltRatio } from '@bolt/global/styles/05-objects/objects-ratio/ratio.standalone';
+import { BoltVideo, BoltVideoMeta } from '@bolt/components-video/src/video.standalone';
+
+customElements.define(`${bolt.namespace}-ratio`, BoltRatio());
+customElements.define(`${bolt.namespace}-video-meta`, BoltVideoMeta());
+customElements.define(`${bolt.namespace}-video`, BoltVideo());
