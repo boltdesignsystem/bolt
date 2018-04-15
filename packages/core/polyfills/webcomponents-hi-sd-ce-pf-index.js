41c766b5f120e8c51e2c5240f532d24c9fd07c7a
Salem Ghoweri <salem.ghoweri@pega.com>
2018-04-15 09:08:05 -0400

    refactor: removing old polyfill bundles no longer used

--- packages/core/polyfills/webcomponents-hi-sd-ce-pf-index.js
+++
@@ -1,8 +0,0 @@
-// Ported over from https://github.com/webcomponents/webcomponentsjs/blob/master/entrypoints/webcomponents-hi-sd-ce-pf-index.js
-
-/*
- * Polyfills loaded: HTML Imports, Custom Elements, Shady DOM/Shady CSS, platform polyfills, template
- * Used in: IE 11
- */
-
-import '@webcomponents/custom-elements/src/custom-elements.js';
