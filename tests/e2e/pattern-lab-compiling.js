2a6158de19a8e16acaf384351ce6fbb0c0f621ea
Salem Ghoweri <me@salemghoweri.com>
2018-04-17 19:20:15 -0400

    Update pattern-lab-compiling.js

--- tests/e2e/pattern-lab-compiling.js
+++ tests/e2e/pattern-lab-compiling.js
@@ -2,9 +2,40 @@
 
 const sauce = require('../../scripts/nightwatch-sauce');
 
+#!/usr/bin/env node
+const url = require('url');
+const querystring = require('querystring');
+const fetch = require("node-fetch");
+const netlifyBase = 'https://api.netlify.com/api/v1';
+const netlifySiteId = 'bolt-design-system.netlify.com';
+const netlifyDeploysEndpoint = `${netlifyBase}/sites/${netlifySiteId}/deploys`;
+
+const { NETLIFY_TOKEN, GITHUB_TOKEN, TRAVIS_PULL_REQUEST, TRAVIS_REPO_SLUG} = process.env;
+
+if (!NETLIFY_TOKEN || !GITHUB_TOKEN || !TRAVIS_PULL_REQUEST || !TRAVIS_REPO_SLUG) {
+  console.error('Need to have env var of NETLIFY_TOKEN, GITHUB_TOKEN, TRAVIS_PULL_REQUEST, TRAVIS_REPO_SLUG set');
+  console.log(`TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}`);
+  console.log(`TRAVIS_REPO_SLUG: ${TRAVIS_REPO_SLUG}`);
+  process.exit(1);
+}
+
+function init() {
+  
+    const netlifyEndpoint = url.resolve(netlifyDeploysEndpoint, `?${querystring.stringify({ access_token: NETLIFY_TOKEN, })}`);
+    const netlifyDeploys = fetch(netlifyEndpoint).then(res => res.json());
+    if (!netlifyDeploys) {
+      console.error('Did not get any info on latest Netlify deploys...');
+      process.exit(1);
+    }
+    //console.log('Latest Netlify Deploy: ', netlifyDeploys[0]);
+}
+init();
+    
+
+
 module.exports = {
   beforeEach: browser => {
-    browser.url('http://localhost:3000/pattern-lab/patterns/02-components/index.html')
+    browser.url(`${netlifyDeploys[0].deploy_ssl_url}/pattern-lab/patterns/02-components/index.html`)
       .waitForElementVisible('body')
       .waitForElementVisible('.sg-main');
   },
