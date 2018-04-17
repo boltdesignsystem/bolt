10a3a420ea89de6240ba371501db759c46d0f3bb
Salem Ghoweri <salem.ghoweri@pega.com>
2018-04-17 13:25:10 -0400

    fix: update the <replace-with-children> component to use the HyperHTML renderer -- fixes noticeable flash in Firefox when button component is getting upgraded

--- packages/components/bolt-button/src/button.standalone.js
+++ packages/components/bolt-button/src/button.standalone.js
@@ -4,7 +4,6 @@ import {
   withComponent,
   css,
   hasNativeShadowDomSupport,
-  withPreact,
   BoltComponent,
   declarativeClickHandler,
   sanitizeBoltClasses,
@@ -15,8 +14,13 @@ import styles from './button.scss';
 import visuallyhiddenUtils from '@bolt/global/styles/07-utilities/_utilities-visuallyhidden.scss';
 
 
+/** 
+  * The ReplaceWithChildren is a helper component used for prerendering components (ex. temp CSS 
+  * classes) that need to get removed when the component's JS kicks in. Once that happens, this 
+  * component automatically replaces itself with the component's child nodes.
+  */
 @define
-export class ReplaceWithChildren extends withPreact(withComponent()) {
+export class ReplaceWithChildren extends BoltComponent() {
   static is = 'replace-with-children';
 
   constructor(self) {
@@ -24,14 +28,25 @@ export class ReplaceWithChildren extends withPreact(withComponent()) {
     return self;
   }
   
-  connectedCallback(){
-    if (hasNativeShadowDomSupport){
-      this.replaceWith(...this.childNodes);
-    } else {
-      this.className = '';
+  connecting(){
+    this.replaceElementWithChildren();
   }
+    
+  replaceElementWithChildren(){
+    const parentElement = this.parentElement;
+  
+    if(!parentElement){
+      Error('The <replace-with-children> element needs a parent element to append to!');
     }
+
+    // Originally was this.replaceWith(...this.childNodes) but IE11 doesn't like that
+    while(this.firstChild){
+      parentElement.appendChild(this.firstChild);
     }
+    this.parentElement.removeChild(this);
+  }
+}
+
 
 @define
 class BoltButton extends BoltComponent() {
