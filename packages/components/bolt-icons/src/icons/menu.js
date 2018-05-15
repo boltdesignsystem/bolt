210718321405c44fe09c6d8350e7c220d5049775
Salem Ghoweri <me@salemghoweri.com>
2018-05-15 10:43:45 -0400

    feat: fix icon build script to support SVG icons using masks; updates CSS so icons with masks render and transition as expected (#674)

--- packages/components/bolt-icons/src/icons/menu.js
+++ packages/components/bolt-icons/src/icons/menu.js
@@ -3,7 +3,7 @@ import { h } from '@bolt/core';
 export const Menu = ({ bgColor, fgColor, size, ...otherProps }) => {
   return (
     <svg width={size} height={size} {...otherProps} viewBox="0 0 24 24">
-      <g fill={bgColor} fill-rule="evenodd">
+      <g fill="none" fill-rule="evenodd">
         <path />
         <g fill={bgColor}>
           <path d="M21 11H3c-.55 0-1 .45-1 1s.45 1 1 1h18c.55 0 1-.45 1-1s-.45-1-1-1M3 6h18c.55 0 1-.45 1-1s-.45-1-1-1H3c-.55 0-1 .45-1 1s.45 1 1 1M21 18H3c-.55 0-1 .45-1 1s.45 1 1 1h18c.55 0 1-.45 1-1s-.45-1-1-1" />
