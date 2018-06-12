b9577e38579ff2c48a38487d5dc538bd4146f55e
Salem Ghoweri <salem.ghoweri@pega.com>
2018-06-12 16:40:22 -0400

    fix: re-enable <bolt-nav-indicator> workaround to prevent animating the line in to the wrong location as soon as the page loads

--- packages/components/bolt-nav-indicator/nav-indicator.js
+++ packages/components/bolt-nav-indicator/nav-indicator.js
@@ -86,14 +86,14 @@ let gumshoeStateModule = (function () {
 
           // if this there's a <bolt-nav-priority> instance, make sure that component's ready to go before proceeding trying to animate anything.
 
-          // Salem - @todo: confirm this is still necessary and if not, remove this.
-          // if (nav.nav.closest('bolt-nav-priority')){
-          //   const priorityNav = nav.nav.closest('bolt-nav-priority');
+          // @todo: iterate on a more refined solution so we animate in ASAP
+          if (nav.nav.closest('bolt-nav-priority')){
+            const priorityNav = nav.nav.closest('bolt-nav-priority');
 
-          //   if (!priorityNav.isReady){
-          //     return;
-          //   }
-          // }
+            if (!priorityNav.isReady){
+              return;
+            }
+          }
 
           const originalTarget = nav.nav;
           let originalTargetHref;
