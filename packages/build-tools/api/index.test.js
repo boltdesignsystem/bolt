a20339a4af02d5ef27453b56cfa4120c8fd91618
Salem Ghoweri <salem.ghoweri@pega.com>
2018-06-27 17:18:51 -0400

    fix tests

--- packages/build-tools/api/index.test.js
+++ packages/build-tools/api/index.test.js
@@ -1,72 +1,74 @@
-
+const qs = require('querystring');
 const { phpServerPort } = require('./index');
 const fetch = require('node-fetch');
 
-async function renderResponse(body){
-  const response = await fetch(`http://localhost:${phpServerPort}`, {
-    method: 'POST',
-    body,
-  });
-  const data = await response.text();
+const buttonTemplate = '@bolt-components-button/button.twig';
+
+async function renderResponse(templatePath, body) {
+  const options = {
+    method: body ? 'POST' : 'GET',
+  };
+  if (body) options.body = JSON.stringify(body);
+  const response = await fetch(`http://localhost:${phpServerPort}?${qs.stringify({
+    templatePath,
+  })}`, options);
+  const html = await response.text();
 
   const { status, statusText, ok } = response;
   return {
     status,
     statusText,
     ok,
-    data,
+    html,
   };
 }
 
 
 describe('Test the Bolt TwigRender API', () => {
   test('renders a basic button', async () => {
-    const result = await renderResponse(JSON.stringify({
-      'templatePath': '@bolt-components-button/button.twig',
-      'data': {
-        'text': 'Hello world!',
-      },
-    }));
+    const buttonData = {
+      'text': 'Hello world!',
+    };
+    const result = await renderResponse(buttonTemplate, buttonData);
+
     expect(result.ok).toEqual(true);
   });
 
 
   test('handles missing data', async () => {
-    const result = await renderResponse(JSON.stringify({
-      'templatePath': '@bolt-components-button/button.twig',
-    }));
+    const result = await renderResponse(buttonTemplate);
+
     expect(result.ok).toEqual(true);
   });
 
 
   test('handles non-existent template path', async () => {
-    const result = await renderResponse(JSON.stringify({
-      'templatePath': '@bolt-components-button/button2.twig',
-      'data': {
+    const result = await renderResponse(
+      '@bolt-components-button/button2.twig',
+      {
         'text': 'Hello world 2!',
       },
-    }));
+    );
 
     expect(result.ok).toEqual(false);
   });
 
 
   test('renders the button component correctly', async () => {
-    const result = await renderResponse(JSON.stringify({
-      'templatePath': '@bolt-components-button/button.twig',
-      'data': {
+    const result = await renderResponse(
+      buttonTemplate,
+      {
         'text': 'Hello world!',
       },
-    }));
+    );
 
-    expect(JSON.parse(result.data).html).toMatchSnapshot();
+    expect(result.html).toMatchSnapshot();
   });
 
 
   test('renders attributes on the button component correctly', async () => {
-    const result = await renderResponse(JSON.stringify({
-      'templatePath': '@bolt-components-button/button.twig',
-      'data': {
+    const result = await renderResponse(
+      buttonTemplate, {
         'text': 'Hello world!',
         'attributes': {
           'class': [
@@ -74,22 +76,21 @@ describe('Test the Bolt TwigRender API', () => {
           ],
         },
       },
-    }));
-    expect(JSON.parse(result.data).html).toMatchSnapshot();
+    );
+
+    expect(result.html).toMatchSnapshot();
   });
 
 
   test('renders the secondary button correctly', async () => {
-    const result = await renderResponse(JSON.stringify({
-      'templatePath': '@bolt-components-button/button.twig',
-      'data': {
+    const result = await renderResponse(
+      buttonTemplate,
+      {
         'text': 'Secondary Button!',
         'style': 'secondary',
       },
-    }));
+    );
 
-    expect(JSON.parse(result.data).html).toMatchSnapshot();
+    expect(result.html).toMatchSnapshot();
   });
-
-
 });
