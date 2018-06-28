7a5e1a7a800fb6d1ede001af6bdc78fe87c28436
Evan Lovely <evanlovely@gmail.com>
2018-06-28 10:29:39 -0400

    Adding API to PL

--- apps/pattern-lab/.boltrc.js
+++ apps/pattern-lab/.boltrc.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   // Environmental variable / preset to use
   env: 'pl',
+  hasTotallyAwesomeApi: true,
   buildDir: '../../www/pattern-lab/build',
   wwwDir: '../../www/',
   startPath: 'pattern-lab/index.html',
