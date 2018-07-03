a7c3d7983dcc05014e64c64b07591c8d675e7c34
Salem Ghoweri <salem.ghoweri@pega.com>
2018-07-03 10:43:55 -0400

    chore: remove pw-site from apps

--- apps/pw-site/src/index.js
+++
@@ -1,9 +0,0 @@
-import { polyfillLoader } from '@bolt/core/polyfills';
-import { BoltRatio } from '@bolt/global/styles/05-objects/objects-ratio/ratio.standalone';
-import { BoltVideo, BoltVideoMeta } from '@bolt/components-video/src/video.standalone';
-
-polyfillLoader.then((res) => {
-  customElements.define(`${bolt.namespace}-ratio`, BoltRatio());
-  customElements.define(`${bolt.namespace}-video-meta`, BoltVideoMeta());
-  customElements.define(`${bolt.namespace}-video`, BoltVideo());
-});
