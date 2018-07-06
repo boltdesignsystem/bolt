2534f801a6214b80d15b7e997de44cf3f7ffbdaf
Salem Ghoweri <salem.ghoweri@pega.com>
2018-07-06 10:01:16 -0400

    feat: add API rendering service to build; 1st version schema form

--- packages/build-tools/tasks/server-tasks.js
+++ packages/build-tools/tasks/server-tasks.js
@@ -2,7 +2,31 @@ const browserSync = require('browser-sync');
 const events = require('../utils/events');
 const config = require('../utils/config-store').getConfig();
 const log = require('../utils/log');
+const sh = require('../utils/sh');
+const { handleRequest } = require('../api');
 const server = browserSync.create();
+const path = require('path');
+const resolve = require('resolve');
+
+async function phpServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Starting up local php render twig api server at port ', config.renderingServicePort);
+    sh('php', ['-S', `127.0.0.1:${config.renderingServicePort}`,
+      'TwigRendererApi.php',
+    ], true, true, true, true)
+      .then(output => {
+        console.log('---');
+        console.log(output);
+        console.log('===');
+      })
+      .catch(error => {
+        console.log('---Error:');
+        console.log(error);
+        console.log('===End: Error');
+        reject(error);
+      });
+  });
+}
 
 // https://www.browsersync.io/docs/options
 const serverConfig = {
@@ -17,17 +41,26 @@ const serverConfig = {
     blacklist: ['/index.html', '/', '/?*'], // prevents double browsersync
     rule: {
       match: /<\/body>/i,
-      fn (snippet, match) {
+      fn(snippet, match) {
         return snippet + match;
       },
     },
   },
 };
 
+if (config.renderingService) {
+  serverConfig.middleware = [
+    {
+      route: '/api',
+      handle: handleRequest,
+    },
+  ];
+}
+
 if (config.webpackDevServer) {
   // proxy the Webpack Dev Server endpoint
   serverConfig.proxy = `http://localhost:${config.proxyPort}/`;
-  if (config.env === 'pl') {
+  if (config.env === 'pl' || config.env === 'hybrid') {
     // https://www.browsersync.io/docs/options#option-server
     serverConfig.serveStatic = [];
     serverConfig.serveStatic.push(config.wwwDir);
@@ -67,4 +100,5 @@ events.on('reload', (files) => {
 module.exports = {
   serve,
   reload,
+  phpServer,
 };
---
+++ packages/ui-toolkit/schema-form/schema-form.js
@@ -0,0 +1,135 @@
+import {
+  h,
+} from '@bolt/core';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Form from 'react-jsonschema-form';
+import qs from 'querystring';
+import debouce from 'lodash.debounce';
+
+export default class SchemaForm extends Component {
+  static getDerivedStateFromProps(props, state) {
+    return {
+      data: state.data ? state.data : props.initialData,
+    };
+  }
+
+  /**
+   * Prepare schema by removing properties that the Form cannot handle.
+   * @param {object} schema
+   * @returns {object} newSchema
+   */
+  static prepareSchema(schema) {
+    // Can't use `Object.assign`
+    const newSchema = JSON.parse(JSON.stringify(schema));
+    delete newSchema.properties.attributes;
+    return newSchema;
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      renderedHtml: '',
+      data: null,
+    };
+    this.requestRender = debouce(this.requestRender.bind(this), 300);
+  }
+
+  async requestRender(data) {
+    this.setState({ data });
+    // console.log('requestingRender', data);
+    const res = await fetch(`/api/render-twig?${qs.stringify({
+      templatePath: this.props.demoTemplate,
+    })}`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+    const body = await res.text();
+    if (!res.ok) {
+      console.error(`Error: rendering ${this.props.demoTemplate}`, body);
+      return;
+    }
+    // console.log('got html back!', html);
+    this.setState({
+      renderedHtml: body,
+    });
+  }
+
+  componentDidMount() {
+    if (this.props.initialData) {
+      this.requestRender(this.props.initialData);
+    }
+  }
+
+  render() {
+    const { layout } = this.props;
+    const schema = SchemaForm.prepareSchema(this.props.schema);
+    console.log('schema form props', this.props);
+
+    const isHorizontal = layout === 'horizontal';
+
+    return (
+      <div style={{
+        display: 'flex',
+        flexDirection: isHorizontal ? 'row' : 'column',
+        flexWrap: 'wrap',
+        alignItems: 'flex-start',
+      }}>
+        <h3 style={{
+          flexShrink: 0,
+          width: '100%',
+          textAlign: 'center',
+          padding: '1rem',
+        }}>Schema Form for {schema.title}</h3>
+        <div
+          style={{
+            flexShrink: 0,
+            width: isHorizontal ? '50%' : '100%',
+            display: 'flex',
+            justifyContent: isHorizontal ? 'center' : 'flex-start',
+            alignItems: 'center',
+          }}
+          dangerouslySetInnerHTML={{ __html: this.state.renderedHtml }}
+        />
+        <div
+          style={{
+            width: isHorizontal ? '50%' : '100%',
+            borderColor: 'hsl(240, 3%, 85%)',
+            borderWidth: '1px',
+            maxHeight: '80vh',
+            overflow: 'auto',
+            borderStyle: 'solid',
+            '-webkit-overflow-scroll': 'touch',
+          }}
+        >
+          <Form
+            schema={schema}
+            formData={this.state.data}
+            onChange={(data) => this.requestRender(data.formData)}
+            onError={(data) => console.error('Error in Schema Form', data)}
+          />
+        </div>
+        <hr />
+      </div>
+    );
+  }
+}
+
+SchemaForm.defaultProps = {
+  initialData: {},
+  layout: 'horizontal',
+};
+
+SchemaForm.propTypes = {
+  schema: PropTypes.object.isRequired,
+  initialData: PropTypes.object,
+  demoTemplate: PropTypes.string.isRequired,
+  layout: PropTypes.oneOf([
+    'vertical',
+    'horizontal',
+  ]),
+};
