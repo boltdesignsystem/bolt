60a7d877dd4b27ae004798657ad43f98b9a7c0c4
Salem Ghoweri <salem.ghoweri@pega.com>
2018-07-16 09:57:26 -0400

    refactor: lazy load icons instead of immediately bunding them together in the initial JS download

--- packages/components/bolt-icon/src/icon.js
+++ packages/components/bolt-icon/src/icon.js
@@ -1,5 +1,5 @@
 import { polyfillLoader } from '@bolt/core';
 
 polyfillLoader.then((res) => {
-  import(/* webpackMode: 'eager', webpackChunkName: 'bolt-icon' */ './icon.standalone.js');
+  import(/* webpackMode: 'lazy', webpackChunkName: 'bolt-icon' */ './icon.standalone.js');
 });
