a9ca44106ba3585dd1071d97cbd212546051518f
Salem Ghoweri <salem.ghoweri@pega.com>
2018-07-27 17:05:35 -0400

    refactor: make sure theming system-generated CSS gets duplicates removed when compiling

--- packages/themify/src/helpers/css.util.js
+++ packages/themify/src/helpers/css.util.js
@@ -1,5 +1,12 @@
 var CleanCSS = require('clean-css');
-var cleanCSS = new CleanCSS({});
+var cleanCSS = new CleanCSS({
+  level: {
+    2: {
+      all: false, // sets all values to `false`
+      removeDuplicateRules: true, // turns on removing duplicate rules
+    },
+  },
+});
 function minifyCSS(css) {
   return cleanCSS.minify(css).styles;
 }
