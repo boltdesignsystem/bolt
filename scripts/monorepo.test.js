b751ad1b315eb1dcf506f71c9451976ea1c3e110
Salem Ghoweri <me@salemghoweri.com>
2018-08-03 16:28:50 -0400

    fix: update jest monorepo test to only test against actual package.json files that exist in the codebase, ignoring anything in a node_modules folder.

--- scripts/monorepo.test.js
+++ scripts/monorepo.test.js
@@ -4,6 +4,16 @@ const globby = require('globby');
 const rootPkg = require(path.join(__dirname, '../package.json'));
 const assert = require('assert');
 
+function flattenDeep(arr1) {
+  return arr1.reduce(
+    (acc, val) =>
+      Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val),
+    [],
+  );
+}
+
+jest.setTimeout(10000);
+
 describe('monorepo', async () => {
   /**
    * Array of object containing the package.json of each monorepo package
@@ -13,12 +23,26 @@ describe('monorepo', async () => {
 
   beforeAll(async () => {
     const pkgDirs = await globby(rootPkg.workspaces.packages);
-    const pkgPaths = pkgDirs.map(pkgDir => path.join(pkgDir, 'package.json'));
-    pkgs = await Promise.all(pkgPaths.map(async (pkgPath) => {
-      const fileString = await fs.readFile(pkgPath);
-      const pkg = JSON.parse(fileString);
-      return pkg;
-    }));
+
+    // Given all the globbed paths, asynchronously walk through everything and find all package.json files to test against
+    let pkgPaths = await Promise.all(
+      pkgDirs.map(async pkgDir =>
+        globby([
+          path.dirname(pkgDir) + '/**/package.json',
+          '!**/node_modules/**',
+        ]),
+      ),
+    );
+
+    pkgPaths = await [...new Set(flattenDeep(pkgPaths))]; // remove duplicates
+
+    pkgs = await Promise.all(
+      pkgPaths.map(async pkgPath => {
+        const fileString = await fs.readFile(pkgPath);
+        const pkg = JSON.parse(fileString);
+        return pkg;
+      }),
+    );
   });
 
   /**
@@ -26,11 +50,22 @@ describe('monorepo', async () => {
    * This is necessary to ensure the Lerna publish to NPM doesn't unexpectedly fail mid-way due to this config option missing.
    */
   test('Check Component Public Config', async () => {
-    pkgs.forEach((pkg) => {
+    pkgs.forEach(pkg => {
       if (!pkg.private && pkg.private !== true) {
-        assert.ok(pkg.publishConfig !== 'undefined', `Please add a publishConfig to ${pkg.name}'s package.json. Check out the Bolt Design System docs section on Adding a New Component for more info: https://bolt-design-system.com/docs/guides/adding-a-new-component.`);
+        assert.ok(
+          pkg.publishConfig !== 'undefined',
+          `Please add a publishConfig to ${
+            pkg.name
+          }'s package.json. Check out the Bolt Design System docs section on Adding a New Component for more info: https://bolt-design-system.com/docs/guides/adding-a-new-component.`,
+        );
 
-        assert.strictEqual(pkg.publishConfig.access, 'public', `Please set publishConfig.access to "public" in ${pkg.name}'s package.json. Check out the Bolt Design System docs section on Adding a New Component for more info: https://bolt-design-system.com/docs/guides/adding-a-new-component.`)
+        assert.strictEqual(
+          pkg.publishConfig.access,
+          'public',
+          `Please set publishConfig.access to "public" in ${
+            pkg.name
+          }'s package.json. Check out the Bolt Design System docs section on Adding a New Component for more info: https://bolt-design-system.com/docs/guides/adding-a-new-component.`,
+        );
       }
     });
   });
