41ff12c4bdb5aa1aea62d8f536a2a37be9e1f032
Mike Mai <emceehao@gmail.com>
2018-08-09 12:23:57 -0400

    fix: remove pattern-lab.css

--- apps/bolt-site/.boltrc.js
+++ apps/bolt-site/.boltrc.js
@@ -1,8 +1,6 @@
 module.exports = {
   // Environmental variable / preset to use
-  //lang: [
-  //  'en',
-  //],
+  lang: ['en'],
   env: 'static',
   buildDir: '../../www/build/',
   srcDir: './content',
