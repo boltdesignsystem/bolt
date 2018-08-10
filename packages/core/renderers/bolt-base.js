275b474116c5fe5b17fb05595a7e958cfc7abfa1
Salem Ghoweri <me@salemghoweri.com>
2018-08-10 11:05:40 -0400

    refactor: update @bolt/core's Base Class to support changing the component's based used when identifying a component's slotted content;

--- packages/core/renderers/bolt-base.js
+++ packages/core/renderers/bolt-base.js
@@ -6,7 +6,13 @@ import { findParentTag } from '../utils/find-parent-tag';
 export function BoltBase(Base = HTMLElement) {
   return class extends Base {
     connectedCallback() {
-      this._checkSlots();
+      // Automatically adjust which inner element inside the custom element gets used as the base when evaluating slotted children. Necessary when including deeply nested slots in the initial HTML being rendered, which might include a few wrapping containers that get removed when the JavaScript kicks in. <-- this is how we get slotted buttons to work!
+      const isShadowRootSelector = this.querySelector('[is="shadow-root"]');
+      if (isShadowRootSelector) {
+        this.slots = this._checkSlots(isShadowRootSelector.childNodes);
+      } else {
+        this.slots = this._checkSlots();
+      }
 
       // Automatically force a component to render if no props exist BUT props are defined.
       if (
@@ -46,23 +52,24 @@ export function BoltBase(Base = HTMLElement) {
     }
 
     // Inspired by https://codepen.io/jovdb/pen/ddRZKo
-    _checkSlots() {
-      this.slots = {
-        default: [],
-      };
-
-      const elem = this;
+    _checkSlots(selector = this.childNodes) {
+      const slots = { default: [] };
 
       // Loop through nodelist
-      this.childNodes.forEach(function(child, index, nodelist) {
+      selector.forEach(function(child, index, nodelist) {
         const slotName = child.getAttribute ? child.getAttribute('slot') : null;
 
         if (!slotName) {
-          elem.slots.default.push(child);
+          slots.default.push(child);
+        } else if (slots[slotName]) {
+          slots[slotName].push(child);
         } else {
-          elem.slots[slotName] = child;
+          slots[slotName] = [];
+          slots[slotName].push(child);
         }
       });
+
+      return slots;
     }
 
     disconnectedCallback() {
