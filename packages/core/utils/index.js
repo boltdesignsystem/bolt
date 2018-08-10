80a5428cb74baaa6b9be3296133fe61ddaa5f9d1
Salem Ghoweri <me@salemghoweri.com>
2018-08-10 11:09:39 -0400

    refactor: update @bolt/core to provide default index.js files aggregating the different utils, renderers, etc for each folder; fixes issues with tree shaking and resolving finicky imports / exports without the use of extra Babel plugins

---
+++ packages/core/utils/index.js
@@ -0,0 +1,15 @@
+export * from './color-contrast';
+export * from './css';
+export * from './declarative-click-handler';
+export * from './environment';
+export * from './find-parent-tag';
+export * from './is-valid-selector';
+export * from './rgb2hex';
+export * from './sanitize-classes';
+export * from './supports-css-vars';
+export * from './supports-passive-event-listener';
+
+// https://www.polymer-project.org/3.0/docs/devguide/custom-elements#defer-work
+export { afterNextRender } from '@polymer/polymer/lib/utils/render-status.js';
+
+export { shadow, withComponent, props, define } from 'skatejs/dist/esnext';
