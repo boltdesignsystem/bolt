45eccb3797478ef3df806f1da5873592bb1fa758
Salem Ghoweri <me@salemghoweri.com>
2018-08-10 14:13:30 -0400

    refactor: split apart the <replace-with-children> component from the button -- now living in @bolt/core/elements

--- packages/components/bolt-button/src/button.standalone.js
+++ packages/components/bolt-button/src/button.standalone.js
@@ -1,55 +1,25 @@
 import {
-  define,
   props,
-  withComponent,
-  css,
-  hasNativeShadowDomSupport,
-  BoltComponent,
+  define,
   declarativeClickHandler,
   sanitizeBoltClasses,
-} from '@bolt/core';
+  hasNativeShadowDomSupport,
+  afterNextRender,
+} from '@bolt/core/utils';
 
-// @todo Salem, since this imports something that imports '@bolt/core', please make sure this doesn't add a huge amount in the wrong place - Evan
+import classNames from 'classnames/bind';
+
+
+import {
+  wire,
+  render,
+  withHyperHtml,
+} from '@bolt/core/renderers/renderer-hyperhtml';
+
 import visuallyhiddenUtils from '@bolt/global/styles/07-utilities/_utilities-visuallyhidden.scss';
-
 import styles from './button.scss';
 
-/**
- * The ReplaceWithChildren is a helper component used for prerendering components (ex. temp CSS
- * classes) that need to get removed when the component's JS kicks in. Once that happens, this
- * component automatically replaces itself with the component's child nodes.
- */
-@define
-export class ReplaceWithChildren extends BoltComponent() {
-  static is = 'replace-with-children';
-
-  constructor(self) {
-    self = super(self);
-    return self;
-  }
-
-  connecting() {
-    this.replaceElementWithChildren();
-  }
-
-  replaceElementWithChildren() {
-    const parentElement = this.parentElement;
-
-    if (!parentElement) {
-      Error(
-        'The <replace-with-children> element needs a parent element to append to!',
-      );
-    }
-
-    // Originally was this.replaceWith(...this.childNodes) but IE11 doesn't like that
-    while (this.firstChild) {
-      parentElement.appendChild(this.firstChild);
-    }
-    if (parentElement) {
-      parentElement.removeChild(this);
-    }
-  }
-}
+let cx = classNames.bind(styles);
 
 @define
 class BoltButton extends BoltComponent() {
