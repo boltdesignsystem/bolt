fcceac07b3667a6e6ee00ab5f004c57f92d5b45e
Rémy Denton <remy.denton@pega.com>
2018-08-24 09:47:15 -0700

    refactor: Update stateful CSS class names
    BDS-554
    
    I was very confused by the existing names-- it almost seemed to me that
    they had been mistakenly switched (i.e. "is-copied" really meant that
    the transition had started, and "is-transitioning" really meant that the
    copying was complete).  Hopefully these new names are an improvement.

--- packages/components/bolt-copy-to-clipboard/src/copy-to-clipboard.standalone.js
+++ packages/components/bolt-copy-to-clipboard/src/copy-to-clipboard.standalone.js
@@ -36,15 +36,15 @@ export class BoltCopyToClipboard extends BoltComponent() {
       // Copying is already successful at this point.  Everything from here on is UX flair.
 
       // Show the "in progress" status.
-      this.parentElem.classList.add('is-copied');
+      this.parentElem.classList.add('is-copying');
 
       // Show the "success" status.
       setTimeout(() => {
-        this.parentElem.classList.add('is-transitioning');
+        this.parentElem.classList.add('has-copied');
 
         // Reset so the link can be used again without refreshing the page.
         setTimeout(() => {
-          this.parentElem.classList.remove('is-transitioning', 'is-copied');
+          this.parentElem.classList.remove('has-copied', 'is-copying');
         }, 3000);
       }, 1000);
     });
