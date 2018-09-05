0a59124dad1638bafb6379814cf074c90a8397f9
Jake Love <jake@basalt.io>
2018-09-05 01:19:07 -0700

    Bolt Carousel - linting fix by cleaning up carousel.js

--- packages/components/bolt-carousel/carousel.js
+++ packages/components/bolt-carousel/carousel.js
@@ -1,6 +1,6 @@
 import { polyfillLoader } from '@bolt/core';
 
 // Eager so the <replace-with-children> component runs when this component is also booting up, resulting in no initial flash of unstyled content
-polyfillLoader.then((res) => {
+polyfillLoader.then(res => {
   import(/* webpackMode: 'eager', webpackChunkName: 'bolt-carousel' */ './carousel.standalone.js');
 });
