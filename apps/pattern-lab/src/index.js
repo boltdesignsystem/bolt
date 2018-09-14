4428396df6b2a2c0c47ff722d25466e3404f47f5
Salem Ghoweri <me@salemghoweri.com>
2018-09-14 12:00:28 -0400

    refactor: add iframe resizing support to allow embedded iframes in the docs site to point at corresponding pages in PL and automatically be sized appropriately

--- apps/pattern-lab/src/index.js
+++ apps/pattern-lab/src/index.js
@@ -1,4 +1,5 @@
 import 'core-js/modules/es6.string.starts-with';
+import 'iframe-resizer/js/iframeResizer.contentWindow.min.js';
 
 // here if you need pl only JS
 document.addEventListener('DOMContentLoaded', () => {
