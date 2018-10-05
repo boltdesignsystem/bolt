da59b0e1902ec49cfa76e95ea53d424e50423c80
Evan Lovely <evanlovely@gmail.com>
2018-10-05 13:20:08 -0700

    fix(build-tools): temp fix for unfinishing build tasks

--- packages/build-tools/tasks/task-collections.js
+++ packages/build-tools/tasks/task-collections.js
@@ -162,6 +162,8 @@ async function build(localDev = false, shouldReturnTime = false) {
       return startTime;
     } else {
       log.info(`Build completed in ${timer.end(startTime)}.`);
+      // @todo find why this isn't exiting on own & remove this line. Most likely to an unresolved Promise.
+      process.exit(0);
     }
   } catch (error) {
     log.errorAndExit('Build failed', error);
