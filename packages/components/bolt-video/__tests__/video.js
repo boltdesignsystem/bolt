be5505a9aad3413588f2b9e5f8611c43cacd9354
Salem Ghoweri <salem.ghoweri@pega.com>
2018-10-30 17:00:03 -0400

    refactor: update video component to include refactored bolt ratio component as a dependency

---
+++ packages/components/bolt-video/__tests__/video.js
@@ -0,0 +1,62 @@
+const { render } = require('@bolt/twig-renderer');
+const { readYamlFileSync } = require('@bolt/build-tools/utils/yaml');
+const { join } = require('path');
+const schema = readYamlFileSync(join(__dirname, '../video.schema.yml'));
+const { tag } = schema.properties;
+
+describe('<bolt-video> Component', async () => {
+  test('<bolt-video> compiles', async () => {
+    const results = await render('@bolt-components-video/video.twig', {
+      videoId: '3861325118001',
+      accountId: '1900410236',
+      playerId: 'r1CAdLzTW',
+    });
+    expect(results.ok).toBe(true);
+    expect(results.html).toMatchSnapshot();
+  });
+
+  test('<bolt-video> compiles with a meta description', async () => {
+    const results = await render('@bolt-components-video/video.twig', {
+      videoId: '3861325118001',
+      accountId: '1900410236',
+      playerId: 'r1CAdLzTW',
+      showMeta: true,
+    });
+    expect(results.ok).toBe(true);
+    expect(results.html).toMatchSnapshot();
+  });
+
+  test('<bolt-video> compiles with a meta description + meta title', async () => {
+    const results = await render('@bolt-components-video/video.twig', {
+      videoId: '3861325118001',
+      accountId: '1900410236',
+      playerId: 'r1CAdLzTW',
+      showMeta: true,
+      showMetaTitle: true,
+    });
+    expect(results.ok).toBe(true);
+    expect(results.html).toMatchSnapshot();
+  });
+
+  test('<bolt-video> compiles with a different `video-id`', async () => {
+    const results = await render('@bolt-components-video/video.twig', {
+      videoId: '5609376179001',
+      accountId: '1900410236',
+      playerId: 'r1CAdLzTW',
+    });
+    expect(results.ok).toBe(true);
+    expect(results.html).toMatchSnapshot();
+  });
+
+  // tag.enum.forEach(async tagChoice => {
+  //   test(`button tag: ${tagChoice}`, async () => {
+  //     const results = await render('@bolt-components-button/button.twig', {
+  //       text: 'Hello World',
+  //       tag: tagChoice,
+  //     });
+  //     // console.log({ results });
+  //     expect(results.ok).toBe(true);
+  //     expect(results.html).toMatchSnapshot();
+  //   });
+  // });
+});
