1c43cc90694460b997b76984cffb26602a96a3a1
Salem Ghoweri <salem.ghoweri@pega.com>
2018-11-12 18:11:33 -0500

    feat: scaffold our version selector branch updates

--- packages/build-tools/utils/bolt-versions.js
+++ packages/build-tools/utils/bolt-versions.js
@@ -7,6 +7,63 @@ const checkLinks = require('check-links');
 const { gitSemverTags } = require('./git-semver-tags');
 const { getConfig } = require('./config-store');
 
+function urlFriendlyBranchAndTagName(string) {
+  return string
+    .replace(/\//g, '-') // `/` => `-`
+    .replace('--', '-') // `--` => `-` now.sh subdomains can't have `--` for some reason
+    .replace(/\./g, '-'); // `.` => `-`
+}
+
+const {
+  // if in Travis, then it's `"true"`
+  TRAVIS,
+  // for push builds, or builds not triggered by a pull request, this is the name of the branch.
+  // for builds triggered by a pull request this is the name of the branch targeted by the pull request.
+  // for builds triggered by a tag, this is the same as the name of the tag(TRAVIS_TAG).
+  TRAVIS_BRANCH,
+  // if the current job is a pull request, the name of the branch from which the PR originated
+  // if the current job is a push build, this variable is empty("").
+  TRAVIS_PULL_REQUEST_BRANCH,
+  // The pull request number if the current job is a pull request, “false” if it’s not a pull request.
+  TRAVIS_PULL_REQUEST,
+  // The slug (in form: owner_name/repo_name) of the repository currently being built.
+  TRAVIS_REPO_SLUG,
+  // If the current build is for a git tag, this variable is set to the tag’s name
+  TRAVIS_TAG,
+} = process.env;
+
+let branchName = 'detached-HEAD';
+    try {
+      branchName = spawnSync('git', ['symbolic-ref', 'HEAD'], {
+        encoding: 'utf8',
+      }).stdout.replace('refs/heads/', '').replace(/\//g, '-').trim();
+    } catch (error) {
+      process.exit(1);
+    }
+
+    if (TRAVIS === 'true') {
+      if (TRAVIS_PULL_REQUEST === 'false') {
+        branchName = TRAVIS_BRANCH;
+      } else {
+        branchName = TRAVIS_PULL_REQUEST_BRANCH;
+      }
+    }
+
+    console.log(`Branch Name: ${branchName}`);
+
+    
+
+    console.log('Aliasing to branch/tag name...');
+    // Making sure branch name is ok to be in URL
+    const branchUrlPart = urlFriendlyBranchAndTagName(branchName);
+      .replace(/\//g, '-') // `/` => `-`
+      .replace('--', '-') // `--` => `-` now.sh subdomains can't have `--` for some reason
+      .replace(/\./g, '-'); // `.` => `-`
+    const aliasedUrlSubdomain = `${encodeURIComponent(branchUrlPart)}.boltdesignsystem`;
+    const aliasedUrl = `https://${aliasedUrlSubdomain}.com`;
+
+
+
 const urlsToCheck = [];
 
 async function writeVersionDataToJson(versionData) {
@@ -39,11 +96,8 @@ async function gatherBoltVersions() {
 
   for (index = 0; index < tags.length; index++) {
     let tag = tags[index];
-    let tagString = tag
-      .replace(/\//g, '-') // `/` => `-`
-      .replace('--', '-') // `--` => `-`
-      .replace(/\./g, '-'); // `.` => `-`
-
+    let tagString = urlFriendlyBranchAndTagName(tag);
+    
     const newSiteUrl = `https://${tagString}.boltdesignsystem.com`;
     const oldSiteUrl = `https://${tagString}.bolt-design-system.com`;
 
