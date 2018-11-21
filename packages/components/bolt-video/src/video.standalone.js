3d6c9d9ef8199b4fa3146ba19c6885344fe808b3
Salem Ghoweri <salem.ghoweri@pega.com>
2018-11-21 11:57:49 -0500

    refactor: split apart <bolt-video> and <bolt-video-meta> into two separate components; update package.json main file path

--- packages/components/bolt-video/src/video.standalone.js
+++ packages/components/bolt-video/src/video.standalone.js
@@ -6,55 +6,6 @@ import Mousetrap from 'mousetrap';
 let index = 0;
 
 @define
-class BoltVideoMeta extends withPreact() {
-  static is = `${bolt.namespace}-video-meta`;
-
-  constructor(self) {
-    self = super(self);
-    return self;
-  }
-
-  static props = {
-    duration: props.string,
-    title: props.string,
-  };
-
-  get renderRoot() {
-    return this;
-  }
-
-  render() {
-    // All of its logic is contained here in render(), but it could be updated to be a property that is set
-    // externally (such as when the video has finished fully loading).
-
-    // [Mai] 'reveal' allows the meta data (title and duration) to be hidden.
-    const reveal = Boolean(this.title || this.duration);
-    return (
-      <div className={`c-${bolt.namespace}-video-meta`}>
-        {reveal ? (
-          <div className={`c-${bolt.namespace}-video-meta__wrapper`}>
-            {this.title ? (
-              <div
-                className={`c-${bolt.namespace}-video-meta__item c-${
-                  bolt.namespace
-                }-video-meta__item--title`}>
-                {this.title}
-              </div>
-            ) : null}
-            <div
-              className={`c-${bolt.namespace}-video-meta__item c-${
-                bolt.namespace
-              }-video-meta__item--duration`}>
-              {this.duration}
-            </div>
-          </div>
-        ) : null}
-      </div>
-    );
-  }
-}
-
-@define
 class BoltVideo extends withPreact() {
   static is = `${bolt.namespace}-video`;
 
@@ -712,4 +663,4 @@ class BoltVideo extends withPreact() {
 
 export default BoltVideo;
 
-export { BoltVideo, BoltVideoMeta };
+export { BoltVideo };
