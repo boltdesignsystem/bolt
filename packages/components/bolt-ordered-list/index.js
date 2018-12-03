fb8ccda69f2974f8fa3d70e946258f42a59c1a9e
Adam Szałapski <adam.szalapski@pega.com>
2018-12-03 19:09:41 +0100

    fix: Update logic andstyling of li elements

--- packages/components/bolt-ordered-list/index.js
+++ packages/components/bolt-ordered-list/index.js
@@ -3,5 +3,4 @@ import { polyfillLoader } from '@bolt/core/polyfills';
 polyfillLoader.then(res => {
   import(/* webpackMode: 'eager', webpackChunkName: 'bolt-unordered-list' */ './src/ordered-list');
   import(/* webpackMode: 'eager', webpackChunkName: 'bolt-unordered-list-item' */ './src/_ordered-list-item');
-  import(/* webpackMode: 'eager', webpackChunkName: 'bolt-unordered-list-item-index' */ './src/_ordered-list-item-index');
 });
