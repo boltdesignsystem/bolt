e76c978b1a4959b93c4a470d37a5c75e80a5880c
Daniel Morse <daniel.morse@pega.com>
2018-12-10 14:45:26 -0500

    style: disable lint on ckeditor plugin

--- apps/pattern-lab/src/_patterns/04-pages/99999-bolt-dev-sandbox/editor-integration/ck4-format-plugin.js
+++ apps/pattern-lab/src/_patterns/04-pages/99999-bolt-dev-sandbox/editor-integration/ck4-format-plugin.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 /**
  * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
