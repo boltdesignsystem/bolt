0a4f9d9763d18d834bf9c7443a0e33c624393d8f
Daniel Morse <daniel.morse@pega.com>
2018-12-11 18:23:40 -0500

    feat: add support for content and author to blockquote web component

--- packages/components/bolt-blockquote/src/blockquote.js
+++ packages/components/bolt-blockquote/src/blockquote.js
@@ -1,6 +1,16 @@
-import { props, define, hasNativeShadowDomSupport } from '@bolt/core/utils';
+import {
+  props,
+  define,
+  sanitizeBoltClasses,
+  hasNativeShadowDomSupport,
+  watchForComponentMutations,
+} from '@bolt/core/utils';
 import classNames from 'classnames/bind';
-import { withLitHtml, html } from '@bolt/core/renderers/renderer-lit-html';
+import {
+  withLitHtml,
+  html,
+  render,
+} from '@bolt/core/renderers/renderer-lit-html';
 import Ajv from 'ajv';
 
 import styles from './blockquote.scss';
@@ -18,8 +28,12 @@ class BoltBlockquote extends withLitHtml() {
     size: props.string,
     alignItems: props.string,
     border: props.string,
+    content: props.string,
     indent: props.boolean,
     fullBleed: props.boolean,
+    authorName: props.string,
+    authorTitle: props.string,
+    authorImage: props.string,
   };
 
   // https://github.com/WebReflection/document-register-element#upgrading-the-constructor-context
@@ -31,66 +45,207 @@ class BoltBlockquote extends withLitHtml() {
   }
 
   // @todo: move to the global Bolt Base component after we're done testing this out with the new refactored Card component
-  // validateProps(propData) {
-  //   var validatedData = propData;
+  validateProps(propData) {
+    var validatedData = propData;
 
-  //   // remove default strings in prop data so schema validation can fill in the default
-  //   for (let property in validatedData) {
-  //     if (validatedData[property] === '') {
-  //       delete validatedData[property];
-  //     }
-  //   }
+    // remove default strings in prop data so schema validation can fill in the default
+    for (let property in validatedData) {
+      if (validatedData[property] === '') {
+        delete validatedData[property];
+      }
+    }
 
-  //   let isValid = this.validate(validatedData);
+    let isValid = this.validate(validatedData);
 
-  //   // bark at any schema validation errors
-  //   if (!isValid) {
-  //     console.log(this.validate.errors);
-  //   }
+    // bark at any schema validation errors
+    if (!isValid) {
+      console.log(this.validate.errors);
+    }
 
-  //   return validatedData;
-  // }
+    return validatedData;
+  }
 
-  // get alignItemsOption() {
-  //   switch (this.props.alignItems) {
-  //     case 'right':
-  //       return 'end';
-  //     case 'center':
-  //       return 'center';
-  //     default:
-  //       // left => start
-  //       return 'start';
-  //   }
-  // }
+  connecting() {
+    const root = this;
 
-  // get borderOption() {
-  //   switch (this.props.border) {
-  //     case 'none':
-  //       return 'borderless';
-  //     case 'horizontal':
-  //       return 'bordered-horizontal';
-  //     default:
-  //       // vertical => bordered-vertical
-  //       return 'bordered-vertical';
-  //   }
-  // }
+    // If the initial <bolt-link> element contains a link, break apart the original HTML so we can retain the a tag but swap out the inner content with slots.
+
+    // Make sure the button component ONLY ever reuses any existing HTML ONCE. This, in part, helps to prevent rendering diff errors in HyperHTML after booting up!
+    if (this._wasInitiallyRendered === false) {
+      this.childNodes.forEach((childElement, i) => {
+        if (childElement.tagName === 'BLOCKQUOTE') {
+          root.rootElement = document.createDocumentFragment();
+
+          // Take any existing elements and move them to the root of the custom element
+          while (childElement.firstChild) {
+            root.appendChild(childElement.firstChild);
+          }
+
+          if (childElement.className) {
+            childElement.className = sanitizeBoltClasses(childElement);
+          }
+
+          if (
+            childElement.getAttribute('is') &&
+            childElement.getAttribute('is') === 'shadow-root'
+          ) {
+            childElement.removeAttribute('is');
+          }
+
+          root.rootElement.appendChild(childElement);
+        }
+      });
+    }
+  }
+
+  rendered() {
+    super.rendered(); // ensure any events emitted by the Bolt Base class fire as expected
+
+    // re-render if Shadow DOM is supported and enabled; temp workaround to dealing w/ components already rendered, but without slot support
+    if (hasNativeShadowDomSupport && this.useShadow) {
+      this.observer = watchForComponentMutations(this);
+
+      this.observer.observe(this, {
+        attributes: false,
+        childList: true,
+        characterData: false,
+      });
+    }
+  }
+
+  disconnecting() {
+    // this.removeEventListener('click', this.clickHandler);
+
+    if (hasNativeShadowDomSupport && this.useShadow) {
+      if (this.observer) {
+        this.observer.disconnect();
+      }
+    }
+  }
+
+  get alignItemsOption() {
+    switch (this.props.alignItems) {
+      case 'right':
+        return 'end';
+      case 'center':
+        return 'center';
+      default:
+        // left => start
+        return 'start';
+    }
+  }
+
+  get borderOption() {
+    switch (this.props.border) {
+      case 'none':
+        return 'borderless';
+      case 'horizontal':
+        return 'bordered-horizontal';
+      default:
+        // vertical => bordered-vertical
+        return 'bordered-vertical';
+    }
+  }
+
+  authorTemplate(name, title, image) {
+    if (name) {
+      const authorName = html`
+        <bolt-text
+          tag="cite"
+          font-size="xsmall"
+          font-weight="bold"
+          color="theme-headline"
+        >
+          ${name}
+        </bolt-text>
+      `;
+
+      const authorTitle = html`
+        <bolt-text font-size="xsmall" color="theme-headline">
+          ${title}
+        </bolt-text>
+      `;
+
+      const authorImage = html`
+        <div class="c-bolt-blockquote__footer-item">
+          <div class="c-bolt-blockquote__image">
+            <bolt-image src="${image}"></bolt-image>
+          </div>
+        </div>
+      `;
+
+      return html`
+        <footer class="c-bolt-blockquote__footer">
+          <div class="c-bolt-blockquote__footer-item">
+            ${image && authorImage} ${authorName} ${title && authorTitle}
+          </div>
+        </footer>
+      `;
+    }
+  }
+
+  quoteTemplate(children = null) {
+    const text = html`
+      <bolt-text
+        tag="div"
+        font-size="${this.props.size}"
+        font-weight="semibold"
+        color="theme-headline"
+      >
+        ${children}
+      </bolt-text>
+    `;
+
+    return html`
+      <div class="c-bolt-blockquote__quote">${text}</div>
+    `;
+  }
+
+  blockquoteTemplate(classes, children = null) {
+    return html`
+      <blockquote class="${classes}">${children}</blockquote>
+    `;
+  }
 
   render() {
     // validate the original prop data passed along -- returns back the validated data w/ added default values
-    // const { size, alignItems, border, indent, fullBleed } = this.validateProps(
-    //   this.props,
-    // );
+    const {
+      size,
+      alignItems,
+      border,
+      content,
+      indent,
+      fullBleed,
+      authorName,
+      authorTitle,
+      authorImage,
+    } = this.validateProps(this.props);
 
-    // const classes = cx('c-bolt-blockquote', {
-    //   [`c-bolt-blockquote--${size}`]: size,
-    //   [`c-bolt-blockquote--align-items-${this.alignItemsOption}`]: alignItems,
-    //   [`c-bolt-blockquote--${this.borderOption}`]: border,
-    //   [`c-bolt-blockquote--indented`]: indent,
-    //   [`c-bolt-blockquote--full`]: fullBleed,
-    // });
+    const classes = cx('c-bolt-blockquote', {
+      [`c-bolt-blockquote--${size}`]: size,
+      [`c-bolt-blockquote--align-items-${this.alignItemsOption}`]: alignItems,
+      [`c-bolt-blockquote--${this.borderOption}`]: border,
+      [`c-bolt-blockquote--indented`]: indent,
+      [`c-bolt-blockquote--full`]: fullBleed,
+    });
+
+    let renderedBlockquote;
+
+    if (this.rootElement) {
+      renderedBlockquote = this.rootElement.firstChild.cloneNode(true);
+      renderedBlockquote.className += ' ' + classes;
+      render(this.slot('default'), renderedBlockquote);
+    } else {
+      const quote = this.quoteTemplate(this.slot('default'));
+      const author = this.authorTemplate(authorName, authorTitle, authorImage);
+      const blockquoteInner = html`
+        ${quote} ${author}
+      `;
+      renderedBlockquote = this.blockquoteTemplate(classes, blockquoteInner);
+    }
 
     return html`
-      ${this.addStyles([styles])} ${this.slot('default')}
+      ${this.addStyles([styles])} ${renderedBlockquote}
     `;
   }
 }
