304eeeb3e4b04dfaea4a0035b33a1bea45e2f79e
Salem Ghoweri <me@salemghoweri.com>
2018-12-18 14:15:53 -0500

    refactor: update cue point plugin logic to account for multiple concurrent player instances + handle cue points that can be displayed when the oncuechange event breaks down

--- packages/components/bolt-video/plugins/cue-points.js
+++ packages/components/bolt-video/plugins/cue-points.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
 import { h } from '@bolt/core/renderers';
 import '@bolt/components-button';
+import isEqual from 'lodash.isequal';
 import { getSubArray } from '../utils';
 
 export const VideoOverlayContent = props => {
@@ -12,58 +13,144 @@ export const VideoOverlayContent = props => {
 };
 
 export function cuePointsPlugin(player, elem) {
+  // utility function - returns back array of cue point meta data used to render dynamic CTAs
+  function parseCuePointMetaData(cuePointData) {
+    return cuePointData.metadata.split(';');
+  }
+
+  /**
+   * Calculates the video timestamp when a specific cue point needs to be hidden
+   * @param {object} $this - The reference to the unique plugin instance
+   * @param {object} currentCuePoint - The cue point being evaluated
+   * @return {number} The timestamp when this specific cue point needs to be hidden
+   */
+  function getCuePointHideTime($this, currentCuePoint) {
+    const allCuePointData = getSubArray(
+      $this.cuePoints,
+      'time',
+      currentCuePoint.startTime,
+    );
+
+    return (
+      currentCuePoint.startTime +
+        Number(parseCuePointMetaData(allCuePointData[0])[0]) ||
+      currentCuePoint.endTime
+    );
+  }
+
+  /**
+   * Checks if a particular cue point should be / can be displayed
+   * @param {any} cuePoint - The cue point being evaluated
+   */
+  function shouldCueBeDisplayed(cuePoint) {
+    const currentTime = player.currentTime();
+    const startTime = cuePoint.startTime;
+    const hideTime = cuePoint.hideTime;
+
+    if (currentTime >= startTime && currentTime < hideTime) {
+      return true;
+    }
+
+    return false;
+  }
+
+  /**
+   * Tear down function when a cue point is being hidden from view
+   * @param {object} $this - The reference to the unique plugin instance
+   */
+  function cleanupCuePoints($this) {
+    $this.previousCTA = {};
+    $this.currentCTA.isActive = false;
+
+    if (elem.hideOverlay) {
+      elem.hideOverlay();
+    }
+  }
+
+  /**
+   * Check to see if the current cue point CTA and the previous cue point CTA are the same before rendering if they aren't
+   * @param {object} $this - The reference to the unique plugin instance
+   */
+  function potentiallyRenderCuePoint($this) {
+    const currentCTA = $this.currentCTA; // assign the currently active CTA
+    const previousCTA = $this.previousCTA;
+    const hideTime = $this.currentCTA.hideTime;
+    const currentTime = player.currentTime();
+
+    // auto hide and internally clean up when the video has hit the expected end time
+    if (currentTime >= hideTime) {
+      cleanupCuePoints($this);
+    } else if (
+      isEqual(previousCTA, {}) ||
+      isEqual(previousCTA, currentCTA) === false
+    ) {
+      $this.previousCTA = $this.currentCTA;
+      $this.currentCTA.isActive = true;
+      renderCuePoint($this.currentCTA);
+    }
+  }
+
+  /**
+   * Extract data pieces from cue point meta data and display overlay
+   * @param {object} currentCta - The cue point cta being rendered
+   */
+  function renderCuePoint(currentCta) {
+    // bail if the video player's overay element (what the CTA gets rendered into) doesn't exist
+    if (!elem.overlayElement) {
+      return;
+    }
+
+    // Split metadata into an array of three pieces based on location of semicolons
+    let ctaProps = parseCuePointMetaData(currentCta),
+      ctaText = ctaProps[1],
+      ctaUrl = ctaProps[2];
+
+    if (elem.showOverlay) {
+      elem.showOverlay();
+    }
+
+    ReactDOM.render(
+      <VideoOverlayContent url={ctaUrl} text={ctaText} />,
+      elem.overlayElement,
+    );
+  }
+
   player.on('loadstart', function() {
-    // Listen for cue change then pass data to be displayed
-    player.cuePointAra = player.mediainfo.cue_points;
-    player.tt = player.textTracks()[0];
+    if (!player.mediainfo.cue_points) {
+      return;
+    }
 
-    // When a cue point starts, extract cue data and pass data to be displayed
-    player.tt.oncuechange = function() {
-      if (player.tt.activeCues[0] !== undefined) {
-        player.allCuePointData = getSubArray(
-          player.cuePointAra,
-          'time',
-          player.tt.activeCues[0].startTime,
-        );
+    player.tt = player.tt || [];
+    player.tt[elem.state.id] = player.textTracks()[0];
 
-        // only render cue points CTA if the overlay element exists to be rendered into
-        if (elem.overlayElement) {
-          _render(player.allCuePointData[0]);
+    const $this = player.tt[elem.state.id]; // re-map to make it easier to grab and modify this unique plugin instance's data
+    $this.cuePoints = player.mediainfo.cue_points;
+    $this.previousCTA = {};
+    $this.currentCTA = {};
+
+    // auto-hide overlay when initially booting up
+    if (elem.hideOverlay) {
+      elem.hideOverlay();
+    }
+
+    // process through available cue point data when the video player instance is playing / the time updates
+    player.on('timeupdate', function() {
+      const cuePointsFound = $this.cuePoints.find(function(cuePoint) {
+        if (!cuePoint.hideTime) {
+          cuePoint.hideTime = getCuePointHideTime($this, cuePoint);
+        }
+
+        return shouldCueBeDisplayed(cuePoint);
+      });
+
+      if (cuePointsFound) {
+        $this.currentCTA = cuePointsFound;
+        potentiallyRenderCuePoint($this);
+      } else {
+        if ($this.currentCTA.isActive === true) {
+          cleanupCuePoints($this);
         }
       }
-    };
-
-    // Extract data pieces from cue point meta data and display overlay
-    function _render(cpData) {
-      // Split metadata into an array of three pieces based on location of semicolons
-      let dataAra = cpData.metadata.split(';'),
-        durationCTA = dataAra[0],
-        textCTA = dataAra[1],
-        urlCTA = dataAra[2],
-        timeoutValue;
-
-      // Set duration, set timeout, inject dynamic html and show overlay
-      timeoutValue = Number(durationCTA) * 1000;
-      elem.overlayTimeout = window.setTimeout(checkCuePointsTime, timeoutValue);
-
-      ReactDOM.render(
-        <VideoOverlayContent url={urlCTA} text={textCTA} />,
-        elem.overlayElement,
-      );
-
-      if (elem.showOverlay) {
-        elem.showOverlay();
-      }
-    }
-
-    // Clear timeout and remove overlay
-    // This function called when cue point timeout is reached
-    function checkCuePointsTime() {
-      window.clearTimeout(elem.overlayTimeout);
-
-      if (elem.hideOverlay) {
-        elem.hideOverlay();
-      }
-    }
+    });
   });
 }
