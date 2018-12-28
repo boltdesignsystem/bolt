659f0d422b2fecd280257d4a29c0bea64b673533
Salem Ghoweri <me@salemghoweri.com>
2018-12-28 08:58:22 -0500

    feat: add jest snapshot formatter to help normalize whitespace + generate better diffs in the console

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,7 @@ module.exports = {
   ],
   testEnvironment: 'node',
   globalSetup: './jest-global-setup.js',
+  snapshotSerializers: ['jest-serializer-html'],
   // Notify not working correctly; we want to only get a notification when tests fail, and then get ONE success notificaiton after it passes
   // notify: true,
   // notifyMode: 'failure-success',
