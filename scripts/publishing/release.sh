ce679aaece7fe73d92d6fddbb536af0cbe3cc5a6
Salem Ghoweri <me@salemghoweri.com>
2019-01-04 09:59:15 -0500

    chore: re-testing release and pre-release scripts

--- scripts/publishing/release.sh
+++ scripts/publishing/release.sh
@@ -5,44 +5,13 @@
 cd "$(dirname "$0")"
 cd ../../
 
-# Exit the script on any command with non 0 return code
-# We assume that all the commands in the pipeline set their return code
-# properly and that we do not need to validate that the output is correct
-# set -e
-
-# # Echo every command being executed
-# set -x
-
-# # Go to root
-# cd ..
-# root_path=$PWD
-
-# if [ -n "$(git status --porcelain)" ]; then
-#   echo "Your git status is not clean. Aborting.";
-#   exit 1;
-# fi
-
-# Compile
-# cd packages/react-error-overlay/
-# npm run build:prod
-# cd ../..
-# Go!
-# ./node_modules/.bin/lerna publish "$@"
-
-# echo "About to publish $1"
-
-# --amend
-# lerna version --amend
-
-# npx lerna version --registry http://localhost:4000 
-# npx lerna publish from-package --registry http://localhost:4000 --canary
-npx lerna publish $1 --registry http://localhost:4000 --npm-tag next --preid rc --no-commit-hooks --no-git-reset --verify-access
-git push origin :refs/tags/v$1
+npx lerna publish $1 --registry http://localhost:4000 --no-commit-hooks --no-git-reset --verify-access
+CURRENT_VERSION=`git describe --abbrev=0`
+git push origin :refs/tags/$CURRENT_VERSION
 node scripts/update-php-package-versions.js
-git status
 
 git add .
-git commit -m "v$1"
-git tag -fa v$1
-git push --force
-git push --tags --force
+git commit --amend
+git tag -f $CURRENT_VERSION
+git push --force --no-verify
+git push --tags --force --no-verify
