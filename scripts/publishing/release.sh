b40f12ee9023ceeb858e97f9ab4f9086d831d936
Salem Ghoweri <me@salemghoweri.com>
2019-01-04 14:37:30 -0500

    fix: move to the right folder before running publish-related commands

--- scripts/publishing/release.sh
+++ scripts/publishing/release.sh
@@ -2,6 +2,7 @@
 # Borrowed with gratitude from Create React App by Facebook; thanks!
 set -e
 cd "$(dirname "$0")" # Start in the scripts/publishing folder, even if run from root directory
+cd ../../
 
 ./scripts/publishing/before-release.sh #verify everything is good to go before publishing
 npx lerna publish $1 --registry http://localhost:4000 --no-commit-hooks --no-git-reset --verify-access
