18171071132916dcf30b3f8a7ca19666ae353fcb
Evan Lovely <evanlovely@gmail.com>
2019-01-07 15:06:46 -0800

    feat: add get-latest-deploy script

---
+++ scripts/get-latest-deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Outputs the latest now.sh deploy
+# Start in this directory even if ran elsewhere
+cd "$(dirname "$0")"
+node -e 'require("./utils").getLatestDeploy().then(x => process.stdout.write(x))'
