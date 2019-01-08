a0935cf3b18b4ab5a97e23523668f738d1f4f2e4
Joseph Karasek <joseph.karasek@gmail.com>
2019-01-07 16:00:36 -0800

    fix: add error catcher to bash getLatestDeploy

--- scripts/get-latest-deploy.sh
+++ scripts/get-latest-deploy.sh
@@ -2,4 +2,4 @@
 # Outputs the latest now.sh deploy
 # Start in this directory even if ran elsewhere
 cd "$(dirname "$0")"
-node -e 'require("./utils").getLatestDeploy().then(x => process.stdout.write(x))'
+node -e 'require("./utils").getLatestDeploy().then(x => process.stdout.write(x)).catch(err => process.stdout.write(JSON.stringify(err)))'
