32d23fe684a84ede29df8736bc3ca9e390c0410d
Evan Lovely <evanlovely@gmail.com>
2019-01-10 11:54:37 -0800

    storing nightwatch result data in filesystem

--- nightwatch-globals.js
+++ nightwatch-globals.js
@@ -1,5 +1,19 @@
 const { outputBanner } = require('ci-utils');
+const { tmpdir } = require('os');
+const { join } = require('path');
+const { readFileSync, writeFileSync } = require('fs');
 const sauce = require('./scripts/nightwatch-sauce');
+
+const dataFilePath = join(tmpdir, 'nightwatch-results.json');
+const read = () => JSON.parse(readFileSync(dataFilePath, 'utf8'));
+const write = data => writeFileSync(dataFilePath, JSON.stringify(data), 'utf8');
+
+console.log({ dataFilePath });
+
+write({
+  testCount: 0,
+  results: [],
+});
 
 const theGlobals = {
   testingUrl: 'https://boltdesignsystem.com',
@@ -7,14 +21,20 @@ const theGlobals = {
   testCount: 0,
 
   afterEach(browser, cb) {
-    theGlobals.testCount += 1;
-    outputBanner(`global afterEach called, testCount at ${testCount}`);
+    const data = read();
+    data.testCount += 1;
+    write(data);
+    // theGlobals.testCount += 1;
+    outputBanner(`global afterEach called, testCount at ${data.testCount}`);
     sauce(browser, cb);
   },
 
   reporter(results, cb) {
     // totalReports += 1;
-    console.log(`global reporter called`);
+    const data = read();
+    data.results.push(results);
+    write(data);
+    console.log(`global reporter called; have total of ${data.results.length}`);
     console.log(results);
     cb();
   },
