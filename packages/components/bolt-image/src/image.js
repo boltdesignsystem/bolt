5327f3d7fd1336b86851f37b40d5dd2dc210df84
Daniel Morse <daniel.morse@pega.com>
2019-01-10 19:49:53 -0500

    feat: update image JS to match latest component patterns, testing basic use cases, WIP

--- packages/components/bolt-image/src/image.js
+++ packages/components/bolt-image/src/image.js
@@ -1,26 +1,21 @@
 import {
   props,
   define,
-  sanitizeBoltClasses,
   hasNativeShadowDomSupport,
-  watchForComponentMutations,
+  containsTagName,
 } from '@bolt/core/utils';
-import classNames from 'classnames/bind';
-import {
-  withLitHtml,
-  html,
-  render,
-} from '@bolt/core/renderers/renderer-lit-html';
+import { withLitHtml, html } from '@bolt/core/renderers/renderer-lit-html';
 import { ifDefined } from 'lit-html/directives/if-defined';
-import Ajv from 'ajv';
+import path from 'path';
+
+import classNames from 'classnames/bind';
 
 import imageStyles from './image.scss';
 import ratioStyles from '@bolt/components-ratio/src/ratio.scss';
 
 import schema from '../image.schema.yml';
-import './_image-lazy-sizes';
 
-const ajv = new Ajv({ useDefaults: 'shared' });
+import './_image-lazy-sizes';
 
 let cx = classNames.bind(imageStyles);
 
@@ -34,7 +29,7 @@ class BoltImage extends withLitHtml() {
     noLazyload: props.boolean,
     srcset: props.string,
     sizes: props.string,
-    useAspectRatio: props.boolean,
+    noUseAspectRatio: props.boolean,
     width: props.string,
     height: props.string,
     zoom: props.boolean,
@@ -44,98 +39,24 @@ class BoltImage extends withLitHtml() {
   constructor(self) {
     self = super(self);
     self.useShadow = hasNativeShadowDomSupport;
-    self.validate = ajv.compile(schema);
+    self.schema = this.getModifiedSchema(schema);
     return self;
   }
 
-  // @todo: move to the global Bolt Base component after we're done testing this out with the new refactored Card component
-  validateProps(propData) {
-    var validatedData = propData;
+  getModifiedSchema(schema) {
+    var modifiedSchema = schema;
 
-    // remove default strings in prop data so schema validation can fill in the default
-    for (let property in validatedData) {
-      if (validatedData[property] === '') {
-        delete validatedData[property];
+    // Remove "lazyload" and "useAspectRatio" from schema, does not apply to web component.
+    for (let property in modifiedSchema.properties) {
+      if (property === 'lazyload' || property === 'useAspectRatio') {
+        delete modifiedSchema.properties[property];
       }
     }
 
-    let isValid = this.validate(validatedData);
-
-    // bark at any schema validation errors
-    if (!isValid) {
-      console.log(this.validate.errors);
-    }
-
-    return validatedData;
+    return modifiedSchema;
   }
 
-  connecting() {
-    const root = this;
-
-    // If the initial <bolt-link> element contains a link, break apart the original HTML so we can retain the a tag but swap out the inner content with slots.
-
-    // Make sure the button component ONLY ever reuses any existing HTML ONCE. This, in part, helps to prevent rendering diff errors in HyperHTML after booting up!
-    if (this._wasInitiallyRendered === false) {
-      this.childNodes.forEach((childElement, i) => {
-        if (
-          childElement.tagName === 'BOLT-RATIO' ||
-          childElement.tagName === 'IMG'
-        ) {
-          root.rootElement = document.createDocumentFragment();
-
-          // Take any existing elements and move them to the root of the custom element
-          while (childElement.firstChild) {
-            root.appendChild(childElement.firstChild);
-          }
-
-          if (childElement.className) {
-            childElement.className = sanitizeBoltClasses(childElement);
-          }
-
-          if (
-            childElement.getAttribute('is') &&
-            childElement.getAttribute('is') === 'shadow-root'
-          ) {
-            childElement.removeAttribute('is');
-          }
-
-          root.rootElement.appendChild(childElement);
-        }
-      });
-    }
-
-    // When possible, use afterNextRender to defer non-critical work until after first paint.
-    // afterNextRender(this, function() {
-    //   this.addEventListener('click', this.clickHandler);
-    // });
-  }
-
-  rendered() {
-    super.rendered(); // ensure any events emitted by the Bolt Base class fire as expected
-
-    // re-render if Shadow DOM is supported and enabled; temp workaround to dealing w/ components already rendered, but without slot support
-    if (hasNativeShadowDomSupport && this.useShadow) {
-      this.observer = watchForComponentMutations(this);
-
-      this.observer.observe(this, {
-        attributes: false,
-        childList: true,
-        characterData: false,
-      });
-    }
-  }
-
-  disconnecting() {
-    // this.removeEventListener('click', this.clickHandler);
-
-    if (hasNativeShadowDomSupport && this.useShadow) {
-      if (this.observer) {
-        this.observer.disconnect();
-      }
-    }
-  }
-
-  getImageData(src) {
+  getImageData(imagePath) {
     // TODO: Get real image data
     return {
       height: 480,
@@ -147,51 +68,21 @@ class BoltImage extends withLitHtml() {
     };
   }
 
-  imageTemplate(src, alt, lazyload, srcset, sizes, zoom, classes, fallback) {
-    const placeholderSrc =
-      'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
-
-    const imageElement = html`
-      <img
-        class="${classes}"
-        src="${lazyload ? placeholderSrc : src}"
-        alt="${alt}"
-        srcset="${ifDefined(!lazyload ? srcset : undefined)}"
-        data-srcset="${ifDefined(lazyload ? srcset : undefined)}"
-        sizes="${ifDefined(!lazyload ? sizes : undefined)}"
-        data-sizes="${ifDefined(lazyload ? sizes : undefined)}"
-        data-zoom="${ifDefined(zoom ? src : undefined)}"
-      />
-    `;
-
-    // Include <noscript> for server-side rendered components
-    const imageFallbackElement = html`
-      <noscript>
-        <img
-          class="${classes}"
-          src="${src}"
-          alt="${alt}"
-          srcset="${ifDefined(!lazyload ? srcset : undefined)}"
-          data-srcset="${ifDefined(lazyload ? srcset : undefined)}"
-        />
-      </noscript>
-    `;
-
-    return html`
-      ${imageElement}${fallback ? imageFallbackElement : ''}
-    `;
+  lazyloadImage(image) {
+    // console.log('@lazyloadImage');
+    if (!this.isLoaded) {
+      // Note: This immediately unveils the image. Add intersection observer?
+      lazySizes.loader.unveil(image);
+      this.isLoaded = true;
+    }
   }
 
-  ratioTemplate(width, height, children = null) {
-    return html`
-      <bolt-ratio
-        attributes=""
-        aspect-ratio-width="${width * 1}"
-        aspect-ratio-height="${height * 1}"
-      >
-        ${children}
-      </bolt-ratio>
-    `;
+  rendered() {
+    super.rendered() && super.rendered();
+    let lazyImage = this.shadowRoot.querySelector('.js-lazyload');
+    if (lazyImage) {
+      this.lazyloadImage(lazyImage);
+    }
   }
 
   render() {
@@ -202,54 +93,107 @@ class BoltImage extends withLitHtml() {
       noLazyload,
       srcset,
       sizes,
-      useAspectRatio,
+      noUseAspectRatio,
       width,
       height,
       zoom,
     } = this.validateProps(this.props);
 
-    const imageClasses = cx('c-bolt-image__image');
-
-    // negate and rename variable for readability
+    // negate and rename variables for readability
     const lazyload = !noLazyload;
+    const useAspectRatio = !noUseAspectRatio;
 
-    // const placeholderClasses = cx('c-bolt-image__image-placeholder');
-
-    const lazyloadClasses = cx({
-      'c-bolt-image__lazyload': lazyload,
-      'c-bolt-image__lazyload--fade': lazyload,
-      'c-bolt-image__lazyload--blur': lazyload,
-      // 'c-bolt-image__lazyload--blur': lazyload && ext == 'jpg', // TODO: get image data
-      'js-lazyload': lazyload,
-    });
-
+    const imageExt = path.extname(src);
     const imageData = this.getImageData(src);
+    const placeholderColor = imageData.color;
+    const placeholderSrc = imageData.base64;
     const imageWidth = width || imageData.width;
     const imageHeight = height || imageData.height;
-    const imageTemplate = this.imageTemplate(
-      src,
-      alt,
-      lazyload,
-      srcset || src,
-      sizes,
-      zoom,
-      cx(imageClasses, lazyloadClasses),
-      lazyload && imageWidth && imageHeight && useAspectRatio,
-    );
+    const canUseAspectRatio = imageWidth && imageHeight && useAspectRatio;
+
+    const classes = cx('c-bolt-image__image', {
+      ['c-bolt-image__lazyload']: lazyload,
+      ['c-bolt-image__lazyload--fade']: lazyload,
+      ['c-bolt-image__lazyload--blur']: lazyload && imageExt == '.jpg',
+      ['js-lazyload']: lazyload,
+    });
+
+    const imageElement = () => {
+      if (src) {
+        return html`
+          <img
+            class="${classes}"
+            src="${lazyload ? placeholderSrc : src}"
+            alt="${ifDefined(alt ? alt : undefined)}"
+            srcset="${
+              ifDefined(!lazyload || this.isLoaded ? srcset || src : undefined)
+            }"
+            data-srcset="${ifDefined(lazyload ? srcset || src : undefined)}"
+            sizes="${ifDefined(!lazyload || this.isLoaded ? sizes : undefined)}"
+            data-sizes="${ifDefined(lazyload ? sizes : undefined)}"
+            data-zoom="${ifDefined(zoom ? src : undefined)}"
+          />
+        `;
+      }
+    };
+
+    const placeholderImageElement = () => {
+      if (useAspectRatio && imageExt == '.jpg') {
+        return html`
+          <img
+            class="${cx('c-bolt-image__image-placeholder')}"
+            src="${placeholderSrc}"
+            alt="${ifDefined(alt ? alt : undefined)}"
+          />
+        `;
+      }
+    };
+
+    // Include <noscript> for server-side rendered components
+    const fallbackImageElement = () => {
+      if (lazyload && src) {
+        return html`
+          <noscript>
+            <img
+              class="${classes}"
+              src="${src}"
+              alt="${ifDefined(alt ? alt : undefined)}"
+              srcset="${ifDefined(!lazyload ? srcset || src : undefined)}"
+              data-srcset="${ifDefined(lazyload ? srcset || src : undefined)}"
+            />
+          </noscript>
+        `;
+      }
+    };
+
+    const imageTemplate = html`
+      ${placeholderImageElement()} ${imageElement()} ${fallbackImageElement()}
+    `;
+
+    const ratioTemplate = children => {
+      return html`
+        <bolt-ratio
+          attributes=""
+          aspect-ratio-width="${imageWidth * 1}"
+          aspect-ratio-height="${imageHeight * 1}"
+        >
+          ${children}
+        </bolt-ratio>
+      `;
+    };
+
+    if (placeholderColor && canUseAspectRatio && imageExt == '.jpg') {
+      this.style.backgroundColor = placeholderColor;
+    }
 
     let renderedImage = this.slot('default');
 
-    if (this.rootElement) {
-      renderedImage = this.rootElement.firstChild.cloneNode(true);
-      renderedImage.className += ' ' + imageClasses;
-      render(this.slot('default'), renderedImage);
-    } else {
-      if (imageWidth && imageHeight && useAspectRatio) {
-        renderedImage = this.ratioTemplate(
-          imageWidth,
-          imageHeight,
-          imageTemplate,
-        );
+    if (
+      !containsTagName(this.childNodes, 'img') &&
+      !containsTagName(this.childNodes, 'bolt-ratio')
+    ) {
+      if (canUseAspectRatio) {
+        renderedImage = ratioTemplate(imageTemplate);
       } else {
         renderedImage = imageTemplate;
       }
