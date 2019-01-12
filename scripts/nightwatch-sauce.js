79ae675084f8e119a521aabcc2ad565b3b385f9f
Evan Lovely <EvanLovely@users.noreply.github.com>
2019-01-11 22:35:34 -0800

    adding Travis env vars to SauceLabs custom data

--- scripts/nightwatch-sauce.js
+++ scripts/nightwatch-sauce.js
@@ -20,6 +20,27 @@ const auth = Buffer.from(`${SAUCE_USERNAME}:${SAUCE_ACCESS_KEY}`).toString(
   'base64',
 );
 
+const {
+      // for push builds, or builds not triggered by a pull request, this is the name of the branch.
+      // for builds triggered by a pull request this is the name of the branch targeted by the pull request.
+      // for builds triggered by a tag, this is the same as the name of the tag(TRAVIS_TAG).
+      TRAVIS_BRANCH,
+      // if the current job is a pull request, the name of the branch from which the PR originated
+      // if the current job is a push build, this variable is empty("").
+      TRAVIS_PULL_REQUEST_BRANCH,
+      // The pull request number if the current job is a pull request, "false" if it’s not a pull request.
+      TRAVIS_PULL_REQUEST,
+      // The slug (in form: owner_name/repo_name) of the repository currently being built.
+      TRAVIS_REPO_SLUG,
+      // If the current build is for a git tag, this variable is set to the tag’s name
+      TRAVIS_TAG,
+      TRAVIS_BUILD_WEB_URL,
+      TRAVIS_JOB_NUMBER,
+    } = process.env;
+
+    const gitSha = getGitSha(true);
+    const gitShaLong = getGitSha();
+
 /**
  * @param {Object} opt
  * @param {Object} opt.currentTest - Meta data of current tests
@@ -215,7 +236,15 @@ function handleNightwatchResults(client, callback) {
         build: `build-${process.env.TRAVIS_JOB_NUMBER}`,
         tags: ['CI'],
         'custom-data': {
-          gitSha: getGitSha(),
+      TRAVIS_JOB_NUMBER,
+      TRAVIS_BRANCH,
+      TRAVIS_PULL_REQUEST_BRANCH,
+      TRAVIS_PULL_REQUEST,
+      TRAVIS_REPO_SLUG,
+      TRAVIS_TAG,
+      TRAVIS_BUILD_WEB_URL,
+      gitSha,
+      gitShaLong,
         },
       }),
     },
