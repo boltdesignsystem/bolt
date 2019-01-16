83147c41a3a394148336d58285abafcc08b5402c
Adam Szałapski <adam.szalapski@pega.com>
2019-01-16 14:08:52 +0100

    fix(Update crad test js to be passing the tests): update crad test js to be passing the tests
    affects: @bolt/components-card

--- packages/components/bolt-card/__tests__/card.js
+++ packages/components/bolt-card/__tests__/card.js
@@ -4,14 +4,27 @@ const { join } = require('path');
 const schema = readYamlFileSync(join(__dirname, '../card.schema.yml'));
 const { tag } = schema.properties;
 
-describe('card', async () => {
-  test('basic card', async () => {
+describe('<bolt-image>', async () => {
+  test('<bolt-card>', async () => {
     const results = await render('@bolt-components-card/card.twig', {
+      media: {
+        image: {
+          src: '/images/placeholders/landscape-16x9-mountains.jpg',
+          alt: 'Image alt.',
+        },
+      },
       body: {
+        eyebrow: 'This is an eyebrow',
+        headline: 'This is a headline',
         paragraph: 'This is a paragraph.',
       },
+      actions: [
+        {
+          text: 'This is a button',
+          url: 'https://pega.com',
+        },
+      ],
     });
-    // console.log({ results });
     expect(results.ok).toBe(true);
     expect(results.html).toMatchSnapshot();
   });
