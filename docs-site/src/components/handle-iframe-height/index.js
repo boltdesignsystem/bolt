0eb17d414dbe29722fc1dcb44950858679d3f9b2
Salem Ghoweri <me@salemghoweri.com>
2019-01-19 19:51:40 -0500

    chore: disable auto-adding min-height util on iframe page load

--- docs-site/src/components/handle-iframe-height/index.js
+++ docs-site/src/components/handle-iframe-height/index.js
@@ -4,8 +4,8 @@ if (window.self !== window.top) {
   // automatically remove the min-height default set to the body element when viewing PL pages from inside an iframe on the docs site, but via a utility class
   window.iFrameResizer = {
     readyCallback() {
-      document.documentElement.classList.add('u-bolt-min-height-none');
-      document.body.classList.add('u-bolt-min-height-none');
+      // document.documentElement.classList.add('u-bolt-min-height-none');
+      // document.body.classList.add('u-bolt-min-height-none');
     },
   };
 } else {
