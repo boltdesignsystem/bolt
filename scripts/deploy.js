a759fae23ce85636ff384441fc0cad77e3c97227
Salem Ghoweri <me@salemghoweri.com>
2019-01-24 17:07:53 -0500

    fix(*): disable meta flag on now.sh deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -92,7 +92,7 @@ async function init() {
         //`--meta TRAVIS_PULL_REQUEST_BRANCH="${TRAVIS_PULL_REQUEST_BRANCH}"`,
         //`--meta TRAVIS_BRANCH="${TRAVIS_BRANCH}"`,
         //`--meta branchName="${branchName}"`,
-        `--meta gitSha="${gitSha}"`,
+        // `--meta gitSha="${gitSha}"`,
         '--env',
         `GIT_SHA=${gitSha}`,
         '--build-env',
