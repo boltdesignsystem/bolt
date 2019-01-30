184737ccf9809701101d13a1b9becf15bdd649c6
Salem Ghoweri <me@salemghoweri.com>
2019-01-30 16:59:01 -0500

    fix: update read-only repo logic to always sync tag of latest release

--- scripts/release/update-read-only-git-repos.sh
+++ scripts/release/update-read-only-git-repos.sh
@@ -36,12 +36,12 @@ else
   echo "This is not a tagged git release but it IS a release-related branch -- updating remote repos!"
   ./scripts/release/git-subsplit.sh init https://${GH_TOKEN}@github.com/bolt-design-system/core-php.git
   ./scripts/release/git-subsplit.sh publish --work-dir=$PWD packages/core-php:https://${GH_TOKEN}@github.com/bolt-design-system/core-php.git \
-    --heads="$CURRENT_BRANCH" --update --no-tags
+    --heads="$CURRENT_BRANCH" --update --tags="$CURRENT_VERSION"
   rm -rf .subsplit
 
   ./scripts/release/git-subsplit.sh init https://${GH_TOKEN}@github.com/bolt-design-system/bolt_connect.git
   ./scripts/release/git-subsplit.sh publish --work-dir=$PWD packages/drupal-modules/bolt_connect:https://${GH_TOKEN}@github.com/bolt-design-system/bolt_connect.git \
-    --heads="$CURRENT_BRANCH" --update --no-tags
+    --heads="$CURRENT_BRANCH" --update --tags="$CURRENT_VERSION"
   rm -rf .subsplit
 
   git checkout $CURRENT_BRANCH # return back to the branch you started on before exiting
