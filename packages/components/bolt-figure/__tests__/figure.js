dc0955b52ed55d7c568345199ee8fb33d22ba50e
Daniel Morse <daniel.morse@pega.com>
2019-02-01 18:36:10 -0500

    test: remove redundant test, update snapshot

--- packages/components/bolt-figure/__tests__/figure.js
+++ packages/components/bolt-figure/__tests__/figure.js
@@ -2,20 +2,6 @@ const { render } = require('@bolt/twig-renderer');
 const { media } = require('./figure-data');
 
 describe('figure', async () => {
-  test('basic figure', async () => {
-    const results = await render('@bolt-components-figure/figure.twig', {
-      media: {
-        image: {
-          src: '/images/placeholders/500x500.jpg',
-          lazyload: false,
-        },
-      },
-      caption: `Figure caption`,
-    });
-    expect(results.ok).toBe(true);
-    expect(results.html).toMatchSnapshot();
-  });
-
   Object.keys(media).forEach(async item => {
     test(`figure with ${item}`, async () => {
       const results = await render('@bolt-components-figure/figure.twig', {
