220f93538f75d8fccd208adc83b30a82600151f8
Adam Szałapski <adam.szalapski@pega.com>
2019-02-08 14:04:02 +0100

    refactor(@bolt/components-image): cleaning the code

--- packages/components/bolt-image/__tests__/bolt-image.e2e.js
+++ packages/components/bolt-image/__tests__/bolt-image.e2e.js
@@ -5,11 +5,8 @@ module.exports = {
   'Bolt Image image is showed': function(browser) {
     const { testingUrl } = browser.globals;
     console.log(`global browser url: ${testingUrl}`);
-    currentBrowser = '--' + typeof browser.currentEnv !== 'undefined' ? browser.currentEnv : 'chrome';
+    currentBrowser = '--' + browser.currentEnv || 'chrome';
     let testName = 'bolt-image-showed';
-
-    console.log('B:', browser.currentEnv);
-    console.log('CB:', currentBrowser);
 
     browser
       .url(
@@ -25,18 +22,15 @@ module.exports = {
   'Bolt Image zoom': function(browser) {
     const { testingUrl } = browser.globals;
     console.log(`global browser url: ${testingUrl}`);
-    currentBrowser = '--' + typeof browser.currentEnv !== 'undefined' ? browser.currentEnv : 'chrome';
+    currentBrowser = '--' + browser.currentEnv || 'chrome';
     let testName = 'bolt-image-zoom';
-
-    console.log('B:', browser.currentEnv);
-    console.log('CB:', currentBrowser);
 
     browser
       .url(
         `${testingUrl}/pattern-lab/patterns/02-components-image-30-image-zoom-variation/02-components-image-30-image-zoom-variation.html`,
       )
       .waitForElementVisible('bolt-device-viewer', 1000)
-      .moveToElement('bolt-image-zoom', 10, 10)
+      .moveToElement('bolt-image-zoom', 50, 50)
       .assert.cssClassPresent('bolt-image-zoom', 'is-mouse-entering')
       .saveScreenshot(
         `screenshots/bolt-image/${testName}--${currentBrowser}.png`,
