8d910b070d8c063b32eb606bb29e6436fe74d296
Salem Ghoweri <me@salemghoweri.com>
2019-02-19 08:42:03 -0500

    feat: automatically boot up webpack dev server to remove the need to compile Pattern Lab before running any Jest tests

--- jest.config.js
+++ jest.config.js
@@ -27,6 +27,7 @@ module.exports = {
     './scripts/monorepo.test.js',
   ],
   globalSetup: './jest-global-setup.js',
+  globalTeardown: './jest-global-teardown.js',
   snapshotSerializers: ['jest-serializer-html'],
   // Notify not working correctly; we want to only get a notification when tests fail, and then get ONE success notificaiton after it passes
   // notify: true,
