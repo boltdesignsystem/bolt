843aeb0955295f334f1df0dfe760a7e6e2e8e4ea
Salem Ghoweri <me@salemghoweri.com>
2019-02-19 08:46:41 -0500

    feat: add simple image screenshot test reporting functionality to Jest to assist with debugging VRT issues until full Github integration is wired up

--- jest.config.js
+++ jest.config.js
@@ -30,6 +30,7 @@ module.exports = {
   globalTeardown: './jest-global-teardown.js',
   setupFilesAfterEnv: ['./jest-setup-files-after-env.js'],
   snapshotSerializers: ['jest-serializer-html'],
+  reporters: ['default', '<rootDir>/scripts/report-jest-screenshots.js'],
   // Notify not working correctly; we want to only get a notification when tests fail, and then get ONE success notificaiton after it passes
   // notify: true,
   // notifyMode: 'failure-success',
