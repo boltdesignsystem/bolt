34b62f74f4b2db17d6cb7b29a6fda759e608589e
Salem Ghoweri <me@salemghoweri.com>
2019-02-22 16:44:43 -0500

    fix: adjust button VRT failure threshold

--- packages/components/bolt-button/__tests__/button.js
+++ packages/components/bolt-button/__tests__/button.js
@@ -139,7 +139,7 @@ describe('button', async () => {
 
     const image = await page.screenshot();
     expect(image).toMatchImageSnapshot({
-      failureThreshold: '0',
+      failureThreshold: '0.5',
       failureThresholdType: 'percent',
     });
 
@@ -179,7 +179,7 @@ describe('button', async () => {
     const image = await page.screenshot();
 
     expect(image).toMatchImageSnapshot({
-      failureThreshold: '0',
+      failureThreshold: '0.5',
       failureThresholdType: 'percent',
     });
 
