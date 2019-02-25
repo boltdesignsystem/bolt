479eb014abc3de8986fb826f5694328b4436c4e8
Salem Ghoweri <me@salemghoweri.com>
2019-02-25 10:53:48 -0500

    refactor: update web component to support legacy aspect-ratio-height/width props

--- packages/components/bolt-ratio/src/ratio.js
+++ packages/components/bolt-ratio/src/ratio.js
@@ -13,8 +13,10 @@ function BoltRatio() {
   return class BoltRatioClass extends withLitHtml() {
     static props = {
       ratio: props.string,
-      ratioH: props.string,
-      ratioW: props.string,
+      _ratioW: props.string, // internal only prop for handling the width-specific data from the ratio prop
+      _ratioH: props.string, // internal only prop for handling the height-specific data from the ratio prop
+      aspectRatioHeight: props.number, // deprecated - will be removed in Bolt v3.0
+      aspectRatioWidth: props.number, // deprecated - will be removed in Bolt v3.0
       noCssVars: {
         ...props.boolean,
         ...{ default: supportsCSSVars ? false : true },
@@ -37,12 +39,13 @@ function BoltRatio() {
      * @param {Number} aspW - the width component of the ratio
      */
     _computeRatio() {
-      this.ratioH = this.props.ratio ? this.props.ratio.split('/')[1] : 1;
-      this.ratioW = this.props.ratio ? this.props.ratio.split('/')[0] : 1;
-
-      // automatically reduce size of ratio to be below 100% based on CSS calc styles
-      this.ratioH = this.ratioH >= 100 ? this.ratioH / 100 : this.ratioH;
-      this.ratioW = this.ratioW >= 100 ? this.ratioW / 100 : this.ratioW;
+      if (this.props.aspectRatioHeight && this.props.aspectRatioWidth) {
+        this._ratioH = this.props.aspectRatioHeight;
+        this._ratioW = this.props.aspectRatioWidth;
+      } else {
+        this._ratioH = this.props.ratio ? this.props.ratio.split('/')[1] : 1;
+        this._ratioW = this.props.ratio ? this.props.ratio.split('/')[0] : 1;
+      }
     }
 
     connecting() {
@@ -54,10 +57,10 @@ function BoltRatio() {
     render() {
       const inlineStyles = this.noCssVars
         ? {
-            'padding-bottom': `${100 * (this.ratioH / this.ratioW)}%`,
+            'padding-bottom': `${100 * (this._ratioH / this._ratioW)}%`,
           }
         : {
-            '--aspect-ratio': `${this.ratioW / this.ratioH}`,
+            '--aspect-ratio': `${this._ratioW / this._ratioH}`,
           };
 
       return html`
