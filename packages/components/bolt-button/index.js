4c36399c42e43105ba757a76db41bdb02ca381c8
Mike Mai <emceehao@gmail.com>
2019-02-26 14:53:31 -0500

    fix: lint js

--- packages/components/bolt-button/index.js
+++ packages/components/bolt-button/index.js
@@ -3,26 +3,3 @@ import { polyfillLoader } from '@bolt/core/polyfills';
 polyfillLoader.then(res => {
   import(/* webpackMode: 'eager', webpackChunkName: 'bolt-button' // */ './src/button');
 });
-
-
-
-
-
-// import { polyfillLoader } from '@bolt/core/polyfills';
-//
-// polyfillLoader.then(res => {
-//   if (!window.customElements.get('replace-with-children')) {
-//     import(/*
-//       webpackMode: 'eager',
-//       webpackChunkName: 'replace-with-children'
-//     */ '@bolt/core/elements/replace-with-children');
-//   }
-//
-//   if (!window.customElements.get('bolt-button')) {
-//     import(/*
-//       webpackMode: 'eager',
-//       webpackChunkName: 'bolt-button'
-//     */ './src/button');
-//   }
-// });
-//
