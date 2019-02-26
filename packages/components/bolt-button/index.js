3e825f86be7c4a3ad73f752fd9ac423e250fa5f7
Mike Mai <emceehao@gmail.com>
2019-02-26 14:51:05 -0500

    chore: re-organize folder to follow conventions

---
+++ packages/components/bolt-button/index.js
@@ -0,0 +1,28 @@
+import { polyfillLoader } from '@bolt/core/polyfills';
+
+polyfillLoader.then(res => {
+  import(/* webpackMode: 'eager', webpackChunkName: 'bolt-button' // */ './src/button');
+});
+
+
+
+
+
+// import { polyfillLoader } from '@bolt/core/polyfills';
+//
+// polyfillLoader.then(res => {
+//   if (!window.customElements.get('replace-with-children')) {
+//     import(/*
+//       webpackMode: 'eager',
+//       webpackChunkName: 'replace-with-children'
+//     */ '@bolt/core/elements/replace-with-children');
+//   }
+//
+//   if (!window.customElements.get('bolt-button')) {
+//     import(/*
+//       webpackMode: 'eager',
+//       webpackChunkName: 'bolt-button'
+//     */ './src/button');
+//   }
+// });
+//
