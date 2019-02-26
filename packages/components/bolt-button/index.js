abb4007a0cf27c93aa00c36891c6143ad08bc3fe
Mike Mai <emceehao@gmail.com>
2019-02-26 15:47:56 -0500

    fix: broken js

--- packages/components/bolt-button/index.js
+++ packages/components/bolt-button/index.js
@@ -1,5 +1,17 @@
 import { polyfillLoader } from '@bolt/core/polyfills';
 
 polyfillLoader.then(res => {
-  import(/* webpackMode: 'eager', webpackChunkName: 'bolt-button' // */ './src/button');
+  if (!window.customElements.get('replace-with-children')) {
+    import(/*
+      webpackMode: 'eager',
+      webpackChunkName: 'replace-with-children'
+    */ '@bolt/core/elements/replace-with-children');
+  }
+
+  if (!window.customElements.get('bolt-button')) {
+    import(/*
+      webpackMode: 'eager',
+      webpackChunkName: 'bolt-button'
+    */ './src/button');
+  }
 });
