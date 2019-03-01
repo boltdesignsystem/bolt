dfcc1ade1957b0dae5ef0cf6c68e81b476f35885
Salem Ghoweri <me@salemghoweri.com>
2019-03-01 07:49:18 -0500

    refactor: update base fit for running smoke tests

--- test/smoke-tests.sh
+++ test/smoke-tests.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
+cd "$(dirname "$0")" # Start in the root of the repo, even if run from the test dir
+cd ../
 
 node ./scripts/lint.js
 node ./scripts/unit.js
