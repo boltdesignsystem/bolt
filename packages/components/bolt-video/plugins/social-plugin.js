50644e9ad30d181abe820907cbe436792779ce50
Daniel Morse <daniel.morse@pega.com>
2019-03-12 18:23:03 -0400

    chore: add comment to social plugin JS explaining a certain conditional statement

--- packages/components/bolt-video/plugins/social-plugin.js
+++ packages/components/bolt-video/plugins/social-plugin.js
@@ -1,5 +1,8 @@
 export const socialPlugin = player => {
   if (player.activePlugins_) {
+    // In the current player, `player.activePlugins_.social` is always `true`, so these settings are never returned.
+    // This check may be in place to avoid overwriting player configuration coming from Brightcove or Drupal.
+    // TODO: verify this is the expected behavior.
     if (!player.activePlugins_.social) {
       return player.social({
         url: '',
