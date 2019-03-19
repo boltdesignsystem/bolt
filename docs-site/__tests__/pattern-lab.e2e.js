6095c835ce109a2ec0c1f4de6b9d531aa55bd57a
Salem Ghoweri <me@salemghoweri.com>
2019-03-19 08:45:33 -0400

    refactor: update Nightwatch.js tests to use the component alias in the URL to find the right HTML page + open in a new window

--- docs-site/__tests__/pattern-lab.e2e.js
+++ docs-site/__tests__/pattern-lab.e2e.js
@@ -20,12 +20,19 @@ module.exports = {
       .url(`${testingUrl}/pattern-lab/?p=components-overview`)
       .waitForElementVisible('pl-header', 3000)
       .assert.elementPresent('.js-c-typeahead__input')
+      .click('.js-c-typeahead__input') // click on the PL search input
+      .keys('Components-Card') // type "Components-Card" in the input field
+      .click('.pl-c-typeahead__result--first') // click on the first result
+      .waitForElementVisible('.pl-js-open-new-window', 3000) // make sure the "Open in a New Tab" UI is there before clicking on it
+      .click('.pl-js-open-new-window')
+      .windowHandles(function(result) {
+        var handle = result.value[1];
+        browser.switchWindow(handle); // switch browser windows to make sure the page we've just opened is in focus
+      })
+      .waitForElementVisible('bolt-card', 3000)
+      .assert.urlContains('components-card')
       .saveScreenshot(
-        `screenshots/pattern-lab/pattern-lab-header--${browser.currentEnv ||
-          'chrome'}.png`,
-      )
-      .saveScreenshot(
-        `screenshots/pattern-lab/pattern-lab-search--${browser.currentEnv ||
+        `screenshots/pattern-lab/pattern-lab-search-results-load-new-page--${browser.currentEnv ||
           'chrome'}.png`,
       )
       .end();
--- docs-site/__tests__/server-side-rendering.e2e.js
+++ docs-site/__tests__/server-side-rendering.e2e.js
@@ -3,15 +3,43 @@ module.exports = {
     const { testingUrl } = browser.globals;
     console.log(`global browser url: ${testingUrl}`);
     browser
-      .url(
-        `${testingUrl}/pattern-lab/patterns/02-components-button-70-button-ssr-example--web-component-wo-shadow-dom/02-components-button-70-button-ssr-example--web-component-wo-shadow-dom.html`,
+      .url(`${testingUrl}/pattern-lab/?p=viewall-components-button`)
+      .assert.urlContains('components-button')
+      .saveScreenshot(
+        `screenshots/pattern-lab/button-component-docs--${browser.currentEnv ||
+          'chrome'}.png`,
       )
+      .click('.pl-js-open-new-window')
+      .windowHandles(function(result) {
+        var handle = result.value[1];
+        browser.switchWindow(handle);
+      })
       .waitForElementVisible('bolt-button', 3000)
       .assert.elementPresent('.c-bolt-button')
       .saveScreenshot(
-        `screenshots/pattern-lab/web-component-ssr--${browser.currentEnv ||
+        `screenshots/pattern-lab/button-component-docs-in-new-window--${browser.currentEnv ||
           'chrome'}.png`,
       )
+      .closeWindow()
+      .windowHandles(function(result) {
+        var handle = result.value[0];
+        browser.switchWindow(handle);
+      })
+      .url(
+        `${testingUrl}/pattern-lab/?p=components-button-ssr--web-component-wo-shadow-dom`,
+      )
+      .click('.pl-js-open-new-window')
+      .windowHandles(function(result) {
+        var handle = result.value[1];
+        browser.switchWindow(handle);
+      })
+      .waitForElementVisible('bolt-button', 3000)
+      .assert.elementPresent('.c-bolt-button')
+      .saveScreenshot(
+        `screenshots/pattern-lab/button-component-ssr--${browser.currentEnv ||
+          'chrome'}.png`,
+      )
+      .closeWindow()
       .end();
   },
 };
