f6c98a20735130faf973340da7a08a621cbffd9a
Daniel Morse <daniel.morse@pega.com>
2019-03-27 17:16:06 -0400

    test: add jest tests for bolt-text web component

--- packages/components/bolt-text/__tests__/text.js
+++ packages/components/bolt-text/__tests__/text.js
@@ -1,8 +1,6 @@
-import {
-  render,
-  renderString,
-  stop as stopTwigRenderer,
-} from '@bolt/twig-renderer';
+import { render } from '@bolt/twig-renderer';
+import { fixture as html } from '@open-wc/testing-helpers';
+
 const { readYamlFileSync } = require('@bolt/build-tools/utils/yaml');
 const { join } = require('path');
 const schema = readYamlFileSync(join(__dirname, '../text.schema.yml'));
@@ -13,20 +11,19 @@ const {
   align,
   opacity,
   quoted,
-  // @todo [Mai] How do I deal with kebab-case?
-  // line-height,
-  // letter-spacing,
-  // text-transform,
-  // font-family,
-  // font-size,
-  // font-style,
-  // font-weight,
+  'line-height': lineHeight,
+  'letter-spacing': letterSpacing,
+  'text-transform': textTransform,
+  'font-family': fontFamily,
+  'font-size': fontSize,
+  'font-style': fontStyle,
+  'font-weight': fontWeight,
 } = schema.properties;
 
-describe('<bolt-text> Component', async () => {
-  afterAll(async () => {
-    await stopTwigRenderer();
-  }, timeout);
+const timeout = 90000;
+
+describe('<bolt-text> Component', () => {
+  let page;
 
   beforeEach(async () => {
     page = await global.__BROWSER__.newPage();
@@ -37,28 +34,48 @@ describe('<bolt-text> Component', async () => {
     });
   }, timeout);
 
-  // Basic Usage
-  test('Default <bolt-text> with Shadow DOM renders', async function() {
-    const defaultTextShadowRoot = await page.evaluate(() => {
-      const boltText = document.createElement('bolt-text');
-      boltText.textContent = 'Text Test -- Shadow Root HTML';
-      document.body.appendChild(boltText);
-      boltText.updated();
-      return boltText.renderRoot.innerHTML;
+  // Basic Usage (Shadow DOM)
+  test('Default <bolt-text> w/ Shadow DOM renders', async function() {
+    const renderedTextHTML = await page.evaluate(() => {
+      const text = document.createElement('bolt-text');
+
+      text.textContent = `This is bolt text`;
+      document.body.appendChild(text);
+
+      return text.outerHTML;
     });
 
-    const defaultTextOuter = await page.evaluate(() => {
-      const boltText = document.createElement('bolt-text');
-      boltText.textContent = 'Text Test -- Outer HTML';
-      document.body.appendChild(boltText);
-      boltText.updated();
-      return boltText.outerHTML;
+    const renderedHTML = await html(renderedTextHTML);
+    const image = await page.screenshot();
+
+    expect(image).toMatchImageSnapshot({
+      failureThreshold: '0.01',
+      failureThresholdType: 'percent',
     });
 
-    const renderedShadowDomHTML = await html(defaultTextShadowRoot);
-    const renderedHTML = await html(defaultTextOuter);
+    expect(renderedHTML).toMatchSnapshot();
+  });
 
-    expect(renderedHTML.textContent).toEqual('Text Test -- Outer HTML');
+  // Basic Usage (Light DOM)
+  test('Default <bolt-text> w/o Shadow DOM renders', async function() {
+    const renderedTextHTML = await page.evaluate(() => {
+      const text = document.createElement('bolt-text');
+
+      text.textContent = `This is bolt text`;
+      document.body.appendChild(text);
+      text.useShadow = false;
+      text.updated();
+
+      return text.outerHTML;
+    });
+
+    const renderedHTML = await html(renderedTextHTML);
+
+    expect(
+      renderedHTML
+        .querySelector('.c-bolt-text-v2')
+        .classList.contains('c-bolt-text-v2--font-size-medium'),
+    ).toBe(true);
 
     const image = await page.screenshot();
 
@@ -67,22 +84,64 @@ describe('<bolt-text> Component', async () => {
       failureThresholdType: 'percent',
     });
 
-    expect(renderedShadowDomHTML).toMatchSnapshot();
     expect(renderedHTML).toMatchSnapshot();
   });
 
-  // Props
-  // @todo [Mai] I need to test combinations of props.
-  // 1. Headline and xxxlarge size
-  // 2. Bold, wide letter spacing, color, and quoted
-  display.enum.forEach(async displayChoice => {
-    test(`text display: ${displayChoice}`, async () => {
-      const results = await renderTwig('@bolt-components-text/text.twig', {
-        display: displayChoice,
-        text: 'This is text',
-      });
-      expect(results.ok).toBe(true);
-      expect(results.html).toMatchSnapshot();
+  // Headline and xxxlarge size (Shadow DOM)
+  test('<bolt-text> with headline and xxxlarge font-size w/ Shadow DOM renders', async function() {
+    const renderedTextHTML = await page.evaluate(() => {
+      const text = document.createElement('bolt-text');
+
+      text.textContent = `This is bolt text`;
+      text.setAttribute('headline', true);
+      text.setAttribute('font-size', 'xxxlarge');
+      document.body.appendChild(text);
+
+      return text.outerHTML;
     });
+
+    const renderedHTML = await html(renderedTextHTML);
+    const image = await page.screenshot();
+
+    expect(image).toMatchImageSnapshot({
+      failureThreshold: '0.01',
+      failureThresholdType: 'percent',
+    });
+
+    expect(renderedHTML).toMatchSnapshot();
   });
+
+  // Headline and xxxlarge size (Light DOM)
+  test('<bolt-text> with headline and xxxlarge font-size w/o Shadow DOM renders', async function() {
+    const renderedTextHTML = await page.evaluate(() => {
+      const text = document.createElement('bolt-text');
+
+      text.textContent = `This is bolt text`;
+      text.setAttribute('headline', true);
+      text.setAttribute('font-size', 'xxxlarge');
+      document.body.appendChild(text);
+      text.useShadow = false;
+      text.updated();
+
+      return text.outerHTML;
+    });
+
+    expect(
+      renderedHTML
+        .querySelector('.c-bolt-text-v2')
+        .classList.contains('c-bolt-text-v2--font-size-xxxlarge'),
+    ).toBe(true);
+
+    const renderedHTML = await html(renderedTextHTML);
+    const image = await page.screenshot();
+
+    expect(image).toMatchImageSnapshot({
+      failureThreshold: '0.01',
+      failureThresholdType: 'percent',
+    });
+
+    expect(renderedHTML).toMatchSnapshot();
+  });
+
+  // @todo: Bold, wide letter spacing, color, and quoted
 });
