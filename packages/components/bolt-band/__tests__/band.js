956487d4aaabc90db31972e6f9a0347c9dbd7c70
Mike Mai <emceehao@gmail.com>
2019-04-03 15:14:33 -0400

    chore: set up js tests

--- packages/components/bolt-band/__tests__/band.js
+++ packages/components/bolt-band/__tests__/band.js
@@ -6,7 +6,12 @@ import {
 const { readYamlFileSync } = require('@bolt/build-tools/utils/yaml');
 const { join } = require('path');
 const schema = readYamlFileSync(join(__dirname, '../band.schema.yml'));
-const { size, theme } = schema.properties;
+const {
+  size,
+  theme,
+  tag,
+  full_bleed: fullBleed,
+} = schema.properties;
 
 async function renderTwig(template, data) {
   return await render(template, data, true);
@@ -34,7 +39,7 @@ describe('<bolt-band> Component', () => {
 
   // Props
   size.enum.forEach(async sizeChoice => {
-    test(`Size of the band: ${sizeChoice}`, async () => {
+    test(`Size variations: ${sizeChoice}`, async () => {
       const results = await renderTwig('@bolt-components-band/band.twig', {
         size: sizeChoice,
         content: 'This is a band.',
@@ -45,7 +50,7 @@ describe('<bolt-band> Component', () => {
   });
 
   theme.enum.forEach(async themeChoice => {
-    test(`Theme of the band: ${themeChoice}`, async () => {
+    test(`Theme variations: ${themeChoice}`, async () => {
       const results = await renderTwig('@bolt-components-band/band.twig', {
         theme: themeChoice,
         content: 'This is a band.',
@@ -53,5 +58,60 @@ describe('<bolt-band> Component', () => {
       expect(results.ok).toBe(true);
       expect(results.html).toMatchSnapshot();
     });
+  });
+
+  tag.enum.forEach(async tagChoice => {
+    test(`Semantic tag usage: ${tagChoice}`, async () => {
+      const results = await renderTwig('@bolt-components-band/band.twig', {
+        tag: tagChoice,
+        content: 'This is a band.',
+      });
+      expect(results.ok).toBe(true);
+      expect(results.html).toMatchSnapshot();
+    });
+  });
+
+  fullBleed.enum.forEach(async fullBleedChoice => {
+    test(`Full bleed: ${fullBleedChoice}`, async () => {
+      const results = await renderTwig('@bolt-components-band/band.twig', {
+        fullBleed: fullBleedChoice,
+        content: 'This is a band.',
+      });
+      expect(results.ok).toBe(true);
+      expect(results.html).toMatchSnapshot();
+    });
+  });
+
+  test(`Pinned content`, async () => {
+    const results = await renderTwig('@bolt-components-band/band.twig', {
+      size: 'large',
+      content: 'This is a band.',
+      pinned_content: {
+        upper: [
+          {
+            content: 'upper pinned 1',
+          },
+          {
+            content: 'upper pinned 2',
+          },
+          {
+            content: 'upper pinned 3',
+          },
+        ],
+        lower: [
+          {
+            content: 'lower pinned 1',
+          },
+          {
+            content: 'lower pinned 2',
+          },
+          {
+            content: 'lower pinned 3',
+          },
+        ]
+      }
+    });
+    expect(results.ok).toBe(true);
+    expect(results.html).toMatchSnapshot();
   });
 });
