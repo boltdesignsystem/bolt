10809fc62aee6527777016430d095378ce26d4d6
Salem Ghoweri <me@salemghoweri.com>
2019-04-07 20:06:14 -0400

    chore: clean up analytics autotrack code

--- packages/analytics/autotrack/index.js
+++ packages/analytics/autotrack/index.js
@@ -69,10 +69,8 @@ const metrics = {
 //  * values on the trackers.
 //  */
 export const init = () => {
-  //   // Initialize the command queue in case analytics.js hasn't loaded yet.
+  // Initialize the command queue in case analytics.js hasn't loaded yet.
   window.ga = window.ga || ((...args) => (ga.q = ga.q || []).push(args));
-  console.log('init analytics');
-
   createTracker();
   trackErrors();
   trackCustomDimensions();
