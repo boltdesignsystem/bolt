cec9099ea91a79775da4dd443e4a9a2c1c3b2aef
Mike Mai <emceehao@gmail.com>
2019-04-12 16:29:37 -0400

    fix: lint js

--- packages/components/bolt-stack/__tests__/stack.js
+++ packages/components/bolt-stack/__tests__/stack.js
@@ -6,9 +6,7 @@ import {
 const { readYamlFileSync } = require('@bolt/build-tools/utils/yaml');
 const { join } = require('path');
 const schema = readYamlFileSync(join(__dirname, '../stack.schema.yml'));
-const {
-  spacing
-} = schema.properties;
+const { spacing } = schema.properties;
 
 async function renderTwig(template, data) {
   return await render(template, data, true);
