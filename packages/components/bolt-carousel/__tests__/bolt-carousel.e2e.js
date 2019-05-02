077928110ee2db7663459adf3e9a53f4592b1977
Salem Ghoweri <me@salemghoweri.com>
2019-05-02 06:20:01 -0400

    fix: update carousel Nightwatch test

--- packages/components/bolt-carousel/__tests__/bolt-carousel.e2e.js
+++ packages/components/bolt-carousel/__tests__/bolt-carousel.e2e.js
@@ -10,46 +10,13 @@ module.exports = {
 
     browser
       .url(
-        `${testingUrl}/pattern-lab/patterns/02-components-carousel-01-carousel/02-components-carousel-01-carousel.html`,
+        `${testingUrl}/pattern-lab/patterns/02-components-carousel-variations-basic-01-basic-carousel/02-components-carousel-variations-basic-01-basic-carousel.html`,
       )
       .waitForElementVisible('bolt-carousel', 1000)
-      // .click('.vjs-big-play-button')
       .assert.elementPresent('.c-bolt-carousel__wrapper')
-      // .execute(function(data) {
-      //   return document.querySelector('button.vjs-playback-rate').click();
-      // })
       .saveScreenshot(
         `screenshots/bolt-carousel/${testName}--${currentBrowser}.png`,
       )
-      // .execute(
-      //   function(data) {
-      //     return document.querySelector('bolt-video').player.playbackRate();
-      //   },
-      //   [],
-      //   function(result) {
-      //     browser.assert.ok(
-      //       result.value === 1.3,
-      //       `verified that <bolt-video> play rate has sped up to ${
-      //         result.value
-      //       }`,
-      //     );
-      //   },
-      // )
-      // .execute(
-      //   function(data) {
-      //     return document.querySelector('.vjs-playback-rate-value').textContent;
-      //   },
-      //   [],
-      //   function(result) {
-      //     browser.assert.ok(
-      //       result.value === '1.3x',
-      //       `verified that <bolt-video> play rate text reads 1.3x.`,
-      //     );
-      //   },
-      // )
-      // .saveScreenshot(
-      //   `screenshots/bolt-video/${testName}--playback-at-1.3x--${currentBrowser}.png`,
-      // )
       .end();
   },
 };
