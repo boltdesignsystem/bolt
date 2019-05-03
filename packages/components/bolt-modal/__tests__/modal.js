22e477c8368c36648cd08bd6c36a87be861292c4
Mike Mai <emceehao@gmail.com>
2019-05-03 14:45:45 -0400

    chore: add basic tests

---
+++ packages/components/bolt-modal/__tests__/modal.js
@@ -0,0 +1,94 @@
+import {
+  render,
+  renderString,
+  stop as stopTwigRenderer,
+} from '@bolt/twig-renderer';
+const { readYamlFileSync } = require('@bolt/build-tools/utils/yaml');
+const { join } = require('path');
+const schema = readYamlFileSync(join(__dirname, '../modal.schema.yml'));
+const {
+  persistent,
+  width,
+  spacing,
+  theme,
+  scroll,
+} = schema.properties;
+
+async function renderTwig(template, data) {
+  return await render(template, data, true);
+}
+
+async function renderTwigString(template, data) {
+  return await renderString(template, data, true);
+}
+
+const timeout = 60000;
+
+describe('<bolt-modal> Component', () => {
+  afterAll(async () => {
+    await stopTwigRenderer();
+  }, timeout);
+
+  test('basic usage', async () => {
+    const results = await renderTwig('@bolt-components-modal/modal.twig', {
+      content: 'This is a modal',
+    });
+    expect(results.ok).toBe(true);
+    expect(results.html).toMatchSnapshot();
+  });
+
+  persistent.enum.forEach(async persistentChoice => {
+    test(`modal persistent: ${persistentChoice}`, async () => {
+      const results = await renderTwig('@bolt-components-modal/modal.twig', {
+        persistent: persistentChoice,
+        content: 'This is a modal',
+      });
+      expect(results.ok).toBe(true);
+      expect(results.html).toMatchSnapshot();
+    });
+  });
+
+  width.enum.forEach(async widthChoice => {
+    test(`modal width: ${widthChoice}`, async () => {
+      const results = await renderTwig('@bolt-components-modal/modal.twig', {
+        width: widthChoice,
+        content: 'This is a modal',
+      });
+      expect(results.ok).toBe(true);
+      expect(results.html).toMatchSnapshot();
+    });
+  });
+
+  spacing.enum.forEach(async spacingChoice => {
+    test(`modal spacing: ${spacingChoice}`, async () => {
+      const results = await renderTwig('@bolt-components-modal/modal.twig', {
+        spacing: spacingChoice,
+        content: 'This is a modal',
+      });
+      expect(results.ok).toBe(true);
+      expect(results.html).toMatchSnapshot();
+    });
+  });
+
+  theme.enum.forEach(async themeChoice => {
+    test(`modal theme: ${themeChoice}`, async () => {
+      const results = await renderTwig('@bolt-components-modal/modal.twig', {
+        theme: themeChoice,
+        content: 'This is a modal',
+      });
+      expect(results.ok).toBe(true);
+      expect(results.html).toMatchSnapshot();
+    });
+  });
+
+  scroll.enum.forEach(async scrollChoice => {
+    test(`modal scroll: ${scrollChoice}`, async () => {
+      const results = await renderTwig('@bolt-components-modal/modal.twig', {
+        scroll: scrollChoice,
+        content: 'This is a modal',
+      });
+      expect(results.ok).toBe(true);
+      expect(results.html).toMatchSnapshot();
+    });
+  });
+});
