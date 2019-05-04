341e5717bbeda79195d0779280bdf667d108f588
Daniel Morse <daniel.morse@pega.com>
2019-05-03 21:29:54 -0400

    fix: add missing AccordionItem imports

---
+++ packages/components/bolt-accordion/src/AccordionItem/AccordionItemHeader.js
@@ -0,0 +1,56 @@
+import { css } from '@bolt/core/utils';
+import { html } from '@bolt/core/renderers/renderer-lit-html';
+
+export const AccordionItemHeader = (children, props, context) => {
+  const headerClasses = css(
+    'c-bolt-accordion-item__trigger',
+    props.center ? 'c-bolt-accordion-item__trigger--center' : '',
+  );
+
+  const spacingClasses = css(
+    context.spacing ? `c-bolt-accordion-item--spacing-${context.spacing}` : '',
+  );
+
+  const headerTemplate = children => {
+    return html`
+      ${props.autoOpen
+        ? html`
+            <div class="${headerClasses}" data-open>
+              ${children}
+            </div>
+          `
+        : html`
+            <div class="${headerClasses}">${children}</div>
+          `}
+    `;
+  };
+
+  const innerHeaderTemplate = children => {
+    return html`
+      <button class="c-bolt-accordion-item__trigger-button ${spacingClasses}">
+        <div class="c-bolt-accordion-item__trigger-content">
+          ${children}
+        </div>
+        <span class="c-bolt-accordion-item__trigger-icons">
+          <div class="c-bolt-accordion-item__trigger-icons-inner">
+            <span
+              class="c-bolt-accordion-item__trigger-icon c-bolt-accordion-item__trigger-icon--open"
+            >
+              <bolt-icon name="chevron-down"></bolt-icon>
+            </span>
+
+            <span
+              class="c-bolt-accordion-item__trigger-icon c-bolt-accordion-item__trigger-icon--close"
+            >
+              <bolt-icon name="chevron-up"></bolt-icon>
+            </span>
+          </div>
+        </span>
+      </button>
+    `;
+  };
+
+  const innerHeader = innerHeaderTemplate(children);
+
+  return headerTemplate(innerHeader);
+};
