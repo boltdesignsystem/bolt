90dc9c5a2f6cc059794b2af7a015a76b10d96ef7
Daniel Morse <daniel.morse@pega.com>
2019-05-08 20:27:15 -0400

    fix: replace `bolt-text` and skip mutation observer to fix IE bug

--- packages/components/bolt-blockquote/src/Author/AuthorTitle.js
+++ packages/components/bolt-blockquote/src/Author/AuthorTitle.js
@@ -1,14 +1,35 @@
 import { html } from '@bolt/core/renderers/renderer-lit-html';
+import classNames from 'classnames/bind';
+import styles from '../blockquote.scss';
+import textStyles from '@bolt/components-text/index.scss';
+
+let cx = classNames.bind([styles, textStyles]);
 
 export const AuthorTitle = elem => {
   const { props, slots } = elem;
+
+  const textClasses = cx(
+    'c-bolt-text-v2',
+    'c-bolt-text-v2--block',
+    'c-bolt-text-v2--body',
+    'c-bolt-text-v2--font-size-xsmall',
+    'c-bolt-text-v2--font-weight-regular',
+    'c-bolt-text-v2--font-style-regular',
+    'c-bolt-text-v2--color-theme-headline',
+    'c-bolt-text-v2--letter-spacing-regular',
+    'c-bolt-text-v2--align-inherit',
+    'c-bolt-text-v2--text-transform-regular',
+    'c-bolt-text-v2--line-height-regular',
+    'c-bolt-text-v2--opacity-100',
+  );
+
   if (slots['author-title'] || props.authorTitle) {
     return html`
-      <bolt-text tag="cite" font-size="xsmall" color="theme-headline">
+      <cite class="${textClasses}">
         ${elem.slots['author-title']
           ? elem.slot('author-title')
           : props.authorTitle}
-      </bolt-text>
+      </cite>
     `;
   }
 };
