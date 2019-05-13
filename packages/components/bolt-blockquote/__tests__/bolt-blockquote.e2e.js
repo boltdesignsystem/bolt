126b2a56b529235649ec6a2acf2705c1378faeeb
Salem Ghoweri <me@salemghoweri.com>
2019-05-13 07:24:30 -0400

    feat: add 1 basic E2E cross browser test for Blockquote

---
+++ packages/components/bolt-blockquote/__tests__/bolt-blockquote.e2e.js
@@ -0,0 +1,34 @@
+let currentBrowser;
+
+module.exports = {
+  tags: ['component', 'blockquote', 'web component'],
+  'Bolt Blockquote': function(browser) {
+    const { testingUrl } = browser.globals;
+    console.log(`global browser url: ${testingUrl}`);
+    currentBrowser = '--' + browser.currentEnv || 'chrome';
+    let testName = 'bolt-blockquote';
+
+    browser
+      .url(
+        `${testingUrl}/pattern-lab/patterns/02-components-blockquote-10-blockquote-alignItems-variation/02-components-blockquote-10-blockquote-alignItems-variation.html`,
+      )
+      .waitForElementVisible('bolt-blockquote', 1000)
+      .assert.elementPresent('img[slot="logo"]')
+      .execute(
+        function(data) {
+          return document.querySelector('bolt-blockquote cite[slot="author-name"]').textContent;
+        },
+        [],
+        function(result) {
+          browser.assert.ok(
+            result.value === 'Michelangelo di Lodovico Buonarroti Simoni',
+            `verified that <bolt-blockquote> rendered author text.`,
+          );
+        },
+      )
+      .saveScreenshot(
+        `screenshots/bolt-blockquote/${testName}--${currentBrowser}.png`,
+      )
+      .end();
+  },
+}
