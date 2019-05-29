e5c80267747d814d0dbac4c9efd46a1626e29aa8
Salem Ghoweri <me@salemghoweri.com>
2019-05-29 11:33:08 +0200

    refactor: consolidate monorepo tests + add new Jest tests to make sure public packages don't use peerDependencies or devDependencies

--- jest.config.js
+++ jest.config.js
@@ -31,7 +31,10 @@ module.exports = {
   ],
   globalSetup: './jest-global-setup.js',
   globalTeardown: './jest-global-teardown.js',
-  setupFilesAfterEnv: ['./jest-setup-files-after-env.js'],
+  setupFilesAfterEnv: [
+    './jest-setup-files-after-env.js',
+    'jest-expect-message',
+  ],
   snapshotSerializers: ['jest-serializer-html'],
   reporters: ['default', '<rootDir>/scripts/report-jest-screenshots.js'],
   // Notify not working correctly; we want to only get a notification when tests fail, and then get ONE success notificaiton after it passes
