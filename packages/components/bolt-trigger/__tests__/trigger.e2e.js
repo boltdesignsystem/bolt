b6b8cb51718f7d7caa26a3a3940c46290778763f
Daniel Morse <daniel.morse@pega.com>
2019-05-30 17:40:13 -0400

    test: add jest and e2e tests for bolt-trigger

---
+++ packages/components/bolt-trigger/__tests__/trigger.e2e.js
@@ -0,0 +1,52 @@
+let currentBrowser;
+
+module.exports = {
+  tags: ['component', 'trigger'],
+  'Bolt Trigger Modal': function(browser) {
+    const { testingUrl } = browser.globals;
+    console.log(`global browser url: ${testingUrl}`);
+    currentBrowser = '--' + browser.currentEnv || 'chrome';
+    let testName = 'bolt-trigger-modal';
+
+    browser
+      .url(
+        `${testingUrl}/pattern-lab/patterns/02-components-trigger-25-trigger-advanced-usage/02-components-trigger-25-trigger-advanced-usage.html`,
+      )
+      .waitForElementVisible('bolt-trigger', 1000)
+      .click('bolt-trigger')
+      .pause(1000)
+      .execute(
+        function(data) {
+          return document.querySelector('bolt-modal').open;
+        },
+        [],
+        function(result) {
+          browser.assert.ok(
+            result.value === true,
+            `verified <bolt-trigger> opened the modal.`,
+          );
+        },
+      )
+      .saveScreenshot(
+        `screenshots/bolt-trigger/${testName}--has-opened-modal--${currentBrowser}.png`,
+      )
+      .click('bolt-modal bolt-trigger')
+      .pause(1000)
+      .execute(
+        function(data) {
+          return document.querySelector('bolt-modal').open;
+        },
+        [],
+        function(result) {
+          browser.assert.ok(
+            result.value === false,
+            `verified <bolt-trigger> closed the modal.`,
+          );
+        },
+      )
+      .saveScreenshot(
+        `screenshots/bolt-trigger/${testName}--has-closed-modal--${currentBrowser}.png`,
+      )
+      .end();
+  },
+};
