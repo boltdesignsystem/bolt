a8a03ba3f08aba212f778dd07a82b5166efb703c
Joe Karasek <joseph.karasek@gmail.com>
2019-06-06 11:27:08 -0700

    refactor: small linting error

--- packages/components/bolt-interactive-step/index.js
+++ packages/components/bolt-interactive-step/index.js
@@ -1,5 +1,7 @@
 import { polyfillLoader } from '@bolt/core/polyfills';
 
 polyfillLoader.then(res => {
-  import(/* webpackMode: 'eager', webpackChunkName: 'bolt-interactive-step' */ './src/interactive-step');
+  import(
+    /* webpackMode: 'eager', webpackChunkName: 'bolt-interactive-step' */ './src/interactive-step'
+  );
 });
