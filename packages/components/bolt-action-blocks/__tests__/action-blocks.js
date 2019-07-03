6067276e33cafef4968d0cc96a9c8054edd3642c
Dan Morse <danielamorse@gmail.com>
2019-07-03 11:29:36 -0400

    Merge pull request #1233 from bolt-design-system/feature/action-block-patch-with-tests
    Action Blocks | Testing | Update include path and add tests
    
    (cherry picked from commit 5b9786bf3685538a329d7fef367bd7318e5df18e)

--- packages/components/bolt-action-blocks/__tests__/action-blocks.js
+++ packages/components/bolt-action-blocks/__tests__/action-blocks.js
@@ -54,6 +54,23 @@ describe('<bolt-action-blocks> Component', () => {
     expect(results.html).toMatchSnapshot();
   });
 
+  // Subcomponent
+  test('Subcomponent renders as expected', async () => {
+    const results = await render(
+      '@bolt-components-action-blocks/action-block.twig',
+      {
+        text: 'Item 1',
+        url: '#!',
+        icon: {
+          name: 'download',
+          size: 'large',
+        },
+      },
+    );
+    expect(results.ok).toBe(true);
+    expect(results.html).toMatchSnapshot();
+  });
+
   // Props
   valign.enum.forEach(async valignChoice => {
     test(`Vertical alignment of each block's content: ${valignChoice}`, async () => {
@@ -132,5 +149,46 @@ describe('<bolt-action-blocks> Component', () => {
       expect(results.ok).toBe(true);
       expect(results.html).toMatchSnapshot();
     });
+  });
+
+  // Deprecated props
+  // @todo: This will be removed with Bolt v3.0
+  test('Deprecated props still render as expected', async () => {
+    const results = await render(
+      '@bolt-components-action-blocks/action-blocks.twig',
+      {
+        maxItemsPerRow: 2,
+        align: 'center',
+        border: false,
+        contentItems: [
+          {
+            text: 'Item 1',
+            url: '#!',
+            icon: {
+              name: 'download',
+              size: 'large',
+            },
+          },
+          {
+            text: 'Item 2',
+            url: '#!',
+            icon: {
+              name: 'copy-to-clipboard',
+              size: 'large',
+            },
+          },
+          {
+            text: 'Item 3',
+            url: '#!',
+            icon: {
+              name: 'calendar',
+              size: 'large',
+            },
+          },
+        ],
+      },
+    );
+    expect(results.ok).toBe(true);
+    expect(results.html).toMatchSnapshot();
   });
 });
