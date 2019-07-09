29c244250045193ecb0e60d53ef313154d97245d
Salem Ghoweri <me@salemghoweri.com>
2019-07-09 16:49:21 -0400

    refactor: temporarily disable iframe resizing-specific functionality

--- packages/uikit-workshop/src/scripts/patternlab-pattern.js
+++ packages/uikit-workshop/src/scripts/patternlab-pattern.js
@@ -1,4 +1,4 @@
 import './components/modal-styleguide';
 import './components/pl-search/pl-search.iframe-helper';
 import './utils/share-inner-iframe-data';
-import 'iframe-resizer/js/iframeResizer.contentWindow.min.js'; 
+// import 'iframe-resizer/js/iframeResizer.contentWindow.min.js';
