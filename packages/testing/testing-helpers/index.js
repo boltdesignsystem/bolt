35a0f85a76a52415d379cea0cc9e649f1dc5f43e
Salem Ghoweri <me@salemghoweri.com>
2019-07-17 17:06:33 -0400

    test: slightly increase VRT diff threshold

--- packages/testing/testing-helpers/index.js
+++ packages/testing/testing-helpers/index.js
@@ -20,8 +20,9 @@ export const vrtDefaultConfig = {
   failureThreshold: '0.001',
   failureThresholdType: 'percent',
   customDiffConfig: {
-    threshold: '0.025',
-    includeAA: true,
+    // Please note the threshold set in the customDiffConfig is the per pixel sensitivity threshold. For example with a source pixel colour of #ffffff (white) and a comparison pixel colour of #fcfcfc (really light grey) if you set the threshold to 0 then it would trigger a failure on that pixel. However if you were to use say 0.5 then it wouldn't, the colour difference would need to be much more extreme to trigger a failure on that pixel, say #000000 (black)
+    threshold: '0.1',
+    includeAA: true, // If true, disables detecting and ignoring anti-aliased pixels. false by default.
   },
 };
 
