4026bd4f76aea45858a94485fb506466a0eff692
Salem Ghoweri <salem.ghoweri@pega.com>
2019-07-18 19:31:00 -0400

    test: retest with timeout re-added

--- packages/components/bolt-carousel/__tests__/carousel.js
+++ packages/components/bolt-carousel/__tests__/carousel.js
@@ -60,6 +60,7 @@ describe('carousel', () => {
   beforeEach(async () => {
     page = await global.__BROWSER__.newPage();
     await page.goto('http://127.0.0.1:4444/', {
+      timeout: 0,
       waitUntil: 'networkidle0',
     });
   }, timeout);
--- packages/components/bolt-ratio/__tests__/ratio.js
+++ packages/components/bolt-ratio/__tests__/ratio.js
@@ -19,6 +19,7 @@ describe('<bolt-ratio> Component', () => {
   beforeEach(async () => {
     page = await global.__BROWSER__.newPage();
     await page.goto('http://127.0.0.1:4444/', {
+      timeout: 0,
       waitUntil: 'networkidle0',
     });
   }, timeout);
