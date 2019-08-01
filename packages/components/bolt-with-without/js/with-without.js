694a0272980a653d198ee0e38795576f5e095490
Jeremy John <jmjohn@riseup.net>
2019-08-01 10:06:02 -0700

    feat(with-without): continued theming

--- packages/components/bolt-with-without/js/with-without.js
+++ packages/components/bolt-with-without/js/with-without.js
@@ -7,7 +7,7 @@ const SimpleSwitch = require('a-simple-switch');
  *
  * @return {void}
  */
-const setActiveRegion = (checked, shouldToggleAnimations) => {
+const setActiveRegion = (checked) => {
   const activeWoClass = 'c-pega-wwo__active-wo';
   const activeWClass = 'c-pega-wwo__active-w';
   const mainWrapper = document.querySelector('.c-pega-wwo__wrapper');
@@ -15,30 +15,13 @@ const setActiveRegion = (checked, shouldToggleAnimations) => {
   mainWrapper.classList.remove(checked ? activeWoClass : activeWClass);
 };
 
-// const toggleAnimations = (classToActivate, classToDeactivate) => {
-//   const animationClasses = ['c-pega-wwo__sm-circle'];
-//   const animationAttribute = 'data-anim-triggered';
-//   animationClasses.forEach((animationClass) => {
-//     const activateEl = document.querySelector(`.${classToActivate} .${animationClass}`);
-//     if (activateEl) {
-//       activateEl.setAttribute(animationAttribute, 'yes');
-//     }
-//     const deActivateEl = document.querySelector(`.${classToDeactivate} .${animationClass}`);
-//     if (deActivateEl) {
-//       deActivateEl.setAttribute(animationAttribute, 'yes');
-//     }
-//   });
-// };
-
-window.onload = () => {
-  setTimeout(() => {
-    const toggleInputClass = '#c-pega-wwo__toggle-input';
-    SimpleSwitch.init();
-    setActiveRegion(document.querySelector(toggleInputClass).checked);
-    document
-      .querySelector('#c-pega-wwo__toggle-input')
-      .addEventListener('change', e => {
-        setActiveRegion(e.target.checked);
-      });
-  }, 4000);
-};
+setTimeout(() => {
+  const toggleInputClass = '#c-pega-wwo__toggle-input';
+  SimpleSwitch.init();
+  setActiveRegion(document.querySelector(toggleInputClass).checked);
+  document
+    .querySelector('#c-pega-wwo__toggle-input')
+    .addEventListener('change', e => {
+      setActiveRegion(e.target.checked);
+    });
+}, 0);
