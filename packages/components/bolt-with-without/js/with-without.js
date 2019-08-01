5d667d78986884996fe26dfad658f094b28a7d9f
Jeremy John <jmjohn@riseup.net>
2019-08-01 12:40:26 -0700

    rename file

--- packages/components/bolt-with-without/js/with-without.js
+++ packages/components/bolt-with-without/js/with-without.js
@@ -8,20 +8,30 @@ const SimpleSwitch = require('a-simple-switch');
  * @return {void}
  */
 const setActiveRegion = (checked) => {
-  const activeWoClass = 'c-pega-wwo__active-wo';
-  const activeWClass = 'c-pega-wwo__active-w';
-  const mainWrapper = document.querySelector('.c-pega-wwo__wrapper');
-  mainWrapper.classList.add(checked ? activeWClass : activeWoClass);
-  mainWrapper.classList.remove(checked ? activeWoClass : activeWClass);
+  setTimeout(() => {
+    const activeWoClass = 'c-pega-wwo__active-wo';
+    const activeWClass = 'c-pega-wwo__active-w';
+    // Use a different class for animations bc dom skips render/animations on display:none--so no transition
+    // const activeAnimWoClass = 'c-pega-wwo__active-anim-wo';
+    // const activeAnimWClass = 'c-pega-wwo__active-anim-w';
+    const mainWrapper = document.querySelector('.c-pega-wwo__wrapper');
+    mainWrapper.classList.add(checked ? activeWClass : activeWoClass);
+    mainWrapper.classList.remove(checked ? activeWoClass : activeWClass);
+    // Both requestAnimationFrame and setTimeout are required to make sure animClass is added after activeClass.
+    // window.requestAnimationFrame(() => {
+    //   setTimeout(() => {
+    //     mainWrapper.classList.add(checked ? activeAnimWClass : activeAnimWoClass);
+    //     mainWrapper.classList.remove(checked ? activeAnimWoClass : activeAnimWClass);
+    //   }, 0);
+    // });
+  }, 0);
 };
 
-setTimeout(() => {
-  const toggleInputClass = '#c-pega-wwo__toggle-input';
-  SimpleSwitch.init();
-  setActiveRegion(document.querySelector(toggleInputClass).checked);
-  document
-    .querySelector('#c-pega-wwo__toggle-input')
-    .addEventListener('change', e => {
-      setActiveRegion(e.target.checked);
-    });
-}, 0);
+const toggleInputClass = '#c-pega-wwo__toggle-input';
+SimpleSwitch.init();
+setActiveRegion(document.querySelector(toggleInputClass).checked);
+document
+  .querySelector('#c-pega-wwo__toggle-input')
+  .addEventListener('change', e => {
+    setActiveRegion(e.target.checked);
+  });
