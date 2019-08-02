8909c3a8387a132da128959a99f0a9503e54ee01
Salem Ghoweri <me@salemghoweri.com>
2019-08-02 15:21:56 -0400

    feat: wire up typeahead Twig + web component buildout

---
+++ packages/components/bolt-typeahead/typeahead.polyfilled.js
@@ -0,0 +1,5 @@
+import { polyfillLoader } from '@bolt/core/polyfills';
+
+polyfillLoader.then(res => {
+  import(/* webpackMode: 'eager', webpackChunkName: 'bolt-typeahead' */ './typeahead.js')
+});
