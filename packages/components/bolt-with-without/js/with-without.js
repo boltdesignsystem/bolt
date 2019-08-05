0666363b665e1d32e7054740d4eea60dc3ceae65
Jeremy John <jmjohn@riseup.net>
2019-08-05 11:24:47 -0500

    WIP

--- packages/components/bolt-with-without/js/with-without.js
+++ packages/components/bolt-with-without/js/with-without.js
@@ -1,37 +1,73 @@
+import { triggerAnims } from '@bolt/components-animate/utils';
 const SimpleSwitch = require('a-simple-switch');
 
+const setActiveRegionClass = (inactiveToRemove, activeToSet) => {
+  const mainWrapper = document.querySelector('.c-pega-wwo__wrapper');
+  const activeWoClass = 'c-pega-wwo__active-';
+  mainWrapper.classList.add(`${activeWoClass}${activeToSet}`);
+  mainWrapper.classList.remove(`${activeWoClass}${inactiveToRemove}`);
+};
+
 /**
- * Sets the active region: Without Pega if checked=false, With Pega if checked=true
+ * Controls the animation in/out of active with/without (w/wo) region.
  *
  * @param {boolean} checked
  *
  * @return {void}
  */
-const setActiveRegion = checked => {
-  setTimeout(() => {
-    const activeWoClass = 'c-pega-wwo__active-wo';
-    const activeWClass = 'c-pega-wwo__active-w';
-    // Use a different class for animations bc dom skips render/animations on display:none--so no transition
-    // const activeAnimWoClass = 'c-pega-wwo__active-anim-wo';
-    // const activeAnimWClass = 'c-pega-wwo__active-anim-w';
+const handleActiveRegionChange = (checked, init = false) => {
+  setTimeout(async () => {
     const mainWrapper = document.querySelector('.c-pega-wwo__wrapper');
-    mainWrapper.classList.add(checked ? activeWClass : activeWoClass);
-    mainWrapper.classList.remove(checked ? activeWoClass : activeWClass);
-    // Both requestAnimationFrame and setTimeout are required to make sure animClass is added after activeClass.
-    // window.requestAnimationFrame(() => {
-    //   setTimeout(() => {
-    //     mainWrapper.classList.add(checked ? activeAnimWClass : activeAnimWoClass);
-    //     mainWrapper.classList.remove(checked ? activeAnimWoClass : activeAnimWClass);
-    //   }, 0);
-    // });
+
+    // w stands for with, wo stands for without
+    const inGroupAttrVal = checked ? 'w' : 'wo';
+    const outGroupAttrVal = checked ? 'wo' : 'w';
+    if (init) {
+      setActiveRegionClass(outGroupAttrVal, inGroupAttrVal);
+    }
+    const animInitOutEls = !init
+      ? []
+      : Array.from(
+          mainWrapper.querySelectorAll('bolt-animate[group="initial"][out]'),
+        );
+    const animInitEls = !init
+      ? []
+      : Array.from(
+          mainWrapper.querySelectorAll('bolt-animate[group="initial"][in]'),
+        );
+    const animInEls = Array.from(
+      mainWrapper.querySelectorAll(
+        `bolt-animate[group="${inGroupAttrVal}"][in]`,
+      ),
+    );
+    const animOutEls = init
+      ? []
+      : Array.from(
+          mainWrapper.querySelectorAll(
+            `bolt-animate[group="${outGroupAttrVal}"][out]`,
+          ),
+        );
+
+    if (init) {
+      triggerAnims({ animEls: animInitOutEls, stage: 'OUT' });
+
+      await triggerAnims({ animEls: animInitEls, stage: 'IN' });
+    } else {
+      console.log('animout', animOutEls);
+      triggerAnims({ animEls: animOutEls, stage: 'OUT' });
+      setActiveRegionClass(outGroupAttrVal, inGroupAttrVal);
+    }
+    // TODO set up phpstorm debugger to figure out why this is not working
+    console.log('animInEls', animInEls);
+    triggerAnims({ animEls: animInEls, stage: 'IN' });
   }, 0);
 };
 
 const toggleInputClass = '#c-pega-wwo__toggle-input';
 SimpleSwitch.init();
-setActiveRegion(document.querySelector(toggleInputClass).checked);
+handleActiveRegionChange(document.querySelector(toggleInputClass).checked, true);
 document
   .querySelector('#c-pega-wwo__toggle-input')
   .addEventListener('change', e => {
-    setActiveRegion(e.target.checked);
+    handleActiveRegionChange(e.target.checked);
   });
