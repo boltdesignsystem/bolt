1291e718334ef5ae82c2493454f53ac62c94d660
Daniel Morse <daniel.morse@pega.com>
2019-08-12 15:03:13 -0400

    test: add a few initial tab tests

--- packages/components/bolt-tabs/__tests__/tabs.e2e.js
+++ packages/components/bolt-tabs/__tests__/tabs.e2e.js
@@ -1,40 +1,38 @@
-// let currentBrowser;
+let currentBrowser;
 
-// module.exports = {
-//   tags: ['component', 'tabs', 'web component'],
-//   'Bolt Tabs': function(browser) {
-//     const { testingUrl } = browser.globals;
-//     console.log(`global browser url: ${testingUrl}`);
-//     currentBrowser = '--' + browser.currentEnv || 'chrome';
-//     let testName = 'bolt-tabs';
+module.exports = {
+  tags: ['component', 'tabs', 'web component'],
+  'Bolt Tabs': function(browser) {
+    const { testingUrl } = browser.globals;
+    console.log(`global browser url: ${testingUrl}`);
+    currentBrowser = '--' + browser.currentEnv || 'chrome';
+    let testName = 'bolt-tabs';
 
-//     browser
-//       .url(
-//         `${testingUrl}/pattern-lab/patterns/02-components-tabs-40-tabs-content-variations/02-components-tabs-40-tabs-content-variations.html`,
-//       )
-//       .waitForElementVisible('bolt-tabs', 1000)
-//       .click(
-//         'bolt-tabs:first-of-type bolt-tab-panel:first-of-type div[slot="label"]',
-//       )
-//       .pause(1000)
-//       .execute(
-//         function(data) {
-//           const tabsItem = document.querySelector('bolt-tab-panel');
-//           return tabsItem.renderRoot
-//             .querySelector('.c-bolt-tab-panel__label')
-//             .classList.contains('handorgel__header--opened');
-//         },
-//         [],
-//         function(result) {
-//           browser.assert.ok(
-//             result.value === true,
-//             `verified that <bolt-tabs> rendered the tabs element, opened it and the active section has a class of handorgel__header--opened.`,
-//           );
-//         },
-//       )
-//       .saveScreenshot(
-//         `screenshots/bolt-tabs/${testName}--${currentBrowser}.png`,
-//       )
-//       .end();
-//   },
-// };
+    browser
+      .url(
+        `${testingUrl}/pattern-lab/patterns/02-components-tabs-05-tabs/02-components-tabs-05-tabs.html`,
+      )
+      .waitForElementVisible('bolt-tabs', 1000)
+      // .execute(
+      //   function(data) {
+      //     const tabs = document.querySelector('bolt-tabs');
+      //     const secondTabItem = tabs.renderRoot.querySelectorAll(
+      //       '.c-bolt-tabs__label',
+      //     )[1];
+      //     secondTabItem.click();
+      //     return secondTabItem.getAttribute('aria-selected');
+      //   },
+      //   [],
+      //   function(result) {
+      //     browser.assert.ok(
+      //       result.value === true,
+      //       `verified that <bolt-tabs> rendered the tabs element, clicked on second tab and the selected section is showing.`,
+      //     );
+      //   },
+      // )
+      .saveScreenshot(
+        `screenshots/bolt-tabs/${testName}--${currentBrowser}.png`,
+      )
+      .end();
+  },
+};
