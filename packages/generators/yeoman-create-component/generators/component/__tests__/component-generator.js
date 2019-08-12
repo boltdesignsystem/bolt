d2df97a045d703e9fad66606b25009fb82da0d11
Adam Szałapski <adam.szalapski@pega.com>
2019-08-12 16:16:12 +0200

    tests for yeoman generator

--- packages/generators/yeoman-create-component/generators/component/__tests__/component-generator.js
+++ packages/generators/yeoman-create-component/generators/component/__tests__/component-generator.js
@@ -1,34 +1,108 @@
 const shell = require('shelljs');
-const helpers = require('yeoman-test');
-var path = require('path');
+const fs = require('fs');
+const internalTasks = require('@bolt/build-tools/tasks/internal-tasks');
+const dirTree = require('directory-tree');
+
+const dirs = {
+  global:
+    'packages/generators/yeoman-create-component/generators/component/tmp',
+  js: 'test.js',
+  scss: 'test.scss',
+  test: 'index.js',
+  package: 'package.json',
+  readme: 'README.md',
+  schema: 'test.schema.yml',
+  boltrc: '.boltrc.js',
+};
+
+dirs.component = `${dirs.global}/packages/components/bolt-test`;
+dirs.src = `${dirs.component}/src`;
+dirs.testing = `${dirs.component}/__tests__`;
+dirs.patternLab = `${dirs.global}/docs-site/src/pages/pattern-lab/_patterns/02-components/test/00-test-docs.twig`;
 
 describe('Yeoman component generator', () => {
-  test('test the NPM script works', async () => {
-    const result = shell.exec('yarn run cc -N Test -D Test description');
+  beforeAll(async () => {
+    shell.exec(`rm -rf ${dirs.global}`);
+    shell.exec('yarn run cc -N Test -D "Test Description" -T');
+  });
 
-    console.log(result);
-    expect(result.code).toBe(1);
-    expect(result.stderr).toBe(null);
+  afterAll(async () => {
+    await internalTasks.clean(dirs.global);
+  });
 
-    // return (
-    //   helpers
-    //     .run(path.join(__dirname, '../../component'))
-    //     // .inDir(path.join(__dirname, 'tmp'))
-    //     // .withArguments(['-N']); // Mock the arguments
-    //     .withPrompts({
-    //       componentName: 'Test',
-    //       name: 'Test',
-    //       description: 'Test desc',
-    //     })
-    //     .withLocalConfig({ name: 'test' })
-    // );
+  test('pattern lab file exist', async () => {
+    const results = fs.readFileSync(dirs.patternLab, 'utf8');
 
-    // .withOptions({ name: 'Test' }) // Mock options passed in
-    // .withArguments(['name-x']) // Mock the arguments
-    // .withPrompts({ coffee: false }) // Mock the prompt answers
-    // .withLocalConfig({ lang: 'en' }) // Mock the local config
-    // .then(function() {
-    //   // assert something about the generator
-    // });
+    expect(results).toMatchSnapshot();
+  });
+
+  test('JS file exist', async () => {
+    const results = fs.readFileSync(`${dirs.src}/${dirs.js}`, 'utf8');
+
+    expect(results).toMatchSnapshot();
+  });
+
+  test('SCSS file exist', async () => {
+    const results = fs.readFileSync(`${dirs.src}/${dirs.scss}`, 'utf8');
+
+    expect(results).toMatchSnapshot();
+  });
+
+  test('tests file exist', async () => {
+    const results = fs.readFileSync(`${dirs.testing}/${dirs.test}`, 'utf8');
+
+    expect(results).toMatchSnapshot();
+  });
+
+  test('index JS file exist', async () => {
+    const results = fs.readFileSync(`${dirs.component}/index.js`, 'utf8');
+
+    expect(results).toMatchSnapshot();
+  });
+
+  test('index SCSS file exist', async () => {
+    const results = fs.readFileSync(`${dirs.component}/index.scss`, 'utf8');
+
+    expect(results).toMatchSnapshot();
+  });
+
+  test('package.json file exist', async () => {
+    const results = fs.readFileSync(
+      `${dirs.component}/${dirs.package}`,
+      'utf8',
+    );
+
+    expect(results).toMatchSnapshot();
+  });
+
+  test('readme file exist', async () => {
+    const results = fs.readFileSync(`${dirs.component}/${dirs.readme}`, 'utf8');
+
+    expect(results).toMatchSnapshot();
+  });
+
+  test('schema file exist', async () => {
+    const results = fs.readFileSync(`${dirs.component}/${dirs.schema}`, 'utf8');
+
+    expect(results).toMatchSnapshot();
+  });
+
+  test('bolt package.json file is updated', async () => {
+    const results = fs.readFileSync(`${dirs.global}/${dirs.package}`, 'utf8');
+
+    expect(results.includes('@bolt/components-test')).toBe(true);
+  });
+
+  test('.boltrc.js is updated', async () => {
+    const results = fs.readFileSync(`${dirs.global}/${dirs.boltrc}`, 'utf8');
+
+    expect(results.includes('@bolt/components-test')).toBe(true);
+  });
+
+  test('folder structure of component package is correct', async () => {
+    const results = dirTree(dirs.global);
+    const stringifyResult = JSON.stringify(results);
+
+    expect(stringifyResult).toMatchSnapshot();
   });
 });
