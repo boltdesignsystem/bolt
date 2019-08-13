9be6494689da1ebddf89b865498c6768aa9e7574
Jeremy John <jmjohn@riseup.net>
2019-08-13 11:59:11 -0500

    fix(with-without): fixing content overflow

--- packages/components/bolt-with-without/js/handleResize.js
+++ packages/components/bolt-with-without/js/handleResize.js
@@ -4,18 +4,27 @@ const handleResize = () => {
   const slideContentInner = Array.from(
     document.querySelectorAll('.c-pega-wwo__content--inner'),
   );
+  const slidesContainer = document.querySelector(
+    '.c-pega-wwo__swiper-container-wrapper',
+  );
 
   if (isMobile) {
     const toggleRegionHeight = window.getComputedStyle(
       document.querySelector('.c-pega-wwo__title-and-toggle--wrapper'),
     ).height;
+    const contentHeight = window.getComputedStyle(
+      document.querySelector('.c-pega-wwo__content'),
+    ).height;
     slideContentInner.forEach(el => {
       el.style.paddingTop = toggleRegionHeight;
     });
+    slidesContainer.style.height = `${parseInt(contentHeight, 10) +
+      parseInt(toggleRegionHeight, 10)}px`;
   } else {
     slideContentInner.forEach(el => {
       el.style.paddingTop = 0;
     });
+    slidesContainer.style.height = '100%';
   }
 };
 
