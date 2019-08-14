ca3abfb429699ade6975a05f1d31e6b512b77e32
Jeremy John <jmjohn@riseup.net>
2019-08-14 10:31:30 -0500

    feat(bolt-animate): add debug flag for determining animation sequencing

--- packages/components/bolt-animate/utils.js
+++ packages/components/bolt-animate/utils.js
@@ -2,9 +2,11 @@
  * @param {Object} opt
  * @param {BoltAnimate[]} opt.animEls
  * @param {string} [opt.stage='IN']
+ * @param {boolean} [opt.debug=false]
+ *
  * @return {Promise<{ success: boolean, animEl: BoltAnimate }[]>}
  */
-async function triggerAnimOnEls({ animEls, stage }) {
+async function triggerAnimOnEls({ animEls, stage, debug = false }) {
   let eventName = '';
   switch (stage) {
     case 'IN':
@@ -44,6 +46,9 @@ async function triggerAnimOnEls({ animEls, stage }) {
               triggered = animEl.triggerAnimOut();
               break;
           }
+          if (debug) {
+            console.debug(`triggering:animation:${stage}`, animEl);
+          }
           if (!triggered) {
             console.error({ animEl });
             reject(
@@ -65,8 +70,9 @@ async function triggerAnimOnEls({ animEls, stage }) {
  * @param {Object} opt
  * @param {BoltAnimate[]} opt.animEls
  * @param {string} [opt.stage='IN']
+ * @param {boolean=} [opt.debug=false]
  */
-export async function triggerAnims({ animEls, stage = 'IN' }) {
+export async function triggerAnims({ animEls, stage = 'IN', debug = false }) {
   let orderProp;
   let hasAnimProp;
   switch (stage) {
@@ -90,11 +96,15 @@ export async function triggerAnims({ animEls, stage = 'IN' }) {
   const animOrders = [...orders].sort((a, b) => a - b);
 
   for (const order of animOrders) {
+    const animElsToTrigger = animEls
+      .filter(a => a[hasAnimProp])
+      .filter(a => a[orderProp] === order);
+    if (debug) {
+      console.debug(`triggering:animations:order:${order} `, animElsToTrigger);
+    }
     // eslint-disable-next-line no-await-in-loop
     await triggerAnimOnEls({
-      animEls: animEls
-        .filter(a => a[hasAnimProp])
-        .filter(a => a[orderProp] === order),
+      animEls: animElsToTrigger,
       stage,
     });
   }
