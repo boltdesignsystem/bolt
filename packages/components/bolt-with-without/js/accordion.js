0df25a0ef3912886096d4340316f9c7c4e020382
Jeremy John <jmjohn@riseup.net>
2019-08-19 17:10:45 -0500

    style(with-without): linter fixes

--- packages/components/bolt-with-without/js/accordion.js
+++ packages/components/bolt-with-without/js/accordion.js
@@ -22,7 +22,8 @@ const handleBlockTitleMobileAccordionClick = async e => {
   }
 
   // @TODO remove tight coupling between markup structure and this.
-  const parentBlock = e.target.parentElement.parentElement.parentElement.parentElement;
+  const parentBlock =
+    e.target.parentElement.parentElement.parentElement.parentElement;
   const targetIsExpanded = parentBlock.classList.contains(expandedClass);
   if (targetIsExpanded) {
     parentBlock.classList.remove(expandedClass);
