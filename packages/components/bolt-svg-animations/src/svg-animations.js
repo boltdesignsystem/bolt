6eaafd3f5005293073570801e30929827ba731c5
Jeremy John <jmjohn@riseup.net>
2019-08-22 16:39:16 -0500

    fix(svg-animations): moving to lithtml

--- packages/components/bolt-svg-animations/src/svg-animations.js
+++ packages/components/bolt-svg-animations/src/svg-animations.js
@@ -1,16 +1,19 @@
 import { props, define, hasNativeShadowDomSupport } from '@bolt/core/utils';
-import { withPreact, h } from '@bolt/core/renderers';
-import * as SVG from './svg';
+import { classMap } from 'lit-html/directives/class-map.js';
+import { withLitHtml, html } from '@bolt/core/renderers/renderer-lit-html';
+import * as SVGs from './svg';
 import schema from '../svg-animations.schema.yml';
+import styles from './svg-animations.scss';
 
 @define
-class SVGAnimations extends withPreact() {
+class SVGAnimations extends withLitHtml() {
   static is = 'bolt-svg-animations';
 
   static props = {
     speed: props.integer,
     animType: props.string,
     theme: props.string,
+    content: props.string,
   };
 
   constructor(self) {
@@ -20,17 +23,43 @@ class SVGAnimations extends withPreact() {
     return self;
   }
 
+  rendered() {
+    // This is an evil hack because lit-html replaces `image` tag with `img`.
+    const animType = this.getAttribute('animType');
+    if (animType === 'Radar') {
+      const theme = this.getAttribute('theme');
+      const SVGTag = SVGs[`${animType}`];
+      if (theme === 'dark') {
+        this.renderRoot.getElementById('el_tbKqO34v2S').innerHTML =
+          SVGTag.darkImage;
+      } else {
+        this.renderRoot.getElementById('el_tbKqO34v2S').innerHTML =
+          SVGTag.lightImage;
+      }
+    }
+  }
+
   render() {
+    const classes = {
+      'c-bolt-svg-animations': true,
+    };
     const animType = this.getAttribute('animType');
     const speed = this.getAttribute('speed');
     const theme = this.getAttribute('theme');
-    const SVGTag = SVG[`${animType}`];
+    const SVGTag = SVGs[`${animType}`];
 
-    return (
-      <div>
-        <SVGTag speed={speed} theme={theme} />
+    console.log(this.slot('default'));
+    console.log(`${animType}`, SVGTag({ speed, theme }));
+
+    return html`
+      ${this.addStyles([styles])}
+      <div class="${classMap(classes)}" is="shadow-root">
+        <div class="c-bolt-svg-animations__content">
+          ${this.slot('default')}
+        </div>
+        ${SVGTag({ speed, theme })}
       </div>
-    );
+    `;
   }
 }
 
