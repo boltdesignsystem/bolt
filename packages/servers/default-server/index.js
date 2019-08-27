de3106e74fbc05c0f3656ae87eb92e33fc53f41a
Adam Szałapski <adam.szalapski@pega.com>
2019-08-27 11:51:31 +0200

    feat: add integrations to default-server and update yarn.lock

--- packages/servers/default-server/index.js
+++ packages/servers/default-server/index.js
@@ -1,10 +1,17 @@
 const { getConfig } = require('@bolt/build-tools/utils/config-store.js');
 const { handleRequest } = require('@bolt/api');
+const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 3123;
+
+const getDirectories = source =>
+  fs
+    .readdirSync(source, { withFileTypes: true })
+    .filter(dir => dir.isDirectory())
+    .map(dir => dir.name);
 
 async function init() {
   const config = await getConfig();
@@ -41,6 +48,31 @@ async function init() {
   app.use('/api', handleRequest);
 
   app.use(express.static(path.relative(process.cwd(), config.wwwDir)));
+
+  if (fs.existsSync(`/integrations`)) {
+    const integrationDirs = getDirectories('/integrations');
+
+    integrationDirs.map(item => {
+      app.use(express.static(`/integrations/${item}`));
+    });
+
+    app.get(['/integrations'], (req, res) => {
+      const generateList = integrationDirs.map(
+        item => `<li><a href="${item}">${item}</a></li>`,
+      );
+
+      res.send(`
+          <h2>Integrations:</h2>
+          <ul>
+            ${generateList}
+          </ul>  
+        `);
+    });
+
+    app.get(['/integrations/drupal-lab'], (req, res) => {
+      res.sendFile('index.html');
+    });
+  }
 
   app.listen(port, () => {
     console.log(`Express listening on http://localhost:${port}`);
