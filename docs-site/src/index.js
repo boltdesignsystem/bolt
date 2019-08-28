baedf7aa2fcd17490b1594f3664067ec3f86f684
Jeremy John <jmjohn@riseup.net>
2019-08-28 15:04:57 -0500

    fix(bolt-animate): move demo JS to separate file so it gets transpiled

--- docs-site/src/index.js
+++ docs-site/src/index.js
@@ -7,29 +7,12 @@ import './pages/pattern-lab/_patterns/01-visual-styles/30-animations';
 // import './pages/pattern-lab/_patterns/01-styleguide/100-rendering-performance/bolt-lit-test';
 // import './pages/pattern-lab/_patterns/01-styleguide/100-rendering-performance/lazy-lit-test';
 import './pages/pattern-lab/_patterns/04-pages/99999-bolt-dev-sandbox/editor-integration';
-import { triggerAnims } from '@bolt/components-animate/utils';
+import './pages/pattern-lab/_patterns/02-components/animate/animate-demo-setup.js';
 
-[...document.getElementsByClassName('demo-animate')].forEach(animDemo => {
-  let triggeredIn = false;
-  const trigger = animDemo.querySelector('.demo-animate__trigger');
-  const wrapper = animDemo.querySelector('.demo-animate__wrapper');
-  const getAnimEls = () => wrapper.getElementsByTagName('bolt-animate');
-  // const animEls = Array.from(getAnimEls());
-  // const animEls = Array.from(animDemo.querySelectorAll('bolt-animate'));
-
-  trigger.addEventListener('click', () => {
-    // const animEls = getAnimEls();
-    const animEls = Array.from(getAnimEls());
-    console.log(animEls.length, animEls);
-    const stage = triggeredIn ? 'OUT' : 'IN';
-    console.log({ stage });
-    triggerAnims({ animEls, stage }).then(() => {
-      console.log('all done');
-    });
-    trigger.innerText = `Trigger Animations ${triggeredIn ? 'IN' : 'OUT'}`;
-    triggeredIn = !triggeredIn;
-  });
-});
+(function() {
+  if (typeof NodeList.prototype.forEach === 'function') return false;
+  NodeList.prototype.forEach = Array.prototype.forEach;
+})();
 
 // here if you need pl only JS
 // document.addEventListener('DOMContentLoaded', () => {
