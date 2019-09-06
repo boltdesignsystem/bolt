08e99e21fa4cac2a337296d1f0d6d743dbe2a5f7
Joe Karasek <joseph.karasek@gmail.com>
2019-09-05 21:39:40 -0700

    feat: add lorem impsum starters for the step top and bottom

--- packages/micro-journeys/starters/index.js
+++ packages/micro-journeys/starters/index.js
@@ -1,15 +1,19 @@
+import ctaTextLorem from './cta-text-lorem.html';
 import ctaText1 from './cta-text-1.html';
 import ctaText2 from './cta-text-2.html';
 import ctaText3 from './cta-text-3.html';
 import ctaText4 from './cta-text-4.html';
 import ctaText5 from './cta-text-5.html';
+import twoCharactersLorem from './two-characters-lorem.html';
 import twoCharacters1 from './two-characters-1.html';
 import twoCharacters2 from './two-characters-2.html';
 import twoCharacters3 from './two-characters-3.html';
 import twoCharacters4 from './two-characters-4.html';
+import oneCharacterLorem from './one-character-lorem.html';
 import oneCharacter1 from './one-character-1.html';
 
 export {
+  ctaTextLorem,
   ctaText1,
   ctaText2,
   ctaText3,
@@ -19,5 +23,7 @@ export {
   twoCharacters2,
   twoCharacters3,
   twoCharacters4,
+  twoCharactersLorem,
   oneCharacter1,
+  oneCharacterLorem,
 };
