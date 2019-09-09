1fff576756306eafe60ab32b6085f38255fe0ea4
Evan Lovely <evanlovely@gmail.com>
2019-09-09 13:52:10 -0700

    fix(animate): animate demo JS

--- docs-site/src/pages/pattern-lab/_patterns/06-experiments/animate/animate-demo-helpers.js
+++ docs-site/src/pages/pattern-lab/_patterns/06-experiments/animate/animate-demo-helpers.js
@@ -1,25 +1,24 @@
 import { triggerAnims } from '@bolt/components-animate/utils';
 
 export function enableAnimDemos() {
-  [...document.getElementsByClassName('demo-animate')].forEach(animDemo => {
-    let triggeredIn = false;
-    const trigger = animDemo.querySelector('.demo-animate__trigger');
-    const wrapper = animDemo.querySelector('.demo-animate__wrapper');
-    const getAnimEls = () => wrapper.getElementsByTagName('bolt-animate');
-    // const animEls = Array.from(getAnimEls());
-    // const animEls = Array.from(animDemo.querySelectorAll('bolt-animate'));
+  setTimeout(() => {
+    [...document.getElementsByClassName('demo-animate')].forEach(animDemo => {
+      let triggeredIn = false;
+      const trigger = animDemo.querySelector('.demo-animate__trigger');
+      const wrapper = animDemo.querySelector('.demo-animate__wrapper');
+      const getAnimEls = () => wrapper.getElementsByTagName('bolt-animate');
 
-    trigger.addEventListener('click', () => {
-      // const animEls = getAnimEls();
-      const animEls = Array.from(getAnimEls());
-      console.log(animEls.length, animEls);
-      const stage = triggeredIn ? 'OUT' : 'IN';
-      console.log({ stage });
-      triggerAnims({ animEls, stage }).then(() => {
-        console.log('all done');
+      trigger.addEventListener('click', () => {
+        const animEls = Array.from(getAnimEls());
+        // console.log(animEls.length, animEls);
+        const stage = triggeredIn ? 'OUT' : 'IN';
+        // console.log({ stage });
+        triggerAnims({ animEls, stage }).then(() => {
+          // console.log('all done');
+        });
+        trigger.innerText = `Trigger  Animations ${triggeredIn ? 'IN' : 'OUT'}`;
+        triggeredIn = !triggeredIn;
       });
-      trigger.innerText = `Trigger Animations ${triggeredIn ? 'IN' : 'OUT'}`;
-      triggeredIn = !triggeredIn;
     });
-  });
+  }, 0);
 }
