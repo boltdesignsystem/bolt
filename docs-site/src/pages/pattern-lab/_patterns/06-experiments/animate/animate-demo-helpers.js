dbe6a75fcc481cc3e934e72c67defdded3f0ad60
Evan Lovely <evanlovely@gmail.com>
2019-09-11 15:25:53 -0700

    fix: another demo animation trigger helper fix

--- docs-site/src/pages/pattern-lab/_patterns/06-experiments/animate/animate-demo-helpers.js
+++ docs-site/src/pages/pattern-lab/_patterns/06-experiments/animate/animate-demo-helpers.js
@@ -1,20 +1,26 @@
 import { triggerAnims } from '@bolt/components-animate/utils';
 
 export function enableAnimDemos() {
+  function setTriggerText({ trigger, totalAnims, stage }) {
+    trigger.innerText = `Trigger ${totalAnims} Animations ${stage}`;
+  }
+
   setTimeout(() => {
     document.querySelectorAll('.demo-animate').forEach(animDemo => {
       let triggeredIn = false;
       const trigger = animDemo.querySelector('.demo-animate__trigger');
       const wrapper = animDemo.querySelector('.demo-animate__wrapper');
-      const animEls = Array.from(animDemo.querySelectorAll('bolt-animate'));
+      let animEls = Array.from(animDemo.querySelectorAll('bolt-animate'));
+      setTriggerText({ trigger, stage: 'IN', totalAnims: animEls.length });
 
       trigger.addEventListener('click', () => {
+        animEls = Array.from(animDemo.querySelectorAll('bolt-animate'));
         const stage = triggeredIn ? 'OUT' : 'IN';
         triggerAnims({ animEls, stage, debug: false }).then(() => {
           // console.log('all done');
         });
-        trigger.innerText = `Trigger  Animations ${triggeredIn ? 'IN' : 'OUT'}`;
         triggeredIn = !triggeredIn;
+        setTriggerText({ trigger, stage, totalAnims: animEls.length });
       });
     });
   }, 0);
