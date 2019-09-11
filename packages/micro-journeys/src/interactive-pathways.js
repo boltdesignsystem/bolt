ceadc0c18cbdacae2a9e5937f01201a08e360fae
Joe Karasek <joseph.karasek@gmail.com>
2019-09-11 16:23:43 -0700

    feat: pathways dropdown closes with a new pathway is selected

--- packages/micro-journeys/src/interactive-pathways.js
+++ packages/micro-journeys/src/interactive-pathways.js
@@ -117,10 +117,27 @@ class BoltInteractivePathways extends withLitHtml() {
           @click=${() => {
             this.showPathway(i);
             setTimeout(() => {
-              this.renderRoot
-                .querySelector('bolt-micro-journeys-dropdown')
-                .renderRoot.querySelector('button')
-                .click();
+              try {
+                let boltMicroJourneyDropdown;
+                let boltMicroJourneyDropdownButton;
+                boltMicroJourneyDropdown = this.renderRoot
+                  ? this.renderRoot.querySelector(
+                      'bolt-micro-journeys-dropdown',
+                    )
+                  : null;
+                if (boltMicroJourneyDropdown) {
+                  boltMicroJourneyDropdownButton = boltMicroJourneyDropdown.renderRoot
+                    ? boltMicroJourneyDropdown.renderRoot.querySelector(
+                        'button',
+                      )
+                    : null;
+                }
+                if (boltMicroJourneyDropdownButton) {
+                  boltMicroJourneyDropdownButton.click();
+                }
+              } catch {
+                console.error('Autoclose of micro-journey dropdown failed');
+              }
             });
           }}
           style=${menuItemTextColor}
