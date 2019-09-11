ff2a74cc085dd72c626357b9ae31f19797e77601
Salem Ghoweri <me@salemghoweri.com>
2019-09-10 21:37:03 -0400

    fix: test fixing Travis build

--- packages/analytics/autolink/__tests__/autolink.test.js
+++ packages/analytics/autolink/__tests__/autolink.test.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const timeout = 90000;
+const timeout = 120000;
 
 describe('analytics autolinker', () => {
   let page;
@@ -8,7 +8,7 @@ describe('analytics autolinker', () => {
   beforeEach(async () => {
     page = await global.__BROWSER__.newPage();
     await page.goto('http://127.0.0.1:4444/', {
-      waitUntil: 'networkidle0',
+      // waitUntil: 'networkidle0',
       timeout: 0,
     });
 
