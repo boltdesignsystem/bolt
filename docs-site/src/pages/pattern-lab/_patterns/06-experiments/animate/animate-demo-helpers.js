26accecd40dddbec08dfca73bac3d5a76f328c90
Evan Lovely <evanlovely@gmail.com>
2019-09-11 13:18:29 -0700

    fix: anim demo helper script

--- docs-site/src/pages/pattern-lab/_patterns/06-experiments/animate/animate-demo-helpers.js
+++ docs-site/src/pages/pattern-lab/_patterns/06-experiments/animate/animate-demo-helpers.js
@@ -2,18 +2,15 @@ import { triggerAnims } from '@bolt/components-animate/utils';
 
 export function enableAnimDemos() {
   setTimeout(() => {
-    [...document.getElementsByClassName('demo-animate')].forEach(animDemo => {
+    document.querySelectorAll('.demo-animate').forEach(animDemo => {
       let triggeredIn = false;
       const trigger = animDemo.querySelector('.demo-animate__trigger');
       const wrapper = animDemo.querySelector('.demo-animate__wrapper');
-      const getAnimEls = () => wrapper.getElementsByTagName('bolt-animate');
+      const animEls = Array.from(animDemo.querySelectorAll('bolt-animate'));
 
       trigger.addEventListener('click', () => {
-        const animEls = Array.from(getAnimEls());
-        // console.log(animEls.length, animEls);
         const stage = triggeredIn ? 'OUT' : 'IN';
-        // console.log({ stage });
-        triggerAnims({ animEls, stage }).then(() => {
+        triggerAnims({ animEls, stage, debug: false }).then(() => {
           // console.log('all done');
         });
         trigger.innerText = `Trigger  Animations ${triggeredIn ? 'IN' : 'OUT'}`;
