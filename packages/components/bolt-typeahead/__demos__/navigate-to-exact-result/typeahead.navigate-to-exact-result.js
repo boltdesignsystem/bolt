76e79b4866cff46b11f72a09bd51062a7c5489b8
Salem Ghoweri <me@salemghoweri.com>
2019-09-12 09:04:37 -0400

    refactor: move demos into component itself + consolidate demo-specific code in PL

---
+++ packages/components/bolt-typeahead/__demos__/navigate-to-exact-result/typeahead.navigate-to-exact-result.js
@@ -0,0 +1,54 @@
+// NOTE: make sure you're running this code through a tool like Babel before shipping for cross browser compatibility!
+const typeaheadDemo = document.querySelector(
+  '.js-typeahead-hook--exact-result',
+);
+
+const typeaheadDemoItems = [
+  {
+    label: 'AI and improving the customer experience',
+    description:
+      '“Artificial intelligence” (AI) presents both distracting hype and powerful opportunities to drive customer engagement.',
+    url: 'https://www.pega.com/ai-and-improving-customer-experience',
+  },
+  {
+    label:
+      'Gartner Magic Quadrant for Enterprise Low-Code Application Platforms 2019',
+    description:
+      'Pega was cited as a Visionary in Gartner’s new 2019 Magic Quadrant for Enterprise Low-Code Application Platforms.',
+    url:
+      'https://www.pega.com/insights/resources/gartner-magic-quadrant-enterprise-low-code-application-platforms-2019',
+  },
+];
+
+typeaheadDemo.addEventListener('ready', function(e) {
+  if (e.detail.name === 'bolt-typeahead') {
+    typeaheadDemo.items = typeaheadDemoItems;
+
+    typeaheadDemo.on('onSelected', (element, event, suggestion) => {
+      const exactMatch = element.items.filter(
+        item => item.label === suggestion.suggestionValue,
+      )[0];
+
+      function navigateTo(url) {
+        if (window.location !== window.parent.location) {
+          const win = window.open(url, '_blank');
+          win.focus();
+        } else {
+          window.location = url;
+        }
+      }
+
+      if (exactMatch && exactMatch.url) {
+        if (exactMatch.url) {
+          navigateTo(exactMatch.url);
+        } else {
+          navigateTo(`https://www.pega.com/search?q=${itemSelected.label}`);
+        }
+      } else if (suggestion.suggestionValue !== '') {
+        navigateTo(
+          `https://www.pega.com/search?q=${suggestion.suggestionValue}`,
+        );
+      }
+    });
+  }
+});
---
+++ packages/components/bolt-typeahead/__demos__/navigate-to-search-results/typeahead.navigate-to-search-results.md
@@ -0,0 +1,7 @@
+# Typeahead Demo: Navigate To Search Results {id=navigate-to-search-results}
+
+Demos populating the `<bolt-typeahead>` component with a larger batch of results + customizing the `onSelected` behavior to always go to the search results page when selecting an item.
+
+Unlike the other demo, only perfect matches will allow you to submit (via the submit button) / select a result.
+
+For example, manually selecting / entering `Case Management` in the input field will allow you to submit via hitting enter / clicking on the search icon however entering in `Case Management2` instead would _not_ submit.
