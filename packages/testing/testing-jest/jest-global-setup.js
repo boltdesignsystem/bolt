2bee4961a316d97d12be6c290d1fe80a0cccec5b
Salem Ghoweri <me@salemghoweri.com>
2019-09-13 17:17:04 -0400

    test: increase Jest server timeout

--- packages/testing/testing-jest/jest-global-setup.js
+++ packages/testing/testing-jest/jest-global-setup.js
@@ -30,7 +30,7 @@ module.exports = async function globalSetup() {
 
   await setupDevServer({
     command: `node packages/servers/testing-server`,
-    launchTimeout: 120000,
+    launchTimeout: 300000,
     port: 4444,
     usedPortAction: 'kill',
   });
