698aa65854c68e9ce8c5cfc5c21920d2305b1f66
Jeremy John <jmjohn@riseup.net>
2019-09-17 12:41:30 -0500

    fix(with-without): whoops, or instead of and caused resize not to fire :)

--- packages/with-without/js/handleResize.js
+++ packages/with-without/js/handleResize.js
@@ -9,14 +9,17 @@ const handleResize = () => {
     window.innerHeight && window.innerHeight !== lastWindowHeight;
   const widthHasChanged =
     window.innerWidth && window.innerWidth !== lastWindowWidth;
-  if (!widthHasChanged || !heightHasChanged) {
+  if (!widthHasChanged && !heightHasChanged) {
     return; // bail if no change.
   }
   lastWindowHeight = window.innerHeight;
   lastWindowWidth = window.innerWidth;
 
   // @TODO replace with theme token.
-  const isMobile = window.matchMedia('(max-width: 1200px)').matches;
+  const isMobile = window.matchMedia('(max-width: 800px)').matches;
+  const isDesktop = window.matchMedia('(min-width: 1201px)').matches;
+  const isTablet = !isDesktop && !isMobile;
+
   const slideContentInner = Array.from(
     document.querySelectorAll('.c-pega-wwo__content--inner'),
   );
@@ -29,7 +32,7 @@ const handleResize = () => {
     : 'w';
   triggerAnimateInOnInOnlyContent(activeSlideString);
 
-  if (isMobile) {
+  if (isTablet || isMobile) {
     const toggleRegionHeight = window.getComputedStyle(
       document.querySelector('.c-pega-wwo__title-and-toggle--wrapper'),
     ).height;
