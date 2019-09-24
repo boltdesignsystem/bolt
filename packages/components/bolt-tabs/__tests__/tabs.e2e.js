656aa33b73df73626332c100a0075769ff7f3ea3
Daniel Morse <daniel.morse@pega.com>
2019-09-24 15:42:38 -0400

    test: add jest and e2e tests for Tabs

--- packages/components/bolt-tabs/__tests__/tabs.e2e.js
+++ packages/components/bolt-tabs/__tests__/tabs.e2e.js
@@ -2,36 +2,50 @@ let currentBrowser;
 
 module.exports = {
   tags: ['component', 'tabs', 'web component'],
-  'Bolt Tabs': function(browser) {
+  'Tabs: tab opened': function(browser) {
     const { testingUrl } = browser.globals;
     console.log(`global browser url: ${testingUrl}`);
     currentBrowser = '--' + browser.currentEnv || 'chrome';
-    let testName = 'bolt-tabs';
+    let testName = 'tabs-tab-opened';
 
     browser
       .url(
         `${testingUrl}/pattern-lab/patterns/02-components-tabs-05-tabs/02-components-tabs-05-tabs.html`,
       )
       .waitForElementVisible('bolt-tabs', 1000)
-      // .execute(
-      //   function(data) {
-      //     const tabs = document.querySelector('bolt-tabs');
-      //     const secondTabItem = tabs.renderRoot.querySelectorAll(
-      //       '.c-bolt-tabs__label',
-      //     )[1];
-      //     secondTabItem.click();
-      //     return secondTabItem.getAttribute('aria-selected');
-      //   },
-      //   [],
-      //   function(result) {
-      //     browser.assert.ok(
-      //       result.value === true,
-      //       `verified that <bolt-tabs> rendered the tabs element, clicked on second tab and the selected section is showing.`,
-      //     );
-      //   },
-      // )
+      .execute(function(data) {
+        return document
+          .querySelector('bolt-tabs')
+          .renderRoot.querySelectorAll('.c-bolt-tabs__label')[1]
+          .click();
+      })
       .saveScreenshot(
         `screenshots/bolt-tabs/${testName}--${currentBrowser}.png`,
+      )
+      .execute(
+        function(data) {
+          const tabs = document.querySelector('bolt-tabs');
+          const selectedTab = tabs.renderRoot.querySelectorAll(
+            '.c-bolt-tabs__label',
+          )[1];
+          const selectedPanel = tabs.querySelectorAll('bolt-tab-panel')[1];
+          const tabIsSelected =
+            selectedTab.getAttribute('aria-selected') === 'true';
+          const panelIsSelected = selectedPanel.hasAttribute('selected');
+
+          return { tabIsSelected, panelIsSelected };
+        },
+        [],
+        function(result) {
+          browser.assert.ok(
+            result.value.tabIsSelected === true,
+            `Clicked on second tab and verified the second tab label has the attribute 'aria-selected="true"'`,
+          );
+          browser.assert.ok(
+            result.value.panelIsSelected === true,
+            `Clicked on second tab and verified the second tab panel has the attribute 'selected'`,
+          );
+        },
       )
       .end();
   },
