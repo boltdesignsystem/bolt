f81b4d629b7937e0d3c525132cdcfd05ab0c0d2f
Daniel Morse <daniel.morse@pega.com>
2019-09-30 14:21:33 -0400

    test: rework accordion tests to use 'assert' and light DOM instead of 'execute' and shadow DOM

--- packages/components/bolt-accordion/__tests__/accordion.e2e.js
+++ packages/components/bolt-accordion/__tests__/accordion.e2e.js
@@ -7,34 +7,18 @@ module.exports = {
     console.log(`global browser url: ${testingUrl}`);
     currentBrowser = '--' + browser.currentEnv || 'chrome';
     let testName = 'accordion-item-opened';
+    const firstTriggerSelector =
+      'bolt-accordion:first-of-type .c-bolt-accordion-item__trigger';
 
     browser
       .url(
-        `${testingUrl}/pattern-lab/patterns/02-components-accordion-40-accordion-content-variations/02-components-accordion-40-accordion-content-variations.html`,
+        `${testingUrl}/pattern-lab/patterns/02-components-accordion--45-accordion-no-shadow/02-components-accordion--45-accordion-no-shadow.html`,
       )
       .waitForElementVisible('bolt-accordion', 1000)
-      .click(
-        'bolt-accordion:first-of-type bolt-accordion-item:first-of-type div[slot="trigger"]',
-      )
-      .pause(1000)
-      .execute(
-        function(data) {
-          const accordionItem = document.querySelector('bolt-accordion-item');
-          return accordionItem.renderRoot
-            .querySelector('.c-bolt-accordion-item__trigger')
-            .classList.contains('handorgel__header--opened');
-        },
-        [],
-        function(result) {
-          browser.assert.ok(
-            result.value === true,
-            `Verified that <bolt-accordion> rendered the accordion element, opened it, and the active section has the class 'handorgel__header--opened'`,
-          );
-        },
-      )
-      .saveScreenshot(
-        `screenshots/bolt-accordion/${testName}--${currentBrowser}.png`,
-      )
+      .assert.elementPresent(firstTriggerSelector)
+
+      .click(firstTriggerSelector)
+      .assert.cssClassPresent(firstTriggerSelector, 'handorgel__header--opened')
       .saveScreenshot(
         `screenshots/bolt-accordion/${testName}--${currentBrowser}.png`,
       )
@@ -46,53 +30,24 @@ module.exports = {
     console.log(`global browser url: ${testingUrl}`);
     currentBrowser = '--' + browser.currentEnv || 'chrome';
     let testName = 'accordion-inactive-item-closed';
+    const inactiveTriggerSelector =
+      'bolt-accordion-item[inactive] .c-bolt-accordion-item__trigger';
+    const inactiveLabelSelector =
+      'bolt-accordion-item[inactive] .c-bolt-accordion-item__trigger-label';
 
     browser
       .url(
-        `${testingUrl}/pattern-lab/patterns/02-components-accordion-40-accordion-content-variations/02-components-accordion-40-accordion-content-variations.html`,
+        `${testingUrl}/pattern-lab/patterns/02-components-accordion--45-accordion-no-shadow/02-components-accordion--45-accordion-no-shadow.html`,
       )
       .waitForElementVisible('bolt-accordion', 1000)
-      .click(
-        'bolt-accordion:first-of-type bolt-accordion-item:nth-child(2) div[slot="trigger"]',
+      .assert.elementPresent(inactiveTriggerSelector)
+      .assert.elementPresent(inactiveLabelSelector)
+      .click(inactiveLabelSelector)
+      .assert.cssClassPresent(
+        inactiveTriggerSelector,
+        'handorgel__header--disabled',
       )
-      .pause(1000)
-      .execute(
-        function(data) {
-          const accordionItem = document.querySelector(
-            'bolt-accordion-item:nth-child(2)',
-          );
-          const label = accordionItem.renderRoot.querySelector(
-            '.c-bolt-accordion-item__trigger-label',
-          );
-          const hasInactiveClass = label.classList.contains(
-            'c-bolt-accordion-item__trigger-label--inactive',
-          );
-          // const hasExpandedAttr =
-          //   label.getAttribute('aria-expanded') === 'false';
-          // const isDiv = label.tagName === 'DIV';
-
-          return hasInactiveClass;
-        },
-        [],
-        function(result) {
-          browser.assert.ok(
-            result.value === true,
-            `Clicked on an inactive accordion item and verified the trigger has the inactive class 'c-bolt-accordion-item__trigger-label--inactive'`,
-          );
-          // browser.assert.ok(
-          //   result.value.hasInactiveClass === true,
-          //   `Clicked on an inactive accordion item and verified the trigger has the inactive class 'c-bolt-accordion-item__trigger-label--inactive'`,
-          // );
-          // browser.assert.ok(
-          //   result.value.hasExpandedAttr === true,
-          //   `Clicked on an inactive accordion item and verified the label has the attribute 'aria-expanded="false"'`,
-          // );
-          // browser.assert.ok(
-          //   result.value.isDiv === true,
-          //   `Clicked on an inactive accordion item and verified the label has the tag name 'DIV'`,
-          // );
-        },
-      )
+      .assert.attributeContains(inactiveLabelSelector, 'aria-expanded', 'false')
       .saveScreenshot(
         `screenshots/bolt-accordion/${testName}--${currentBrowser}.png`,
       )
