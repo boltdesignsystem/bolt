636b379860f81ee67aac3bab7545006921895a00
Jeremy John <jmjohn@riseup.net>
2019-10-01 16:25:12 -0500

    feat(with-without): fire animations on intersection; clean up dom querying

--- packages/with-without/js/with-without.js
+++ packages/with-without/js/with-without.js
@@ -6,15 +6,36 @@ import {
   handleActiveRegionChangeRequest,
 } from './handleActiveRegionChange';
 
+const fireInitialAnimations = (toggleInputs, checkedValue, wwoSwiper) => {
+  // Pushed to bottom of call stack b/c w/o shadowdom enabled it breaks if not.
+  // Set up the resize listener which helps with some of the abs. pos. stuff.
+  handleResize()();
+  setTimeout(() => {
+    // Initialize the page.
+    triggerActiveRegionChange(
+      toggleInputs[0].id === checkedValue,
+      wwoSwiper,
+      true,
+    );
+    // Force just the size-setting event to set height properly after dom loaded to fix content overlap bug on tablet.
+    // We want height to be accurate as soon as possible *and* afterward, so this lightweight double-check should stay.
+    handleResize(true, false)();
+  }, 0);
+};
+
 (() => {
-  const toggleInputClass = '.c-pega-www__toggle-input';
-  // short-circuit if no toggler is found.
-  if (!document.querySelector(toggleInputClass)) {
+  const toggleInputsWrapper = document.getElementById('c-pega-wwo__toggle');
+
+  // short-circuit all the things if no toggler is found.
+  if (!toggleInputsWrapper) {
     return;
   }
 
-  // Set up the resize listener which helps with some of the abs. pos. stuff.
-  handleResize()();
+  const toggleInputs = Array.from(
+    document.querySelectorAll('.c-pega-www__toggle-input'),
+  );
+
+  handleResize(true, false)();
   window.addEventListener('resize', handleResize());
 
   // This was moved from a checkbox to a radio.
@@ -28,7 +49,7 @@ import {
   });
 
   // Wire up the toggler to the event region switcher.
-  Array.from(document.querySelectorAll(toggleInputClass)).forEach(el => {
+  toggleInputs.forEach(el => {
     el.addEventListener('change', e => {
       handleActiveRegionChangeRequest(e.target.id === 'w', wwoSwiper);
     });
@@ -46,9 +67,7 @@ import {
       e.target.removeAttribute('anim-in-progress');
       // If the animation state doesn't match the state of the toggle, transition.
       const activeAttr = animControllerEl.getAttribute('active');
-      const checkedRadio = Array.from(
-        document.querySelectorAll(toggleInputClass),
-      ).find(input => input.checked);
+      const checkedRadio = toggleInputs.find(input => input.checked);
       if (checkedRadio.id !== activeAttr) {
         handleActiveRegionChangeRequest(
           checkedRadio.id === checkedValue,
@@ -85,21 +104,25 @@ import {
     });
   }
 
-  // Force just the size-setting event to set height properly after dom loaded to fix content overlap bug on tablet.
-  // We want height to be accurate as soon as possible *and* afterward, so this lightweight double-check should stay.
-  setTimeout(() => {
-    handleResize(true, false)();
-  }, 0);
-
-  // Pushed to bottom of call stack b/c w/o shadowdom enabled it breaks if not.
-  if (document.querySelector(toggleInputClass)) {
-    setTimeout(() => {
-      // Initialize the page.
-      triggerActiveRegionChange(
-        document.querySelector(toggleInputClass).id === checkedValue,
-        wwoSwiper,
-        true,
-      );
-    }, 0);
+  // Only fire initial animations on intersection.
+  if (window.IntersectionObserver) {
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            fireInitialAnimations(toggleInputs, checkedValue, wwoSwiper);
+            observer.unobserve(toggleInputsWrapper);
+          }
+        });
+      },
+      {
+        root: null,
+        rootMargin: '0px 0px 50px 0px',
+        threshold: 1,
+      },
+    );
+    observer.observe(toggleInputsWrapper);
+  } else {
+    fireInitialAnimations(toggleInputs, checkedValue, wwoSwiper);
   }
 })();
