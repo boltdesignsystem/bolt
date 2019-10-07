4a3ca92180f4922a73c99cb27fca65e3080c8805
Jeremy John <jmjohn@riseup.net>
2019-10-07 16:35:48 -0500

    feat(with-without): detect pointer hover and only remove focus if found

--- packages/with-without/js/blurEdgeFocus.js
+++ packages/with-without/js/blurEdgeFocus.js
@@ -1,7 +1,10 @@
 // This is a hack to remove focus on the Edge browser, which draws an unmodifiable outline around checkboxes.
 
 const blurEdgeFocus = () => {
-  if (navigator.userAgent.indexOf('Edge') >= 0) {
+  if (
+    navigator.userAgent.indexOf('Edge') >= 0 &&
+    window.matchMedia('(any-hover: hover)').matches
+  ) {
     document.activeElement.blur();
   }
 };
