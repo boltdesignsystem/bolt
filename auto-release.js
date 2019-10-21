ef3e13cedca3e6986391b15e859eb69b77cd8824
Salem Ghoweri <me@salemghoweri.com>
2019-10-21 14:35:44 -0400

    feat: fully wire up and enable auto releases

--- auto-release.js
+++ auto-release.js
@@ -5,12 +5,26 @@ const isFullRelease = branchName === 'release-2.x';
 const { normalizeUrlAlias } = require('./scripts/utils/normalize-url-alias');
 const { gitSha } = require('./scripts/utils');
 const execSync = require('child_process').execSync;
+const { spawn } = require('child_process');
 const { getLatestDeploy } = require('./scripts/utils');
 const { IncomingWebhook } = require('@slack/webhook');
 const chalk = require('chalk');
+const semver = require('semver');
+
+const lernaConfig = require('./lerna.json');
+const currentVersion = lernaConfig.version;
 
 const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL;
 const SLACK_WEBPACK_URL_CANARY = process.env.SLACK_WEBPACK_URL_CANARY;
+
+// figure out what a canary release version would be
+const canaryVersion = `.${process.env.TRAVIS_PULL_REQUEST_SHA ||
+  process.env.TRAVIS_JOB_NUMBER ||
+  gitSha}`;
+
+// force color output in CLI
+// @ts-ignore
+process.env.FORCE_COLOR = true;
 
 async function getLernaPackages() {
   const packages = shell.exec('npx lerna ls -pl', {
@@ -28,36 +42,27 @@ async function getLernaPackages() {
   return formattedPackages;
 }
 
-async function getIndependentPackageList() {
-  return getLernaPackages().then(packages =>
-    packages.map(p => `\n - \`${p.name}@${p.version.split('+')[0]}\``).join(''),
-  );
-}
-
 async function init() {
   if (isCanaryRelease) {
     try {
       const version = shell
-        .exec('auto version', {
-          silent: true,
-        })
+        .exec('auto version', { silent: true })
         .stdout.trim();
 
-      const canaryVersion = `.${process.env.TRAVIS_PULL_REQUEST_SHA ||
-        process.env.TRAVIS_JOB_NUMBER ||
-        gitSha}`;
-
-      await shell.exec(
+      shell.exec(
         `npx lerna publish pre${version} --dist-tag canary --preid canary${canaryVersion} --no-git-reset --no-git-tag-version --exact --ignore-scripts --no-push --yes -m "[skip travis] chore(release): pre-release %s"`,
+        { async: true },
+        (code, stdout, stderr) => {
+          console.log('', stdout);
+          console.log('', stderr);
+        },
       );
-      console.log('Canary published to NPM.');
+
+      console.log(
+        'Canary release successfully published to NPM. Doing a fresh build + deploying to now.sh.',
+      );
+
       const packages = await getLernaPackages();
-
-      await shell.exec('git reset --hard HEAD').stdout;
-
-      // const independentPackages = await getIndependentPackageList();
-      // console.log(independentPackages);
-
       const versioned = packages.find(p => p.version.includes('canary'));
       if (!versioned) {
         console.log(
@@ -65,6 +70,7 @@ async function init() {
         );
       }
 
+      // get the version we just published
       const canaryReleaseVersion = `v${versioned.version.split('+')[0].trim()}`; // ex. 2.9.0-canary.6b70020b5.0
 
       const branchSpecificUrl = await normalizeUrlAlias(branchName);
@@ -75,18 +81,21 @@ async function init() {
       nowAliases.push(await normalizeUrlAlias('canary'));
       nowAliases.push(tagSpecificUrl);
 
-      console.log(nowAliases);
-
       await shell.exec(`
+        git reset --hard HEAD
         npx json -I -f docs-site/.incache -e 'this["bolt-tags"].expiresOn = "2019-06-14T12:30:26.377Z"'
         npx json -I -f docs-site/.incache -e 'this["bolt-urls-to-test"].expiresOn = "2019-06-14T12:30:26.377Z"'
+        npx now alias boltdesignsystem.com ${tagSpecificUrl}
       `);
 
-      shell.exec(`npx now alias boltdesignsystem.com ${tagSpecificUrl}`);
-
-      execSync('npm run build');
+      // do the full build + output CLI in real time
+      shell.exec('npm run build', { async: true }, (code, stdout, stderr) => {
+        console.log('Exit code:', code);
+        console.log('', stdout);
+        console.log('', stderr);
+      });
       shell.exec(
-        `npx now deploy --meta gitSha="${gitSha}" --token=${process.env.NOW_TOKEN}`,
+        `npx now deploy --meta gitSha='${gitSha}' --token=${process.env.NOW_TOKEN}`,
       );
 
       const latestUrl = await getLatestDeploy();
@@ -94,11 +103,6 @@ async function init() {
       nowAliases.forEach(alias => {
         shell.exec(`npx now alias ${latestUrl} ${alias}`);
       });
-
-      shell.exec(`
-        git tag -d ${canaryReleaseVersion}
-        git push origin :refs/tags/${canaryReleaseVersion}
-      `);
 
       await shell.exec('git reset --hard HEAD').stdout;
 
@@ -116,178 +120,112 @@ async function init() {
           ),
         );
       }
-
-      console.log('Finished publishing canary release!');
-
-      // const urlVersion = urlFriendlyString(newVersion);
-
-      // await shell.exec(`
-      //   npx now alias ${latestUrl} next.boltdesignsystem.com
-      //   npx now alias ${latestUrl} next.boltdesignsystem.com
-      // `);
     } catch (error) {
       console.error(error);
     }
   } else if (isFullRelease) {
-    console.log('full release!');
+    try {
+      const version = shell
+        .exec('auto version', { silent: true })
+        .stdout.trim();
 
-    // publish
-    // alias new version
+      const nextVersion = semver.inc(currentVersion, version);
 
-    // console.log(
-    //   `https://github.com/boltdesignsystem/bolt/releases/tag/v${currentVersion}`,
-    // );
+      shell.exec(`
+        node scripts/release/update-php-package-versions.js -v ${nextVersion}
+        git add packages/core-php/composer.json packages/drupal-modules/bolt_connect/bolt_connect.info.yml packages/drupal-modules/bolt_connect/composer.json
+        git commit -m "[skip travis] chore: version bump PHP-related dependencies to v${nextVersion}"
+      `);
+
+      shell.exec(
+        `npx lerna publish ${nextVersion} --yes -m "[skip travis] chore(release): release %s"`,
+        { async: true },
+        (code, stdout, stderr) => {
+          console.log('', stdout);
+          console.log('', stderr);
+        },
+      );
+
+      console.log(
+        'Bolt release successfully published to NPM. Doing a fresh build + deploying to now.sh.',
+      );
+
+      // get the version we just published
+      const releaseVersion = `v${nextVersion}`; // ex. v2.9.0
+
+      const branchSpecificUrl = await normalizeUrlAlias(branchName);
+      const tagSpecificUrl = await normalizeUrlAlias(releaseVersion);
+
+      const nowAliases = [];
+      nowAliases.push(branchSpecificUrl);
+      nowAliases.push(tagSpecificUrl);
+      nowAliases.push('www.boltdesignsystem.com');
+      nowAliases.push('boltdesignsystem.com');
+      nowAliases.push('www.bolt-design-system.com');
+      nowAliases.push('bolt-design-system.com');
+      nowAliases.push(await normalizeUrlAlias('latest'));
+
+      await shell.exec(`
+        git reset --hard HEAD
+        npx json -I -f docs-site/.incache -e 'this["bolt-tags"].expiresOn = "2019-06-14T12:30:26.377Z"'
+        npx json -I -f docs-site/.incache -e 'this["bolt-urls-to-test"].expiresOn = "2019-06-14T12:30:26.377Z"'
+        npx now alias boltdesignsystem.com ${tagSpecificUrl}
+      `);
+
+      // do the full build + output CLI in real time
+      shell.exec('npm run build', { async: true }, (code, stdout, stderr) => {
+        console.log('Exit code:', code);
+        console.log('', stdout);
+        console.log('', stderr);
+      });
+
+      shell.exec(
+        `npx now deploy --meta gitSha='${gitSha}' --token=${process.env.NOW_TOKEN}`,
+      );
+
+      const latestUrl = await getLatestDeploy();
+
+      nowAliases.forEach(alias => {
+        shell.exec(`npx now alias ${latestUrl} ${alias}`);
+      });
+
+      await shell.exec(`
+        git add docs-site/.incache
+        git commit -m "[skip travis] chore: update .incache file"
+        git tag -fa ${releaseVersion} -m ${releaseVersion}
+        git push --no-verify
+        git push origin ${releaseVersion} --no-verify --force
+      `);
+      await shell.exec('git reset --hard HEAD').stdout;
+
+      if (SLACK_WEBHOOK_URL) {
+        const webhook = new IncomingWebhook(SLACK_WEBHOOK_URL);
+        await webhook.send({
+          text: `Bolt \`${releaseVersion}\` has been released! Check out the <https://github.com/boltdesignsystem/bolt/releases/tag/${releaseVersion}|latest release notes> for more details!
+           - <https://boltdesignsystem.com|Updated Docs Site>`,
+        });
+      } else {
+        console.log(
+          chalk.yellow(
+            'Skipped sending Slack notification about upcoming Bolt release -- missing `SLACK_WEBPACK_URL` env variable!',
+          ),
+        );
+      }
+
+      await shell.exec(`
+        git fetch origin master
+        git checkout master
+        git pull
+        git merge release/2.x
+        git push
+      `);
+    } catch (error) {
+      console.error(error);
+    }
   } else {
     console.log(
       `Skipping doing an auto-release since the current branch, ${branchName}, isn't master or a release branch!`,
     );
-
-    // shell.exec(`
-    //   now alias ${latestUrl} ${branchAlias}
-    //   now alias ${latestUrl} v${urlVersion}.boltdesignsystem.com
-    //   now alias ${latestUrl} boltdesignsystem.com
-    //   now alias ${latestUrl} www.boltdesignsystem.com
-    //   now alias ${latestUrl} bolt-design-system.com
-    //   now alias ${latestUrl} www.bolt-design-system.com
-    // `);
   }
-
-  // const lernaConfig = require('./lerna.json');
-  // const currentVersion = lernaConfig.version;
-
-  // const releaseType = isCanaryRelease ? shell
-  //   .exec(`auto version`, {
-  //     silent: true,
-  //   })
-  //   .stdout.trim();
-  // console.log(releaseType);
 }
 init();
-
-// // clear docs site .incache file
-
-//
-// const url = process.env.SLACK_WEBHOOK_URL;
-// // Read a url from the environment variables
-
-// // npm run build
-
-// // npm run deploy
-
-// function urlFriendlyString(string) {
-//   return string
-//     .replace(/\//g, '-') // `/` => `-`
-//     .replace('--', '-') // `--` => `-` now.sh subdomains can't have `--` for some reason
-//     .replace(/\./g, '-'); // `.` => `-`
-// }
-
-// // let normalizedUrlBase = `${encodeURIComponent(normalizedUrlPrefix)}`;
-
-// const { getLatestDeploy } = require('./scripts/utils');
-//
-
-// const { aliasNowUrl } = require('./scripts/utils/handle-now-aliases');
-//
-// const { TRAVIS_TAG } = require('./scripts/utils/travis-vars');
-
-// const { promisify } = require('util');
-// const gitSemverTags = require('git-semver-tags');
-// const semver = require('semver');
-// const { IncomingWebhook } = require('@slack/webhook');
-// const promisifyGitTags = promisify(gitSemverTags);
-// // console.log(latestVersion.version);
-
-// // const NOW_URL = require('./scripts/get-latest-deploy.js');
-// async function postRelease() {
-//   const latestUrl = await getLatestDeploy();
-//
-
-//   // console.log(latestUrl);
-
-//   const urlVersion = urlFriendlyString(currentVersion);
-
-//   const url = process.env.SLACK_WEBHOOK_URL;
-
-//   const webhook = new IncomingWebhook(url, {
-//     icon_emoji: ':bolt:',
-//   });
-
-// Send the notification
-// (async () => {
-//   await webhook.send({
-//     text: `Bolt v${currentVersion} has been released! Check out the <https://github.com/boltdesignsystem/bolt/releases/tag/v${currentVersion}|latest release notes> for more details!`,
-//     color: 'good', // Can aeither be one of 'good', 'warning', 'danger', or any hex color code
-//   });
-// })();
-
-//
-// let upcomingVersion;
-
-// console.log(isCanaryRelease);
-
-//
-//
-
-// const gitTags = await promisifyGitTags();
-// const latestGitTag = gitTags[0];
-
-// console.log(branchAlias);
-// console.log(tagAlias);
-// console.log(latestTag);
-
-//
-// const newVersion = semver.inc(version, releaseType);
-
-// // auto version
-// if (tags.includes(`v${newVersion}`)){
-//   console.log(`v${newVersion} tag already exists! Aborting...`);
-// }
-
-// if (TRAVIS_TAG) {
-//   aliasNowUrl(url, latestTag);
-// }
-// shell.exec(`
-//   v2.8.3
-// `);
-
-// now alias ${latestUrl} release-2-x.boltdesignsystem.com
-// now alias ${latestUrl} v${urlVersion}.boltdesignsystem.com
-
-// tagged tagged release
-// if (branchName === 'release-2.x') {
-//   console.log('full release!');
-//   shell.exec(`
-//     now alias ${latestUrl} boltdesignsystem.com
-//     now alias ${latestUrl} www.boltdesignsystem.com
-//     now alias ${latestUrl} bolt-design-system.com
-//     now alias ${latestUrl} www.bolt-design-system.com
-//   `);
-// } else if (branchName === 'master') {
-
-// } else {
-
-// }
-
-// await aliasNowUrl(latestUrl, branchName);
-
-// shell.exec(`
-//   now alias ${latestUrl} boltdesignsystem.com
-//   now alias ${latestUrl} www.boltdesignsystem.com
-//   now alias ${latestUrl} bolt-design-system.com
-//   now alias ${latestUrl} www.bolt-design-system.com
-//   now alias ${latestUrl} release-2-x.boltdesignsystem.com
-//   now alias ${latestUrl} v2-5-5.boltdesignsystem.com
-// `);
-
-// https://boltdesignsystem-riaqedhmc.now.sh
-// }
-
-// postRelease();
-// git checkout docs-site/src/assets
-
-// now alias https://boltdesignsystem-123ab99sz.now.sh boltdesignsystem.com
-// now alias https://boltdesignsystem-123ab99sz.now.sh www.boltdesignsystem.com
-// now alias https://boltdesignsystem-123ab99sz.now.sh bolt-design-system.com
-// now alias https://boltdesignsystem-123ab99sz.now.sh www.bolt-design-system.com
-// now alias https://boltdesignsystem-123ab99sz.now.sh release-2-x.boltdesignsystem.com
-// now alias https://boltdesignsystem-123ab99sz.now.sh v2-5-5.boltdesignsystem.com
