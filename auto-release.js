98b77d36c173e6d57247d33f3a570eddb2bb2e6a
Salem Ghoweri <me@salemghoweri.com>
2019-11-05 14:31:49 -0500

    chore: update auto-release script

--- auto-release.js
+++ auto-release.js
@@ -116,45 +116,37 @@ async function init() {
     }
   } else if (isFullRelease) {
     try {
-      const version =
-        (await shell.exec('auto version', { silent: true }).stdout.trim()) ||
-        'patch';
+      const version = await shell
+        .exec('auto version', { silent: true })
+        .stdout.trim();
       const nextVersion = await semver.inc(currentVersion, version);
 
-      if (!nextVersion || nextVersion === null) {
+      if (
+        !version ||
+        !currentVersion ||
+        !nextVersion ||
+        nextVersion === currentVersion
+      ) {
         console.error(`Unknown version to publish to!`);
         return;
-      }
-
-      // temp disable PHP version bump till after v2.9.1 released
-      await shell.exec(`
-        node scripts/release/update-php-package-versions.js -v ${nextVersion}
-        git add packages/core-php/composer.json packages/drupal-modules/bolt_connect/bolt_connect.info.yml packages/drupal-modules/bolt_connect/composer.json
-        git commit -m "[skip travis] chore: version bump PHP-related dependencies to v${nextVersion}"
-      `);
-
-      if (nextVersion === currentVersion) {
-        console.warn(
-          `No packages were changed so the full release was not published! The expected version to release was: ${nextVersion}`,
-        );
-        return;
       } else {
+        await shell.exec(`
+          node scripts/release/update-php-package-versions.js -v ${nextVersion}
+          git add packages/core-php/composer.json packages/drupal-modules/bolt_connect/bolt_connect.info.yml packages/drupal-modules/bolt_connect/composer.json
+          git commit -m "[skip travis] chore: version bump PHP-related dependencies to v${nextVersion}"
+        `);
         await shell.exec(`
           git reset --hard
           rm scripts/bolt-design-system-bot.private-key.pem
           npx lerna publish ${version} --yes -m "[skip travis] chore(release): release %s"
         `);
-
         await shell.exec(`
           ./node_modules/.bin/auto release --use-version v${currentVersion}
         `);
-
         // get the version we just published
         const releaseVersion = `v${nextVersion}`; // ex. v2.9.0
-
         const branchSpecificUrl = await normalizeUrlAlias(branchName);
         const tagSpecificUrl = await normalizeUrlAlias(releaseVersion);
-
         const nowAliases = [];
         nowAliases.push(branchSpecificUrl);
         nowAliases.push(tagSpecificUrl);
@@ -163,7 +155,6 @@ async function init() {
         nowAliases.push('www.bolt-design-system.com');
         nowAliases.push('bolt-design-system.com');
         nowAliases.push(await normalizeUrlAlias('latest'));
-
         await shell.exec(`
           npx json -I -f docs-site/.incache -e 'this["bolt-tags"].expiresOn = "2019-06-14T12:30:26.377Z"'
           npx json -I -f docs-site/.incache -e 'this["bolt-urls-to-test"].expiresOn = "2019-06-14T12:30:26.377Z"'
@@ -171,13 +162,12 @@ async function init() {
           npm run build
           npx now deploy --meta gitSha='${gitSha}' --token=${NOW_TOKEN}
         `);
-
         const latestUrl = await getLatestDeploy();
-
         nowAliases.forEach(alias => {
-          shell.exec(`npx now alias ${latestUrl} ${alias} --token=${NOW_TOKEN}`);
+          shell.exec(
+            `npx now alias ${latestUrl} ${alias} --token=${NOW_TOKEN}`,
+          );
         });
-
         await shell.exec(`
           git add docs-site/.incache
           git commit -m "[skip travis] chore: update .incache file"
@@ -186,28 +176,27 @@ async function init() {
           git push origin ${releaseVersion} --no-verify --force
           git reset --hard HEAD
         `);
-
-        if (SLACK_WEBHOOK_URL) {
-          const webhook = new IncomingWebhook(SLACK_WEBHOOK_URL);
-          await webhook.send({
-            text: `Bolt \`${releaseVersion}\` has been released! Check out the <https://github.com/boltdesignsystem/bolt/releases/tag/${releaseVersion}|latest release notes> for more details!
-             - <https://boltdesignsystem.com|Updated Docs Site>`,
-          });
-        } else {
-          console.log(
-            chalk.yellow(
-              'Skipped sending Slack notification about upcoming Bolt release -- missing `SLACK_WEBPACK_URL` env variable!',
-            ),
-          );
+        if (nextVersion && nextVersion !== null && releaseVersion !== 'vnull') {
+          if (SLACK_WEBHOOK_URL) {
+            const webhook = new IncomingWebhook(SLACK_WEBHOOK_URL);
+            await webhook.send({
+              text: `Bolt \`${releaseVersion}\` has been released! Check out the <https://github.com/boltdesignsystem/bolt/releases/tag/${releaseVersion}|latest release notes> for more details!
+                   - <https://boltdesignsystem.com|Updated Docs Site>`,
+            });
+          } else {
+            console.log(
+              chalk.yellow(
+                'Skipped sending Slack notification about upcoming Bolt release -- missing `SLACK_WEBPACK_URL` env variable!',
+              ),
+            );
+          }
+          await shell.exec(`
+            git checkout master
+            git pull
+            git merge release/2.x
+            git push --no-verify
+          `);
         }
-
-        await shell.exec(`
-          git fetch origin master
-          git checkout master
-          git pull
-          git merge release/2.x
-          git push --no-verify
-        `);
       }
     } catch (error) {
       console.error(error);
