876f5f041b76992ae2418f9951de114d358a3c66
Rémy Denton <remy.denton@pega.com>
2019-11-12 12:16:54 -0500

    refactor: Split accordion child and parent into separate schemas

--- packages/components/bolt-accordion/accordion.schema.js
+++ packages/components/bolt-accordion/accordion.schema.js
@@ -1,3 +1,5 @@
+const accordionItem = require('./accordion-item.schema');
+
 module.exports = {
   title: 'Accordion',
   type: 'object',
@@ -6,63 +8,7 @@ module.exports = {
       type: 'array',
       description:
         'All of the items in the accordion. Each item should contain a header and a content.',
-      items: {
-        type: 'object',
-        properties: {
-          trigger: {
-            type: ['string', 'object', 'array'],
-            description: 'Trigger content of an item.',
-            default: 'Toggle Accordion',
-          },
-          content: {
-            type: ['string', 'object', 'array'],
-            description: 'Expandable content of an item.',
-          },
-          open: {
-            type: 'boolean',
-            description: 'Automatically expand an item on page load.',
-            default: false,
-            enum: [true, false],
-          },
-          inactive: {
-            type: 'boolean',
-            description:
-              'Display item trigger as an inactive element. Item content will be collapsed and the toggle icon hidden.',
-            default: false,
-          },
-          uuid: {
-            type: 'string',
-            description:
-              'Unique ID for an item, randomly generated if not provided.',
-          },
-          open_label: {
-            type: 'string',
-            description:
-              'Accessible label for the open trigger to expand an item.',
-            default: 'Open Accordion',
-          },
-          close_label: {
-            type: 'string',
-            description:
-              'Accessible label for the close trigger to collapse an item.',
-            default: 'Close Accordion',
-          },
-          trigger_spacing: {
-            type: 'string',
-            hidden: true,
-            description:
-              'Overrides the default trigger spacing (by default, inherited from the parent bolt-accordion)',
-            $ref: '#/definitions/spacing',
-          },
-          content_spacing: {
-            type: 'string',
-            hidden: true,
-            description:
-              'Overrides the default content spacing (by default, inherited from the parent bolt-accordion)',
-            $ref: '#/definitions/spacing',
-          },
-        },
-      },
+      items: accordionItem,
     },
     single: {
       type: 'boolean',
