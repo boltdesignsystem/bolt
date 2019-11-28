b017ef0ac4f78b66e5df8cf02659daeea4a73002
Adam Szałapski <adam.szalapski@pega.com>
2019-11-28 16:23:18 +0100

    feature: add test for modal with band and long text

--- packages/components/bolt-modal/__tests__/modal.js
+++ packages/components/bolt-modal/__tests__/modal.js
@@ -45,15 +45,19 @@ const viewportSizes = [
 ];
 
 const modalContent = [
-  `<bolt-text tag="h3" slot="header">This is the header</bolt-text><bolt-text>This is the body (default).</bolt-text><bolt-text slot="footer">This is the footer</bolt-text>`,
-  `<bolt-text tag="h3" slot="header">This is the header</bolt-text>
-    <bolt-text>
-      <p>This is the very long body text(default).</p>
+  {
+    name: 'Simple usage',
+    content: `<bolt-text>This is the body (default).</bolt-text>`,
+  },
+  {
+    name: 'Long content usage',
+    content: `
+      <bolt-text headline font-size="xxlarge">This is the very long body text.</bolt-text>
       <bolt-image src="/fixtures/1200x660.jpg" alt="Placeholder"></bolt-image>
-      <p>Amet tellus cras adipiscing enim eu turpis egestas pretium aenean. Enim eu turpis egestas pretium aenean. Diam sit amet nisl suscipit adipiscing bibendum est. Elementum nisi quis eleifend quam adipiscing vitae proin. Dolor purus non enim praesent. Laoreet id donec ultrices tincidunt arcu non. Arcu ac tortor dignissim convallis. Quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis. Tellus integer feugiat scelerisque varius morbi enim nunc. Porttitor massa id neque aliquam vestibulum morbi. Eros in cursus turpis massa tincidunt dui. Est pellentesque elit ullamcorper dignissim cras.</p>
-      <p>Sed elementum tempus egestas sed sed risus pretium quam vulputate. Faucibus turpis in eu mi bibendum. In ornare quam viverra orci sagittis eu volutpat. Pellentesque habitant morbi tristique senectus et netus et malesuada. Non tellus orci ac auctor augue. Imperdiet massa tincidunt nunc pulvinar sapien. Ut aliquam purus sit amet luctus. Fames ac turpis egestas sed tempus urna et pharetra pharetra. Lacinia quis vel eros donec ac odio tempor orci dapibus. Varius vel pharetra vel turpis nunc eget lorem dolor sed. Libero id faucibus nisl tincidunt eget nullam. Congue quisque egestas diam in arcu cursus euismod quis.</p>
-    </bolt-text>
-    <bolt-text slot="footer">This is the footer</bolt-text>`,
+      <bolt-text>Amet tellus cras adipiscing enim eu turpis egestas pretium aenean. Enim eu turpis egestas pretium aenean. Diam sit amet nisl suscipit adipiscing bibendum est. Elementum nisi quis eleifend quam adipiscing vitae proin. Dolor purus non enim praesent. Laoreet id donec ultrices tincidunt arcu non. Arcu ac tortor dignissim convallis. Quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis. Tellus integer feugiat scelerisque varius morbi enim nunc. Porttitor massa id neque aliquam vestibulum morbi. Eros in cursus turpis massa tincidunt dui. Est pellentesque elit ullamcorper dignissim cras.</bolt-text>
+      <bolt-text>Sed elementum tempus egestas sed sed risus pretium quam vulputate. Faucibus turpis in eu mi bibendum. In ornare quam viverra orci sagittis eu volutpat. Pellentesque habitant morbi tristique senectus et netus et malesuada. Non tellus orci ac auctor augue. Imperdiet massa tincidunt nunc pulvinar sapien. Ut aliquam purus sit amet luctus. Fames ac turpis egestas sed tempus urna et pharetra pharetra. Lacinia quis vel eros donec ac odio tempor orci dapibus. Varius vel pharetra vel turpis nunc eget lorem dolor sed. Libero id faucibus nisl tincidunt eget nullam. Congue quisque egestas diam in arcu cursus euismod quis.</bolt-text>
+    </bolt-text>`,
+  },
 ];
 
 describe('<bolt-modal> Component', () => {
@@ -130,11 +134,13 @@ describe('<bolt-modal> Component', () => {
   });
 
   modalContent.forEach(async contentChoice => {
-    test('Default <bolt-modal> with Shadow DOM renders', async () => {
+    test(`${contentChoice.name} <bolt-modal> with Shadow DOM renders`, async () => {
       const renderedModal = await page.evaluate(async contentChoice => {
         const modal = document.createElement('bolt-modal');
         modal.setAttribute('uuid', '12345');
-        modal.innerHTML = contentChoice;
+        modal.innerHTML = `<bolt-text tag="h3" slot="header">This is the header</bolt-text>
+            ${contentChoice.content}
+            <bolt-text slot="footer">This is the footer</bolt-text>`;
         document.body.appendChild(modal);
         modal.updated();
         modal.show();
@@ -153,11 +159,13 @@ describe('<bolt-modal> Component', () => {
   });
 
   modalContent.forEach(async contentChoice => {
-    test('Default <bolt-modal> w/o Shadow DOM renders', async () => {
+    test(`${contentChoice.name} <bolt-modal> w/o Shadow DOM renders`, async () => {
       const renderedModal = await page.evaluate(contentChoice => {
         const modal = document.createElement('bolt-modal');
         modal.setAttribute('uuid', '12345');
-        modal.innerHTML = contentChoice;
+        modal.innerHTML = `<bolt-text tag="h3" slot="header">This is the header</bolt-text>
+            ${contentChoice.content}
+            <bolt-text slot="footer">This is the footer</bolt-text>`;
         document.body.appendChild(modal);
         modal.useShadow = false;
         modal.updated();
@@ -176,14 +184,16 @@ describe('<bolt-modal> Component', () => {
     });
   });
 
-  modalContent.forEach(async modalContentChoice => {
+  modalContent.forEach(async contentChoice => {
     test(
-      '<bolt-modal> rendered by Twig',
+      `${contentChoice.name} <bolt-modal> rendered by Twig`,
       async () => {
         const { html, ok } = await renderTwig(
           '@bolt-components-modal/modal.twig',
           {
-            content: modalContentChoice,
+            content: `<bolt-text tag="h3" slot="header">This is the header</bolt-text>
+              ${contentChoice.content}
+              <bolt-text slot="footer">This is the footer</bolt-text>`,
           },
         );
         expect(ok).toBe(true);
@@ -237,4 +247,75 @@ describe('<bolt-modal> Component', () => {
       timeout,
     );
   });
+
+  test(
+    '<bolt-band> with long text in <bolt-modal> rendered by Twig',
+    async () => {
+      const bandWithLongContent = await render(
+        '@bolt-components-band/band.twig',
+        {
+          content: modalContent[1].content,
+          full_bleed: false,
+          theme: 'none',
+        },
+      );
+
+      const { html, ok } = await renderTwig(
+        '@bolt-components-modal/modal.twig',
+        {
+          content: `<bolt-text tag="h3" slot="header">This is the header</bolt-text>
+            ${bandWithLongContent.html}
+            <bolt-text slot="footer">This is the footer</bolt-text>`,
+        },
+      );
+      expect(ok).toBe(true);
+      expect(html).toMatchSnapshot();
+
+      await page.evaluate(html => {
+        const div = document.createElement('div');
+        const trigger = document.createElement('bolt-button');
+        trigger.textContent = 'Open Modal';
+        trigger.setAttribute('class', 'js-modal-trigger--open');
+        trigger.setAttribute('onclick', 'this.nextElementSibling.show()');
+        div.innerHTML = `${html}`;
+        div.prepend(trigger);
+        document.body.appendChild(div);
+      }, html);
+
+      const screenshots = [];
+
+      async function isVisible(selector) {
+        return await page.evaluate(selector => {
+          const e = document.querySelector(selector);
+          if (!e) return false;
+          const style = window.getComputedStyle(e);
+          return style &&
+            style.display !== 'none' &&
+            style.visibility !== 'hidden' &&
+            style.opacity !== '0'
+            ? true
+            : false;
+        }, selector);
+      }
+
+      for (const item of viewportSizes) {
+        const { height, width, size } = item;
+        screenshots[size] = [];
+
+        await page.setViewport({ height, width });
+        await page.tap('.js-modal-trigger--open');
+        await page.waitFor(500);
+
+        if (await isVisible('bolt-modal')) {
+          screenshots[size].modalOpened = await page.screenshot();
+          expect(screenshots[size].modalOpened).toMatchImageSnapshot(
+            imageVrtConfig,
+          );
+          await page.tap('bolt-button'); // closes modal
+          await page.waitFor(500);
+        }
+      }
+    },
+    timeout,
+  );
 });
