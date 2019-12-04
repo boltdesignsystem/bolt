3e6dfd5715920cfc93541bc003d7ceb69c301652
Salem Ghoweri <me@salemghoweri.com>
2019-12-04 14:29:20 -0500

    feat: port over Core base class / Bolt Element updates from https://github.com/boltdesignsystem/bolt/pull/1579

--- packages/core/utils/environment.js
+++ packages/core/utils/environment.js
@@ -1,4 +1,4 @@
-import { hasNativeShadowDomSupport } from '@bolt/element';
+import { hasNativeShadowDomSupport } from '@bolt/element/src/lib/utils';
 
 // Helper util to check the current node environment + check if the browser supports shadow DOM for shimming purposes
 export const IS_DEV = process.env.NODE_ENV === 'development';
