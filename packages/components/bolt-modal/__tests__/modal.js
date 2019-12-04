652395ef4dfe75abeaac6c6e944008abcbdee8f4
Daniel Morse <daniel.morse@pega.com>
2019-12-03 23:18:15 -0500

    test: add modal test with band at various viewport sizes

--- packages/components/bolt-modal/__tests__/modal.js
+++ packages/components/bolt-modal/__tests__/modal.js
@@ -199,4 +199,58 @@ describe('<bolt-modal> Component', () => {
     },
     timeout,
   );
+
+  test(
+    '<bolt-modal> with band at various viewport sizes',
+    async () => {
+      const bandContent = `<bolt-text headline font-size="xxlarge">This is the very long body text.</bolt-text>
+        <bolt-image src="/fixtures/1200x660.jpg" alt="Placeholder"></bolt-image>
+        <bolt-text>Amet tellus cras adipiscing enim eu turpis egestas pretium aenean. Enim eu turpis egestas pretium aenean. Diam sit amet nisl suscipit adipiscing bibendum est. Elementum nisi quis eleifend quam adipiscing vitae proin. Dolor purus non enim praesent. Laoreet id donec ultrices tincidunt arcu non. Arcu ac tortor dignissim convallis. Quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis. Tellus integer feugiat scelerisque varius morbi enim nunc. Porttitor massa id neque aliquam vestibulum morbi. Eros in cursus turpis massa tincidunt dui. Est pellentesque elit ullamcorper dignissim cras.</bolt-text>
+        <bolt-text>Sed elementum tempus egestas sed sed risus pretium quam vulputate. Faucibus turpis in eu mi bibendum. In ornare quam viverra orci sagittis eu volutpat. Pellentesque habitant morbi tristique senectus et netus et malesuada. Non tellus orci ac auctor augue. Imperdiet massa tincidunt nunc pulvinar sapien. Ut aliquam purus sit amet luctus. Fames ac turpis egestas sed tempus urna et pharetra pharetra. Lacinia quis vel eros donec ac odio tempor orci dapibus. Varius vel pharetra vel turpis nunc eget lorem dolor sed. Libero id faucibus nisl tincidunt eget nullam. Congue quisque egestas diam in arcu cursus euismod quis.</bolt-text>`;
+
+      const renderedBand = await render('@bolt-components-band/band.twig', {
+        content: bandContent,
+        full_bleed: false,
+        theme: 'none',
+      });
+
+      const { html, ok } = await render('@bolt-components-modal/modal.twig', {
+        content: `<bolt-text tag="h3" slot="header">This is the header</bolt-text>
+          ${renderedBand.html}
+          <bolt-text slot="footer">This is the footer</bolt-text>`,
+      });
+      expect(ok).toBe(true);
+      expect(html).toMatchSnapshot();
+
+      await page.evaluate(html => {
+        document.body.innerHTML = html;
+      }, html);
+
+      const screenshots = [];
+
+      for (const item of viewportSizes) {
+        const { height, width, size } = item;
+
+        screenshots[size] = [];
+
+        await page.setViewport({ height, width });
+        await page.evaluate(() => {
+          document.querySelector('bolt-modal').show();
+        });
+        await page.waitFor(500);
+
+        screenshots[size].modalOpened = await page.screenshot();
+        expect(screenshots[size].modalOpened).toMatchImageSnapshot({
+          ...vrtConfig,
+          failureThreshold: '0.05',
+        });
+
+        await page.evaluate(() => {
+          document.querySelector('bolt-modal').hide();
+        });
+        await page.waitFor(500);
+      }
+    },
+    timeout,
+  );
 });
