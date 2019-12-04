e0f830c70341e6679d00d445e52df3d05936b630
Mike Mai <emceehao@gmail.com>
2019-12-04 17:04:00 -0500

    chore: change to js schema

--- packages/components/bolt-menu/src/menu.js
+++ packages/components/bolt-menu/src/menu.js
@@ -13,25 +13,26 @@ let cx = classNames.bind(menuStyles);
 
 @customElement('bolt-menu')
 class BoltMenu extends BoltElement {
-  // static get properties() {
-  //   return {
-  //     ratio: String,
-  //     _ratioW: String, // internal only prop for handling the width-specific data from the ratio prop
-  //     _ratioH: String, // internal only prop for handling the height-specific data from the ratio prop
-  //     aspectRatioHeight: {
-  //       type: Number, // deprecated - will be removed in Bolt v3.0
-  //       attribute: 'aspect-ratio-height',
-  //     },
-  //     aspectRatioWidth: {
-  //       type: Number, // deprecated - will be removed in Bolt v3.0
-  //       attribute: 'aspect-ratio-width',
-  //     },
-  //     noCssVars: {
-  //       type: Boolean,
-  //       attribute: 'no-css-vars',
-  //     },
-  //   };
-  // }
+  static get properties() {
+    return {
+      // ratio: String,
+      // _ratioW: String, // internal only prop for handling the width-specific data from the ratio prop
+      // _ratioH: String, // internal only prop for handling the height-specific data from the ratio prop
+      // aspectRatioHeight: {
+      //   type: Number, // deprecated - will be removed in Bolt v3.0
+      //   attribute: 'aspect-ratio-height',
+      // },
+      // aspectRatioWidth: {
+      //   type: Number, // deprecated - will be removed in Bolt v3.0
+      //   attribute: 'aspect-ratio-width',
+      // },
+      spacing: String,
+      noCssVars: {
+        type: Boolean,
+        attribute: 'no-css-vars',
+      },
+    };
+  }
 
   constructor() {
     super();
@@ -44,11 +45,15 @@ class BoltMenu extends BoltElement {
 
   render() {
     console.log('rendering');
+
+    const classes = cx('c-bolt-menu__title', {
+      [`c-bolt-menu__title--spacing-${this.spacing}`]: this.spacing,
+    });
     return html`
       <div class="${cx(`c-bolt-menu`)}">
         ${this.templateMap.get('title') &&
           html`
-            <div class="${cx(`c-bolt-menu__title`)}">
+            <div class="${classes}">
               ${this.slotify('title')}
             </div>
           `}
