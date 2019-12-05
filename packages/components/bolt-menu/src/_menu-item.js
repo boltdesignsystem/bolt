0ad766f3853bdd4e7765d25ac268b3eccd061ef0
Daniel Morse <daniel.morse@pega.com>
2019-12-05 17:21:49 -0500

    feat: setup ssr-keep in Menu component, needs refactor

--- packages/components/bolt-menu/src/_menu-item.js
+++ packages/components/bolt-menu/src/_menu-item.js
@@ -12,6 +12,10 @@ import menuStyles from './_menu-item.scss';
 
 let cx = classNames.bind(menuStyles);
 
+/*
+ * 1 - @todo: remove SSR Hydration from this file, move to base
+ */
+
 @customElement('bolt-menu-item')
 class BoltMenuItem extends BoltElement {
   static get properties() {
@@ -29,6 +33,37 @@ class BoltMenuItem extends BoltElement {
     return [unsafeCSS(menuStyles)];
   }
 
+  /* [1] */
+  connectedCallback() {
+    super.connectedCallback && super.connectedCallback();
+
+    // Check if any `<ssr-keep>` elements have registered themselves here. If so, kick off the one-time hydration prep task.
+    if (this.ssrKeep && !this.ssrPrepped) {
+      this.ssrHydrationPrep();
+    }
+  }
+
+  /* [1] */
+  ssrHydrationPrep() {
+    // @todo: Move this to base-element, possibly as decorator
+    this.nodesToKeep = [];
+
+    this.ssrKeep.forEach(item => {
+      while (item.firstChild) {
+        this.nodesToKeep.push(item.firstChild); // track the nodes that will be preserved
+        this.appendChild(item.firstChild);
+      }
+    });
+
+    // Remove all children not in the "keep" array
+    Array.from(this.children)
+      .filter(item => !this.nodesToKeep.includes(item))
+      .forEach(node => {
+        node.parentElement.removeChild(node);
+      });
+
+    this.ssrPrepped = true;
+  }
   render() {
     console.log('rendering menu item');
     return html`
