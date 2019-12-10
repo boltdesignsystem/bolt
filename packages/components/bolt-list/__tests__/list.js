f20479285836c11245284d5f00f714d3ddab87ee
Mike Mai <emceehao@gmail.com>
2019-12-10 11:17:55 -0500

    fix: linting

--- packages/components/bolt-list/__tests__/list.js
+++ packages/components/bolt-list/__tests__/list.js
@@ -118,7 +118,27 @@ describe('<bolt-list> Component', () => {
       const results = await render('@bolt-components-list/list.twig', {
         display: 'inline',
         nowrap: nowrapChoice,
-        items: ['item 1', 'item 2', 'item 3', 'item 4', 'item 5', 'item 6', 'item 7', 'item 8', 'item 9', 'item 10', 'item 11', 'item 12', 'item 13', 'item 14', 'item 15', 'item 16', 'item 17', 'item 18', 'item 19'],
+        items: [
+          'item 1',
+          'item 2',
+          'item 3',
+          'item 4',
+          'item 5',
+          'item 6',
+          'item 7',
+          'item 8',
+          'item 9',
+          'item 10',
+          'item 11',
+          'item 12',
+          'item 13',
+          'item 14',
+          'item 15',
+          'item 16',
+          'item 17',
+          'item 18',
+          'item 19',
+        ],
       });
       expect(results.ok).toBe(true);
       expect(results.html).toMatchSnapshot();
