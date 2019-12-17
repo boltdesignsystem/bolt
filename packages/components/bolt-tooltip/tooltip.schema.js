874c482483d6ae36082709ecaa9dd43844a9623b
Mike Mai <emceehao@gmail.com>
2019-12-17 12:33:03 -0500

    styles: placement

--- packages/components/bolt-tooltip/tooltip.schema.js
+++ packages/components/bolt-tooltip/tooltip.schema.js
@@ -10,7 +10,7 @@ module.exports = {
         'A Drupal attributes object. Applies extra HTML attributes to the outer &lt;bolt-menu&gt; tag.',
     },
     trigger: {
-      type: 'any',
+      type: 'object',
       description: 'Renders the trigger of the tooltip.',
     },
     content: {
