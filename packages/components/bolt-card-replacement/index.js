74ed5832895e49306a55c1365989420ce21ff9f5
Salem Ghoweri <me@salemghoweri.com>
2020-01-03 11:40:57 -0500

    fix: minor CSS fixes per PR feedback

--- packages/components/bolt-card-replacement/index.js
+++ packages/components/bolt-card-replacement/index.js
@@ -1,22 +1,6 @@
-import { polyfillLoader } from '@bolt/core-v3.x/polyfills';
-
-polyfillLoader.then(res => {
-  import(
-    /* webpackMode: 'eager', webpackChunkName: 'bolt-card-replacement' */ './src/card-replacement/card-replacement'
-  );
-  import(
-    /* webpackMode: 'eager', webpackChunkName: 'bolt-card-replacement-media' */ './src/card-replacement-media/card-replacement-media'
-  );
-  import(
-    /* webpackMode: 'eager', webpackChunkName: 'bolt-card-replacement-body' */ './src/card-replacement-body/card-replacement-body'
-  );
-  import(
-    /* webpackMode: 'eager', webpackChunkName: 'bolt-card-replacement-link' */ './src/card-replacement-link/card-replacement-link'
-  );
-  import(
-    /* webpackMode: 'eager', webpackChunkName: 'bolt-card-replacement-actions' */ './src/card-replacement-actions/card-replacement-actions'
-  );
-  import(
-    /* webpackMode: 'eager', webpackChunkName: 'bolt-card-replacement-action' */ './src/card-replacement-actions/card-replacement-action'
-  );
-});
+import './src/card-replacement/card-replacement';
+import './src/card-replacement-media/card-replacement-media';
+import './src/card-replacement-body/card-replacement-body';
+import './src/card-replacement-link/card-replacement-link';
+import './src/card-replacement-actions/card-replacement-actions';
+import './src/card-replacement-actions/card-replacement-action';
