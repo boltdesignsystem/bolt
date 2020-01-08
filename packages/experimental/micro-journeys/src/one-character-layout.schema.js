a50e9274215ffc24462cec783bfaa59951f8a875
Jeremy John <jeremy@basalt.io>
2020-01-08 15:44:08 -0600

    chore(micro-journeys): add schema for one and two character layouts

---
+++ packages/experimental/micro-journeys/src/one-character-layout.schema.js
@@ -0,0 +1,5 @@
+module.exports = {
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  title: 'One-Character Layout',
+  type: 'object',
+};
---
+++ packages/experimental/micro-journeys/src/two-character-layout.schema.js
@@ -0,0 +1,5 @@
+module.exports = {
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  title: 'Two-Character Layout',
+  type: 'object',
+};
