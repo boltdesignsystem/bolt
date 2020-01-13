0b8a771e7863c89692d0dc1e03a517db1ae321c3
Rémy Denton <remy.denton@pega.com>
2020-01-13 14:47:37 -0500

    feat: Add suggested bolt.rc file for external builds

---
+++ .bolt-release/README.md
@@ -0,0 +1,1 @@
+Files in this directory serve as a starting point for pre-built Bolt releases.
--- docs-site/.boltrc.js
+++ docs-site/.boltrc.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const resolve = require('resolve');
 const argv = require('yargs').argv;
+const deepmerge = require('deepmerge');
+const baseConfig = require('../.bolt-release/.boltrc.suggested.js');
 
-const config = {
+const config = deepmerge(baseConfig, {
   // array of languages to compile the design system. note, these are ignored when the --i18n flag is set to false
   // Note: if lang is defined, the first item is currently the one used by default in the Pattern Lab build, pending further iterations on this!
   // lang: ['en', 'ja'],
@@ -75,74 +77,26 @@ const config = {
 
   components: {
     global: [
+      /* IMPORTANT: Do NOT list components here that are intended to be used
+       * outside of the docs site-- those should instead be pulled in through
+       * baseConfig.  Use this only for internal components.
+       */
+
       // helper components that are only used internally
+      '@bolt/analytics-autolink',
+      '@bolt/analytics-autotrack',
+      '@bolt/components-banner',
+      '@bolt/components-radio-switch',
+      '@bolt/components-page-footer',
+      '@bolt/components-page-header',
+      '@bolt/docs-search',
+      // '@bolt/schema-form', // Component Explorer being temporarily disabled until we've migrated our Twig Rendering Service to Now.sh v2
       '@bolt/shadow-toggle',
       '@bolt/theme-switcher',
 
-      '@bolt/components-radio-switch',
-      '@bolt/components-carousel',
-      '@bolt/global',
-      '@bolt/animations',
-      '@bolt/components-animate',
-      '@bolt/docs-search',
-      '@bolt/components-typeahead',
-      // '@bolt/schema-form', // Component Explorer being temporarily disabled until we've migrated our Twig Rendering Service to Now.sh v2
-      '@bolt/analytics-autolink',
-      '@bolt/analytics-autotrack',
-      '@bolt/components-placeholder',
-      '@bolt/components-accordion',
-      '@bolt/components-action-blocks',
-      '@bolt/components-banner',
-      '@bolt/components-dropdown',
-      '@bolt/components-background',
-      '@bolt/components-background-shapes',
-      '@bolt/components-band',
-      '@bolt/components-block-list',
-      '@bolt/components-blockquote',
-      '@bolt/components-breadcrumb',
-      '@bolt/components-button',
-      '@bolt/components-button-group',
-      '@bolt/components-card',
-      '@bolt/components-card-replacement',
-      '@bolt/components-chip',
-      '@bolt/components-chip-list',
-      '@bolt/components-code-snippet',
-      '@bolt/components-copy-to-clipboard',
-      '@bolt/components-device-viewer',
-      '@bolt/components-figure',
-      '@bolt/components-form',
-      '@bolt/components-headline',
-      '@bolt/components-icon',
-      '@bolt/components-image',
-      '@bolt/components-link',
-      '@bolt/components-list',
-      '@bolt/components-modal',
-      '@bolt/components-nav-indicator',
-      '@bolt/components-nav-priority',
-      '@bolt/components-navbar',
-      '@bolt/components-navlink',
-      '@bolt/components-logo',
-      '@bolt/components-page-footer',
-      '@bolt/components-page-header',
-      '@bolt/components-pagination',
-      '@bolt/components-share',
-      '@bolt/components-search-filter',
-      '@bolt/components-site',
-      '@bolt/components-smooth-scroll',
-      '@bolt/components-sticky',
-      '@bolt/components-stack',
-      '@bolt/components-table',
-      '@bolt/components-tabs',
-      '@bolt/components-teaser',
-      '@bolt/components-text',
-      '@bolt/components-tooltip',
-      '@bolt/components-trigger',
-      '@bolt/components-ul',
-      '@bolt/components-ol',
-      '@bolt/components-video',
-      '@bolt/components-grid',
+      // Components that are excluded from the base release build.
       '@bolt/micro-journeys',
-      '@bolt/components-editor',
+
       /**
        * note: resolving these paths isn't typically required when
        * the .boltrc config is run through the bolt CLI tool (ie.
@@ -156,11 +110,7 @@ const config = {
       resolve.sync('./src/index.scss'),
       resolve.sync('./src/index.js'),
     ],
-    individual: [
-      '@bolt/components-critical-fonts',
-      '@bolt/components-critical-css',
-      '@bolt/components-critical-css-vars',
-    ],
+    individual: [],
   },
   copy: [
     {
@@ -200,6 +150,6 @@ const config = {
       functions: ['addBoltCoreExtensions', 'addBoltExtraExtensions'],
     },
   ],
-};
+});
 
 module.exports = config;
