11ade2064a97215457f6c2f76067ac05350945cb
Salem Ghoweri <me@salemghoweri.com>
2020-01-23 14:37:31 -0500

    feat: upgrade Swiper to the latest version; only pull in the modules being used

--- packages/components/bolt-carousel/src/carousel.js
+++ packages/components/bolt-carousel/src/carousel.js
@@ -3,7 +3,19 @@ import { html, customElement } from '@bolt/element';
 import { withLitHtml } from '@bolt/core-v3.x/renderers/renderer-lit-html';
 import classNames from 'classnames/bind';
 import changeCase from 'change-case';
-import Swiper from 'swiper';
+
+// Import Swiper and modules
+import {
+  Swiper,
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+} from 'swiper/js/swiper.esm.js';
+
+// Install modules
+Swiper.use([Navigation, Pagination, Scrollbar, A11y]);
+
 import styles from '../index.scss';
 import originalSchema from '../carousel.schema.yml';
 
