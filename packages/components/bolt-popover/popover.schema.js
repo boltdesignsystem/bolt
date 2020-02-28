d532c62cb1cb7a67a32523d9de70e0f2bf9e663e
Salem Ghoweri <me@salemghoweri.com>
2020-02-28 10:09:08 -0500

    feat: add built-in theming support to Popover

--- packages/components/bolt-popover/popover.schema.js
+++ packages/components/bolt-popover/popover.schema.js
@@ -50,6 +50,12 @@ module.exports = {
       enum: ['none', 'xsmall', 'small', 'medium'],
       default: 'small',
     },
+    theme: {
+      type: 'string',
+      description:
+        'Applies a Bolt color theme to the bubble that contains the main Popover content.',
+      enum: ['none', 'xlight', 'light', 'dark', 'xdark'],
+    },
     boundary: {
       type: 'string',
       description:
