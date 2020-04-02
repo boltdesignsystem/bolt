5de1a1b3a2b489e0429858c096efd72e0ce0911e
Mike Mai <emceehao@gmail.com>
2020-04-02 15:52:13 -0400

    fix: update tests

--- packages/components/bolt-action-blocks/__tests__/action-blocks.js
+++ packages/components/bolt-action-blocks/__tests__/action-blocks.js
@@ -8,7 +8,7 @@ import {
 const { readYamlFileSync } = require('@bolt/build-tools/utils/yaml');
 const { join } = require('path');
 const schema = readYamlFileSync(join(__dirname, '../action-blocks.schema.yml'));
-const { valign, borderless } = schema.properties;
+const { spacing, valign, borderless } = schema.properties;
 
 const timeout = 120000;
 
@@ -72,6 +72,46 @@ describe('<bolt-action-blocks> Component', () => {
   });
 
   // Props
+  spacing.enum.forEach(async spacingChoice => {
+    test(`Vertical alignment of each block's content: ${spacingChoice}`, async () => {
+      const results = await render(
+        '@bolt-components-action-blocks/action-blocks.twig',
+        {
+          spacing: spacingChoice,
+          contentItems: [
+            {
+              text: 'Item 1',
+              url: '#!',
+              icon: {
+                name: 'download',
+                size: 'large',
+              },
+            },
+            {
+              text:
+                'Item 2: this item has more text, so it can demonstrate the vertical alignment',
+              url: '#!',
+              icon: {
+                name: 'copy-to-clipboard',
+                size: 'large',
+              },
+            },
+            {
+              text: 'Item 3',
+              url: '#!',
+              icon: {
+                name: 'calendar',
+                size: 'large',
+              },
+            },
+          ],
+        },
+      );
+      expect(results.ok).toBe(true);
+      expect(results.html).toMatchSnapshot();
+    });
+  });
+
   valign.enum.forEach(async valignChoice => {
     test(`Vertical alignment of each block's content: ${valignChoice}`, async () => {
       const results = await render(
@@ -88,8 +128,7 @@ describe('<bolt-action-blocks> Component', () => {
               },
             },
             {
-              text:
-                'Item 2: this item has more text, so it can demonstrate the vertical alignment',
+              text: 'Item 2',
               url: '#!',
               icon: {
                 name: 'copy-to-clipboard',
