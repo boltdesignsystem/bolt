bf7a77b42184ccd9b29e00bc3140ace48f44badc
Salem Ghoweri <me@salemghoweri.com>
2020-04-09 11:14:21 -0400

    feat: update Chip and Video to use the new LazyQueue functionality

--- packages/components/bolt-video/index.js
+++ packages/components/bolt-video/index.js
@@ -1,6 +1,13 @@
-import { polyfillLoader } from '@bolt/core-v3.x/polyfills';
+import { lazyQueue } from '@bolt/lazy-queue';
 
-polyfillLoader.then(res => {
-  import(/* webpackChunkName: 'bolt-video' */ './src/video.standalone.js');
-  import(/* webpackChunkName: 'bolt-video-meta' */ './src/video-meta.js');
+lazyQueue(['bolt-video'], async () => {
+  await Promise.all([
+    import(/* webpackChunkName: 'bolt-video' */ './src/video.standalone.js'),
+    import(/* webpackChunkName: 'bolt-video-meta' */ './src/video-meta.js'),
+
+    // @todo: possibly re-enable once all components converted over to use lazyQueue
+    // import(
+    //   /* webpackChunkName: "bolt-ratio" */ '@bolt/components-ratio/src/ratio'
+    // ),
+  ]);
 });
