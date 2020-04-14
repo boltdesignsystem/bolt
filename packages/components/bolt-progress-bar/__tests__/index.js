5d4f4ee328955259214ec3d70c04667df13bf9d7
Salem Ghoweri <me@salemghoweri.com>
2020-04-14 14:27:11 -0400

    refactor: update all Progress Bar tests

--- packages/components/bolt-progress-bar/__tests__/index.js
+++ packages/components/bolt-progress-bar/__tests__/index.js
@@ -1,125 +1,168 @@
 import {
+  isConnected,
   render,
   renderString,
-  stop as stopTwigRenderer,
-} from '@bolt/twig-renderer';
-import { fixture as html } from '@open-wc/testing-helpers';
+  stopServer,
+  html,
+  vrtDefaultConfig,
+} from '../../../testing/testing-helpers';
+const timeout = 90000;
 
-const { readYamlFileSync } = require('@bolt/build-tools/utils/yaml');
-const { join } = require('path');
-const schema = readYamlFileSync(join(__dirname, '../progress-bar.schema.yml'));
-const { tag } = schema.properties;
+import schema from '../progress-bar.schema';
+const { valuePosition } = schema.properties;
 
-async function renderTwig(template, data) {
-  return await render(template, data, true);
-}
-
-async function renderTwigString(template, data) {
-  return await renderString(template, data, true);
-}
-
-const timeout = 120000;
-
-describe('progress bar', async () => {
+describe('Progress Bar', () => {
   let page;
 
   afterAll(async () => {
     await stopServer();
-  }, timeout);
+    await page.close();
+  });
 
   beforeEach(async () => {
+    await page.evaluate(() => {
+      document.body.innerHTML = '';
+    });
+  }, timeout);
+
+  beforeAll(async () => {
     page = await global.__BROWSER__.newPage();
     await page.goto('http://127.0.0.1:4444/', {
-      waitUntil: 'networkidle0',
       timeout: 0,
     });
   }, timeout);
 
-  test('basic progress-bar component renders', async () => {
-    const results = await renderTwig(
-      '@bolt-components-progress-bar/progress-bar.twig',
-    );
-    expect(results.ok).toBe(true);
-    expect(results.html).toMatchSnapshot();
-  });
-
-  test('basic progress-bar component with the global `no-shadow` prop added', async () => {
-    const results = await renderTwig(
+  test('Basic Progress Bar usage', async () => {
+    const results = await render(
       '@bolt-components-progress-bar/progress-bar.twig',
       {
-        no_shadow: true,
+        value: 50,
       },
     );
     expect(results.ok).toBe(true);
     expect(results.html).toMatchSnapshot();
+
+    const renderedProgressBarMarkup = await page.evaluate(async results => {
+      document.body.insertAdjacentHTML('beforeend', results.html);
+      const progressBar = document.querySelector('bolt-progress-bar');
+      await progressBar.updateComplete;
+      return progressBar.renderRoot.innerHTML;
+    }, results);
+
+    const renderedHTML = await html(`<div>${renderedProgressBarMarkup}</div>`);
+    expect(renderedHTML).toMatchSnapshot();
+
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot(vrtDefaultConfig);
   });
 
-  test('progress-bar with outer CSS class via Drupal Attributes', async () => {
-    const results = await renderTwig(
+  test('Progress Bar w/o Shadow DOM', async () => {
+    const results = await render(
       '@bolt-components-progress-bar/progress-bar.twig',
       {
+        value: 50,
         attributes: {
-          class: ['u-bolt-margin-top-medium'],
+          'no-shadow': true,
         },
       },
     );
     expect(results.ok).toBe(true);
     expect(results.html).toMatchSnapshot();
-  });
 
-  test('Default <bolt-progress-bar> w/o Shadow DOM renders', async function() {
-    const renderedComponentHTML = await page.evaluate(() => {
-      const component = document.createElement('bolt-progress-bar');
-      btn.textContent = 'progress-bar Test';
-      document.body.appendChild(btn);
-      component.useShadow = false;
-      component.updated();
-      return component.outerHTML;
-    });
+    const renderedProgressBarMarkup = await page.evaluate(async results => {
+      document.body.insertAdjacentHTML('beforeend', results.html);
+      const progressBar = document.querySelector('bolt-progress-bar');
+      await progressBar.updateComplete;
+      return progressBar.outerHTML;
+    }, results);
 
-    const renderedHTML = await html(renderedComponentHTML);
+    const renderedHTML = await html(`<div>${renderedProgressBarMarkup}</div>`);
+    expect(renderedHTML).toMatchSnapshot();
 
     const image = await page.screenshot();
-    expect(image).toMatchImageSnapshot({
-      failureThreshold: '0.01',
-      failureThresholdType: 'percent',
-    });
-
-    expect(renderedHTML).toMatchSnapshot();
+    expect(image).toMatchImageSnapshot(vrtDefaultConfig);
   });
 
-  test('Default <bolt-progress-bar> with Shadow DOM renders', async function() {
-    const defaultComponentShadowRoot = await page.evaluate(() => {
-      const component = document.createElement('bolt-progress-bar');
-      component.textContent = 'progress-bar Component Test -- Shadow Root HTML';
-      document.body.appendChild(component);
-      component.updated();
-      return component.renderRoot.innerHTML;
-    });
-
-    const defaultComponentOuter = await page.evaluate(() => {
-      const component = document.createElement('bolt-progress-bar');
-      component.textContent = 'progress-bar Component Test -- Outer HTML';
-      document.body.appendChild(component);
-      component.updated();
-      return component.outerHTML;
-    });
-
-    const renderedShadowDomHTML = await html(defaultComponentShadowRoot);
-    const renderedHTML = await html(defaultComponentOuter);
-
-    expect(renderedHTML.textContent).toEqual(
-      'progress-bar Component Test -- Outer HTML',
+  test('100% Complete + Title', async () => {
+    const results = await render(
+      '@bolt-components-progress-bar/progress-bar.twig',
+      {
+        title: 'Percent Complete',
+        value: 100,
+      },
     );
+    expect(results.ok).toBe(true);
+    expect(results.html).toMatchSnapshot();
+
+    const renderedProgressBarMarkup = await page.evaluate(async results => {
+      document.body.insertAdjacentHTML('beforeend', results.html);
+      const progressBar = document.querySelector('bolt-progress-bar');
+      await progressBar.updateComplete;
+      return progressBar.renderRoot.innerHTML;
+    }, results);
+
+    const renderedHTML = await html(`<div>${renderedProgressBarMarkup}</div>`);
+    expect(renderedHTML).toMatchSnapshot();
 
     const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot(vrtDefaultConfig);
+  });
 
-    expect(image).toMatchImageSnapshot({
-      failureThreshold: '0.01',
-      failureThresholdType: 'percent',
-    });
+  test('Custom Label Text', async () => {
+    const results = await render(
+      '@bolt-components-progress-bar/progress-bar.twig',
+      {
+        title: 'Current Progress',
+        labelText: 'Step 2 of 6',
+        min: 1,
+        max: 6,
+        value: 2,
+      },
+    );
+    expect(results.ok).toBe(true);
+    expect(results.html).toMatchSnapshot();
 
-    expect(renderedShadowDomHTML).toMatchSnapshot();
+    const renderedProgressBarMarkup = await page.evaluate(async results => {
+      document.body.insertAdjacentHTML('beforeend', results.html);
+      const progressBar = document.querySelector('bolt-progress-bar');
+      await progressBar.updateComplete;
+      return progressBar.renderRoot.innerHTML;
+    }, results);
+
+    const renderedHTML = await html(`<div>${renderedProgressBarMarkup}</div>`);
     expect(renderedHTML).toMatchSnapshot();
+
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot(vrtDefaultConfig);
+  });
+
+  valuePosition.enum.forEach(position => {
+    test(`valuePosition: ${position}`, async () => {
+      const results = await render(
+        '@bolt-components-progress-bar/progress-bar.twig',
+        {
+          valuePosition: position,
+          title: 'Percent Downloaded',
+          value: 75,
+        },
+      );
+      expect(results.ok).toBe(true);
+      expect(results.html).toMatchSnapshot();
+
+      const renderedProgressBarMarkup = await page.evaluate(async results => {
+        document.body.insertAdjacentHTML('beforeend', results.html);
+        const progressBar = document.querySelector('bolt-progress-bar');
+        await progressBar.updateComplete;
+        return progressBar.renderRoot.innerHTML;
+      }, results);
+
+      const renderedHTML = await html(
+        `<div>${renderedProgressBarMarkup}</div>`,
+      );
+      expect(renderedHTML).toMatchSnapshot();
+
+      const image = await page.screenshot();
+      expect(image).toMatchImageSnapshot(vrtDefaultConfig);
+    });
   });
 });
