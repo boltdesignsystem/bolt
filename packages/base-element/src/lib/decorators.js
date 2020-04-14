d2ecdfc33e9b44f344d45430407732b7c35d36b2
Salem Ghoweri <me@salemghoweri.com>
2020-04-14 12:03:40 -0400

    fix: update lit-helpers package + use default imports for camelCase and paramCase deps

--- packages/base-element/src/lib/decorators.js
+++ packages/base-element/src/lib/decorators.js
@@ -1,5 +1,5 @@
-import { camelCase } from 'camel-case';
-import { paramCase } from 'param-case';
+import camelCase from 'camel-case';
+import paramCase from 'param-case';
 import styleInjector from './style-injector';
 import { getComponentRootElement, shouldUseShadowDom } from './utils';
 
