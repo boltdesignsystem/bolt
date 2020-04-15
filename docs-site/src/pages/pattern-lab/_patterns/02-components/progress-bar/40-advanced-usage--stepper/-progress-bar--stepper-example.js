d37b376dc0a7a4b8a85826b9b1524c12b99db57d
Salem Ghoweri <me@salemghoweri.com>
2020-04-15 14:27:34 -0400

    refactor: minor JS demo fixes

--- docs-site/src/pages/pattern-lab/_patterns/02-components/progress-bar/40-advanced-usage--stepper/-progress-bar--stepper-example.js
+++ docs-site/src/pages/pattern-lab/_patterns/02-components/progress-bar/40-advanced-usage--stepper/-progress-bar--stepper-example.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', function() {
-  var progressBar = document.querySelector('.js-progress-bar-stepper');
+  const progressBar = document.querySelector('.js-progress-bar-stepper');
 
-  var progressBarBackward = document.querySelector(
+  const progressBarBackward = document.querySelector(
     '.js-progress-bar-stepper--back',
   );
-  var progressBarForward = document.querySelector(
+  const progressBarForward = document.querySelector(
     '.js-progress-bar-stepper--forward',
   );
 
@@ -12,35 +12,41 @@ window.addEventListener('load', function() {
     progressBar.valueText = `${progressBar.value} of ${progressBar.max}`;
   }
 
-  progressBarBackward.addEventListener('click', () => {
-    if (progressBar.value > progressBar.min) {
-      progressBar.value -= 1;
-      updateProgressText();
-    }
-  });
+  if (progressBarBackward) {
+    progressBarBackward.addEventListener('click', () => {
+      if (progressBar.value > progressBar.min) {
+        progressBar.value -= 1;
+        updateProgressText();
+      }
+    });
+  }
 
-  progressBarForward.addEventListener('click', () => {
-    if (progressBar.value < progressBar.max) {
-      progressBar.value += 1;
-      updateProgressText();
-    }
-  });
+  if (progressBarForward) {
+    progressBarForward.addEventListener('click', () => {
+      if (progressBar.value < progressBar.max) {
+        progressBar.value += 1;
+        updateProgressText();
+      }
+    });
+  }
 
-  progressBar.addEventListener('rendered', function() {
-    if (progressBar.value === progressBar.min) {
-      progressBarBackward.setAttribute('disabled', '');
-    }
+  if (progressBar) {
+    progressBar.addEventListener('rendered', function() {
+      if (progressBar.value === progressBar.min) {
+        progressBarBackward.setAttribute('disabled', '');
+      }
 
-    if (progressBar.value === progressBar.max) {
-      progressBarForward.setAttribute('disabled', '');
-    }
+      if (progressBar.value === progressBar.max) {
+        progressBarForward.setAttribute('disabled', '');
+      }
 
-    if (progressBar.value > progressBar.min) {
-      progressBarBackward.removeAttribute('disabled');
-    }
+      if (progressBar.value > progressBar.min) {
+        progressBarBackward.removeAttribute('disabled');
+      }
 
-    if (progressBar.value < progressBar.max) {
-      progressBarForward.removeAttribute('disabled');
-    }
-  });
+      if (progressBar.value < progressBar.max) {
+        progressBarForward.removeAttribute('disabled');
+      }
+    });
+  }
 });
