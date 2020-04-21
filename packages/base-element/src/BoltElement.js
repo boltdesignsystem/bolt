6aaa5369a0229889ba521480c02efa1b96769d5f
Salem Ghoweri <me@salemghoweri.com>
2020-04-21 10:48:49 -0400

    refactor: update to now wait for SSR-related elements to be ready before rendering

--- packages/base-element/src/BoltElement.js
+++ packages/base-element/src/BoltElement.js
@@ -13,13 +13,18 @@ import {
 @conditionalShadowDom()
 @jsonSchemaProps()
 class BoltElement extends Slotify {
-  connectedCallback() {
-    super.connectedCallback && super.connectedCallback();
+  async connectedCallback() {
+    // wait for Bolt's SSR hydration-related helper elements to be ready before continuing
+    await customElements.whenDefined('ssr-keep');
+    await customElements.whenDefined('replace-with-children');
+    await customElements.whenDefined('replace-with-grandchildren');
 
     // Check if any `<ssr-keep>` elements have registered themselves here. If so, kick off the one-time hydration prep task.
     if (this.ssrKeep && !this.ssrPrepped) {
       this.ssrHydrationPrep();
     }
+
+    super.connectedCallback && super.connectedCallback();
   }
 
   /**
