90d997f5fc1416d515428e34551759e1fed84343
Salem Ghoweri <me@salemghoweri.com>
2020-04-29 15:27:48 -0400

    refactor: cherry pick remaining Accordion async updates for testing

--- packages/components/bolt-accordion/index.js
+++ packages/components/bolt-accordion/index.js
@@ -1,17 +1,3 @@
-import { polyfillLoader } from '@bolt/core-v3.x/polyfills';
-
-polyfillLoader.then(res => {
-  import(
-    /*
-    webpackMode: 'eager',
+import(/*
     webpackChunkName: 'bolt-accordion'
-  */ './src/accordion'
-  );
-
-  import(
-    /*
-    webpackMode: 'eager',
-    webpackChunkName: 'bolt-accordion-item'
-  */ './src/AccordionItem'
-  );
-});
+  */ './main');
