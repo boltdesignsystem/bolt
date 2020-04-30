e2f2d58204f1b0a4f467477c6ba684a0dcc466f5
Salem Ghoweri <me@salemghoweri.com>
2020-04-30 12:24:04 -0400

    refactor: tweak loading behavior so SSR-related helper elements are proactively loaded

--- packages/core-v3.x/elements/index.js
+++ packages/core-v3.x/elements/index.js
@@ -3,9 +3,6 @@ import { lazyQueue } from '@bolt/lazy-queue';
 // this import shouldn't be required but keeping these here just in case
 // import(/* webpackChunkName: 'bolt-action' */ './bolt-action');
 
-lazyQueue(
-  ['replace-with-children', 'replace-with-grandchildren', 'ssr-keep'],
-  async () => {
-    import(/* webpackChunkName: 'bolt-elements' */ './elements');
-  },
-);
+lazyQueue([], async () => {
+  import(/* webpackChunkName: 'bolt-elements' */ './elements');
+});
