874f84a65e40f14bbfb41021ff7ef14a15f96ab6
Salem Ghoweri <me@salemghoweri.com>
2020-05-04 14:03:06 -0400

    chore: temporarily switch back to using a static import till https://github.com/boltdesignsystem/bolt/pull/1858 is merged in

--- packages/core-v3.x/elements/index.js
+++ packages/core-v3.x/elements/index.js
@@ -1,3 +1,6 @@
-import(
-  /* webpackChunkName: 'bolt-ssr-helper-elements' */ './ssr-helper-elements'
-);
+// import(
+//   /* webpackChunkName: 'bolt-ssr-helper-elements' */ './ssr-helper-elements'
+// );
+
+// @todo: switch from a static import ▼ to a dynamic import ▲ once Accordion is migrated to LitElement (https://github.com/boltdesignsystem/bolt/pull/1858)
+import './ssr-helper-elements';
