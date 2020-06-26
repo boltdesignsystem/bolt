0a5d2c7a97ee143c0512d29b8a62505e0bcf0328
Marcin Religa <marcin.religa@pega.com>
2020-06-26 14:37:02 +0200

    Remove calling main.js file with a chunkname.

--- packages/experimental/micro-journeys/index.js
+++ packages/experimental/micro-journeys/index.js
@@ -1,5 +1,5 @@
 import { lazyQueue } from '@bolt/lazy-queue';
 
 lazyQueue(['bolt-interactive-pathway'], async () => {
-  await import(/* webpackChunkName: "bolt-micro-journeys" */ './main');
+  await import('./main');
 });
