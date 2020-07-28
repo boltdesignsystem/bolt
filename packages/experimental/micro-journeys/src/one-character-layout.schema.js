a3328d129734f1a237b7750403e070fd51d91430
Rémy Denton <Remy.Denton@pega.com>
2020-07-28 12:47:37 -0400

    Add empty properties to micro-journeys schemas

--- packages/experimental/micro-journeys/src/one-character-layout.schema.js
+++ packages/experimental/micro-journeys/src/one-character-layout.schema.js
@@ -2,4 +2,7 @@ module.exports = {
   $schema: 'http://json-schema.org/draft-04/schema#',
   title: 'One-Character Layout',
   type: 'object',
+  properties: {
+    // One-character layout has no defined properties.
+  },
 };
--- packages/experimental/micro-journeys/src/two-character-layout.schema.js
+++ packages/experimental/micro-journeys/src/two-character-layout.schema.js
@@ -2,4 +2,7 @@ module.exports = {
   $schema: 'http://json-schema.org/draft-04/schema#',
   title: 'Two-Character Layout',
   type: 'object',
+  properties: {
+    // Two-character layout has no defined properties.
+  },
 };
