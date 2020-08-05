332fb6d9089ec9449ca721f72f8191bb8c350589
Colby Cook <colby.cook@pega.com>
2020-08-05 16:43:33 -0400

    DS-161: Updated the calculator twig template to use the bolt components

--- packages/core-v3.x/modules/modules.js
+++ packages/core-v3.x/modules/modules.js
@@ -1,7 +1,6 @@
 // Note: These modules are included everywhere, so keep it light. Otherwise,
 // consider adding to a component where JS will be loaded only when needed.
 import ClickHandler from './src/click-handler';
-import Calculator from './src/calculator';
 
 const Modules = { ClickHandler, Calculator };
 const elements = document.querySelectorAll('[data-module]');
