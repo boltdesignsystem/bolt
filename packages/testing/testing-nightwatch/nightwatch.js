ebf25569b2d9a21a2f5ce06769a417cd97a5e4b7
Colby Cook <colby.cook@pega.com>
2020-08-26 16:24:40 -0400

    Updated the Chromedriver version to the supported number

--- packages/testing/testing-nightwatch/nightwatch.js
+++ packages/testing/testing-nightwatch/nightwatch.js
@@ -87,7 +87,7 @@ module.exports = {
         javascriptEnabled: true,
         acceptSslCerts: true,
         chromeOptions: {
-          // args: ['headless'],
+          args: ['headless'],
           w3c: false,
         },
       },
