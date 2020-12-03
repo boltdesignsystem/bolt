699499c0674e33000fae7072fd32bd65ee837619
Daniel Morse <daniel.morse@pega.com>
2020-12-03 12:09:25 -0500

    DS-268: Comment out unused JS

--- packages/components/bolt-copy-to-clipboard/src/copy-to-clipboard.standalone.js
+++ packages/components/bolt-copy-to-clipboard/src/copy-to-clipboard.standalone.js
@@ -1,26 +1,32 @@
 import { html, customElement, BoltElement, unsafeCSS } from '@bolt/element';
 import ClipboardJS from 'clipboard';
-import classNames from 'classnames/bind';
-import styles from './copy-to-clipboard.scss';
-import schema from '../copy-to-clipboard.schema';
 
-let cx = classNames.bind(styles);
+/**
+ * 1. This web component does not currently use props or the shadow dom, so these lines of code are not required.
+ *    Uncomment if we later decide to rebuild this as a full-featured web component.
+ */
+
+// import classNames from 'classnames/bind'; /* [1] */
+// import styles from './copy-to-clipboard.scss'; /* [1] */
+// import schema from '../copy-to-clipboard.schema'; /* [1] */
+
+// let cx = classNames.bind(styles); /* [1] */
 
 @customElement('bolt-copy-to-clipboard')
 class BoltCopyToClipboard extends BoltElement {
-  static schema = schema;
+  // static schema = schema; /* [1] */
 
-  static get properties() {
-    return {
-      ...this.props,
-    };
-  }
+  // static get properties() {
+  //   return {
+  //     ...this.props,
+  //   };
+  // } /* [1] */
 
   static useShadow = false;
 
-  static get styles() {
-    return [unsafeCSS(styles)];
-  }
+  // static get styles() {
+  //   return [unsafeCSS(styles)];
+  // } /* [1] */
 
   connectedCallback() {
     super.connectedCallback && super.connectedCallback();
