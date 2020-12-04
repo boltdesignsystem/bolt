299b7e7460d9709ecedf0f49385cd02a665da7bf
CJ White <hello@cjwhitedev.com>
2020-12-04 14:50:39 -0500

    DS-267: renamed style variable

--- packages/components/bolt-card-replacement/src/card-replacement-body/card-replacement-body.js
+++ packages/components/bolt-card-replacement/src/card-replacement-body/card-replacement-body.js
@@ -1,17 +1,17 @@
 import { unsafeCSS, BoltElement, customElement, html } from '@bolt/element';
 import { withContext } from 'wc-context/lit-element';
 import classNames from 'classnames/bind';
-import bodyStyles from './_card-replacement-body.scss';
+import styles from './_card-replacement-body.scss';
 import schema from '../../card-replacement.schema';
 
-let cx = classNames.bind(bodyStyles);
+let cx = classNames.bind(styles);
 
 @customElement('bolt-card-replacement-body')
 class BoltCardReplacementBody extends withContext(BoltElement) {
   static schema = schema;
 
   static get styles() {
-    return [unsafeCSS(bodyStyles)];
+    return [unsafeCSS(styles)];
   }
 
   static get properties() {
