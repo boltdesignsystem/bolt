0d47c81ed50e890b2eb5cfb3a77736389bfce370
Mike Mai <emceehao@gmail.com>
2020-12-17 12:47:50 -0500

    ds-152: add page header schema and fix templates

---
+++ packages/components/bolt-page-header/page-header-primary-nav.schema.js
@@ -0,0 +1,17 @@
+module.exports = {
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  title: 'Page Header Primary Nav',
+  type: 'object',
+  required: 'content',
+  properties: {
+    attributes: {
+      type: 'object',
+      description:
+        'A Drupal attributes object. Applies extra HTML attributes to the parent element.',
+    },
+    content: {
+      type: ['string', 'array', 'object'],
+      description: 'Content of page header primary nav.',
+    },
+  },
+};
--- packages/components/bolt-page-header/page-header.schema.js
+++ packages/components/bolt-page-header/page-header.schema.js
@@ -1,6 +1,59 @@
 module.exports = {
   $schema: 'http://json-schema.org/draft-04/schema#',
   title: 'Page Header',
-  category: 'components',
   type: 'object',
+  required: ['content', 'logo'],
+  properties: {
+    attributes: {
+      type: 'object',
+      description:
+        'A Drupal attributes object. Applies extra HTML attributes to the parent element.',
+    },
+    content: {
+      type: ['string', 'array', 'object'],
+      description:
+        'Primary content of page header (primary nav, utility nav, etc.).',
+    },
+    secondary_content: {
+      type: ['string', 'array', 'object'],
+      description:
+        'Secondary content of page header (sub nav, visible search bar, breadcrumbs, etc.).',
+    },
+    logo: {
+      type: 'object',
+      description:
+        'Set the site logo. This can be turned into a link by passing the proper attributes such as "href" and "target".',
+      properties: {
+        attributes: {
+          type: 'object',
+          description:
+            'A Drupal attributes object. Applies extra HTML attributes to the parent element.',
+        },
+        image_src: {
+          type: 'string',
+          description: 'Set the path to the logo image.',
+        },
+        label: {
+          type: 'string',
+          description: 'Set a label (visually hidden) for the logo.',
+        },
+      },
+    },
+    cta: {
+      type: 'object',
+      description:
+        'Set the main call-to-action. This can be either a semantic link or button. Passing the "href" attribute will turn it into a link while the "type" attribute will turn it into a button.',
+      properties: {
+        attributes: {
+          type: 'object',
+          description:
+            'A Drupal attributes object. Applies extra HTML attributes to the parent element.',
+        },
+        content: {
+          type: 'string',
+          description: 'Content of the call-to-action.',
+        },
+      },
+    },
+  },
 };
