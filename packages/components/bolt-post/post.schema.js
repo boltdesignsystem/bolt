a66650a937616f78cbe3e4d4b677a8a924016710
Mike Mai <emceehao@gmail.com>
2021-02-01 18:18:46 -0500

    ds-214: set up schema and docs

--- packages/components/bolt-post/post.schema.js
+++ packages/components/bolt-post/post.schema.js
@@ -6,13 +6,107 @@ module.exports = {
     attributes: {
       type: 'object',
       description:
-        'A Drupal attributes object. Applies extra HTML attributes to the &lt;bolt-post&gt; tag.',
+        'A Drupal-style attributes object with extra attributes to append to this component.',
     },
-    disabled: {
-      type: 'boolean',
+    signifier: {
+      type: 'any',
       description:
-        'Default `disabled` prop supported globally by most Bolt components.',
-      default: false,
+        'Set a visual signifier for the post. An icon or decorative image usually goes here.',
+    },
+    headline: {
+      type: 'any',
+      description: 'Render the headline of the post.',
+      properties: {
+        text: {
+          type: 'string',
+          description: 'Set the text of the headline.',
+        },
+        tag: {
+          type: 'string',
+          description: 'Set the semantic HTML tag for the headline.',
+        },
+        size: {
+          type: 'string',
+          description: 'Set the size of the headline.',
+        },
+        link_attributes: {
+          type: 'object',
+          description:
+            'A Drupal-style attributes object with extra attributes to append to the headline link.',
+        },
+      },
+    },
+    meta_items: {
+      type: 'array',
+      description: 'Render an array of meta data for the post.',
+    },
+    status: {
+      type: 'object',
+      description: 'Set the status of the post.',
+      properties: {
+        solved: {
+          type: 'boolean',
+          description: 'Mark the post as solved.',
+        },
+        solved_label: {
+          type: 'string',
+          description: 'Set a custom label for the "solved indicator".',
+        },
+        locked: {
+          type: 'boolean',
+          description: 'Mark the post as locked.',
+        },
+        locked_label: {
+          type: 'string',
+          description: 'Set a custom label for the "locked indicator".',
+        },
+        replies: {
+          type: 'string',
+          description: 'Set the count of replies.',
+        },
+        views: {
+          type: 'string',
+          description: 'Set the count of views.',
+        },
+        favorited: {
+          type: 'boolean',
+          description: 'Mark the post as favorited.',
+        },
+        subscribed: {
+          type: 'boolean',
+          description: 'Mark the post as subscribed.',
+        },
+      },
+    },
+    show_actions: {
+      type: 'object',
+      description: 'Render a popover action menu for the post.',
+      properties: {
+        reply: {
+          type: 'boolean',
+          description: 'Show the reply action.',
+        },
+        favorite: {
+          type: 'boolean',
+          description:
+            'Show the favorite action. Label for this action is updated according to the status of the post.',
+        },
+        subscribe: {
+          type: 'boolean',
+          description:
+            'Show the subscribe action. Label for this action is updated according to the status of the post.',
+        },
+      },
+    },
+    more_info: {
+      type: 'string',
+      description:
+        'Render more information for the post. Short post summary usually goes here.',
+    },
+    warning: {
+      type: 'string',
+      description:
+        'Render warning text for the post, which appears below the "more info" content.',
     },
   },
 };
