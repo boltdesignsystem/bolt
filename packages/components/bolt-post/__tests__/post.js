54be44e6f13c513e7c0d00eccb84019d79ff00db
Mike Mai <emceehao@gmail.com>
2021-02-02 14:50:45 -0500

    ds-214: fix test snaps

--- packages/components/bolt-post/__tests__/post.js
+++ packages/components/bolt-post/__tests__/post.js
@@ -32,69 +32,39 @@ describe('Post', () => {
   }, timeout);
 
   test('basic usage', async () => {
-    const results = await render('@bolt-components-post/post.twig');
-    expect(results.ok).toBe(true);
-    expect(results.html).toMatchSnapshot();
-  });
-
-  test('adds class via Drupal Attributes', async () => {
     const results = await render('@bolt-components-post/post.twig', {
-      attributes: {
-        class: ['u-bolt-margin-top-medium'],
+      headline: {
+        text: 'This is a post',
+        tag: 'h3',
+        size: 'xlarge',
+        link_attributes: {
+          href: 'https://pega.com',
+        },
       },
-    });
-    expect(results.ok).toBe(true);
-    expect(results.html).toMatchSnapshot();
-  });
-
-  test('renders with Shadow DOM', async function() {
-    const shadowRoot = await page.evaluate(async () => {
-      document.body.insertAdjacentHTML(
-        'beforeend',
-        '<bolt-post>Post test</bolt-post>',
-      );
-      const el = document.querySelector('bolt-post');
-      await el.updateComplete;
-      return el.renderRoot.innerHTML;
-    });
-
-    const outerHTML = await page.evaluate(async () => {
-      const el = document.querySelector('bolt-post');
-      await el.updateComplete;
-      return el.outerHTML;
-    });
-
-    const renderedShadowDomHTML = await html(shadowRoot);
-    expect(renderedShadowDomHTML).toMatchSnapshot();
-
-    const renderedHTML = await html(outerHTML);
-    expect(renderedHTML).toMatchSnapshot();
-
-    const image = await page.screenshot();
-    expect(image).toMatchImageSnapshot(vrtDefaultConfig);
-  });
-
-  test('renders without Shadow DOM', async function() {
-    const outerHTML = await page.evaluate(async () => {
-      document.body.insertAdjacentHTML(
-        'beforeend',
-        '<bolt-post no-shadow>Post test</bolt-post>',
-      );
-      const el = document.querySelector('bolt-post');
-      await el.updateComplete;
-      return el.outerHTML;
-    });
-
-    const renderedHTML = await html(outerHTML);
-    expect(renderedHTML).toMatchSnapshot();
-
-    const image = await page.screenshot();
-    expect(image).toMatchImageSnapshot(vrtDefaultConfig);
-  });
-
-  test(`sets 'disabled' prop`, async () => {
-    const results = await render('@bolt-components-post/post.twig', {
-      disabled: true,
+      meta_items: [
+        'Posted 8 hours 15 minutes ago',
+        'Last activity: 2 minutes ago',
+      ],
+      status: {
+        solved: true,
+        solved_label: 'custom solved label',
+        locked: true,
+        locked_label: 'custom locked label',
+        replies: '33',
+        views: '123',
+        favorited: true,
+        subscribed: true,
+      },
+      show_actions: {
+        reply: true,
+        favorite: true,
+        subscribe: true,
+      },
+      more_info: 'This is the more information',
+      warning: 'This is a warning message',
+      attributes: {
+        'data-foo': 'bar',
+      },
     });
     expect(results.ok).toBe(true);
     expect(results.html).toMatchSnapshot();
