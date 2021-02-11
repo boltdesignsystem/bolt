55642ff80418d16ea3a73976d6e645c5406b9404
Mike Mai <emceehao@gmail.com>
2021-02-11 16:57:26 -0500

    ds-296: set up proper docs

--- packages/elements/bolt-image/image.schema.js
+++ packages/elements/bolt-image/image.schema.js
@@ -13,5 +13,22 @@ module.exports = {
       description:
         'Render the image 100% wide, filling up the full width of its parent container.',
     },
+    background: {
+      type: 'object',
+      description:
+        'Render the image as a background image. This sets the image to be absolute positioned, only use this prop if the image is inside a non-static container.',
+      properties: {
+        fit: {
+          type: 'string',
+          description: 'Control the fitting of the background image.',
+          enum: ['cover', 'contain', 'none'],
+        },
+        position: {
+          type: 'string',
+          description:
+            'Control the position of the background image. It accepts placement tokens (e.g. top left, top right, bottom left, bottom right, center) and x-axis/y-axis percentage (e.g. 50% 50%, 0% 100%, 50% 0%).',
+        },
+      },
+    },
   },
 };
