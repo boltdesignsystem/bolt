4df30fb5b4296649f9e27bb1c2444458429b4728
CJ White <hello@cjwhitedev.com>
2021-02-25 09:08:50 -0500

    DS-322: rework schema

--- packages/components/bolt-teaser/teaser.schema.js
+++ packages/components/bolt-teaser/teaser.schema.js
@@ -1,3 +1,4 @@
+const imageSchema = require('@bolt/components-image/image.schema.js');
 const headlineSchema = require('@bolt/components-headline/headline.schema.js');
 const chipListSchema = require('@bolt/components-chip-list/chip-list.schema.js');
 
@@ -11,16 +12,18 @@ module.exports = {
       description:
         'A Drupal attributes object. Applies extra HTML attributes to the &lt;bolt-teaser&gt; tag.',
     },
-    image_trigger: {
+    image: imageSchema,
+    headline: headlineSchema,
+    modal_id: {
+      type: 'string',
+      description: '',
+    },
+    modal_content: {
       type: 'object',
       description: '',
     },
-    text_trigger: {
-      type: 'object',
-      description: '',
-    },
-    modal: {
-      type: 'object',
+    url: {
+      type: 'string',
       description: '',
     },
     chip_list: chipListSchema,
