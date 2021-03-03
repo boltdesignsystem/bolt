2ffeb3f31b2da41628b85c1ec6f48afdfe103a63
CJ White <hello@cjwhitedev.com>
2021-03-03 13:36:54 -0500

    DS-322: split demos, remove js, add meta line, horizontal, and show_description.

--- packages/components/bolt-teaser/teaser.schema.js
+++ packages/components/bolt-teaser/teaser.schema.js
@@ -1,4 +1,3 @@
-const imageSchema = require('@bolt/components-image/image.schema.js');
 const chipListSchema = require('@bolt/components-chip-list/chip-list.schema.js');
 
 module.exports = {
@@ -11,7 +10,10 @@ module.exports = {
       description:
         'A Drupal attributes object. Applies extra HTML attributes to the &lt;bolt-teaser&gt; tag.',
     },
-    image: imageSchema,
+    image: {
+      type: 'object',
+      description: '',
+    },
     headline: {
       type: 'object',
       description: '',
@@ -34,10 +36,32 @@ module.exports = {
       type: 'boolean',
       description: '',
     },
+    horizontal: {
+      type: 'boolean',
+      description: '',
+    },
+    show_description: {
+      type: 'boolean',
+      default: false,
+      description: '',
+    },
     type: {
       type: 'string',
       enum: ['pdf', 'video', 'article'],
     },
     chip_list: chipListSchema,
+    meta_items: {
+      type: 'array',
+      description: 'Render an array of meta data for the listing.',
+    },
+    like_button: {
+      type: 'object',
+      description: '',
+    },
+    share_menu: {
+      type: 'any',
+      description:
+        'Render a popover share menu for the listing. Passing the Share component is mandatory.',
+    },
   },
 };
