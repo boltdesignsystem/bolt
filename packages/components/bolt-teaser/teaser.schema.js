c86969d9845a0587b01c219c50026d9754960250
CJ White <hello@cjwhitedev.com>
2021-03-04 14:00:09 -0500

    DS-322: update schema

--- packages/components/bolt-teaser/teaser.schema.js
+++ packages/components/bolt-teaser/teaser.schema.js
@@ -10,13 +10,36 @@ module.exports = {
       description:
         'A Drupal attributes object. Applies extra HTML attributes to the &lt;bolt-teaser&gt; tag.',
     },
+    layout: {
+      type: 'string',
+      description: '',
+      enum: ['vertical', 'horizontal', 'responsive'],
+      default: 'vertical',
+    },
     image: {
       type: 'object',
-      description: '',
+      description: 'The image to fill the thumbnail, accepts rendered images.',
+    },
+    premium: {
+      type: 'boolean',
+      description:
+        'If set to true, shows a premium tag in the upper left corner of the thumbnail',
+      default: 'false',
+    },
+    type: {
+      type: 'string',
+      description:
+        'Will add an icon to the bottom left corner of the thumbnail to denote teaser type.',
+      enum: ['pdf', 'video', 'article'],
+    },
+    eyebrow_text: {
+      type: 'string',
+      description: 'Used to put an eyebrow above the headline.',
     },
     headline: {
       type: 'object',
-      description: '',
+      description:
+        'Used to set the headline text, size, and the link for the entire teaser.',
       properties: {
         text: {
           type: 'string',
@@ -34,46 +57,30 @@ module.exports = {
         },
       },
     },
-    eyebrow_text: {
-      type: 'string',
-      description: '',
-    },
-    description: {
-      type: 'string',
-      description: '',
-    },
-    premium: {
-      type: 'boolean',
-      description: '',
-    },
-    layout: {
-      type: 'string',
-      description: '',
-      enum: ['vertical', 'horizontal', 'responsive'],
-      default: 'vertical',
-    },
-    show_description: {
-      type: 'boolean',
-      default: false,
-      description: '',
-    },
-    type: {
-      type: 'string',
-      enum: ['pdf', 'video', 'article'],
-    },
-    chip_list: chipListSchema,
     meta_items: {
       type: 'array',
       description: 'Render an array of meta data for the listing.',
     },
     like_button: {
       type: 'object',
-      description: '',
+      description: 'Pass a rendered button to display.',
     },
     share_menu: {
       type: 'any',
       description:
         'Render a popover share menu for the listing. Passing the Share component is mandatory.',
     },
+    description: {
+      type: 'string',
+      description:
+        'A breif description of the intended link. Trimmed to 100 characters when displayed inside the thumbnail.',
+    },
+    show_description: {
+      type: 'boolean',
+      default: false,
+      description:
+        'If set to true, will move the description out of the thumbnail area.',
+    },
+    chip_list: chipListSchema,
   },
 };
