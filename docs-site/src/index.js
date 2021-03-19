8b5be96aaa691811af0a90b83ebacbf218cac595
Mike Mai <emceehao@gmail.com>
2021-03-19 17:32:13 -0400

    ds-298: set up mobile sidebar toggles

--- docs-site/src/index.js
+++ docs-site/src/index.js
@@ -114,3 +114,16 @@ sideNavLinkToggleTrigger.forEach(function(el) {
     }
   });
 });
+
+var holyGrailSidebarToggleTrigger = document.querySelectorAll(
+  '.js-bolt-holy-grail-sidebar-toggle-trigger',
+);
+holyGrailSidebarToggleTrigger.forEach(function(el) {
+  el.addEventListener('click', function(event) {
+    if (el.getAttribute('aria-expanded') === 'true') {
+      el.setAttribute('aria-expanded', 'false');
+    } else {
+      el.setAttribute('aria-expanded', 'true');
+    }
+  });
+});
