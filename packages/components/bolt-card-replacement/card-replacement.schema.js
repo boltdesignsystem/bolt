e5c2c9a389654b5fad3735373b828c4c15e9ce0b
CJ White <hello@cjwhitedev.com>
2021-03-23 09:23:57 -0400

    DS-308: deprecate borderRadius

--- packages/components/bolt-card-replacement/card-replacement.schema.js
+++ packages/components/bolt-card-replacement/card-replacement.schema.js
@@ -25,7 +25,7 @@ module.exports = {
       default: 'auto',
       enum: ['auto', 'full'],
     },
-    borderRadius: {
+    border_radius: {
       type: 'string',
       description: 'Controls the border-radius of the card-replacement.',
       default: 'small',
@@ -143,6 +143,9 @@ module.exports = {
       description:
         'Manually switch on / off the raised (shadow + animation effect) treament. By default this config option is applied if the card-replacement contains a bolt-card-replacement-link OR includes the `url` prop.',
       hidden: true,
+    },
+    borderRadius: {
+      title: 'DEPRECATED',
     },
   },
 };
