f0506dc6f5b5c25c63fd9c3d8d44f3121e4b3799
Mike Mai <emceehao@gmail.com>
2021-03-26 16:44:30 -0400

    ds-298: set up article twig

--- packages/layouts/bolt-holy-grail/src/holy-grail.js
+++ packages/layouts/bolt-holy-grail/src/holy-grail.js
@@ -6,8 +6,10 @@ holyGrailSidebarToggleTrigger.forEach(function(el) {
   el.addEventListener('click', function(event) {
     if (el.getAttribute('aria-expanded') === 'true') {
       el.setAttribute('aria-expanded', 'false');
+      document.body.classList.remove('u-bolt-overflow-hidden');
     } else {
       el.setAttribute('aria-expanded', 'true');
+      document.body.classList.add('u-bolt-overflow-hidden');
     }
   });
 });
