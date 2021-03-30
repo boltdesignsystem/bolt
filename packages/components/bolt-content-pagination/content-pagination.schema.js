929f4491a889d272114926523aa31dbf07ebf64e
Mike Mai <emceehao@gmail.com>
2021-03-30 18:04:47 -0400

    ds-298: finish content pagination

--- packages/components/bolt-content-pagination/content-pagination.schema.js
+++ packages/components/bolt-content-pagination/content-pagination.schema.js
@@ -8,5 +8,62 @@ module.exports = {
       description:
         'A Drupal attributes object. Applies extra HTML attributes to the parent element.',
     },
+    parent: {
+      type: 'object',
+      description: 'Set the parent page link.',
+      properties: {
+        content: {
+          type: 'string',
+          description: 'Text content of the link.',
+        },
+        tooltip_content: {
+          type: 'string',
+          description: 'Text content of the tooltip.',
+        },
+        link_attributes: {
+          type: 'object',
+          description:
+            'A Drupal attributes object. Applies extra HTML attributes to the link element.',
+        },
+      },
+    },
+    previous: {
+      type: 'object',
+      description: 'Set the previous page link.',
+      properties: {
+        content: {
+          type: 'string',
+          description: 'Text content of the link.',
+        },
+        tooltip_content: {
+          type: 'string',
+          description: 'Text content of the tooltip.',
+        },
+        link_attributes: {
+          type: 'object',
+          description:
+            'A Drupal attributes object. Applies extra HTML attributes to the link element.',
+        },
+      },
+    },
+    next: {
+      type: 'object',
+      description: 'Set the next page link.',
+      properties: {
+        content: {
+          type: 'string',
+          description: 'Text content of the link.',
+        },
+        tooltip_content: {
+          type: 'string',
+          description: 'Text content of the tooltip.',
+        },
+        link_attributes: {
+          type: 'object',
+          description:
+            'A Drupal attributes object. Applies extra HTML attributes to the link element.',
+        },
+      },
+    },
   },
 };
