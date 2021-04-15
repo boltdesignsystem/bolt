63ca29c0378af7db6afd3f67bd71e7fa674178f3
Dan Morse <danielamorse@gmail.com>
2021-04-15 17:57:29 -0400

    DS-298: Close modal on escape, refactor JS into Classes

--- packages/components/bolt-side-nav/src/side-nav.js
+++ packages/components/bolt-side-nav/src/side-nav.js
@@ -1,13 +1,28 @@
-const sideNavLinkToggleTrigger = document.querySelectorAll(
-  '[data-bolt-side-nav-toggle-trigger]',
-);
+export class BoltSideNav {
+  constructor(el) {
+    if (!el) return;
+    this.el = el;
+    this.init();
+  }
 
-sideNavLinkToggleTrigger.forEach(el => {
-  el.addEventListener('click', event => {
-    if (el.getAttribute('aria-expanded') === 'true') {
-      el.setAttribute('aria-expanded', 'false');
-    } else {
-      el.setAttribute('aria-expanded', 'true');
-    }
-  });
+  init() {
+    const linkToggleTrigger = this.el.querySelectorAll(
+      '[data-bolt-side-nav-toggle-trigger]',
+    );
+    linkToggleTrigger.forEach(el => {
+      el.addEventListener('click', event => {
+        if (el.getAttribute('aria-expanded') === 'true') {
+          el.setAttribute('aria-expanded', 'false');
+        } else {
+          el.setAttribute('aria-expanded', 'true');
+        }
+      });
+    });
+  }
+}
+
+const sideNavs = document.querySelectorAll('[data-bolt-side-nav]');
+
+sideNavs.forEach(el => {
+  const component = new BoltSideNav(el);
 });
--- packages/layouts/bolt-holy-grail/src/holy-grail.js
+++ packages/layouts/bolt-holy-grail/src/holy-grail.js
@@ -1,15 +1,63 @@
-const holyGrailToggleTrigger = document.querySelectorAll(
-  '[data-bolt-holy-grail-toggle-trigger]',
+export class BoltHolyGrail {
+  constructor(el) {
+    if (!el) return;
+    this.el = el;
+    this.handleEscapeKeypress = this.handleEscapeKeypress.bind(this);
+    this.state = {
+      activeElement: null,
+    };
+    this.init();
+  }
+
+  init() {
+    const toggleTrigger = this.el.querySelectorAll(
+      '[data-bolt-holy-grail-toggle-trigger]',
+    );
+    toggleTrigger.forEach(el => {
+      el.addEventListener('click', event => {
+        if (el.getAttribute('aria-expanded') === 'true') {
+          this.hide(el);
+        } else {
+          this.show(el);
+        }
+      });
+    });
+  }
+
+  show(el) {
+    this.state.activeElement = el;
+    el.setAttribute('aria-expanded', 'true');
+    document.body.classList.add('u-bolt-overflow-hidden');
+    document.addEventListener('keydown', this.handleEscapeKeypress);
+  }
+
+  hide(el) {
+    el.setAttribute('aria-expanded', 'false');
+    document.body.classList.remove('u-bolt-overflow-hidden');
+    this.state.activeElement.focus();
+    this.state.activeElement = null;
+    document.removeEventListener('keydown', this.handleEscapeKeypress);
+  }
+
+  getKey(e) {
+    if (e.key !== undefined) {
+      return e.key;
+    } else if (e.keyCode !== undefined) {
+      return e.keyCode;
+    }
+  }
+
+  handleEscapeKeypress(e) {
+    if (this.getKey(e) === 'Escape' || this.getKey(e) === 27) {
+      this.hide(this.state.activeElement);
+    }
+  }
+}
+
+const holyGrailLayouts = document.querySelectorAll(
+  '[data-bolt-holy-grail-layout]',
 );
 
-holyGrailToggleTrigger.forEach(el => {
-  el.addEventListener('click', event => {
-    if (el.getAttribute('aria-expanded') === 'true') {
-      el.setAttribute('aria-expanded', 'false');
-      document.body.classList.remove('u-bolt-overflow-hidden');
-    } else {
-      el.setAttribute('aria-expanded', 'true');
-      document.body.classList.add('u-bolt-overflow-hidden');
-    }
-  });
+holyGrailLayouts.forEach(el => {
+  const component = new BoltHolyGrail(el);
 });
