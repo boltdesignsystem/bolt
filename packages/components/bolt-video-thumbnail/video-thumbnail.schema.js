62063125a1bad5d3c2f3203c7a7d7c7ea782d459
Mike Mai <emceehao@gmail.com>
2021-05-03 17:33:38 -0400

    ds-443: set up video thumbnail

---
+++ packages/components/bolt-video-thumbnail/video-thumbnail.schema.js
@@ -0,0 +1,34 @@
+module.exports = {
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  title: 'Video Thumbnail',
+  type: 'object',
+  properties: {
+    attributes: {
+      type: 'object',
+      description:
+        'A Drupal attributes object. Applies extra HTML attributes to the parent element.',
+    },
+    content: {
+      type: 'any',
+      description:
+        'Content of the video thumbnail. An image pointing to the video poster is recommended.',
+    },
+    border_radius: {
+      type: 'string',
+      description: 'Set the border radius of the video thumbnail.',
+      enum: ['none', 'small', 'large'],
+      default: 'small',
+    },
+    aspect_ratio: {
+      type: 'string',
+      description: 'Set the aspect ratio of the video thumbnail.',
+      enum: ['1:1', '16:9', '4:3'],
+      default: '4:3',
+    },
+    duration: {
+      type: 'string',
+      description:
+        'Display the video duration that overlays the video thumbnail.',
+    },
+  },
+};
