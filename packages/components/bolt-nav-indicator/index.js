ac0950c688e9f6075e3dab41ae5b82c5d8dc8c48
Dan Morse <danielamorse@gmail.com>
2021-05-06 10:15:17 -0400

    DS-248: Remove nav-indicator, nav-priority, navlink

--- packages/components/bolt-nav-indicator/index.js
+++
@@ -1,7 +0,0 @@
-import { lazyQueue } from '@bolt/lazy-queue';
-
-lazyQueue(['bolt-nav-indicator'], async () => {
-  await import(
-    /* webpackChunkName: 'bolt-nav-indicator' */ './nav-indicator.js'
-  );
-});
