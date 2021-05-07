0f71e7aebbd48d8cadcad579d7adb742927f5c4e
Colby Cook <colby.cook@pega.com>
2021-05-07 08:57:50 -0400

    DS-411: Updated the blockquote and breadcrumb Jests tests and started to work on the carousel Jest test

--- packages/components/bolt-breadcrumb/__tests__/breadcrumb.js
+++ packages/components/bolt-breadcrumb/__tests__/breadcrumb.js
@@ -44,7 +44,6 @@ afterAll(async () => {
 
 describe('Bolt Breadcrumb', () => {
   test('default', async () => {
-    //console.log(fixtures.linkOne);
     const results = await render(
       '@bolt-components-breadcrumb/breadcrumb.twig',
       {
@@ -56,150 +55,31 @@ describe('Bolt Breadcrumb', () => {
       },
     );
 
-    console.log(componentSelector);
-    console.log(results.html);
-    console.log(page);
+    await expect(results.ok).toBe(true);
+    await expect(results.html).toMatchSnapshot();
+  });
 
-    const { innerHTML, outerHTML } = await renderWC(
-      componentSelector,
-      results.html,
-      page,
+  test('basic usage with contentItems including rendered components and strings', async () => {
+    const linkFour = await render('@bolt-components-link/link.twig', {
+      text: 'Sub Page',
+      url: '#!',
+      attributes: {
+        'aria-current': true,
+      },
+    });
+    const results = await render(
+      '@bolt-components-breadcrumb/breadcrumb.twig',
+      {
+        contentItems: [
+          fixtures.linkOne.html,
+          fixtures.linkTwo.html,
+          fixtures.linkThree.html,
+          linkFour,
+        ],
+      },
     );
 
     await expect(results.ok).toBe(true);
     await expect(results.html).toMatchSnapshot();
-    await expect(innerHTML).toMatchSnapshot();
-    await expect(outerHTML).toMatchSnapshot();
   });
-
-  // test('basic usage with contentItems including rendered components and strings', async () => {
-  //   const linkFour = await render('@bolt-components-link/link.twig', {
-  //     text: 'Sub Page',
-  //     url: '#!',
-  //     attributes: {
-  //       "aria-current": true
-  //     },
-  //   });
-  // });
 });
-
-// describe('<bolt-breadcrumb> Component', () => {
-//   test('basic usage with attributes', async () => {
-//     const linkOne = await render('@bolt-components-link/link.twig', {
-//       text: 'Home',
-//       url: '#!',
-//     });
-//     const linkTwo = await render('@bolt-components-link/link.twig', {
-//       text: 'Other Page',
-//       url: '#!',
-//     });
-//     const results = await render(
-//       '@bolt-components-breadcrumb/breadcrumb.twig',
-//       {
-//         contentItems: [linkOne.html, linkTwo.html],
-//         attributes: {
-//           'data-foobar': 'baz',
-//           'aria-role': 'list',
-//         },
-//       },
-//     );
-//     expect(results.ok).toBe(true);
-//     expect(results.html).toMatchSnapshot();
-//   });
-
-//   test('basic usage with contentItems including rendered components and strings', async () => {
-//     const results = await renderString(`
-// {% include "@bolt-components-breadcrumb/breadcrumb.twig" with {
-//   contentItems: [
-//     include("@bolt-components-link/link.twig", {
-//       text: "Home",
-//       url: "#!"
-//     }),
-//     include("@bolt-components-link/link.twig", {
-//       text: "Landing Page",
-//       url: "#!"
-//     }),
-//     include("@bolt-components-link/link.twig", {
-//       text: "Sub Page",
-//       url: "#!"
-//     }),
-//     "Current Page"
-//   ]
-// } only %}
-//     `);
-//     expect(results.ok).toBe(true);
-//     expect(results.html).toMatchSnapshot();
-//   });
-
-//   test('current page aria variation', async () => {
-//     const results = await renderString(`
-// {% include "@bolt-components-breadcrumb/breadcrumb.twig" with {
-//   contentItems: [
-//     include("@bolt-components-link/link.twig", {
-//       text: "Home",
-//       url: "#!"
-//     }),
-//     include("@bolt-components-link/link.twig", {
-//       text: "Landing Page",
-//       url: "#!"
-//     }),
-//     include("@bolt-components-link/link.twig", {
-//       text: "Sub Page",
-//       url: "#!"
-//     }),
-//     include("@bolt-components-link/link.twig", {
-//       text: "Current Page",
-//       url: "#!",
-//       attributes: {
-//         "aria-current": true
-//       },
-//     }),
-//   ]
-// } only %}
-//     `);
-//     expect(results.ok).toBe(true);
-//     expect(results.html).toMatchSnapshot();
-//   });
-// });
-
-// {% include "@bolt-components-breadcrumb/breadcrumb.twig" with {
-//   contentItems: [
-//     include("@bolt-components-link/link.twig", {
-//       text: "Home",
-//       url: "#!"
-//     }),
-//     include("@bolt-components-link/link.twig", {
-//       text: "Landing Page",
-//       url: "#!"
-//     }),
-//     include("@bolt-components-link/link.twig", {
-//       text: "Sub Page",
-//       url: "#!"
-//     }),
-//     "Current Page"
-//   ]
-// } only %}
-
-// {% include "@bolt-components-breadcrumb/breadcrumb.twig" with {
-//   contentItems: [
-//     include("@bolt-components-link/link.twig", {
-//       text: "Home",
-//       url: "#!"
-//     }),
-//     include("@bolt-components-link/link.twig", {
-//       text: "Landing Page",
-//       url: "#!"
-//     }),
-//     include("@bolt-components-link/link.twig", {
-//       text: "Sub Page",
-//       url: "#!"
-//     }),
-//     include("@bolt-components-link/link.twig", {
-//       text: "Current Page",
-//       url: "#!",
-//       attributes: {
-//         "aria-current": true
-//       },
-//     }),
-//   ]
-// } only %}
