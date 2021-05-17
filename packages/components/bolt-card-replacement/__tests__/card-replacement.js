1e1c880609b8e7550face764187710d8e63222c0
Colby Cook <colby.cook@pega.com>
2021-05-17 18:14:14 -0400

    Reverted the card test back to the original version

--- packages/components/bolt-card-replacement/__tests__/card-replacement.js
+++ packages/components/bolt-card-replacement/__tests__/card-replacement.js
@@ -1,153 +1,171 @@
 import { render, stopServer } from '../../../testing/testing-helpers';
 import schema from '../card-replacement.schema';
 const { tag, theme, spacing, border_radius } = schema.properties;
-let page, fixtures;
 
-afterAll(async () => {
-  await stopServer();
-  await page.close();
-}, 100);
+describe('<bolt-card-replacement> Component', () => {
+  afterAll(async () => {
+    await stopServer();
+  }, 100);
 
-beforeEach(async () => {
-  await page.evaluate(() => {
-    document.body.innerHTML = '';
-  });
-});
-
-beforeAll(async () => {
-  page = await global.__BROWSER__.newPage();
-  await page.goto('http://127.0.0.1:4444/', {
-    timeout: 0,
-  });
-
-  const video = `
-    <video-js
-    data-account="1900410236"
-    data-player="O3FkeBiaDz"
-    data-embed="default"
-    data-video-id="5609376179001"
-    data-media-title
-    data-media-duration
-    controls
-    class="c-base-video"></video-js>
-  `;
-
-  const mediaData = {
-    media: {
-      image: {
-        src: '/fixtures/landscape-16x9-mountains.jpg',
-        alt: 'Image alt.',
-      },
-    },
-  };
-
-  const bodyData = {
-    body: {
-      eyebrow: 'This is an eyebrow',
-      headline: 'This is a headline',
-      paragraph: 'This is a paragraph.',
-    },
-  };
-
-  const actionsData = {
-    actions: [
-      {
-        text: 'This is a button',
-        url: 'https://pega.com',
-      },
-    ],
-  };
-
-  const videoData = await render('@bolt-components-ratio/ratio.twig', {
-    children: video,
-    ratio: '16/9',
-  });
-
-  fixtures = {
-    mediaData,
-    bodyData,
-    actionsData,
-    videoData,
-  };
-});
-
-describe('Bolt Card', () => {
-  test(`default`, async () => {
+  test('basic usage', async () => {
     const results = await render(
       '@bolt-components-card-replacement/card-replacement.twig',
       {
-        ...fixtures.mediaData,
-        ...fixtures.bodyData,
-        ...fixtures.actionsData,
-      },
-    );
-
-    await expect(results.ok).toBe(true);
-    await expect(results.html).toMatchSnapshot();
-  });
-
-  test(`Two Actions`, async () => {
-    const results = await render(
-      '@bolt-components-card-replacement/card-replacement.twig',
-      {
-        ...fixtures.mediaData,
-        ...fixtures.bodyData,
+        media: {
+          image: {
+            src: '/fixtures/landscape-16x9-mountains.jpg',
+            alt: 'Image alt.',
+          },
+        },
+        body: {
+          eyebrow: 'This is an eyebrow',
+          headline: 'This is a headline',
+          paragraph: 'This is a paragraph.',
+        },
         actions: [
           {
             text: 'This is a button',
             url: 'https://pega.com',
           },
+        ],
+      },
+    );
+    expect(results.ok).toBe(true);
+    expect(results.html).toMatchSnapshot();
+  });
+
+  test('with two buttons as actions', async () => {
+    const results = await render(
+      '@bolt-components-card-replacement/card-replacement.twig',
+      {
+        media: {
+          image: {
+            src: '/fixtures/landscape-16x9-mountains.jpg',
+            alt: 'Image alt.',
+          },
+        },
+        body: {
+          eyebrow: 'This is an eyebrow',
+          headline: 'This is a headline',
+          paragraph: 'This is a paragraph.',
+        },
+        actions: [
           {
-            text: 'This is a sceond button',
+            text: 'This is the 1st button',
             url: 'https://pega.com',
+          },
+          {
+            text: 'This is the 2nd button',
+            url: 'https://google.com',
           },
         ],
       },
     );
-
-    await expect(results.ok).toBe(true);
-    await expect(results.html).toMatchSnapshot();
+    expect(results.ok).toBe(true);
+    expect(results.html).toMatchSnapshot();
   });
 
-  test('Without Actions', async () => {
+  test('with video as media', async () => {
     const results = await render(
       '@bolt-components-card-replacement/card-replacement.twig',
       {
-        ...fixtures.mediaData,
-        ...fixtures.bodyData,
-      },
-    );
-    await expect(results.ok).toBe(true);
-    await expect(results.html).toMatchSnapshot();
-  });
-
-  test('Without Media and Actions', async () => {
-    const results = await render(
-      '@bolt-components-card-replacement/card-replacement.twig',
-      {
-        ...fixtures.bodyData,
-      },
-    );
-    await expect(results.ok).toBe(true);
-    await expect(results.html).toMatchSnapshot();
-  });
-
-  test('Video Card', async () => {
-    const results = await render(
-      '@bolt-components-card-replacement/card-replacement.twig',
-      {
-        ...fixtures.actionsData,
-        ...fixtures.bodyData,
+        link: {
+          attributes: {
+            'on-click': 'toggle',
+            'on-click-target': 'js-bolt-video-uuid',
+          },
+        },
         media: {
-          video: fixtures.videoData.html,
+          video: {
+            videoId: '5609376179001',
+            accountId: '1900410236',
+            playerId: 'r1CAdLzTW',
+            videoUuid: 'js-bolt-video-uuid',
+            showMeta: true,
+            showMetaTitle: false,
+          },
+        },
+        body: {
+          headline: 'With link and video',
+          paragraph:
+            'This card-replacement has a link, which makes the whole card-replacement clickable, and you can make it play/pause the video. Action button is optional in this case.',
+        },
+        actions: [
+          {
+            text: 'This button is video control',
+            attributes: {
+              'on-click': 'toggle',
+              'on-click-target': 'js-bolt-video-uuid',
+            },
+          },
+        ],
+      },
+    );
+    expect(results.ok).toBe(true);
+    expect(results.html).toMatchSnapshot();
+  });
+
+  test('without actions', async () => {
+    const results = await render(
+      '@bolt-components-card-replacement/card-replacement.twig',
+      {
+        media: {
+          image: {
+            src: '/fixtures/landscape-16x9-mountains.jpg',
+            alt: 'Image alt.',
+          },
+        },
+        body: {
+          eyebrow: 'This is an eyebrow',
+          headline: 'This is a headline',
+          paragraph: 'This is a paragraph.',
         },
       },
     );
-    await expect(results.ok).toBe(true);
-    await expect(results.html).toMatchSnapshot();
+    expect(results.ok).toBe(true);
+    expect(results.html).toMatchSnapshot();
   });
 
-  test('Clickable Card', async () => {
+  test('without media and actions', async () => {
+    const results = await render(
+      '@bolt-components-card-replacement/card-replacement.twig',
+      {
+        body: {
+          eyebrow: 'This is an eyebrow',
+          headline: 'This is a headline',
+          paragraph: 'This is a paragraph.',
+        },
+      },
+    );
+    expect(results.ok).toBe(true);
+    expect(results.html).toMatchSnapshot();
+  });
+
+  tag.enum.forEach(async option => {
+    test(`tag variations: ${option}`, async () => {
+      const results = await render(
+        '@bolt-components-card-replacement/card-replacement.twig',
+        {
+          tag: option,
+          media: {
+            image: {
+              src: '/fixtures/landscape-16x9-mountains.jpg',
+              alt: 'Image alt.',
+            },
+          },
+          body: {
+            eyebrow: 'This is an eyebrow',
+            headline: 'This is a headline',
+            paragraph: 'This is a paragraph.',
+          },
+        },
+      );
+      expect(results.ok).toBe(true);
+      expect(results.html).toMatchSnapshot();
+    });
+  });
+
+  test('clickable card-replacement', async () => {
     const results = await render(
       '@bolt-components-card-replacement/card-replacement.twig',
       {
@@ -155,81 +173,92 @@ describe('Bolt Card', () => {
           url: 'https://pega.com',
           text: 'This entire card-replacement is clickable',
         },
-        ...fixtures.mediaData,
-        ...fixtures.bodyData,
+        media: {
+          image: {
+            src: '/fixtures/landscape-16x9-mountains.jpg',
+            alt: 'Image alt.',
+          },
+        },
+        body: {
+          eyebrow: 'This is an eyebrow',
+          headline: 'This is a headline',
+          paragraph: 'This is a paragraph.',
+        },
       },
     );
-    await expect(results.ok).toBe(true);
-    await expect(results.html).toMatchSnapshot();
-  });
-});
-
-describe('Bolt Card Props', () => {
-  tag.enum.forEach(async option => {
-    test(`tag items: ${option}`, async () => {
-      const results = await render(
-        '@bolt-components-card-replacement/card-replacement.twig',
-        {
-          ...fixtures.mediaData,
-          ...fixtures.bodyData,
-          ...fixtures.actionsData,
-          tag: option,
-        },
-      );
-
-      await expect(results.ok).toBe(true);
-      await expect(results.html).toMatchSnapshot();
-    });
+    expect(results.ok).toBe(true);
+    expect(results.html).toMatchSnapshot();
   });
 
   theme.enum.forEach(async option => {
-    test(`theme items: ${option}`, async () => {
+    test(`theme variations: ${option}`, async () => {
       const results = await render(
         '@bolt-components-card-replacement/card-replacement.twig',
         {
-          ...fixtures.mediaData,
-          ...fixtures.bodyData,
-          ...fixtures.actionsData,
           theme: option,
+          media: {
+            image: {
+              src: '/fixtures/landscape-16x9-mountains.jpg',
+              alt: 'Image alt.',
+            },
+          },
+          body: {
+            eyebrow: 'This is an eyebrow',
+            headline: 'This is a headline',
+            paragraph: 'This is a paragraph.',
+          },
         },
       );
-
-      await expect(results.ok).toBe(true);
-      await expect(results.html).toMatchSnapshot();
+      expect(results.ok).toBe(true);
+      expect(results.html).toMatchSnapshot();
     });
   });
 
   spacing.enum.forEach(async option => {
-    test(`spacing items: ${option}`, async () => {
+    test(`spacing variations: ${option}`, async () => {
       const results = await render(
         '@bolt-components-card-replacement/card-replacement.twig',
         {
-          ...fixtures.mediaData,
-          ...fixtures.bodyData,
-          ...fixtures.actionsData,
           spacing: option,
+          media: {
+            image: {
+              src: '/fixtures/landscape-16x9-mountains.jpg',
+              alt: 'Image alt.',
+            },
+          },
+          body: {
+            eyebrow: 'This is an eyebrow',
+            headline: 'This is a headline',
+            paragraph: 'This is a paragraph.',
+          },
         },
       );
-
-      await expect(results.ok).toBe(true);
-      await expect(results.html).toMatchSnapshot();
+      expect(results.ok).toBe(true);
+      expect(results.html).toMatchSnapshot();
     });
   });
 
   border_radius.enum.forEach(async option => {
-    test(`border_radius items: ${option}`, async () => {
+    test(`border radius variations: ${option}`, async () => {
       const results = await render(
         '@bolt-components-card-replacement/card-replacement.twig',
         {
-          ...fixtures.mediaData,
-          ...fixtures.bodyData,
-          ...fixtures.actionsData,
           border_radius: option,
+          media: {
+            image: {
+              src: '/fixtures/landscape-16x9-mountains.jpg',
+              alt: 'Image alt.',
+            },
+          },
+          body: {
+            eyebrow: 'This is an eyebrow',
+            headline: 'This is a headline',
+            paragraph: 'This is a paragraph.',
+          },
         },
       );
-
-      await expect(results.ok).toBe(true);
-      await expect(results.html).toMatchSnapshot();
+      expect(results.ok).toBe(true);
+      expect(results.html).toMatchSnapshot();
     });
   });
 });
