04446ae84e3ae142e38ce50ef89e191904a6dee6
Mike Mai <emceehao@gmail.com>
2021-06-16 17:36:01 -0400

    ds-335: fix typos

--- packages/layouts/bolt-site/site.schema.js
+++ packages/layouts/bolt-site/site.schema.js
@@ -8,32 +8,29 @@ module.exports = {
       description:
         'A Drupal attributes object. Applies extra HTML attributes to the parent element.',
     },
+    header: {
+      type: 'any',
+      description: 'The header of the site.',
+    },
     main: {
       type: 'object',
       description:
-        'The main content of the site. <code>site_content</code> block is also available.',
+        'The main area of the site. This creates the <code>&lt;main&gt;</code> element.',
       properties: {
         attributes: {
           type: 'object',
           description:
-            'A Drupal attributes object. Applies extra HTML attributes to the main element.',
+            'A Drupal attributes object. Applies extra HTML attributes to the <code>&lt;main&gt;</code> element.',
         },
         content: {
           type: 'any',
-          description:
-            'The main content of the site. <code>site_content</code> block is also available.',
+          description: 'The main content of the site.',
         },
       },
     },
-    header: {
-      type: 'any',
-      description:
-        'The header of the site. <code>site_header</code> block is also available.',
-    },
     footer: {
       type: 'any',
-      description:
-        'The footer of the site. <code>site_footer</code> block is also available.',
+      description: 'The footer of the site.',
     },
   },
 };
