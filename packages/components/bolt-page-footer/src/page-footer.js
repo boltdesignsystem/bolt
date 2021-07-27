909955a67f7d729f318cd54c756194a34b2807e7
Mroczko <mrocm@l32956meu.local.rpega.com>
2021-07-27 12:36:57 +0200

    DS-520: accordion expanding draft

--- packages/components/bolt-page-footer/src/page-footer.js
+++ packages/components/bolt-page-footer/src/page-footer.js
@@ -6,6 +6,19 @@ export class BoltPageFooter {
   }
 
   init() {
-    console.log('hello form footer');
+    var acc = document.getElementsByClassName('c-page-footer__nav-trigger');
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + 'px';
+        }
+      });
+    }
   }
 }
