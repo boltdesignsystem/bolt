93506305743dd6829341d68bbedb10f2bc85ff87
Mroczko <mrocm@l32956meu.local.rpega.com>
2021-08-05 17:49:46 +0200

    DS-520: Footer review - mobile first approach

--- packages/components/bolt-page-footer/src/page-footer.js
+++ packages/components/bolt-page-footer/src/page-footer.js
@@ -6,31 +6,48 @@ export class BoltPageFooter {
   }
 
   init() {
-    const toggleAccordion = this.toggleAccordion();
-  }
-
-  toggleAccordion = () => {
-    const accTrigger = this.el.getElementsByClassName(
-      'c-page-footer__nav-trigger',
+    const linkToggleTrigger = this.el.querySelectorAll(
+      '[data-bolt-page-footer-toggle-trigger]',
     );
-    for (let i = 0; i < accTrigger.length; i++) {
-      accTrigger[i].addEventListener('click', function() {
-        //Toggle aria-expanded
-        let ariaExpanded = this.getAttribute('aria-expanded');
-        if (ariaExpanded === 'true') {
-          ariaExpanded = 'false';
+    linkToggleTrigger.forEach(el => {
+      el.addEventListener('click', event => {
+        if (el.getAttribute('aria-expanded') === 'true') {
+          el.setAttribute('aria-expanded', 'false');
         } else {
-          ariaExpanded = 'true';
-        }
-        this.setAttribute('aria-expanded', ariaExpanded);
-        //Toggle accordion panels
-        const panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-          panel.style.maxHeight = null;
-        } else {
-          panel.style.maxHeight = panel.scrollHeight + 'px';
+          el.setAttribute('aria-expanded', 'true');
         }
       });
-    }
-  };
+    });
+  }
+
+  // toggleAccordion = () => {
+  //   const accTrigger = this.el.getElementsByClassName(
+  //     'c-page-footer__nav-trigger',
+  //   );
+  //   for (let i = 0; i < accTrigger.length; i++) {
+  //     accTrigger[i].addEventListener('click', function() {
+  //       //Toggle aria-expanded
+  //       let ariaExpanded = this.getAttribute('aria-expanded');
+  //       if (ariaExpanded === 'true') {
+  //         ariaExpanded = 'false';
+  //       } else {
+  //         ariaExpanded = 'true';
+  //       }
+  //       this.setAttribute('aria-expanded', ariaExpanded);
+  //       //Toggle accordion panels
+  //       // const panel = this.nextElementSibling;
+  //       // if (panel.style.maxHeight) {
+  //       //   panel.style.maxHeight = null;
+  //       // } else {
+  //       //   panel.style.maxHeight = panel.scrollHeight + 'px';
+  //       // }
+  //     });
+  //   }
+  // };
 }
+
+const pageFooterNavs = document.querySelectorAll('[data-bolt-page-footer-nav]');
+
+pageFooterNavs.forEach(el => {
+  const component = new BoltPageFooter(el);
+});
