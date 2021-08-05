03c10d095d166153688751ec9cc5b2ee67399a0c
Mroczko <mrocm@l32956meu.local.rpega.com>
2021-08-05 13:59:02 +0200

    DS-409: Update tests

--- packages/generators/bolt-generator/templates/component/component.test.js
+++ packages/generators/bolt-generator/templates/component/component.test.js
@@ -1,7 +1,8 @@
 // Refer to https://github.com/boltdesignsystem/bolt/wiki/Jest-Test:-Example-Jest-Test for more testing examples
 import { render, renderWC, stopServer } from '../../../testing/testing-helpers';
 import schema from '../{{ kebabCase name }}.schema';
-const componentSelector = 'bolt-{{ kebabCase name }}'
+const componentSelector = 'bolt-{{ kebabCase name }}';
+//const { [PROP KEY], [PROP KEY]... } = schema.properties;
 let page, fixtures;
 
 afterAll(async () => {
@@ -28,15 +29,16 @@ beforeAll(async () => {
   fixtures = {
     defaultData,
   };
-
 });
 
 describe('Bolt {{ titleCase name }} component', () => {
   test('default', async () => {
     const results = await render(
-      '@bolt-components-{{ kebabCase name }}/{{ kebabCase name }}.twig', {
-      ...fixtures.defaultData,
-    });
+      '@bolt-components-{{ kebabCase name }}/{{ kebabCase name }}.twig',
+      {
+        ...fixtures.defaultData,
+      },
+    );
 
     /*
       Comment out if testing a Web Component
@@ -57,3 +59,26 @@ describe('Bolt {{ titleCase name }} component', () => {
     // await expect(outerHTML).toMatchSnapshot();
   });
 });
+
+/*
+  Uncomment and follow a pattern below if your schema has
+  enum to go thru. Do not forget to uncomment the consts
+  at the very top of this file.
+*/
+// describe('Bolt {{ titleCase name }} prop - , () => {
+//   // Target each of the schema keys with the following pattern
+//   [PROP KEY].enum.forEach(async (option) => {
+//     test(`[PROP KEY] items: ${option}`, async () => {
+//       const results = await render(
+//         '@bolt-components-{{ kebabCase name }}/{{ kebabCase name }}.twig',
+//         {
+//           ...fixtures.defaultData,
+//           [PROP KEY]: option,
+//         }
+//       );
+
+//       await expect(results.ok).toBe(true);
+//       await expect(results.html).toMatchSnapshot();
+//     });
+//   });
+// });
