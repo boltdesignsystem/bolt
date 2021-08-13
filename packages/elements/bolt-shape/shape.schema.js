e3b04a9d1248b21e37cd982ba28757cc6fc5eba2
Mike Mai <emceehao@gmail.com>
2021-08-13 18:50:01 -0400

    ds-501: rework CSS and update Shape docs

--- packages/elements/bolt-shape/shape.schema.js
+++ packages/elements/bolt-shape/shape.schema.js
@@ -16,21 +16,15 @@ module.exports = {
     size: {
       type: 'string',
       description: 'Changes the size of the interior element',
-      enum: ['small', 'medium', 'large'],
+      default: 'small',
+      enum: ['small', 'medium', 'large', 'xlarge'],
     },
     border_radius: {
       type: 'string',
       description:
         'Customizes the outside radius of the shape. "none" will render as a square shape, "small" and "large" will render as a rounded corners square and "full" will render as a circle.',
-      default: 'none',
+      default: 'full',
       enum: ['none', 'small', 'large', 'full'],
-    },
-    spacing: {
-      type: 'string',
-      description:
-        'Customizes the spacing between the interior element and the outside of the shape',
-      default: 'none',
-      enum: ['none', 'xsmall', 'small', 'medium'],
     },
   },
 };
