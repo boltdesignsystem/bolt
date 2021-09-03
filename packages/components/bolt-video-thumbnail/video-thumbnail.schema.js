ea9fa21fbd582d51856cc25f4f3d43980786564c
Colby Cook <colby.cook@pega.com>
2021-09-03 09:42:21 -0400

    Added a center_component prop to the video thumbnail component, updated the OCLP Video page with a Preview button

--- packages/components/bolt-video-thumbnail/video-thumbnail.schema.js
+++ packages/components/bolt-video-thumbnail/video-thumbnail.schema.js
@@ -30,5 +30,10 @@ module.exports = {
       description:
         'Display the video duration that overlays the video thumbnail.',
     },
+    center_component: {
+      type: 'any',
+      description:
+        'Displays a vertically and horizontally centered component that overlays the video thumbnail.',
+    },
   },
 };
