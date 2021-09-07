807383b514d49d8fff5fbcf371ac8c35ca9093bf
Mroczko <mrocm@l32956meu.local.rpega.com>
2021-09-07 14:44:11 +0200

    DS-529: Add download slides cta

--- packages/components/bolt-teaser/teaser.schema.js
+++ packages/components/bolt-teaser/teaser.schema.js
@@ -104,6 +104,10 @@ module.exports = {
       type: 'object',
       description: 'Render the share button. Share menu is expected here.',
     },
+    download_cta: {
+      type: 'object',
+      description: 'Render the download link. Link element is expected here.',
+    },
     chip_list: {
       type: 'object',
       description:
