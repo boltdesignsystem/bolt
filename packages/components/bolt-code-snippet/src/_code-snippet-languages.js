08243b25607956ad620820def24c26b040e4e6bc
Dan Morse <dan.morse@pega.com>
2021-09-10 14:50:14 -0400

    DS-483: Rebuild code-snippet component, new features and language support

---
+++ packages/components/bolt-code-snippet/src/_code-snippet-languages.js
@@ -0,0 +1,42 @@
+/*
+Language map, converts language id to readable title.
+*/
+
+export const languages = {
+  // Base languages...
+  markup: 'Markup',
+
+  // "Markup" includes:
+  html: 'HTML',
+  xml: 'XML',
+  svg: 'SVG',
+  mathml: 'MathML',
+  ssml: 'SSML',
+  atom: 'Atom',
+  rss: 'RSS',
+
+  // Additional base languages:
+  javascript: 'JavaScript',
+  js: 'JavaScript',
+  clike: 'C-like',
+  css: 'CSS',
+
+  // Begin Add-on languages...
+  scss: 'Sass (Scss)',
+  twig: 'Twig',
+  java: 'Java',
+  json: 'JSON',
+  rest: 'reST (reStructuredText)',
+  bash: 'Bash',
+  shell: 'Bash',
+  csv: 'CSV',
+  docker: 'Docker',
+  dockerfile: 'Docker',
+  http: 'HTTP',
+  jsx: 'React JSX',
+  tsx: 'React TSX',
+  md: 'Markdown',
+  markdown: 'Markdown',
+  yml: 'YAML',
+  yaml: 'YAML',
+};
--- packages/components/bolt-code-snippet/src/code-snippet.js
+++ packages/components/bolt-code-snippet/src/code-snippet.js
@@ -1,7 +1,180 @@
-import { lazyQueue } from '@bolt/lazy-queue';
+import Prism from 'prismjs';
+import { html, render } from '@bolt/element';
+import cx from 'classnames/bind';
+import ClipboardJS from 'clipboard';
 
-lazyQueue(['bolt-code-snippet'], async () => {
-  await import(
-    /* webpackChunkName: 'bolt-code-snippet' */ './code-snippet.standalone.js'
-  );
-});
+// Prism only runs automatically on elements with the "language-*" class, but switch to manual just in case.
+Prism.manual = true;
+
+// Included by default: markup, html, xml, svg, mathml, ssml, atom, rss, javascript, js, clike
+// Add-on languages:
+import 'prismjs/components/prism-java';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-rest';
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-csv';
+import 'prismjs/components/prism-docker';
+import 'prismjs/components/prism-http';
+import 'prismjs/components/prism-jsx';
+import 'prismjs/components/prism-tsx';
+import 'prismjs/components/prism-sql';
+import 'prismjs/components/prism-css-extras';
+import 'prismjs/components/prism-scss';
+import 'prismjs/components/prism-twig';
+import 'prismjs/components/prism-markdown';
+import 'prismjs/components/prism-yaml';
+
+// Note: If adding a new language, be sure to add corresponding label to `./_code-snippet-languages`
+import { languages } from './_code-snippet-languages';
+
+export class BoltCodeSnippet {
+  constructor(el) {
+    if (!el) return;
+    this.el = el;
+    this.init();
+  }
+
+  init() {
+    this.preElement = this.el.querySelector('pre');
+    this.codeElement = this.el.querySelector('code');
+    this.lang = this.el.dataset.boltLang;
+    this.langTitle = this.el.dataset.boltLangTitle;
+
+    if (!(this.preElement && this.codeElement && this.lang)) return;
+
+    this.originalHTML = this.codeElement.innerHTML;
+    this.filteredHTML = this.replaceEntities(this.originalHTML);
+    this.hideTitle = this.el.hasAttribute('data-bolt-hide-title');
+    this.hideCopy = this.el.hasAttribute('data-bolt-hide-copy');
+
+    if (!(this.hideTitle && this.hideCopy)) {
+      this.setupHeader();
+    }
+
+    this.highlightHTML();
+    this.el.setAttribute('data-bolt-ready', '');
+  }
+
+  replaceEntities(string) {
+    // Twig encodes HTML entities. Replace them before syntax highlighting.
+    return string
+      .replace(/&amp;/g, '&')
+      .replace(/&lt;/g, '<')
+      .replace(/&gt;/g, '>');
+  }
+
+  highlightHTML() {
+    try {
+      this.codeElement.innerHTML = Prism.highlight(
+        this.filteredHTML,
+        Prism.languages[this.lang],
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  setupHeader() {
+    this.header = document.createElement('div');
+    this.header.classList.add('c-bolt-code-snippet__header');
+
+    this.el.prepend(this.header);
+
+    // Custom language title overrides default
+    this.title = this.langTitle || languages[this.lang];
+    this.copied = false;
+
+    this.renderHeader();
+
+    if (!this.hideCopy) {
+      // Must come after renderHeader
+      this.setupClipboard();
+    }
+  }
+
+  setupClipboard() {
+    const clipboardTrigger = this.header.querySelector(
+      '.js-bolt-code-snippet__copy-trigger',
+    );
+
+    if (!clipboardTrigger) return;
+
+    this.preElement?.setAttribute('tabindex', '0');
+
+    const clip = new ClipboardJS(clipboardTrigger, {
+      text: () => {
+        return this.filteredHTML;
+      },
+    });
+
+    clip.on('success', () => {
+      this.copied = true;
+
+      this.renderHeader();
+      this.preElement?.focus();
+
+      setTimeout(() => {
+        this.copied = false;
+        this.renderHeader();
+      }, 2000);
+    });
+    clip.on('error', function() {
+      console.log('There was an error copying this code');
+      this.copied = false;
+    });
+  }
+
+  renderHeader() {
+    const copyClasses = cx(
+      'c-bolt-code-snippet__copy',
+      'js-bolt-code-snippet__copy',
+    );
+
+    const copyButtonClasses = cx(
+      'e-bolt-text-link',
+      'e-bolt-text-link--reversed-underline',
+      'js-bolt-code-snippet__copy-trigger',
+      { 'u-bolt-hidden': this.copied },
+    );
+
+    const copyConfirmationClasses = cx(
+      'u-bolt-color-gray',
+      'e-bolt-text-link--reversed-underline',
+      'js-bolt-code-snippet__copy-confirmation',
+      { 'js-bolt-code-snippet__copy--copied': this.copied },
+    );
+
+    render(
+      html`
+        ${!this.hideTitle
+          ? html`
+              <span class="c-bolt-code-snippet__title">${this.title}</span>
+            `
+          : ``}
+        ${!this.hideCopy
+          ? html`
+              <div class="${copyClasses}">
+                <button type="button" class="${copyButtonClasses}">
+                  <span class="e-bolt-text-link__icon-before"
+                    ><bolt-icon name="copy-to-clipboard"></bolt-icon></span
+                  >Copy</button
+                >${this.copied
+                  ? html`
+                      <span
+                        class="${copyConfirmationClasses}"
+                        aria-live="polite"
+                      >
+                        <span class="e-bolt-text-link__icon-before"
+                          ><bolt-icon name="check"></bolt-icon></span
+                        >Copied
+                      </span>
+                    `
+                  : ``}
+              </div>
+            `
+          : ``}
+      `,
+      this.header,
+    );
+  }
+}
