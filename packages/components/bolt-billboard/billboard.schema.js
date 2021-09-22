2d08e0a1b9be60efb6f5cd454093c2611ea5bd74
CJ White <hello@cjwhitedev.com>
2021-09-22 16:15:03 -0400

    DS-622 fill out schema, refactor src, cleanup demos

--- packages/components/bolt-billboard/billboard.schema.js
+++ packages/components/bolt-billboard/billboard.schema.js
@@ -1,3 +1,5 @@
+const layoutSchema = require('@bolt/layouts-layout/layout.schema');
+
 module.exports = {
   $schema: 'http://json-schema.org/draft-04/schema#',
   title: 'Billboard',
@@ -10,27 +12,45 @@ module.exports = {
     },
     content: {
       type: 'any',
-      description: 'The main Example content.',
+      description: 'The main content.',
     },
-    template: {
-      type: 'string',
-      default: 'halves@from-small',
-    },
+    template: layoutSchema.properties.template,
     theme: {
       type: 'string',
+      description: 'Controls the theme of the Billboard.',
       default: 'light',
     },
-    image: {},
-    image_valign: {
+    supplement: {
+      type: 'any',
+      description: 'Additional content, usually an image.',
+    },
+    supplement_valign: {
       type: 'string',
+      description: 'Controle the vertical alignment of the suppplement item.',
+      enum: ['start', 'center', 'end', 'start-offset', 'end-offset'],
       default: 'start',
+    },
+    size: {
+      type: 'string',
+      description:
+        'Set the spacing between layout items and the layout padding.',
+      enum: ['none', 'small', 'medium', 'large', 'xlarge'],
+      default: 'large',
+    },
+    background_image: {
+      type: 'any',
+      description:
+        'An object to be used as a background that displays underneath the content / foreground in the Billboard.',
     },
     center_content: {
       type: 'boolean',
+      description:
+        'Used to center the main content. Must use a single column template for this to work.',
       default: false,
     },
     reverse_order: {
       type: 'boolean',
+      description: 'Controle the order of the content and supplement items.',
       default: false,
     },
   },
