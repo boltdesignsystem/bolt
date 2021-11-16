9c58bf858c259636c5561f26c3189cccdb66312d
Morse, Dan <Dan.Morse@pega.com>
2021-11-16 14:39:18 -0500

    DS-590: Add info-section component JS

--- packages/components/bolt-info-section/src/info-section.js
+++ packages/components/bolt-info-section/src/info-section.js
@@ -1,11 +1,44 @@
 export class BoltInfoSection {
+  static infoSections = [];
+
   constructor(el) {
     if (!el) return;
     this.el = el;
-    this.init();
+    this.toggleButton = this.el.querySelector(
+      '.js-bolt-info-section-toggle-button',
+    );
+    if (this.toggleButton) {
+      this.isExpanded =
+        this.toggleButton.getAttribute('aria-expanded') === 'true';
+      this.init();
+    }
+  }
+
+  expandSection() {
+    this.toggleButton.setAttribute('aria-expanded', true);
+    this.isExpanded = true;
+  }
+
+  collapseSection() {
+    this.toggleButton.setAttribute('aria-expanded', false);
+    this.isExpanded = false;
   }
 
   init() {
-    console.log('This is a info-section component.');
+    if (!BoltInfoSection.infoSections.includes(this)) {
+      BoltInfoSection.infoSections.push(this);
+    }
+
+    this.toggleButton.addEventListener('click', e => {
+      if (this.isExpanded) {
+        this.collapseSection();
+      } else {
+        const otherSections = BoltInfoSection.infoSections.filter(
+          section => section !== this,
+        );
+        otherSections.forEach(section => section.collapseSection());
+        this.expandSection();
+      }
+    });
   }
 }
