8ed91073ddc2e4a9ff47e545685eb22c253a317a
CJ White <hello@cjwhitedev.com>
2022-01-13 10:25:37 -0500

    DS-541 documentation. prepare for code review.

--- packages/components/bolt-satellite/satellite.schema.js
+++ packages/components/bolt-satellite/satellite.schema.js
@@ -8,31 +8,35 @@ module.exports = {
       description:
         'A Drupal attributes object. Applies extra HTML attributes to the &lt;bolt-satellite&gt; tag.',
     },
-    title: {
-      type: 'any',
-      description: 'Title of Example.',
-    },
     content: {
       type: 'any',
-      description: 'The main Example content.',
-    },
-    disabled: {
-      type: 'boolean',
       description:
-        'Default `disabled` prop supported globally by most Bolt components.',
-      default: false,
+        'The main content. Typically, this will be a number of icon-only buttons.',
     },
     position: {
       type: 'string',
-      description: '',
+      description:
+        'The desired location of the floating element. The static option is used solely for testing purposes.',
       enum: ['bottom-right', 'middle-right', 'top-right', 'static'],
       default: 'bottom-right',
     },
     stash: {
       type: 'number',
-      description: '',
-      enum: [1, 2, 3],
+      description:
+        'The number of items visisble when a "more" button is needed. It can be set to 0 to turn off the button.',
+      enum: [1, 2, 3, 0],
       default: 2,
+    },
+    showOnLoad: {
+      type: 'boolean',
+      description:
+        'Please leave this set to true unless you plan on adding javascript to show the element yourself.',
+      default: true,
+    },
+    showOnScroll: {
+      type: 'string',
+      description:
+        'This will leave the element hidden until the page is scrolled to a certain position. A percentage or px value must be passed here as a string. ie. "200px" or "20%".',
     },
   },
 };
--- packages/components/bolt-satellite/src/satellite.js
+++ packages/components/bolt-satellite/src/satellite.js
@@ -1,58 +1,71 @@
 export class BoltSatellite {
   constructor(el) {
-    // move to props?
-    this.showOnLoad = true;
-
     if (!el) return;
     this.el = el;
     this.init();
   }
 
   show() {
-    // navbar has ready state conventions
+    // @TODO: navbar has ready state conventions.
     this.el.setAttribute('ready', 'true');
   }
 
-  showOnScroll() {
+  showOnScroll(revealData) {
     var scrollPosition = window.scrollY;
+    var value = parseInt(revealData, 10);
+    var revealPosition = 0;
+
+    // @TODO: is this unecessarily comprehensive?
+    var pageHeight = Math.max(
+      document.body.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.clientHeight,
+      document.documentElement.scrollHeight,
+      document.documentElement.offsetHeight,
+    );
+
+    if (revealData.includes('px')) {
+      revealPosition = value;
+    } else if (revealData.includes('%')) {
+      // set a pixel value based on the percentage value.
+      revealPosition = pageHeight * (value / 100);
+    }
 
     window.addEventListener('scroll', () => {
-      // prevent this firing more than once
+      // prevent this from firing more than once.
       if (this.el.getAttribute('ready') !== 'true') {
         scrollPosition = window.scrollY;
-        // attach a pixel value for when the component should show
-        if (scrollPosition >= 200) {
+        if (revealPosition > 0 && scrollPosition >= revealPosition) {
           this.show();
         }
       }
     });
   }
 
+  toggleRevealChildren() {
+    // @TODO: refactor with states?
+    if (
+      this.el.firstElementChild.getAttribute('data-reveal-children') === 'false'
+    ) {
+      this.el.firstElementChild.setAttribute('data-reveal-children', 'true');
+    } else {
+      this.el.firstElementChild.setAttribute('data-reveal-children', 'false');
+    }
+  }
+
   init() {
-    if (this.showOnLoad) {
+    if (this.el.getAttribute('data-show-on-scroll')) {
+      this.showOnScroll(this.el.getAttribute('data-show-on-scroll'));
+    } else if (this.el.getAttribute('data-show-on-load') != null) {
       this.show();
     }
 
-    // refactor ↓
-
-    this.el.querySelector('.c-bolt-satellite__more > *').onclick = function() {
-      revealChildren();
-    };
-
-    function revealChildren() {
+    this.el.lastElementChild.onclick = () => {
       if (
-        document
-          .querySelector('.c-bolt-satellite__content')
-          .getAttribute('data-reveal-children') === 'false'
+        this.el.lastElementChild.classList.contains('c-bolt-satellite__more')
       ) {
-        document
-          .querySelector('.c-bolt-satellite__content')
-          .setAttribute('data-reveal-children', 'true');
-      } else {
-        document
-          .querySelector('.c-bolt-satellite__content')
-          .setAttribute('data-reveal-children', 'false');
+        this.toggleRevealChildren();
       }
-    }
+    };
   }
 }
