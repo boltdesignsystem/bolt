95169612a79598b7d6d6b3c1db6e1f0e15ea500d
Mroczko <mrocm@l32956meu.local.rpega.com>
2022-02-01 15:32:28 +0100

    DS-516: Experiments

--- packages/components/bolt-responsive-table/src/responsive-table.js
+++ packages/components/bolt-responsive-table/src/responsive-table.js
@@ -2,10 +2,81 @@ export class BoltResponsiveTable {
   constructor(el) {
     if (!el) return;
     this.el = el;
-    this.init();
+    this.toggleNextRows();
+    this.toggleAriaExpanded();
   }
 
-  init() {
-    console.log('This is a responsive-table component.');
+  toggleNextRows() {
+    console.log('toggletable rows');
+    var nextUntil = function(elem, selector, filter) {
+      // Setup siblings array
+      var siblings = [];
+
+      // Get the next sibling element
+      elem = elem.nextElementSibling;
+
+      // As long as a sibling exists
+      while (elem) {
+        // If we've reached our match, bail
+        if (elem.matches(selector)) break;
+
+        // If filtering by a selector, check if the sibling matches
+        if (filter && !elem.matches(filter)) {
+          elem = elem.nextElementSibling;
+        }
+
+        // Otherwise, push it to the siblings array
+        siblings.push(elem);
+
+        // Get the next sibling element
+        elem = elem.nextElementSibling;
+      }
+
+      return siblings;
+    };
+
+    var start = this.el.querySelectorAll(
+      '.c-bolt-responsive-table__row--trigger',
+    );
+
+    for (let i = 0; i < start.length; i++) {
+      const btn = start[i].querySelector(
+        '.js-bolt-responsive-table-toggle-row',
+      );
+      btn.addEventListener('click', function() {
+        var next = nextUntil(
+          start[i],
+          '.c-bolt-responsive-table__row--trigger',
+        );
+        next.forEach(el => {
+          el.classList.toggle('c-bolt-responsive-table__row--hidden');
+        });
+      });
+    }
+  }
+
+  toggleAriaExpanded() {
+    console.log('toggle aria expanded');
+    const tableButton = this.el.querySelectorAll(
+      '.js-bolt-responsive-table-toggle-row',
+    );
+    console.log(tableButton);
+    tableButton.forEach(el => {
+      el.addEventListener('click', event => {
+        if (el.getAttribute('aria-expanded') === 'true') {
+          el.setAttribute('aria-expanded', 'false');
+        } else {
+          el.setAttribute('aria-expanded', 'true');
+        }
+      });
+    });
   }
 }
+
+const responsiveTable = document.querySelectorAll(
+  '.c-bolt-reponsive-table__table',
+);
+
+responsiveTable.forEach(el => {
+  const component = new BoltResponsiveTable(el);
+});
