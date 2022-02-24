boltdesignsystem/bolt

commit 3a8a24ca83c3e2fd44bafb4290decc250638c5b9

DS-395: Delete themes styles import

packages/experimental/micro-journeys/src/interactive-pathways.js
@@ -7,7 +7,6 @@ import {
 import { withLitContext } from '@bolt/core-v3.x/renderers';
 import classNames from 'classnames/bind';
 import debounce from 'lodash.debounce';
-import themes from '@bolt/global/styles/06-themes/_themes.all.scss';
 import styles from './interactive-pathways.scss';
 import schema from './interactive-pathways.schema';
 // @TODO this default image should be located in schema

packages/global/styles/index.scss
@@ -33,7 +33,6 @@
 @import '05-objects/objects-ui-list/_objects-ui-list';
 @import '05-objects/objects-wrapper/_objects-wrapper';
 
-@import '06-themes/_themes.all';
 
 @import '07-utilities/_utilities-border-radius';
 @import '07-utilities/_utilities-clearfix';

packages/layouts/bolt-list/src/list.js
@@ -9,7 +9,6 @@ import {
 import classNames from 'classnames/bind';
 import { withLitHtml } from '@bolt/core-v3.x/renderers/renderer-lit-html';
 
-import themes from '@bolt/global/styles/06-themes/_themes.all.scss';
 import styles from './list.scss';
 import schema from '../list.schema';
 

commit a74aab243fd4aa825593b5761f2273afc6031037

WWW-1284: Remove reference to undefined `themes` variable
This appears to have originally broken with
https://github.com/boltdesignsystem/bolt/commit/3a8a24ca83c3e2fd44bafb4290decc250638c5b9

packages/experimental/micro-journeys/src/interactive-pathways.js
@@ -295,7 +295,7 @@ class BoltInteractivePathways extends withLitContext {
     `;
 
     return html`
-      ${this.addStyles([styles, themes])}
+      ${this.addStyles([styles])}
       <div class="${classes}">
         <div class="c-bolt-interactive-pathways__header">
           ${props.hidePathwaysImage