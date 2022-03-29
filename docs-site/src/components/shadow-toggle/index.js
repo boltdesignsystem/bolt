94027da941ebded62567143ba7ff7faf9b01fa43
Morse, Dan <Dan.Morse@pega.com>
2022-03-29 16:02:03 -0400

    DS-752: Remove remaining unused docs-site components

--- docs-site/src/components/shadow-toggle/index.js
+++
@@ -1,7 +0,0 @@
-import { lazyQueue } from '@bolt/lazy-queue';
-
-lazyQueue(['bolt-shadow-toggle'], async () => {
-  await import(
-    /* webpackChunkName: "bolt-shadow-toggle" */ './shadow-toggle.js'
-  );
-});
