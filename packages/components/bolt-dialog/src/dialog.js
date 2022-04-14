41f77d086965c7b6924affc4fd74dccb477c3e18
Morse, Dan <Dan.Morse@pega.com>
2022-04-14 10:00:23 -0400

    ds-459: Rework dialog JS

--- packages/components/bolt-dialog/src/dialog.js
+++ packages/components/bolt-dialog/src/dialog.js
@@ -1,182 +1,123 @@
+import * as focusTrap from 'focus-trap';
+
 export class BoltDialog {
+  static dialogs = [];
+
+  static getDialog(el) {
+    if (el) {
+      return BoltDialog.dialogs.find(dialog => el === dialog.el);
+    }
+  }
+
+  static open = el => {
+    const dialog = BoltDialog.getDialog(el);
+
+    if (dialog) {
+      dialog.open();
+    }
+  };
+
+  static close = el => {
+    const dialog = BoltDialog.getDialog(el);
+
+    if (dialog) {
+      dialog.close();
+    }
+  };
+
   constructor(el) {
     if (!el) return;
+
     this.el = el;
+    this.isOpen = false;
     this.init();
   }
 
-  getFocusableNodes() {
-    const focusableElements = [
-      'a[href]',
-      'area[href]',
-      'input:not([disabled]):not([type="hidden"]):not([aria-hidden])',
-      'select:not([disabled]):not([aria-hidden])',
-      'textarea:not([disabled]):not([aria-hidden])',
-      'button:not([disabled]):not([aria-hidden])',
-      'iframe',
-      'object',
-      'embed',
-      '[contenteditable]',
-      '[tabindex]:not([tabindex^="-"])',
-    ];
-
-    const nodes = this.el.querySelectorAll(focusableElements);
-    return Array(...nodes);
-  }
-
-  setFocusToFirstNode() {
-    const focusableNodes = this.getFocusableNodes();
-
-    // no focusable nodes
-    if (focusableNodes.length === 0) return;
-
-    // remove nodes on whose click, the dialog closes
-    const nonCloseNodes = focusableNodes.filter(node => {
-      return !node.hasAttribute('data-dialog-close');
-    });
-
-    if (nonCloseNodes.length > 0) nonCloseNodes[0].focus();
-    if (nonCloseNodes.length === 0) focusableNodes[0].focus();
-  }
-
-  retainFocus(event) {
-    let focusableNodes = this.getFocusableNodes();
-
-    // no focusable nodes
-    if (focusableNodes.length === 0) return;
-
-    // Filters nodes which are hidden to prevent focus leak outside dialog
-    focusableNodes = focusableNodes.filter(node => {
-      return node.offsetParent !== null;
-    });
-
-    const focusedItemIndex = focusableNodes.indexOf(document.activeElement);
-
-    if (event.shiftKey && focusedItemIndex === 0) {
-      focusableNodes[focusableNodes.length - 1].focus();
-      event.preventDefault();
+  init() {
+    if (!BoltDialog.dialogs.includes(this)) {
+      BoltDialog.dialogs.push(this);
     }
 
-    if (
-      !event.shiftKey &&
-      focusableNodes.length > 0 &&
-      focusedItemIndex === focusableNodes.length - 1
-    ) {
-      focusableNodes[0].focus();
-      event.preventDefault();
-    }
+    this.isPersistent = this.el.hasAttribute('data-dialog-persistent');
+    this.overlay = this.el.querySelector('.c-bolt-dialog__overlay');
+    this.trap = focusTrap.createFocusTrap(this.el);
+    this.handleEscape = this.handleEscape.bind(this);
+    this.handleOutsideClick = this.handleOutsideClick.bind(this);
+
+    this.setupTriggers();
+    this.el.setAttribute('data-bolt-ready', '');
+    this.el.dispatchEvent(new CustomEvent('dialog:ready', { bubbles: true }));
   }
 
-  showDialog() {
+  setupTriggers() {
+    if (this.el.id) {
+      const targetTriggers = document.querySelectorAll(
+        `[data-bolt-dialog-target="${this.el.id}"]`,
+      );
+
+      targetTriggers.forEach(el => {
+        el.addEventListener('click', () => {
+          this.toggle();
+        });
+      });
+    }
+
+    const closeTriggers = this.el.querySelectorAll('[data-dialog-close]');
+
+    closeTriggers.forEach(el => {
+      el.addEventListener('click', () => {
+        this.close();
+      });
+    });
+  }
+
+  open() {
     this.el.setAttribute('aria-hidden', 'false');
     this.el.classList.add('is-open');
-    this.addEventListeners();
     document.body.classList.add('u-bolt-overflow-hidden');
-    this.setFocusToFirstNode();
-    // scroll to the top of the scrollable section of the dialog
-    if (this.el.classList.contains('c-bolt-dialog__scroll-overall')) {
-      this.el.querySelector('.c-bolt-dialog__overlay').scrollTop = 0;
-    } else {
-      this.el.querySelector('.c-bolt-dialog__body').scrollTop = 0;
+
+    if (!this.isPersistent) {
+      this.overlay.addEventListener('click', this.handleOutsideClick);
+      document.addEventListener('keydown', this.handleEscape);
     }
+
+    this.trap.activate();
+    this.isOpen = true;
   }
 
-  closeDialog() {
+  close() {
     this.el.setAttribute('aria-hidden', 'true');
     this.el.classList.remove('is-open');
-    this.removeEventListeners();
     document.body.classList.remove('u-bolt-overflow-hidden');
-    if (this.activeElement && this.activeElement.focus) {
-      this.activeElement.focus();
+
+    if (!this.isPersistent) {
+      this.overlay.removeEventListener('click', this.handleOutsideClick);
+      document.removeEventListener('keydown', this.handleEscape);
+    }
+
+    this.trap.deactivate();
+    this.isOpen = false;
+  }
+
+  toggle() {
+    if (this.isOpen) {
+      this.close();
+    } else {
+      this.open();
     }
   }
 
-  addEventListeners() {
-    this.el.addEventListener('touchstart', event => this.onClick(event));
-    this.el.addEventListener('click', event => this.onClick(event));
-    document.addEventListener('keydown', event => this.onKeydown(event));
-  }
+  handleOutsideClick(e) {
+    const isInsideDialog = e.target.closest('.c-bolt-dialog__container');
 
-  removeEventListeners() {
-    this.el.removeEventListener('touchstart', this.onClick);
-    this.el.removeEventListener('click', this.onClick);
-    document.removeEventListener('keydown', this.onKeydown);
-  }
-
-  onClick(event) {
-    if (event.target.hasAttribute('data-dialog-close')) {
-      this.closeDialog();
-    }
-
-    // add support for the icon child of the close button
-    if (
-      event.target.closest('.c-bolt-dialog__close-button') &&
-      event.target
-        .closest('.c-bolt-dialog__close-button')
-        .hasAttribute('data-dialog-close')
-    ) {
-      this.closeDialog();
+    if (!isInsideDialog) {
+      this.close();
     }
   }
 
-  onKeydown(event) {
-    // tab
-    if (event.keyCode === 9) this.retainFocus(event);
-    // esc
-    if (event.keyCode === 27) {
-      if (typeof this.el.dataset.dialogPersistent === 'undefined') {
-        this.closeDialog(event);
-      } else {
-        console.log('Cannot close a persistent dialog.');
-      }
+  handleEscape(e) {
+    if (e.keyCode === 27) {
+      this.close();
     }
-  }
-
-  init() {
-    const trigger = document.querySelector(
-      '[data-bolt-dialog-target="' + this.el.id + '"]',
-    );
-    trigger.addEventListener('click', event => this.showDialog());
   }
 }
-
-//     closeDialogById(targetDialog) {
-//       this.el = document.getElementById(targetDialog);
-//       if (this.el) this.closeDialog();
-//     }
-
-//   /**
-//    * Shows a particular dialog
-//    * @param  {string} targetDialog [The id of the dialog to display]
-//    * @param  {object} config [The configuration object to pass]
-//    * @return {void}
-//    */
-//   const show = (targetDialog, config) => {
-//     const options = config || {};
-//     options.targetDialog = targetDialog;
-
-//     // Checks if dialogs and triggers exist in dom
-//     if (
-//       options.debugMode === true &&
-//       validateDialogPresence(targetDialog) === false
-//     )
-//       return;
-
-//     // clear events in case previous dialog wasn't close
-//     if (activeDialog) activeDialog.removeEventListeners();
-
-//     // stores reference to active dialog
-//     activeDialog = new Dialog(options); // eslint-disable-line no-new
-//     activeDialog.showDialog();
-//   };
-
-//   /**
-//    * Closes the active dialog
-//    * @param  {string} targetDialog [The id of the dialog to close]
-//    * @return {void}
-//    */
-//   const close = targetDialog => {
-//     targetDialog
-//       ? activeDialog.closeDialogById(targetDialog)
-//       : activeDialog.closeDialog();
-//   };
