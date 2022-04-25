bb778aa0a036a2d6d115860f1a049e5ac8944508
Morse, Dan <Dan.Morse@pega.com>
2022-04-25 16:09:50 -0400

    DS-516: Remove old JS

--- packages/components/bolt-table/index.js
+++ packages/components/bolt-table/index.js
@@ -1,11 +1,1 @@
-const responsiveTables = document.querySelectorAll('.c-bolt-table');
-
-if (responsiveTables.length) {
-  import(/* webpackChunkName: 'bolt-responsive-table' */ './src/table').then(
-    ({ BoltResponsiveTable }) => {
-      responsiveTables.forEach(el => {
-        const responsiveTableComponent = new BoltResponsiveTable(el);
-      });
-    },
-  );
-}
+// @TODO: Setup advanced table features that require JS
