1ad30e521b2deec01d755bc925ed9cc94c83314f
Mroczko <mrocm@l32956meu.local.rpega.com>
2022-05-19 17:01:52 +0200

    DS-602: Revert delete pattern template func

--- packages/twig-integration/twig-extensions-shared/src/TwigExtensions/BoltCoreCompat.php
+++ packages/twig-integration/twig-extensions-shared/src/TwigExtensions/BoltCoreCompat.php
@@ -13,6 +13,7 @@ class BoltCoreCompat extends Twig_Extension implements Twig_ExtensionInterface {
   public function getFunctions() {
     return [
       Bolt\TwigFunctions::create_attribute(),
+      Bolt\TwigFunctions::pattern_template(),
       Bolt\TwigFunctions::link(),
     ];
   }
