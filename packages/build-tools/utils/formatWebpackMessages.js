a2f51a7cea9075f068447b23e75d6fd2d338e3bb
Morse, Dan <Dan.Morse@pega.com>
2022-10-21 18:09:23 -0400

    DS-672: remove comment, run lint, fix warning in dialog schema

--- packages/build-tools/utils/formatWebpackMessages.js
+++ packages/build-tools/utils/formatWebpackMessages.js
@@ -25,7 +25,6 @@ function isLikelyASyntaxError(message) {
 }
 
 // Cleans up webpack error messages.
-// eslint-disable-next-line no-unused-vars
 function formatMessage({ message }) {
   var lines = message.split('\n');
 
