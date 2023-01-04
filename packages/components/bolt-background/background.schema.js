a34a37e1f81251248815e7a7b59e0d1b0fed0cc0
Colby Cook <colby.cook@pega.com>
2023-01-04 12:19:14 -0500

    Removed unused schema props

--- packages/components/bolt-background/background.schema.js
+++ packages/components/bolt-background/background.schema.js
@@ -4,19 +4,6 @@ module.exports = {
   description:
     'A content container that delivers important messages to the user.',
   type: 'object',
-  not: {
-    anyOf: [
-      {
-        required: ['contentItems'],
-      },
-      {
-        required: ['content_items'],
-      },
-      {
-        required: ['fill_color'],
-      },
-    ],
-  },
   properties: {
     attributes: {
       type: 'object',
@@ -73,41 +60,6 @@ module.exports = {
     shape_alignment: {
       title: 'DEPRECATED',
       description: 'Alignment of shape group.',
-    },
-    overlay: {
-      title: 'DEPRECATED',
-      description: 'Use opacity instead',
-    },
-    shapeGroup: {
-      title: 'DEPRECATED',
-      description: 'Use shape_group instead.',
-    },
-    shapeAlignment: {
-      title: 'DEPRECATED',
-      description: 'Use shape_alignment instead.',
-    },
-    fillColor: {
-      title: 'DEPRECATED',
-      description:
-        'This is deprecated. Solid overlay on the background component can be achieved by adding <a href="/pattern-lab/?p=visual-styles-theming-system" class="e-bolt-text-link" target="_blank" rel="noopener">theming class</a> <code>t-bolt-*</code> to the parent of a background component.',
-    },
-    focalPoint: {
-      title: 'DEPRECATED',
-      description: 'Use focal_point instead.',
-    },
-    contentItems: {
-      title: 'DEPRECATED',
-      description: 'Use content_items instead.',
-    },
-    content_items: {
-      title: 'DEPRECATED',
-      description:
-        'Use items instead (each item must be renderable, e.g. a string or render array.  Arrays inteded to be rendered with pattern_template() are no longer supported).',
-    },
-    fill_color: {
-      title: 'DEPRECATED',
-      description:
-        'This is deprecated. Solid overlay on the background component can be achieved by adding <a href="/pattern-lab/?p=visual-styles-theming-system" class="e-bolt-text-link" target="_blank" rel="noopener">theming class</a> <code>t-bolt-*</code> to the parent of a background component.',
     },
   },
 };
